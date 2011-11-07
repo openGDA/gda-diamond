442703a48815956b64be7c75f4814ac7b9d69508
Visanthi Nagalingam <Visanthi.Nagalingam@diamond.ac.uk>
2011-11-07 14:20:34 +0000

    fixed indendation errors

--- configurations/i18-config/scripts/i18_exafs/setupExperiment.py
+++ configurations/i18-config/scripts/i18_exafs/setupExperiment.py
@@ -27,57 +27,57 @@ from uk.ac.gda.beans.exafs import QEXAFSParameters
 rootnamespace = {}
 
 def finish():
-    pass
-    
+	pass
+	
 
 def setup(beanGroup):
-if beanGroup.getDetector().getExperimentType() == "Fluorescence":
-        if (beanGroup.getDetector().getFluorescenceParameters().getDetectorType() == "Germanium" ):
-            fullFileName = beanGroup.getScriptFolder() + beanGroup.getDetector().getFluorescenceParameters().getConfigFileName()
-            bean = BeansFactory.getBean(File(fullFileName));
-            bean.setReadoutMode(XspressDetector.READOUT_MCA);
-            bean.setResGrade(ResGrades.NONE);
-            elements = bean.getDetectorList();
-            for element in elements: 
-                rois = element.getRegionList();
-                element.setWindow(rois.get(0).getRegionStart(), rois.get(0).getRegionEnd())
-            BeansFactory.saveBean(File(fullFileName), bean)
-        configFluoDetector(beanGroup)
-    #setup topup
-    scan = beanGroup.getScan()
-    collectionTime = 0.0
-    if isinstance(scan,XanesScanParameters):
-        regions = scan.getRegions()        
-        for region in regions:
-            if(collectionTime < region.getTime()):
-                collectionTime =region.getTime()
-    elif isinstance(scan,QEXAFSParameters):
-        pass
-    else:
-        collectionTime = scan.getExafsTime()
-        if(scan.getExafsToTime() > collectionTime):
-            collectionTime = scan.getExafsToTime()
-    print "setting collection time to" , str(collectionTime)
-    command_server = Finder.getInstance().find("command_server")    
-    topupMonitor = command_server.getFromJythonNamespace("topupMonitor", None)    
-    beam = command_server.getFromJythonNamespace("beam", None)
-    add_default(beam)
-    detectorFillingMonitor = command_server.getFromJythonNamespace("detectorFillingMonitor", None)
-    trajBeamMonitor = command_server.getFromJythonNamespace("trajBeamMonitor", None)
-    topupMonitor.setPauseBeforePoint(True)
-    topupMonitor.setPauseBeforeLine(False)
-    topupMonitor.setCollectionTime(collectionTime)
-    beam.setPauseBeforePoint(True)
-    beam.setPauseBeforeLine(True)
-    if(beanGroup.getDetector().getExperimentType() == "Fluorescence" and beanGroup.getDetector().getFluorescenceParameters().getDetectorType() == "Germanium"): 
-        add_default(detectorFillingMonitor)
-        detectorFillingMonitor.setPauseBeforePoint(True)
-        detectorFillingMonitor.setPauseBeforeLine(False) 
-    trajBeamMonitor.setActive(False)
-    sampleParameters = beanGroup.getSample()
-    stage = sampleParameters.getSampleStageParameters()
-    att1 = sampleParameters.getAttenuatorParameter1()
-    att2 = sampleParameters.getAttenuatorParameter2()
-    pos([rootnamespace['sc_MicroFocusSampleX'], stage.getX(), rootnamespace['sc_MicroFocusSampleY'], stage.getY(), rootnamespace['sc_sample_z'], stage.getZ()])
-    pos([rootnamespace['D7A'], att1.getSelectedPosition(), rootnamespace['D7B'], att2.getSelectedPosition()])
-    Finder.getInstance().find("RCPController").openPesrpective("org.diamond.exafs.ui.PlottingPerspective")
+	if beanGroup.getDetector().getExperimentType() == "Fluorescence":
+		if (beanGroup.getDetector().getFluorescenceParameters().getDetectorType() == "Germanium" ):
+			fullFileName = beanGroup.getScriptFolder() + beanGroup.getDetector().getFluorescenceParameters().getConfigFileName()
+			bean = BeansFactory.getBean(File(fullFileName));
+			bean.setReadoutMode(XspressDetector.READOUT_MCA);
+			bean.setResGrade(ResGrades.NONE);
+			elements = bean.getDetectorList();
+			for element in elements: 
+				rois = element.getRegionList();
+				element.setWindow(rois.get(0).getRegionStart(), rois.get(0).getRegionEnd())
+			BeansFactory.saveBean(File(fullFileName), bean)
+		configFluoDetector(beanGroup)
+		#setup topup
+		scan = beanGroup.getScan()
+		collectionTime = 0.0
+		if isinstance(scan,XanesScanParameters):
+			regions = scan.getRegions()		
+			for region in regions:
+				if(collectionTime < region.getTime()):
+					collectionTime =region.getTime()
+		elif isinstance(scan,QEXAFSParameters):
+			pass
+		else:
+			collectionTime = scan.getExafsTime()
+			if(scan.getExafsToTime() > collectionTime):
+				collectionTime = scan.getExafsToTime()
+		print "setting collection time to" , str(collectionTime)
+		command_server = Finder.getInstance().find("command_server")	
+		topupMonitor = command_server.getFromJythonNamespace("topupMonitor", None)	
+		beam = command_server.getFromJythonNamespace("beam", None)
+		add_default(beam)
+		detectorFillingMonitor = command_server.getFromJythonNamespace("detectorFillingMonitor", None)
+		trajBeamMonitor = command_server.getFromJythonNamespace("trajBeamMonitor", None)
+		topupMonitor.setPauseBeforePoint(True)
+		topupMonitor.setPauseBeforeLine(False)
+		topupMonitor.setCollectionTime(collectionTime)
+		beam.setPauseBeforePoint(True)
+		beam.setPauseBeforeLine(True)
+		if(beanGroup.getDetector().getExperimentType() == "Fluorescence" and beanGroup.getDetector().getFluorescenceParameters().getDetectorType() == "Germanium"): 
+			add_default(detectorFillingMonitor)
+			detectorFillingMonitor.setPauseBeforePoint(True)
+			detectorFillingMonitor.setPauseBeforeLine(False) 
+		trajBeamMonitor.setActive(False)
+		sampleParameters = beanGroup.getSample()
+		stage = sampleParameters.getSampleStageParameters()
+		att1 = sampleParameters.getAttenuatorParameter1()
+		att2 = sampleParameters.getAttenuatorParameter2()
+		pos([rootnamespace['sc_MicroFocusSampleX'], stage.getX(), rootnamespace['sc_MicroFocusSampleY'], stage.getY(), rootnamespace['sc_sample_z'], stage.getZ()])
+		pos([rootnamespace['D7A'], att1.getSelectedPosition(), rootnamespace['D7B'], att2.getSelectedPosition()])
+		Finder.getInstance().find("RCPController").openPesrpective("org.diamond.exafs.ui.PlottingPerspective")
