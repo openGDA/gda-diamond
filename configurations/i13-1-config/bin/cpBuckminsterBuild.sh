8a382851da6f2fce150bd595a4df02bfcf62068c
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-02-24 11:38:52 +0000

    Add autocollimator script Add nextract

--- configurations/i13-1-config/bin/cpBuckminsterBuild.sh
+++ configurations/i13-1-config/bin/cpBuckminsterBuild.sh
@@ -2,7 +2,7 @@ cd /dls_sw/i13-1/software/gda
 echo "rm old client"
 rm -rf ./client
 echo "chmod o+w new client"
-chmod -R o+w /tmp/uk.ac.gda.beamline.i13j.site/output/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64
+chmod -R o+w /tmp/buckminster/build/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64
 echo "cp new client"
-cp -r /tmp/uk.ac.gda.beamline.i13j.site/output/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64 ./client
+cp -r /tmp/buckminster/build/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64 ./client
 echo "done"
