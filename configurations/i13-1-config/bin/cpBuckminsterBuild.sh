b97106e6a7ac0b5345ca1a6cf42bcae0165801ff
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-03-19 13:48:26 +0000

    Remove dawb visualisation as it is causing confusion

--- configurations/i13-1-config/bin/cpBuckminsterBuild.sh
+++ configurations/i13-1-config/bin/cpBuckminsterBuild.sh
@@ -1,6 +1,6 @@
 cd /dls_sw/i13-1/software/gda
 echo "rm old client"
-rm -rf ./client
+mv ./client ./client_old
 echo "chmod o+w new client"
 chmod -R o+w /tmp/uk.ac.gda.beamline.i13j.site/output/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64
 echo "cp new client"
