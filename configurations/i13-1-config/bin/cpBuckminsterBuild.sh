d2099b01323db34fb62ef344fba78095f1c35f93
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-03-19 15:24:36 +0000

    Add I13J specific Help

--- configurations/i13-1-config/bin/cpBuckminsterBuild.sh
+++ configurations/i13-1-config/bin/cpBuckminsterBuild.sh
@@ -1,6 +1,6 @@
 cd /dls_sw/i13-1/software/gda
 echo "rm old client"
-mv ./client ./client_old
+rm -rf ./client 
 echo "chmod o+w new client"
 chmod -R o+w /tmp/uk.ac.gda.beamline.i13j.site/output/uk.ac.gda.beamline.i13j.site_1.0.0-eclipse.feature/i13j.gtk.linux.x86_64
 echo "cp new client"
