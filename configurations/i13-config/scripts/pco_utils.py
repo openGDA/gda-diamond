091f303b0e342d5b74f621a2391717378c2d3479
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-07-14 07:16:23 +0100

    Changes to try using I12 GUI

--- configurations/i13-config/scripts/pco_utils.py
+++ configurations/i13-config/scripts/pco_utils.py
@@ -11,21 +11,28 @@ class PCO_Util:
             raise Exception(name + " not found")
         return controller
     def __init__(self):
-        self.pco1_continuousSoftwareTrigger = self.__getController("pco1_continuousSoftwareTrigger")
+        self.pco1_autoContinuousTrigger = self.__getController("pco1_autoContinuousTrigger")
         self.pco1_ffmpeg1 = self.__getController("pco1_ffmpeg1")
         self.pco1_proc1 = self.__getController("pco1_proc1")
         self.pco1_cam_base = self.__getController("pco1_cam_base")
-    def setupForAlignment(self):
+
+    def setupForAlignment(self, collectionTime=1.0, scale=1):
         """
         Turns camera on to auto continuous mode 
         """
-        self.pco1_continuousSoftwareTrigger.prepareForCollection(1.0,1)
-        self.pco1_continuousSoftwareTrigger.collectData()
-#        self.pco1_ffmpeg1.
-        self.pco1_proc1.getPluginBase().setNDArrayPort("pco1.cam")
+        self.pco1_autoContinuousTrigger.prepareForCollection(collectionTime,1)
+        self.pco1_proc1.getPluginBase().setNDArrayPort(self.pco1_cam_base.getPortName_RBV())
         self.pco1_proc1.getPluginBase().enableCallbacks()
-        self.pco1_proc1.setEnableOffsetScale(1)
-        self.pco1_ffmpeg1.getPluginBase().setNDArrayPort(self.pco1_proc1.getPluginBase().getPortName())
+        self.pco1_proc1.getPluginBase().setBlockingCallbacks(0)
+        self.pco1_proc1.setEnableOffsetScale(scale)
+
+        self.pco1_ffmpeg1.getPluginBase().setNDArrayPort(self.pco1_proc1.getPluginBase().getPortName_RBV())
         self.pco1_ffmpeg1.getPluginBase().enableCallbacks()
+        self.pco1_ffmpeg1.getPluginBase().setBlockingCallbacks(0)
+
+#        self.pco1_autoContinuousTrigger.collectData()
+
         
+def setupPCO400ForAlignment(collectionTime=1.0, scale=1):
+    PCO_Util().setupForAlignment(collectionTime, scale)
         
