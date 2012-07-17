44c3fceb8175b1a8f8cd4066fd45a15975c652f4
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-07-17 15:56:22 +0100

    I13i - Remove i12 components

--- configurations/i13-config/bin/cpBuckminsterBuild.sh
+++ configurations/i13-config/bin/cpBuckminsterBuild.sh
@@ -8,8 +8,8 @@ cd /dls_sw/i13/software/gda
 echo "rm old client"
 rm -rf ./client
 #mv client client_old
-echo "chmod o+w new client"
-chmod -R o+w /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64
+echo "chgrp dls_dasc client"
+chgrp -R dls_dasc /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64
 echo "cp new client"
 cp -r /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64 ./client
 echo "done"
--- configurations/i13-config/bin/servers.sh
+++ configurations/i13-config/bin/servers.sh
@@ -18,12 +18,18 @@ rm -f $SERVER_STARTUP_FILE
 
 echo "Starting GDA. Output is being logged to $LOGFILE"
 
-export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
+export JAVA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 
 nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --restart -v --mode=$GDAMODE nameserver > $LOGFILE 2>&1 &
+
+
 nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --restart -v --mode=$GDAMODE logserver > $LOGFILE 2>&1 &
+
+
 nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --debug -p 8002 --restart -v --mode=$GDAMODE eventserver > $LOGFILE 2>&1 &
+
+export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
+
 nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --debug -p 8001 --restart -v --mode=$GDAMODE objectserver > $LOGFILE 2>&1 &
-#nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --debug -p 8001 --restart -v --mode=$GDAMODE servers > $LOGFILE 2>&1 &
 echo "Looking for file $SERVER_STARTUP_FILE"
 $GDAFOLDER/$BEAMLINE-config/bin/lookForFile $SERVER_STARTUP_FILE
