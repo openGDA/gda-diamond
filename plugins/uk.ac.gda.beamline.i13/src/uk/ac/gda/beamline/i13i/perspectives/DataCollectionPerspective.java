e8f6293b48375d5859591e7f9296f5aac92c0677
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-08-23 18:28:27 +0100

    Change DataCollection perspective to use the new live plot view

--- plugins/uk.ac.gda.beamline.i13/src/uk/ac/gda/beamline/i13i/perspectives/DataCollectionPerspective.java
+++ plugins/uk.ac.gda.beamline.i13/src/uk/ac/gda/beamline/i13i/perspectives/DataCollectionPerspective.java
@@ -27,7 +27,7 @@ import org.eclipse.ui.IViewLayout;
 
 import uk.ac.gda.beamline.i13i.views.ViewFactoryIds;
 import uk.ac.gda.client.CommandQueueViewFactory;
-import uk.ac.gda.client.XYPlotView;
+import uk.ac.gda.client.liveplot.LivePlotView;
 
 /**
  */
@@ -67,7 +67,7 @@ public class DataCollectionPerspective implements IPerspectiveFactory {
 		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
 
 		IFolderLayout right = factory.createFolder("right", IPageLayout.RIGHT, (float) 0.50, factory.getEditorArea()); //$NON-NLS-1$
-		right.addView(XYPlotView.ID);
+		right.addView(LivePlotView.ID);
 		
 		IFolderLayout rightBottom = factory.createFolder("rightBottom", IPageLayout.BOTTOM, (float) 0.50, "right");
 		rightBottom.addView("uk.ac.gda.beamline.i13i.DetectorPlot");
