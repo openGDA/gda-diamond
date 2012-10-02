e2ff0fda8f4b8e2007e4459d6450622af6a5bbf1
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2012-10-02 11:36:04 +0100

    updated so it could run with jprofiler

--- configurations/i20-config/bin/servers.sh
+++ configurations/i20-config/bin/servers.sh
@@ -18,7 +18,21 @@ ln -s $LOGFILE /dls_sw/i20/logs/gda_output.txt
 SERVER_STARTUP_FILE=/tmp/object_server_startup_server_main; export SERVER_STARTUP_FILE
 rm -f $SERVER_STARTUP_FILE
 
-nohup /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --config=/dls_sw/i20/software/gda/config --mode=live --debug -p 8001 --restart servers > $LOGFILE 2>&1 &
+#nohup /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --config=/dls_sw/i20/software/gda/config --mode=live --debug -p 8001 --profiler --restart servers > $LOGFILE 2>&1 &
+
+
+nohup python /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=/dls_sw/i20/software/gda/config --restart -v --mode=live nameserver > $LOGFILE 2>&1 &
+
+
+nohup python /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=/dls_sw/i20/software/gda/config --restart -v --mode=live logserver > $LOGFILE 2>&1 &
+
+
+nohup python /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=/dls_sw/i20/software/gda/config --debug -p 8002 --restart -v --mode=live eventserver > $LOGFILE 2>&1 &
+
+export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
+
+nohup python /dls_sw/i20/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=/dls_sw/i20/software/gda/config --debug -p 8001 --restart -v --mode=live objectserver > $LOGFILE 2>&1 &
+
 
 export THIS_MACHINE=`uname -n`
 echo ""
