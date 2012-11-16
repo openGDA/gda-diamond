243bbe711b0740f608ff9849cec668a8f3db97c5
chris coles <chris.coles@diamond.ac.uk>
2012-11-16 18:44:00 +0000

    I18: New script structure

--- configurations/i18-config/scripts/localStation.py
+++ configurations/i18-config/scripts/localStation.py
@@ -14,7 +14,7 @@ from gda.factory import Finder
 from exafsscripts.exafs.i18_detector_preparer import I18DetectorPreparer
 from exafsscripts.exafs.i18_sample_preparer import I18SamplePreparer
 from exafsscripts.exafs.output_preparer import OutputPreparer
-from exafsscripts.exafs.xas_scan import XasScan
+from exafsscripts.exafs.i18ScanScripts import I18XasScan
 from exafsscripts.exafs.qexafs_scan import QexafsScan
 
 print "Initialization Started";
@@ -25,25 +25,6 @@ gdaConfigDir = gdaConfigDir + "/"
 map.rootnamespace = globals()
 raster_map.rootnamespace = globals()
 rootnamespace = globals()
-
-detectorPreparer = I18DetectorPreparer()
-samplePreparer = I18SamplePreparer()
-outputPreparer = OutputPreparer()
-
-loggingcontroller = Finder.getInstance().find("XASLoggingScriptController")
-xas = XasScan(loggingcontroller,detectorPreparer, samplePreparer, outputPreparer,None)
-qexafs = QexafsScan(loggingcontroller,detectorPreparer, samplePreparer, outputPreparer, qexafs_energy, qexafs_counterTimer01)
-xanes = xas
-
-alias("map")
-alias("xas")
-alias("xanes")
-alias("vortex")
-alias("xspress")
-alias("qexafs")
-alias("map")
-alias("raster_map")
-alias("raster_map_return_write")
 
 if (LocalProperties.get("gda.mode") == 'live'):
     print "Create topup , detector and beam monitors to pause and resume scans"
@@ -86,6 +67,32 @@ if (LocalProperties.get("gda.mode") == 'live'):
 
     execfile(gdaConfigDir + "scripts/I18Scans/StruckV2F.py")
     execfile("/dls_sw/i18/scripts/focus/SesoMethod/Setup_KBMotors_IDT_Mechanical.py")
+
+detectorPreparer = I18DetectorPreparer()
+rcpController = finder.find("RCPController")
+samplePreparer = I18SamplePreparer(rcpController, sc_MicroFocusSampleX, sc_MicroFocusSampleY, sc_sample_z, D7A, D7B)
+outputPreparer = OutputPreparer()
+
+loggingcontroller = Finder.getInstance().find("XASLoggingScriptController")
+
+xas = I18XasScan(loggingcontroller,detectorPreparer, samplePreparer, outputPreparer, None)
+if (LocalProperties.get("gda.mode") == 'live'):
+    xas.addMonitors(topupMonitor, beam, detectorFillingMonitor, trajBeamMonitor)
+else:
+    xas.addMonitors(None, None, None, None)
+
+qexafs = QexafsScan(loggingcontroller,detectorPreparer, samplePreparer, outputPreparer, qexafs_energy, qexafs_counterTimer01)
+xanes = xas
+
+alias("map")
+alias("xas")
+alias("xanes")
+alias("vortex")
+alias("xspress")
+alias("qexafs")
+alias("map")
+alias("raster_map")
+alias("raster_map_return_write")
 
 test = DummyScannable("test")
 
