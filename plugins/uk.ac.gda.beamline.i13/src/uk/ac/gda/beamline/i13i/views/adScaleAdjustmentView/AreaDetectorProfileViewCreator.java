2e74c480fff74a1d472f830d016c1737045b1a04
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2012-12-21 17:09:12 +0000

    Add ADArrayView

---
+++ plugins/uk.ac.gda.beamline.i13/src/uk/ac/gda/beamline/i13i/views/adScaleAdjustmentView/AreaDetectorProfileViewCreator.java
@@ -0,0 +1,59 @@
+/*-
+ * Copyright © 2009 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.beamline.i13i.views.adScaleAdjustmentView;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExecutableExtension;
+import org.eclipse.core.runtime.IExecutableExtensionFactory;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.gda.beamline.i13i.I13IBeamlineActivator;
+
+public class AreaDetectorProfileViewCreator implements IExecutableExtensionFactory, IExecutableExtension{
+	private static final Logger logger = LoggerFactory.getLogger(AreaDetectorProfileViewCreator.class);
+	
+	@Override
+	public Object create() throws CoreException {
+		Object namedService = I13IBeamlineActivator.getNamedService(ADController.class, serviceName);
+		ADController adController = (ADController) namedService;
+		AreaDetectorProfileView view = new AreaDetectorProfileView(adController);
+		try {
+			view.afterPropertiesSet();
+			return view;
+		} catch (Exception e) {
+			logger.error("Error creating view ", e);
+			throw new CoreException(new Status(IStatus.ERROR, I13IBeamlineActivator.PLUGIN_ID,
+					"Error creating view :'" + e.getMessage() + "'"));
+		}		
+	}
+
+	String serviceName="";
+	@Override
+	public void setInitializationData(IConfigurationElement config, String propertyName, Object data)
+			throws CoreException {
+		if( propertyName.equals("class") && data instanceof String){
+			serviceName = (String)data;
+		}
+
+	}
+}
