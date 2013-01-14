d7a54eeb635472f12a51b3e1b3ee419a1d3bbdc1
Mark Booth <Mark.Booth@diamond.ac.uk>
2013-01-14 18:55:14 +0000

    Cherry pick r49590 from 8.14 to git - CONFIG I15 - Add PerpendicularSampleMotion scannables, thermocouples and PT100s.
    Also add some more patch devices and disable the example wirescanner, as
    it is causing a scan of the motor to be run when a 'pos' of all scannables
    is requested.

---
+++ i15/scripts/scannables/tests/PerpendicularSampleMotionTest.py
@@ -0,0 +1,17 @@
+"""
+Unit tests for Scannables which move the sample perpendicular to and parallel
+to the beam, for I15 & I16.
+"""
+
+import unittest
+#from mock import Mock
+#from scannables.tests.PerpendicularSampleMotionI15Test import SampleMotionI15Test
+#from scannables.tests.PerpendicularSampleMotionI16Test import SampleMotionI16Test
+
+class SampleMotionTest(unittest.TestCase):
+    # This should call both SampleMotionI15Test and SampleMotionI16Test
+    pass
+
+#if __name__ == "__main__":
+#    #import sys;sys.argv = ['', 'Test.testName']
+#    unittest.main()
