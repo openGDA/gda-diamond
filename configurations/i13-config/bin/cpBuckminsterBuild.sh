8b3139f2bfecef4dd27765b3c1e6b63aeb46056e
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-01-17 12:13:59 +0000

    Changes made in last run

--- configurations/i13-config/bin/cpBuckminsterBuild.sh
+++ configurations/i13-config/bin/cpBuckminsterBuild.sh
@@ -9,6 +9,7 @@ echo "rm old client"
 rm -rf ./client
 echo "chgrp dls_dasc client"
 chgrp -R dls_dasc /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64
+chmod  -R o+rw /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64
 echo "cp new client"
 cp -r /tmp/uk.ac.gda.beamline.i13i.site/output/uk.ac.gda.beamline.i13i.site_1.0.0-eclipse.feature/i13i.gtk.linux.x86_64 ./client
 #cp config/gda-i13i.ini client/
