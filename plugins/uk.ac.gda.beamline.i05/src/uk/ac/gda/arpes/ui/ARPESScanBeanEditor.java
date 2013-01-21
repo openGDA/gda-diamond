410e33f56d41db87734a1e48b1c39e09e4b7f409
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-01-21 13:21:01 +0000

    allow multiple instances of the editor to be open

--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/arpes/ui/ARPESScanBeanEditor.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/arpes/ui/ARPESScanBeanEditor.java
@@ -20,11 +20,16 @@ package uk.ac.gda.arpes.ui;
 
 import java.net.URL;
 
+import org.eclipse.ui.IEditorInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import uk.ac.gda.arpes.beans.ARPESScanBean;
 import uk.ac.gda.richbeans.editors.RichBeanEditorPart;
 import uk.ac.gda.richbeans.editors.RichBeanMultiPageEditorPart;
 
 public final class ARPESScanBeanEditor extends RichBeanMultiPageEditorPart {
+	private static final Logger logger = LoggerFactory.getLogger(ARPESScanBeanEditor.class);
 
 	@Override
 	public Class<?> getBeanClass() {
@@ -44,5 +49,16 @@ public final class ARPESScanBeanEditor extends RichBeanMultiPageEditorPart {
 	@Override
 	public URL getSchemaUrl() {
 		return ARPESScanBean.schemaURL; // Please make sure this field is present and the schema
+	}
+	
+	@Override
+	public void setInput(final IEditorInput input) {
+		try{
+	        assignInput(input);
+	        createBean();
+	        linkUI();
+		} catch (Throwable th){
+			logger.error("Error setting input for editor from input " + input.getName(), th);
+		}
 	}
 }
