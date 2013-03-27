d3de3c26f55f12f078740d799a80dd78131244f6
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-03-27 10:11:15 +0000

    BLXIIII-33: Install 8.30 on I13 http://jira.diamond.ac.uk/browse/BLXIIII-33
    Use /dls_sw./i13/software/gda/config rather than i13-config
    Use gdaclient in dls-config

--- configurations/i13-config/bin/servers.sh
+++ configurations/i13-config/bin/servers.sh
@@ -4,7 +4,7 @@
 source /dls_sw/i13/etc/i13_profile.sh
 
 . /usr/share/Modules/init/bash
-module load java/gda
+module load java/gda830-64
 
 #this is needed to ensure the acls work properly
 umask 0002
@@ -23,16 +23,16 @@ echo "Starting GDA. Output is being logged to $LOGFILE"
 
 export JAVA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 
-nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --restart -v --mode=$GDAMODE nameserver > $LOGFILE 2>&1 &
+nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/config --restart -v --mode=$GDAMODE nameserver > $LOGFILE 2>&1 &
 
 
-nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --restart -v --mode=$GDAMODE logserver > $LOGFILE 2>&1 &
+nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/config --restart -v --mode=$GDAMODE logserver > $LOGFILE 2>&1 &
 
 
-nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --debug -p 8002 --restart -v --mode=$GDAMODE eventserver > $LOGFILE 2>&1 &
+nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/config --debug -p 8002 --restart -v --mode=$GDAMODE eventserver > $LOGFILE 2>&1 &
 
 export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 
-nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/$BEAMLINE-config --debug -p 8001 --restart -v --mode=$GDAMODE objectserver > $LOGFILE 2>&1 &
+nohup python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/config --debug -p 8001 --restart -v --mode=$GDAMODE objectserver > $LOGFILE 2>&1 &
 echo "Looking for file $SERVER_STARTUP_FILE"
-$GDAFOLDER/$BEAMLINE-config/bin/lookForFile $SERVER_STARTUP_FILE
+$GDAFOLDER/config/bin/lookForFile $SERVER_STARTUP_FILE
