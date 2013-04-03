1ca8f5204e01144ac222d595fd6c0640db6f0525
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-04-03 15:21:10 +0100

    BLXX-71 after initial tests on beamline

--- configurations/i20-config/scripts/mono_calibration.py
+++ configurations/i20-config/scripts/mono_calibration.py
@@ -6,36 +6,30 @@ from gdascripts.analysis.datasetprocessor.oned.GaussianPeakAndBackground import 
 from gdascripts.scan import specscans
 import math
 
-def calibrate_mono(calibration_energy,energy_range=50,energy_step=5,diagnostic_scannable=None):
+def calibrate_mono(calibration_energy,offset_range=0.1,offset_step=0.01,diagnostic_scannable=None):
     
     scan_processor = ScanDataProcessor( [MaxPositionAndValue(),MinPositionAndValue(),CentreOfMass(), GaussianPeakAndBackground() ], globals() )
     ascan  = specscans.Ascan([scan_processor])
-#    alias('ascan');print ascan.__doc__.split('\n')[3]
-    
-    # get what we need from the system
-    finder = Finder.getInstance()
-    bragg1 = finder.find("bragg1")
-    bragg_offset = finder.find("bragg_offset")
-    bragg1_cal_energy = finder.find("bragg1_cal_energy")
-    bragg1_cal_set = finder.find("bragg1_cal_set")
-    bragg2_cal_energy = finder.find("bragg2_cal_energy")
-    bragg2_cal_set = finder.find("bragg2_cal_set")
     
     # move bragg1 to the calibration energy
     if diagnostic_scannable == None:
         diagnostic_scannable = finder.find("d5_current")
-#    myscan = CentroidScan([bragg1,energy_range,energy_step,diagnostic_scannable])
-#    myscan.runScan()
-#    filename = myscan.getDataWriter().
-    start_energy = calibration_energy - float(energy_range/2.)
-    final_energy = calibration_energy + float(energy_range/2.)
-    result = ascan([bragg1, start_energy, final_energy, energy_step, diagnostic_scannable])
     
-    calculated_peak = scan_processor.result.peak
-    if Math.abs(calculated_peak - calibration_energy) > energy_range:
-        raise DeviceException("Calibration failed!")
+    bragg1(calibration_energy)
+    
+    start_energy = - float(offset_range/2.)
+    final_energy = float(offset_range/2.)
+    intervals = (final_energy-start_energy)/offset_step
+    
+    print "Running scan over",braggoffset.getName(),"from",start_energy,"to",final_energy,"in steps of",offset_step,"using",diagnostic_scannable.getName()
+    result = ascan([braggoffset, start_energy, final_energy, intervals, diagnostic_scannable])
+    calculated_peak = result.peak.result.pos
+    print "Peak found at",calculated_peak
 
     # set the calibration values and press the 'set' buttons
-    scan_processor.go("peak")
-    bragg1_cal_set(1)
-    bragg2_cal_set(1)
+    print "Now will move",braggoffset.getName(),"to",calculated_peak,"and set the calibration."
+    #braggoffset(calculated_peak)
+    #bragg1_cal_energy(calculated_peak)
+    #bragg2_cal_energy(calculated_peak)
+    #bragg1_cal_set(1)
+    #bragg2_cal_set(1)
