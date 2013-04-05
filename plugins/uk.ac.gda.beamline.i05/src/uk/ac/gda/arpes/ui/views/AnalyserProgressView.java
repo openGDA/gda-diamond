9c4b53c15ce1ac428665af815c1ef0a5c779317c
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-04-05 15:24:43 +0100

    finish GUI for APRES-102

--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/arpes/ui/views/AnalyserProgressView.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/arpes/ui/views/AnalyserProgressView.java
@@ -22,30 +22,31 @@ import gda.factory.Finder;
 import gda.jython.JythonServerFacade;
 import gda.observable.IObserver;
 
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.part.ViewPart;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.ProgressBar;
-import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Spinner;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridData;
+import org.eclipse.ui.part.ViewPart;
 
 import uk.ac.gda.arpes.detector.FlexibleFrameDetector;
 import uk.ac.gda.arpes.detector.FrameUpdate;
-import uk.ac.gda.richbeans.components.scalebox.IntegerBox;
-import uk.ac.gda.richbeans.event.ValueAdapter;
-import uk.ac.gda.richbeans.event.ValueEvent;
 
 public class AnalyserProgressView extends ViewPart implements IObserver {
 	private Text csweep;
 	private Text msweep;
-	private IntegerBox sweepEntry;
 	private FlexibleFrameDetector analyser;
+	private Spinner sweepSpinner;
+	private int oldMax = -1;
 	public AnalyserProgressView() {
 	}
 
@@ -58,9 +59,9 @@ public class AnalyserProgressView extends ViewPart implements IObserver {
 		
 		Label lblCurrentSweep = new Label(parent, SWT.NONE);
 		lblCurrentSweep.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
-		lblCurrentSweep.setText("Current Sweep");
+		lblCurrentSweep.setText("Completed Sweeps");
 		
-		csweep = new Text(parent, SWT.BORDER);
+		csweep = new Text(parent, SWT.BORDER | SWT.RIGHT);
 		csweep.setEditable(false);
 		csweep.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
@@ -68,24 +69,25 @@ public class AnalyserProgressView extends ViewPart implements IObserver {
 		lblMaximumSweep.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblMaximumSweep.setText("Maximum Sweep");
 		
-		msweep = new Text(parent, SWT.BORDER);
+		msweep = new Text(parent, SWT.BORDER | SWT.RIGHT);
 		msweep.setEditable(false);
 		msweep.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
 		Label lblNewMaximum = new Label(parent, SWT.NONE);
 		lblNewMaximum.setText("New Maximum");
 		
-		sweepEntry = new IntegerBox(parent, SWT.BORDER);
-		sweepEntry.setMinimum(1);
-		sweepEntry.setMaximum(10000);
-		sweepEntry.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
-		sweepEntry.addValueListener(new ValueAdapter("sweepEntry") {
+		sweepSpinner = new Spinner(parent, SWT.BORDER);
+		sweepSpinner.setIncrement(1);
+		sweepSpinner.setMinimum(1);
+		sweepSpinner.setSelection(1);
+		sweepSpinner.addModifyListener(new ModifyListener() {
+			
 			@Override
-			public void valueChangePerformed(ValueEvent e) {
-				analyser.setMaximumFrame(sweepEntry.getIntegerValue());				
+			public void modifyText(ModifyEvent e) {
+				oldMax = sweepSpinner.getSelection();
+				analyser.setMaximumFrame(oldMax);
 			}
 		});
-		sweepEntry.on();
 		
 		Button btnCompleteAndStop = new Button(parent, SWT.NONE);
 		btnCompleteAndStop.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, false, false, 3, 1));
@@ -134,6 +136,9 @@ public class AnalyserProgressView extends ViewPart implements IObserver {
 				public void run() {
 					csweep.setText(String.valueOf(fu.cFrame));
 					msweep.setText(String.valueOf(fu.mFrame));
+					if (fu.mFrame != oldMax) {
+						sweepSpinner.setSelection(fu.mFrame);
+					}
 				}
 			});
 		}
