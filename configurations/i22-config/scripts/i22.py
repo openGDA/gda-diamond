2d1a555cb776c7d3b487e12ea77229cc006f74a2
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-04-18 18:10:10 +0100

    checkin current beamline state

--- configurations/i22-config/scripts/i22.py
+++ configurations/i22-config/scripts/i22.py
@@ -38,7 +38,7 @@ class I22(gda.device.scannable.PseudoDevice):
 	def wakeUp(self):
 		valves.open()
 		mirrorsVoltages.on()		
-		hotwaxs.on()
+		#hotwaxs.on()
 
 	def isAwake(self):
 		status = valves.isAwake() and mirrorsVoltages.isOn(self.VFM) and mirrorsVoltages.isOn(self.HFM) and hotwaxs.isOn()
@@ -161,97 +161,6 @@ class MirrorsVoltages():
 			return "Shutdown"
 		return "Awake"
 		
-class Hotwaxs():
-
-	def __init__(self):	
-		self.PVname = "BL22I-EA-HV-01:"
-		self.cathodes = HotWaxsVoltages( self.PVname , "Cathodes" , 0 )
-		self.window = HotWaxsVoltages( self.PVname , "Window" , 1 )
-		self.side = HotWaxsVoltages( self.PVname , "Side" , 2 )
-		self.drift = HotWaxsVoltages( self.PVname , "Drift" , 3 )
-		
-	def on(self):
-		if ( self.cathodes.isEnabled() == 0 or self.window.isEnabled() == 0 or self.side.isEnabled() == 0 or self.drift.isEnabled()==0 ):
-			print "One or more channels are off. Turn the power supply on"
-			return
-		print "Set HOTWAXS high voltages ON"
-		pos self.cathodes 200
-		pos self.window 600
-		pos self.side 600
-		pos self.drift 1200
-		
-		inc self.cathodes 200
-		inc self.window 600
-		inc self.side 600
-		inc self.drift 1200
-
-		inc self.cathodes 70
-		inc self.window 300
-		inc self.side 300
-		inc self.drift 600
-		
-		print "Cathodes at: "+str(self.cathodes.getPosition())
-		print "Window at: "+str(self.window.getPosition())
-		print "Side at: "+str(self.side.getPosition())
-		print "Drift at: "+str(self.drift.getPosition())
-	
-	def off(self):
-		if ( self.cathodes.isEnabled() ):
-			pos self.cathodes 0.0
-			self.cathodes.off()
-			
-		if (self.window.isEnabled() ):
-			pos self.window 0.0
-			self.window.off()
-			
-		if ( self.side.isEnabled() ):
-			pos self.side 0.0
-			self.side.off()
-		
-		if ( self.drift.isEnabled() ):
-			pos self.drift 0.0
-			self.drift.off()
-
-		print "All voltages at 0.0V"
-
-	def isOn(self):
-		return (self.cathodes.isEnabled() and self.window.isEnabled and self.side.isEnabled() and self.drift.isEnabled())
-		
-class HotWaxsVoltages(gda.device.scannable.PseudoDevice):
-	
-	def __init__(self , pvName , name , channel):
-		self.name = name
-		self.pvName = pvName
-		self.pvOn = self.pvName+"ON"+str(channel)
-		self.pvSet = self.pvName+"VSET"+str(channel)
-		self.readout = self.pvName+"VMON"+str(channel)+":RBV"
-		self.rampUp = self.pvName+"STAT"+str(channel)+":RBV.B0"
-		self.rampUp = self.pvName+"STAT"+str(channel)+":RBV.B1"
-		self.rampDown = self.pvName+"STAT"+str(channel)+":RBV.B2"
-		self.pvTripped = self.pvName+"STAT"+str(channel)+":RBV.B7"
-		self.pvEnabled = self.pvName+"STAT"+str(channel)+":RBV.BA"
-
-	def isBusy(self):
-		return 0
-
-	def getPosition(self):
-		return caget(self.readout) 
-
-	def asynchronousMoveTo(self,X):
-		if ( caget(self.pvEnabled) == 1 ):
-			print "Channel OFF."
-			return
-		caput(self.pvSet , X)
-		if ( caget( self.pvOn ) == 0 ):
-			caput ( self.pvOn , 1)
-
-	def isEnabled(self):
-		return caget(self.pvEnabled)
-	
-	def off(self):
-			caput ( self.pvOn , 0)
-		
 valves = Valves()
 mirrorsVoltages = MirrorsVoltages()
-hotwaxs = Hotwaxs()
 i22 = I22()
--- configurations/i22-config/scripts/pilatusThreshold.py
+++ configurations/i22-config/scripts/pilatusThreshold.py
@@ -14,36 +14,38 @@ class PilatusThreshold(ScannableMotionBase):
         self.timer=tictoc()
         self.waitUntilTime = 0
         self.demand = 0.0
-	self.gain = CAClient(pvbase+":GainMenu")
-	self.thres = CAClient(pvbase+":ThresholdEnergy")
-	self.gain.configure()
-	self.thres.configure()
-	self.gainranges = { 0 : [6.5, 19.7], 1 : [4.4, 14.0], 2 : [3.8, 11.4] }
-	self.thresholdtolerance = 0.1
-	self.waittime = 30
+        self.gain = CAClient(pvbase+":GainMenu")
+        self.thres = CAClient(pvbase+":ThresholdEnergy")
+        self.gain.configure()
+        self.thres.configure()
+        #self.gainranges = { 0 : [6.5, 19.7], 1 : [4.4, 14.0], 2 : [3.8, 11.4] }
+        self.gainranges = { } # P3 does autogain
+        self.thresholdtolerance = 0.1
+        #self.waittime = 30
+        self.waittime = 8 # P3 faster
 
     def rawGetPosition(self):
         return [self.thres.caget() * 2.0]
 
     def rawAsynchronousMoveTo(self,newpos):
-	gain = int(self.gain.caget())
-	if newpos >= self.gainranges[gain][0] and newpos <= self.gainranges[gain][1]:
-		# gain ok
-		pass
-	else:
-		for i in self.gainranges.keys():
-			if newpos >= self.gainranges[i][0] and newpos <= self.gainranges[i][1]:
-				self.gain.caput(i)	
-				self.timer.reset()
-				break
-		# raise exception, value out of range
-	thres = self.thres.caget()
-	if abs((thres * 2.0) - newpos) < newpos * self.thresholdtolerance:
-		# threshold ok
-		pass
-	else:
-		self.thres.caput(newpos / 2.0)
-		self.timer.reset()
-	
+        gain = int(self.gain.caget())
+        if newpos >= self.gainranges[gain][0] and newpos <= self.gainranges[gain][1]:
+            # gain ok
+            pass
+        else:
+            for i in self.gainranges.keys():
+                if newpos >= self.gainranges[i][0] and newpos <= self.gainranges[i][1]:
+                    self.gain.caput(i)	
+                    self.timer.reset()
+                    break
+            # raise exception, value out of range
+        thres = self.thres.caget()
+        if abs((thres * 2.0) - newpos) < newpos * self.thresholdtolerance:
+            # threshold ok
+            pass
+        else:
+            self.thres.caput(newpos / 2.0)
+            self.timer.reset()
+
     def rawIsBusy(self):
         return (self.timer()<self.waittime)
