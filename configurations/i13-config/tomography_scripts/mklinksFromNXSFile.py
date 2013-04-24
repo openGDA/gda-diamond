435b265569f51304e6d06ad7f4e7e701dff3d67d
Kaz Wanelik <kaz.wanelik@diamond.ac.uk>
2013-04-24 16:06:14 +0100

    Simplified mklinksFromNXSFile by removing the need to input inBeamPos and outOfBeamPos as this is now needed only if image keys are not available.

--- configurations/i13-config/tomography_scripts/mklinksFromNXSFile.py
+++ configurations/i13-config/tomography_scripts/mklinksFromNXSFile.py
@@ -1083,6 +1083,11 @@ def makeLinksForNXSFile(\
 		N=min(10, N)
 
 	if not imgkeyNXS:
+		if (inBeamPos is None) or (outOfBeamPos is None):
+			msg = "INFO: Image-key data are not available in input NeXus file - please re-run this script using \n"
+			msg += "the stageInBeamPhys and stageOutOfBeamPhys options, followed by appropriate values \n"
+			msg += "(the latter can be found in scan_command recorded in scan's NeXus file)."
+			raise Exception(msg)
 		image_key_curr={}
 	# identify each entry as DARK, FLAT, PROJ or UNCLASSIFIED image
 		for i in range(0, N):
@@ -1350,7 +1355,7 @@ creates directories and links to projection, dark and flat images required for s
 	#parser.print_version()
 
 # Make sure all mandatory input variables have some values
-	mandatories=['filename', 'shutOpenPos', 'shutClosedPos', 'inBeamPos', 'outOfBeamPos', 'shutNXSPath', 'stagePosNXSPath', 'stageRotNXSPath', 'tifNXSPath']
+	mandatories=['filename', 'shutOpenPos', 'shutClosedPos', 'shutNXSPath', 'stagePosNXSPath', 'stageRotNXSPath', 'tifNXSPath']
 	for m in mandatories:
 		#print opts_dict[m]
 		if opts_dict[m] is None:
