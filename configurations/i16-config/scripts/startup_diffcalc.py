fef616dd4e01b7a2e6d763e22a6d1f5b3c100eb7
Rob Walton <rob.walton@diamond.ac.uk>
2013-05-10 09:17:14 +0100

    I16: Configure diffcalc to use I16's euler device order rather than Diffcalc's order

--- configurations/i16-config/scripts/startup_diffcalc.py
+++ configurations/i16-config/scripts/startup_diffcalc.py
@@ -1,50 +1,40 @@
 print "<<< Entering: startup_diffcalc.py ..."
-
-from gda.configuration.properties import LocalProperties
-
 import diffcalc.hkl.you.geometry
+from diffcalc.hkl.you.geometry import YouPosition, YouGeometry
 from diffcalc.gdasupport.factory import create_objects, add_objects_to_namespace
+from gdascripts.pd.dummy_pds import DummyPD
 from scannable.extraNameHider import ExtraNameHider
 
-
-
-from gdascripts.pd.dummy_pds import DummyPD
 dummy_energy = DummyPD('dummy_energy')
-
 simple_energy = ExtraNameHider('energy', dummy_energy)  # @UndefinedVariable
-# For dummy: del mu, delta, gam, eta, chi, phi  # @UndefinedVariable
-demo_commands = []
-_tmp_names = list(euler.inputNames)
-_tmp_names[2] = 'nu'
-euler.inputNames = _tmp_names
+
+
+class SixCircleI16(YouGeometry):
+    def __init__(self):
+        diffcalc.hkl.you.geometry.YouGeometry.__init__(self, 'sixc', {})
+
+    def physical_angles_to_internal_position(self, physical_angle_tuple):
+        #    i16:   phi, chi, eta, mu, delta, gam
+        # H. You:   mu, delta, nu, eta, chi, phi
+        phi, chi, eta, mu, delta, gam = physical_angle_tuple     
+        return YouPosition(mu, delta, gam, eta, chi, phi)
+
+    def internal_position_to_physical_angles(self, internal_position):
+        mu, delta, nu, eta, chi, phi = internal_position.totuple()
+        return phi, chi, eta, mu, delta, nu
+
 
 diffcalcObjects=create_objects(
-    #dummy_axis_names = ['mu', 'delta', 'gam', 'eta', 'chi', 'phi'],
-    axes_group_scannable = euler, #@UndefinedVariable
+    axes_group_scannable = euler,  # @UndefinedVariable
     energy_scannable = simple_energy,
-    #dummy_energy_name = 'dummyenergy',
     energy_scannable_multiplier_to_get_KeV = 1,
-    geometry = diffcalc.hkl.you.geometry.SixCircle(),
+    geometry = SixCircleI16(),
     hklverbose_virtual_angles_to_report=('theta','alpha','beta','psi'),
-    demo_commands = demo_commands,
+    demo_commands = [],
     engine_name = 'you'
-    
 )
 
-#diffcalcObjects['diffcalcdemo'].commands = demo_commands;
-add_objects_to_namespace( diffcalcObjects, globals() )
-
+add_objects_to_namespace( diffcalcObjects, globals() ) #@UndefinedVariable
 hkl.setLevel(6) #@UndefinedVariable
-
-#demo_commands.append( "newub 'cubic'" )
-#demo_commands.append( "setlat 'cubic' 1 1 1 90 90 90" )
-#demo_commands.append( "pos wl 1" )
-#demo_commands.append( "pos sixc [0 60 0 30 1 0]" )
-#demo_commands.append( "addref 1 0 0" )
-#demo_commands.append( "pos chi 91" )
-#demo_commands.append( "addref 0 0 1" )
-#demo_commands.append( "checkub" )
-#demo_commands.append( "ub" )
-#demo_commands.append( "hklmode" )
 
 print "... Leaving: startup_diffcalc.py >>>"
