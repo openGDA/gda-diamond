4690c55507713616a925694c0a944534948483b4
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2013-05-28 09:16:13 +0100

    I13J - Add Excalibur

--- configurations/i13-1-config/bin/gda_environment.sh
+++ configurations/i13-1-config/bin/gda_environment.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
-# This script assumes that $BEAMLINE is set (e.g. i02, i18, b16) if not exit...
-
-# **** DO NOT EDIT THIS SCRIPT AS IT MAY BE AUTOMATICALLY OVERWRITTEN *****
-
-if [ ! -n "$BEAMLINE" ]; 
-then
-  echo "Please set BEAMLINE environment variable."
-  exit 1
-fi
-if [ ! -n "$GDAFOLDER" ]; 
-then
-  echo "Please set GDAFOLDER environment variable."
-  exit 1
-fi
-GDA_CLIENT=${GDAFOLDER}/client/gda-i13j; export GDA_CLIENT
+export BEAMLINE=i13-1
+export GDAFOLDER=/dls_sw/$BEAMLINE/software/gda
+export GDAVAR=/dls_sw/$BEAMLINE/software/gda_versions/var
+export GDALOGS=/dls_sw/$BEAMLINE/software/logs
+export GDAMODE=live
 
 export PATH=${GDAFOLDER}/config/bin:${GDAFOLDER}/workspace_git/gda-diamond.git/dls-config/bin:${PATH}
 
