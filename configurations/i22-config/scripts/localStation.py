979a60f17c3d60f09dc873ab09789dc4a2fd38d6
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-06-27 17:53:15 +0100

    SCATTER-141 avoid name confict with mjpg view (seems to cause issues in sci soft)

--- configurations/i22-config/scripts/localStation.py
+++ configurations/i22-config/scripts/localStation.py
@@ -86,7 +86,7 @@ gridxy=ScannableGroup()
 gridxy.setName("gridxy")
 gridxy.setGroupMembers([pimf_x, pimf_y])
 gridxy.configure()
-ncdgridscan=gridscan.Grid("Camera View", "Mapping Grid", mfgige, gridxy, ncddetectors)
+ncdgridscan=gridscan.Grid("Microscope View", "Mapping Grid", mfgige, gridxy, ncddetectors)
 ncdgridscan.snap()
 
 import metadatatweaks
