d98a9b0502421698420740cd29418e17ac0f7843
Mark Booth <Mark.Booth@diamond.ac.uk>
2013-07-17 17:29:06 +0100

    BLXV (8.30) Switch from software/gda/i15-config to software/gda/config

--- configurations/i15-config/bin/remotestartupscript.sh
+++ configurations/i15-config/bin/remotestartupscript.sh
@@ -10,8 +10,8 @@ SOFTWAREFOLDER=dls_sw; export SOFTWAREFOLDER
 OBJECT_SERVER_STARTUP_FILE=/$SOFTWAREFOLDER/$BEAMLINE/var/object_server_startup_i15server
 rm -f $OBJECT_SERVER_STARTUP_FILE
 
-/dls_sw/i15/software/gda/i15-config/bin/GDA_StartServers $CMD
+/dls_sw/i15/software/gda/config/bin/GDA_StartServers $CMD
 
 # look for the output file which will tell us when the servers have started
 
-/$SOFTWAREFOLDER/$BEAMLINE/software/gda/i15-config/bin/lookForFile $OBJECT_SERVER_STARTUP_FILE
+/$SOFTWAREFOLDER/$BEAMLINE/software/gda/config/bin/lookForFile $OBJECT_SERVER_STARTUP_FILE
