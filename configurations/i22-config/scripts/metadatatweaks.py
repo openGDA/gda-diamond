5e0169f732fe0f4b7c5ae0e0c98f6b761bdb5dca
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-08-08 14:58:57 +0100

    add setSubdirectory and get

--- configurations/i22-config/scripts/metadatatweaks.py
+++ configurations/i22-config/scripts/metadatatweaks.py
@@ -19,3 +19,32 @@ def setVisit(visit):
 
 def getVisit():
     return GDAMetadataProvider.getInstance().getMetadataValue("visit")
+
+def setSubdirectory(title):
+    GDAMetadataProvider.getInstance().setMetadataValue("subdirectory", title)
+
+def getSubdirectory():
+    return GDAMetadataProvider.getInstance().getMetadataValue("subdirectory")
+
+
+class SampleNameScannable(gda.device.scannable.ScannableBase):
+  
+   """ This is the constructor for the class. """
+   def __init__(self, name, metadataname):
+       self.setName(name)
+       self.setInputNames([name])
+       self.setOutputFormat(["%s"])
+       self.metadata=Finder.getInstance().find(metadataname)
+
+   """ Mandatory method.  It should return 1 if the device is moving and 0 otherwise."""
+   def isBusy(self):
+       return False
+
+   """ Mandatory method.  Return the current position as a scalar or as a vector."""
+   def getPosition(self):
+       return self.metadata.readActualValue()
+ 
+   """ Mandatory method.  Must return immediately; the move should be controlled by a separate thread."""
+   def asynchronousMoveTo(self,newPosition):
+       self.metadata.setValue(newPosition) 
+
