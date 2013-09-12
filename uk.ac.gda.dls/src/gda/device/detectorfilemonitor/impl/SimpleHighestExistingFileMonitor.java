openGDA/gda-diamond

commit be3129c7ad01bd250175590cb8489cfc4d4bf6a2

MXGDA-1357 Reduce code duplication by using HighestExitingFileMonitorSettings.getFullTemplate in a couple of places

uk.ac.gda.dls/src/gda/device/detectorfilemonitor/impl/SimpleHighestExistingFileMonitor.java
@@ -90,13 +90,7 @@ public class SimpleHighestExistingFileMonitor implements HighestExistingFileMoni
 					latestNumberFound = null;
 					highestExitingFileMonitorSettings_InUse = highestExitingFileMonitorSettings;
 					numberToLookFor = highestExitingFileMonitorSettings_InUse.startNumber;
-					
-					templateInUse = highestExitingFileMonitorSettings_InUse.fileTemplatePrefix;
-					if (!templateInUse.endsWith(File.separator)) {
-						templateInUse += File.separator;
-					}
-					templateInUse += highestExitingFileMonitorSettings_InUse.fileTemplate;
-					
+					final String templateInUse = highestExitingFileMonitorSettings_InUse.getFullTemplate();
 					numberFound = null;
 					if (highestExitingFileMonitorSettings_InUse == null) {
 						running = false;

uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/views/LatestFileNameCompositeFactory.java
@@ -56,7 +56,6 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchPartSite;
 import org.eclipse.ui.PlatformUI;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.StringUtils;
 
 import swing2swt.layout.BorderLayout;
 import uk.ac.gda.common.rcp.util.EclipseWidgetUtils;
@@ -568,11 +567,18 @@ class LatestFileNameComposite extends Composite {
 			return;
 		int index = getSelectedIndex();
 		if (latestSettings != null) {
-			String prefix = latestSettings.fileTemplatePrefix;
-			String filename = String.format(prefix + latestSettings.fileTemplate, index);
-			String text = StringUtils.hasLength(filename) ? (String) filename : WAITING;
-			if (prefix.length() > 0 && text.startsWith(prefix)) {
-				text = text.substring(prefix.length());
+			
+			String filename;
+			String text;
+			
+			if (latestSettings.isEmpty()) {
+				filename = "";
+				text = WAITING;
+			}
+			
+			else {
+				filename = String.format(latestSettings.getFullTemplate(), index);
+				text = String.format(latestSettings.fileTemplate, index);
 			}
 
 			int currentLength = fileNameText.getText().length();

commit ebb0094c2849d135aab3a9d001f84115800314f3

Fix bug in SimpleHighestExistingFileMonitor introduced in be3129c

uk.ac.gda.dls/src/gda/device/detectorfilemonitor/impl/SimpleHighestExistingFileMonitor.java
@@ -90,7 +90,7 @@ public class SimpleHighestExistingFileMonitor implements HighestExistingFileMoni
 					latestNumberFound = null;
 					highestExitingFileMonitorSettings_InUse = highestExitingFileMonitorSettings;
 					numberToLookFor = highestExitingFileMonitorSettings_InUse.startNumber;
-					final String templateInUse = highestExitingFileMonitorSettings_InUse.getFullTemplate();
+					templateInUse = highestExitingFileMonitorSettings_InUse.getFullTemplate();
 					numberFound = null;
 					if (highestExitingFileMonitorSettings_InUse == null) {
 						running = false;