a24162b929c163b2d90c46a1a3c4cc9b4f8f4636
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2013-10-03 15:16:31 +0100

    BLXXJ-146 added topupChecker, absorberChecker and shutterChecker to EDE config

--- configurations/i20-1-config/scripts/localStation.py
+++ configurations/i20-1-config/scripts/localStation.py
@@ -1,11 +1,21 @@
 from uk.ac.gda.exafs.ui.data import EdeScanParameters
 
-#energyTable = finder.find("energyTable")
-
-#if energyTable != None:
-#    def getenergies(energyValue):
-#        return energyTable.getPositionsForEnergy(energyValue)
-#    alias getenergies
-
-
 run "roi_control"
+
+# These scannables are checked before any scan data point
+# You may comment them out to remove the checking.
+if LocalProperties.get("gda.mode") == "live":
+    # to speed up step scans
+    LocalProperties.set("gda.scan.concurrentScan.readoutConcurrently","true")
+    LocalProperties.set("gda.scan.multithreadedScanDataPointPipeline.length","10")
+    if (machineMode() == "No Beam"):
+        remove_default([topupChecker])
+        remove_default([absorberChecker])
+        remove_default([shutterChecker])
+    else:
+        add_default([topupChecker])
+        add_default([absorberChecker])
+        add_default([shutterChecker])
+else:
+    remove_default([topupChecker])
+    remove_default([absorberChecker])
