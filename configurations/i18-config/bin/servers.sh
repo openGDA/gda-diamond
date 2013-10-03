403f4a0c9d4180d43059e54cbc9a2f4beccb7f04
chris.coles@diamond.ac.uk <chris.coles@diamond.ac.uk>
2013-10-03 15:43:38 +0100

    i18: updated server and log startup

--- configurations/i18-config/bin/servers.sh
+++ configurations/i18-config/bin/servers.sh
@@ -1,15 +1,21 @@
 
 # runs the servers locally
-source /dls_sw/dasc/tools_versions/set_tools.sh
+#source /dls_sw/dasc/tools_versions/set_tools.sh
+
+
+echo "got here"
+touch /dls_sw/i18/software/gda_versions/var/test
+
+module load java/gda/830
 
 killall java
 #/dls_sw/i18/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --stop objectserver
 #/dls_sw/i18/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --stop eventserver
 #/dls_sw/i18/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --stop nameserver
 
-#echo "got here"
-#touch /dls_sw/i18/software/gda_versions/var/test
+
 
 #module load java
 #whoami > /dls_sw/i18/software/gda_versions/var/test2
+JAVA_OPTS="-Xms1024m -Xmx4096m -XX:PermSize=512m -XX:MaxPermSize=1024m" /dls_sw/i18/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --config=/dls_sw/i18/software/gda/config --stop servers
 JAVA_OPTS="-Xms1024m -Xmx4096m -XX:PermSize=512m -XX:MaxPermSize=1024m" /dls_sw/i18/software/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --config=/dls_sw/i18/software/gda/config --debug -p 8001 --restart servers
