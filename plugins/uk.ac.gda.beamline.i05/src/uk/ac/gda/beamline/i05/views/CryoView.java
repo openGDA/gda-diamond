aeaa3e343dcb684d3a6eb8d30211617cdb28c50c
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2013-10-14 12:05:20 +0100

    ARPES-142

--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/views/CryoView.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/views/CryoView.java
@@ -16,7 +16,7 @@
  * with GDA. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package uk.ac.gda.arpes.ui.views;
+package uk.ac.gda.beamline.i05.views;
 
 import gda.device.Scannable;
 import gda.factory.Finder;
