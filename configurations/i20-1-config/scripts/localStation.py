9750291f70d192c03e5b9a3f1e3152f5682a9ac9
Phyo Kyaw <phyo.kyaw@diamond.ac.uk>
2013-12-05 10:28:36 +0000

    Moved javajythonutil.py location to core.

--- configurations/i20-1-config/scripts/localStation.py
+++ configurations/i20-1-config/scripts/localStation.py
@@ -2,7 +2,7 @@ from uk.ac.gda.exafs.ui.data import EdeScanParameters
 from gda.configuration.properties import LocalProperties
 
 run("roi_control.py")
-run("javajythonutil.py")
+run("gdascripts/javajythonutil.py")
 
 das = finder.find("DAServer")
 
