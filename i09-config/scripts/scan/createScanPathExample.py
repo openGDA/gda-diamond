5151f4bb46324a2bc0d1389c4f6ff3f63e72cfb2
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-01-23 17:29:27 +0000

    create example on how to create scan path from lists

---
+++ i09-config/scripts/scan/createScanPathExample.py
@@ -0,0 +1,20 @@
+'''
+Created on 23 Jan 2014
+
+@author: fy65
+'''
+from scan.dRangeUtil import drange
+
+
+xpoints=drange(1, 2, 0.01)
+print "x-points: ",xpoints
+ypoints=drange(2, 3, 0.01)
+print "y-points: ", ypoints
+zpoints=drange(3, 4, 0.01)
+print "z-points: ", zpoints
+if len(xpoints) != len(ypoints) or len(xpoints) != len(zpoints):
+    raise ValueError("The number of path points for each scannables must the same.")
+scanpath=zip(xpoints, ypoints, zpoints)
+print "scan path: ", scanpath
+
+#pathscan((x,y,z),scanpath,[])
