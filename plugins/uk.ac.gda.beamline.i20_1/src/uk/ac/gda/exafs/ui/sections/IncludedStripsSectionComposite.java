8992f6f3f83aa539e68ed7dbef6e15d5e5d5a475
Phyo Kyaw <phyo.kyaw@diamond.ac.uk>
2014-02-03 10:12:22 +0000

    Refactored included strips UI section.

---
+++ plugins/uk.ac.gda.beamline.i20_1/src/uk/ac/gda/exafs/ui/sections/IncludedStripsSectionComposite.java
@@ -0,0 +1,156 @@
+/*-
+ * Copyright © 2014 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.exafs.ui.sections;
+
+import gda.device.detector.XHDetector;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import org.eclipse.core.databinding.Binding;
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.jface.databinding.swt.WidgetProperties;
+import org.eclipse.jface.databinding.viewers.ViewersObservables;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.ui.forms.widgets.ExpandableComposite;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+import org.eclipse.ui.forms.widgets.Section;
+
+import uk.ac.gda.exafs.data.DetectorModel;
+import uk.ac.gda.exafs.ui.data.UIHelper;
+
+public class IncludedStripsSectionComposite extends ResourceComposite {
+
+	private Section section;
+	private final FormToolkit toolkit;
+	private ComboViewer cmbFirstStripViewer;
+	private ComboViewer cmbLastStripViewer;
+
+	private Binding cmbFirstStripViewerBinding;
+	private Binding cmbLastStripViewerBinding;
+
+	private final DataBindingContext dataBindingCtx = new DataBindingContext();
+
+
+	public IncludedStripsSectionComposite(Composite parent, int style, FormToolkit toolkit) {
+		super(parent, style);
+		this.toolkit = toolkit;
+		setupUI();
+	}
+
+	private void setupUI() {
+		this.setLayout(UIHelper.createGridLayoutWithNoMargin(1, false));
+		section = toolkit.createSection(this, ExpandableComposite.TITLE_BAR | ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
+		section.setText("Included strips");
+		section.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		Composite sectionComposite = toolkit.createComposite(section, SWT.NONE);
+		sectionComposite.setLayout(UIHelper.createGridLayoutWithNoMargin(1, false));
+		toolkit.paintBordersFor(sectionComposite);
+		section.setClient(sectionComposite);
+
+		Composite stripsComposite = new Composite(sectionComposite, SWT.NONE);
+		stripsComposite.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, true));
+		stripsComposite.setLayout(new GridLayout(4, false));
+
+		final Label lblFirstStrip = toolkit.createLabel(stripsComposite, "First strip", SWT.NONE);
+		lblFirstStrip.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		CCombo cmbFirstStrip = new CCombo(stripsComposite, SWT.BORDER | SWT.FLAT | SWT.READ_ONLY);
+		cmbFirstStrip.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		cmbFirstStripViewer = new ComboViewer(cmbFirstStrip);
+		cmbFirstStripViewer.setContentProvider(new ArrayContentProvider());
+		cmbFirstStripViewer.setLabelProvider(new LabelProvider());
+		cmbFirstStripViewer.setInput(XHDetector.getStrips());
+
+		Label lblLastStrip = toolkit.createLabel(stripsComposite, "Last strip", SWT.NONE);
+		lblLastStrip.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
+		CCombo cmbLastStrip = new CCombo(stripsComposite, SWT.BORDER | SWT.FLAT | SWT.READ_ONLY);
+		cmbLastStrip.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		cmbLastStripViewer = new ComboViewer(cmbLastStrip);
+		cmbLastStripViewer.setContentProvider(new ArrayContentProvider());
+		cmbLastStripViewer.setLabelProvider(new LabelProvider());
+		cmbLastStripViewer.setInput(XHDetector.getStrips());
+
+		DetectorModel.INSTANCE.addPropertyChangeListener(DetectorModel.DETECTOR_CONNECTED_PROP_NAME, dectectorChangeListener);
+
+		if (DetectorModel.INSTANCE.getCurrentDetector() != null) {
+			bindUpperAndLowerChannelComboViewers();
+		}
+
+
+		if (DetectorModel.INSTANCE.getCurrentDetector() != null) {
+			cmbFirstStripViewer.setSelection(new StructuredSelection(DetectorModel.INSTANCE.getCurrentDetector().getLowerChannel()));
+			cmbLastStripViewer.setSelection(new StructuredSelection(DetectorModel.INSTANCE.getCurrentDetector().getUpperChannel()));
+		}
+
+		dataBindingCtx.bindValue(
+				WidgetProperties.enabled().observe(cmbFirstStripViewer.getControl()),
+				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.DETECTOR_CONNECTED_PROP_NAME));
+
+		dataBindingCtx.bindValue(
+				WidgetProperties.enabled().observe(cmbLastStripViewer.getControl()),
+				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.DETECTOR_CONNECTED_PROP_NAME));
+	}
+
+	private void bindUpperAndLowerChannelComboViewers() {
+		cmbFirstStripViewerBinding = dataBindingCtx.bindValue(
+				ViewersObservables.observeSingleSelection(cmbFirstStripViewer),
+				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.LOWER_CHANNEL_PROP_NAME));
+		cmbLastStripViewerBinding = dataBindingCtx.bindValue(
+				ViewersObservables.observeSingleSelection(cmbLastStripViewer),
+				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.UPPER_CHANNEL_PROP_NAME));
+	}
+
+	private final PropertyChangeListener dectectorChangeListener = new PropertyChangeListener() {
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+			boolean detectorConnected = (boolean) evt.getNewValue();
+			if (detectorConnected) {
+				bindUpperAndLowerChannelComboViewers();
+			} else {
+				if (cmbFirstStripViewerBinding != null) {
+					dataBindingCtx.removeBinding(cmbFirstStripViewerBinding);
+					cmbFirstStripViewerBinding.dispose();
+					cmbFirstStripViewerBinding = null;
+				}
+				if (cmbLastStripViewerBinding != null) {
+					dataBindingCtx.removeBinding(cmbLastStripViewerBinding);
+					cmbLastStripViewerBinding.dispose();
+					cmbLastStripViewerBinding = null;
+				}
+			}
+		}
+	};
+
+	@Override
+	protected void disposeResource() {
+		dataBindingCtx.dispose();
+		DetectorModel.INSTANCE.removePropertyChangeListener(DetectorModel.DETECTOR_CONNECTED_PROP_NAME, dectectorChangeListener);
+	}
+
+}
--- plugins/uk.ac.gda.beamline.i20_1/src/uk/ac/gda/exafs/ui/sections/SingleSpectrumParametersSection.java
+++ plugins/uk.ac.gda.beamline.i20_1/src/uk/ac/gda/exafs/ui/sections/SingleSpectrumParametersSection.java
@@ -18,12 +18,6 @@
 
 package uk.ac.gda.exafs.ui.sections;
 
-import gda.device.detector.XHDetector;
-
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
-import org.eclipse.core.databinding.Binding;
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.BeanProperties;
@@ -31,14 +25,7 @@ import org.eclipse.core.databinding.beans.BeansObservables;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
-import org.eclipse.jface.databinding.viewers.ViewersObservables;
-import org.eclipse.jface.viewers.ArrayContentProvider;
-import org.eclipse.jface.viewers.ComboViewer;
-import org.eclipse.jface.viewers.LabelProvider;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.custom.CCombo;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -72,13 +59,7 @@ public class SingleSpectrumParametersSection extends ResourceComposite {
 	private final DataBindingContext dataBindingCtx = new DataBindingContext();
 	private Section section;
 
-	private ComboViewer cmbFirstStripViewer;
 
-	private StructuredViewer cmbLastStripViewer;
-
-	protected Binding cmbFirstStripViewerBinding;
-
-	protected Binding cmbLastStripViewerBinding;
 
 	private final boolean forExperiment;
 
@@ -313,90 +294,12 @@ public class SingleSpectrumParametersSection extends ResourceComposite {
 	}
 
 	private void createIncludedStripsSelection() {
-		section = toolkit.createSection(this, ExpandableComposite.TITLE_BAR | ExpandableComposite.TWISTIE | ExpandableComposite.EXPANDED);
-		section.setText("Included strips");
-		section.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
-		Composite sectionComposite = toolkit.createComposite(section, SWT.NONE);
-		sectionComposite.setLayout(UIHelper.createGridLayoutWithNoMargin(1, false));
-		toolkit.paintBordersFor(sectionComposite);
-		section.setClient(sectionComposite);
-
-		Composite stripsComposite = new Composite(sectionComposite, SWT.NONE);
-		stripsComposite.setLayoutData(new GridData(SWT.FILL, SWT.BEGINNING, true, true));
-		stripsComposite.setLayout(new GridLayout(4, false));
-
-		final Label lblFirstStrip = toolkit.createLabel(stripsComposite, "First strip", SWT.NONE);
-		lblFirstStrip.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-		CCombo cmbFirstStrip = new CCombo(stripsComposite, SWT.BORDER | SWT.FLAT);
-		cmbFirstStrip.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		cmbFirstStripViewer = new ComboViewer(cmbFirstStrip);
-		cmbFirstStripViewer.setContentProvider(new ArrayContentProvider());
-		cmbFirstStripViewer.setLabelProvider(new LabelProvider());
-		cmbFirstStripViewer.setInput(XHDetector.getStrips());
-
-		Label lblLastStrip = toolkit.createLabel(stripsComposite, "Last strip", SWT.NONE);
-		lblLastStrip.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
-		CCombo cmbLastStrip = new CCombo(stripsComposite, SWT.BORDER | SWT.FLAT);
-		cmbLastStrip.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
-		cmbLastStripViewer = new ComboViewer(cmbLastStrip);
-		cmbLastStripViewer.setContentProvider(new ArrayContentProvider());
-		cmbLastStripViewer.setLabelProvider(new LabelProvider());
-		cmbLastStripViewer.setInput(XHDetector.getStrips());
-
-		DetectorModel.INSTANCE.addPropertyChangeListener(DetectorModel.DETECTOR_CONNECTED_PROP_NAME, dectectorChangeListener);
-
-		if (DetectorModel.INSTANCE.getCurrentDetector() != null) {
-			bindUpperAndLowerChannelComboViewers();
-		}
-
-
-		if (DetectorModel.INSTANCE.getCurrentDetector() != null) {
-			cmbFirstStripViewer.setSelection(new StructuredSelection(DetectorModel.INSTANCE.getCurrentDetector().getLowerChannel()));
-			cmbLastStripViewer.setSelection(new StructuredSelection(DetectorModel.INSTANCE.getCurrentDetector().getUpperChannel()));
-		}
-
-		dataBindingCtx.bindValue(
-				WidgetProperties.enabled().observe(cmbFirstStripViewer.getControl()),
-				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.DETECTOR_CONNECTED_PROP_NAME));
-
-		dataBindingCtx.bindValue(
-				WidgetProperties.enabled().observe(cmbLastStripViewer.getControl()),
-				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.DETECTOR_CONNECTED_PROP_NAME));
-	}
-
-	private final PropertyChangeListener dectectorChangeListener = new PropertyChangeListener() {
-		@Override
-		public void propertyChange(PropertyChangeEvent evt) {
-			boolean detectorConnected = (boolean) evt.getNewValue();
-			if (detectorConnected) {
-				bindUpperAndLowerChannelComboViewers();
-			} else {
-				if (cmbFirstStripViewerBinding != null) {
-					dataBindingCtx.removeBinding(cmbFirstStripViewerBinding);
-					cmbFirstStripViewerBinding.dispose();
-					cmbFirstStripViewerBinding = null;
-				}
-				if (cmbLastStripViewerBinding != null) {
-					dataBindingCtx.removeBinding(cmbLastStripViewerBinding);
-					cmbLastStripViewerBinding.dispose();
-					cmbLastStripViewerBinding = null;
-				}
-			}
-		}
-	};
-
-	private void bindUpperAndLowerChannelComboViewers() {
-		cmbFirstStripViewerBinding = dataBindingCtx.bindValue(
-				ViewersObservables.observeSingleSelection(cmbFirstStripViewer),
-				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.LOWER_CHANNEL_PROP_NAME));
-		cmbLastStripViewerBinding = dataBindingCtx.bindValue(
-				ViewersObservables.observeSingleSelection(cmbLastStripViewer),
-				BeansObservables.observeValue(DetectorModel.INSTANCE, DetectorModel.UPPER_CHANNEL_PROP_NAME));
+		IncludedStripsSectionComposite includedStripsSectionComposite = new IncludedStripsSectionComposite(this, SWT.None, toolkit);
+		includedStripsSectionComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 	}
 
 	@Override
 	protected void disposeResource() {
 		dataBindingCtx.dispose();
-		DetectorModel.INSTANCE.removePropertyChangeListener(DetectorModel.DETECTOR_CONNECTED_PROP_NAME, dectectorChangeListener);
 	}
 }
