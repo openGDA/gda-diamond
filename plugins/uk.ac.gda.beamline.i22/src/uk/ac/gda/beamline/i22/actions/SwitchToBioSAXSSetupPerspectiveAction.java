f621714e690a0d850ef903ffad026ee5a3ad5979
Conor Lehane <xlw00930@ws141.diamond.ac.uk>
2014-02-07 10:28:56 +0000

    Saving Spring setup changes

--- plugins/uk.ac.gda.beamline.i22/src/uk/ac/gda/beamline/i22/actions/SwitchToBioSAXSSetupPerspectiveAction.java
+++ plugins/uk.ac.gda.beamline.i22/src/uk/ac/gda/beamline/i22/actions/SwitchToBioSAXSSetupPerspectiveAction.java
@@ -2,6 +2,8 @@ package uk.ac.gda.beamline.i22.actions;
 
 import java.util.Properties;
 
+import org.eclipse.ui.IPerspectiveDescriptor;
+import org.eclipse.ui.IPerspectiveRegistry;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -13,8 +15,6 @@ import org.eclipse.ui.intro.config.IIntroAction;
 public class SwitchToBioSAXSSetupPerspectiveAction implements IIntroAction {
 
 	public SwitchToBioSAXSSetupPerspectiveAction() {
-		System.out.println("SwitchPerspective");
-
 		IWorkbench workbench = PlatformUI.getWorkbench();
 		IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 
@@ -23,6 +23,23 @@ public class SwitchToBioSAXSSetupPerspectiveAction implements IIntroAction {
 			workbench.showPerspective("uk.ac.gda.devices.bssc.biosaxsresultperspective", window);
 			workbench.showPerspective("uk.ac.gda.devices.bssc.biosaxsprogressperspective", window);
 			workbench.showPerspective("uk.ac.gda.devices.bssc.biosaxssetupperspective", window);
+
+			IPerspectiveRegistry iPerspectiveRegistry = PlatformUI.getWorkbench().getPerspectiveRegistry();
+			
+			for (IPerspectiveDescriptor descriptor : iPerspectiveRegistry.getPerspectives())
+			{
+				System.out.println("descriptor id : " + descriptor.getId());
+				System.out.println("descriptor label : " + descriptor.getLabel());
+			}
+			iPerspectiveRegistry.setDefaultPerspective("uk.ac.gda.devices.bssc.biosaxssetupperspective");
+			IPerspectiveDescriptor perspectiveDescriptor = iPerspectiveRegistry.findPerspectiveWithLabel("Scripts");
+
+			try {
+				iPerspectiveRegistry.deletePerspective(perspectiveDescriptor);
+			} catch (Exception e) {
+				System.out.println("=====DELETED=====");
+			}
+
 		} catch (WorkbenchException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
