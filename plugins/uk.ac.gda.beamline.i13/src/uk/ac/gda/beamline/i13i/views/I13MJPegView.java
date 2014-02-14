63149c3d9964f78d7f7d08ce4fdddc852c845248
Paul Gibbons <paul.gibbons@diamond.ac.uk>
2014-02-14 13:52:01 +0000

    GDA-5840 - Subclass AD views for PCO

--- plugins/uk.ac.gda.beamline.i13/src/uk/ac/gda/beamline/i13i/views/I13MJPegView.java
+++ plugins/uk.ac.gda.beamline.i13/src/uk/ac/gda/beamline/i13i/views/I13MJPegView.java
@@ -18,30 +18,37 @@
 
 package uk.ac.gda.beamline.i13i.views;
 
-import org.eclipse.core.runtime.IConfigurationElement;
+import java.util.List;
+import java.util.Vector;
+
+import org.eclipse.jface.action.IAction;
 import org.eclipse.swt.widgets.Composite;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import uk.ac.diamond.scisoft.analysis.rcp.AnalysisRCPActivator;
 import uk.ac.gda.beamline.i13i.ADViewerImpl.I13ADControllerImpl;
 import uk.ac.gda.beamline.i13i.ADViewerImpl.I13MJPEGViewComposite;
-import uk.ac.gda.epics.adviewer.ADController;
 import uk.ac.gda.epics.adviewer.composites.MJPeg;
+import uk.ac.gda.epics.adviewer.views.ADActionUtils;
 import uk.ac.gda.epics.adviewer.views.MJPegView;
 
 public class I13MJPegView extends MJPegView {
 	private static final Logger logger = LoggerFactory.getLogger(I13MJPegView.class);
-	
-	public I13MJPegView(ADController config, IConfigurationElement configurationElement) {
-		super(config, configurationElement);
-		i13ADControllerImpl = (I13ADControllerImpl) config;
+	@SuppressWarnings("hiding")
+	static final String Id = "uk.ac.gda.beamline.i13i.PCOAreaDetectorLiveView";
+	I13ADControllerImpl i13ADControllerImpl;
+
+	public I13MJPegView() {
+		super("i13");
 	}
 
-	I13ADControllerImpl i13ADControllerImpl;
 	@Override
 	protected MJPeg createPartControlEx(Composite parent) {
 		try {
-			I13MJPEGViewComposite i13MJPEGViewComposite = new I13MJPEGViewComposite(parent, i13ADControllerImpl.getStagesCompositeFactory() );
+			i13ADControllerImpl = (I13ADControllerImpl) getAdController();
+			I13MJPEGViewComposite i13MJPEGViewComposite = new I13MJPEGViewComposite(parent,
+					i13ADControllerImpl.getStagesCompositeFactory());
 			i13MJPEGViewComposite.setADController(i13ADControllerImpl, this);
 			return i13MJPEGViewComposite.getMJPeg();
 		} catch (Exception e) {
@@ -50,4 +57,17 @@ public class I13MJPegView extends MJPegView {
 		return null;
 	}
 
+	@Override
+	protected void createShowViewAction() {
+		List<IAction> actions = new Vector<IAction>();
+		{
+			actions.add(ADActionUtils.addShowViewAction("Show Stats", I13PCOHistogramView.Id, null,
+					"Show stats view for selected camera", uk.ac.gda.epics.adviewer.Activator.getHistogramViewImage()));
+			actions.add(ADActionUtils.addShowViewAction("Show Array", I13PCOArrayView.Id, null,
+					"Show array view for selected camera", uk.ac.gda.epics.adviewer.Activator.getTwoDArrayViewImage()));
+		}
+		for (IAction iAction : actions) {
+			getViewSite().getActionBars().getToolBarManager().add(iAction);
+		}
+	}
 }
