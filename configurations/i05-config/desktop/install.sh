f4d613bfad1ad4ee13f9b5df127f23cdc3481e8d
Chris Coles <chris.coles@diamond.ac.uk>
2014-03-16 09:04:59 +0000

    I05 config: renamed workstation specific folders to dummy

--- configurations/i05-config/desktop/install.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-# create/update dataaqc.menu
-
-for host in `host -l diamond.ac.uk |awk '/^'$BEAMLINE'-ws[0-9][0-9][0-9]/ {print $1}'` ; do
-
-ssh $host "SUDO_ASKPASS=/usr/libexec/openssh/ssh-askpass sudo -A ln -sf /dls_sw/$BEAMLINE/software/gda/config/desktop/gda-xinit.sh /etc/X11/xinit/xinitrc.d/gda-xinit.sh"
-
-done
