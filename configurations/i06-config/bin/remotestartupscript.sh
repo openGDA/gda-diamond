64b2badcf291516c96963e429147b4f20bfcd8e3
Mark Booth <Mark.Booth@diamond.ac.uk>
2014-04-04 12:13:11 +0100

    BLVI (8.38) Switch object_server_startup to being written to var folder.

--- configurations/i06-config/bin/remotestartupscript.sh
+++ configurations/i06-config/bin/remotestartupscript.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -l
 
+################################################################################
+##### IMPORTANT NOTE:
+#####
+##### While this file is present in both i06 and i06-1 config/bin folders, only
+##### the one in /dls_sw/i06/software is actually run when gdaservers ssh's to
+##### the i06-control machine.
+#####
+################################################################################
+
 . /usr/share/Modules/init/bash
 . /etc/profile.d/beamline.sh
 
 echo BEAMLINE defaults to $BEAMLINE
-echo SSH_ORIGINAL_COMMAND = $SSH_ORIGINAL_COMMAND
 
 BEAMLINE=$SSH_ORIGINAL_COMMAND
 CMD="$SSH_ORIGINAL_COMMAND"
 : ${CMD:="$*"}
 
-SOFTWAREFOLDER=dls_sw; export SOFTWAREFOLDER
-#OBJECT_SERVER_STARTUP_FILE=/$SOFTWAREFOLDER/$BEAMLINE/software/gda_versions/var/object_server_startup_server_main
-OBJECT_SERVER_STARTUP_FILE=/tmp/object_server_startup_server_i06
-if [ "$BEAMLINE" = "i06-1" ]; then
-  OBJECT_SERVER_STARTUP_FILE=/tmp/object_server_startup_server_i06-1
-fi
-
-rm -f $OBJECT_SERVER_STARTUP_FILE
+echo BEAMLINE now set by SSH_ORIGINAL_COMMAND to $SSH_ORIGINAL_COMMAND
 
 export GDA_MODE=$BEAMLINE
-export GDA_ROOT=$(readlink -f $(dirname $0)/../../../../..)
+# Since i06-1 runs this from i06, we need a GDA_ROOT based on $BEAMLINE
+#      GDA_ROOT must also be set *before* gda_setup_env is run.
+#export GDA_ROOT=$(readlink -f $(dirname $0)/../../../../..)
+export GDA_ROOT=$(readlink -f /dls_sw/$BEAMLINE/software/gda)
+echo GDA_ROOT=$GDA_ROOT
+
 . $GDA_ROOT/workspace_git/gda-mt.git/configurations/mt-config/bin/gda_setup_env gda_servers_output
+
+GDA_VAR=$(readlink -f $GDA_ROOT/../var)
+OBJECT_SERVER_STARTUP_FILE=$GDA_VAR/object_server_startup_server_$BEAMLINE
+rm -f $OBJECT_SERVER_STARTUP_FILE
 
 umask 0002 # Some voodoo copied from GDA-mt/configurations/i16-config/bin/remotestartupscript.sh at d024aae
 # This should fix a problem where sub-directories created in a visit folder end up
@@ -31,24 +41,17 @@ umask 0002 # Some voodoo copied from GDA-mt/configurations/i16-config/bin/remote
 export JAVA_OPTS="-Dgda.deploytype=1 -XX:MaxPermSize=1024m"
 
 #GDA_CORE_SCRIPT_OPTIONS="--headless servers --debug"
-GDA_CORE_SCRIPT_OPTIONS="--headless servers"
-
-# i06 & i06-1 all share the same i06-config directory, so override the one from gda_setup_env
-export GDA_CONFIG=$GDA_ROOT/workspace_git/gda-mt.git/configurations/i06-config
+GDA_CORE_SCRIPT_OPTIONS="-v --headless servers"
 
 ARGS="--properties $GDA_CONFIG/properties/java.properties_$GDA_MODE"
 ARGS="--jacorb     $GDA_CONFIG/properties/jacorb_$GDA_MODE                $ARGS"
 ARGS="--jca        $GDA_CONFIG/properties/JCALibrary.properties_$GDA_MODE $ARGS"
-ARGS="--vardir     $GDA_ROOT/../var                                       $ARGS"
+ARGS="--vardir     $GDA_VAR                                               $ARGS"
 ARGS="--logsdir    /dls_sw/$BEAMLINE/logs                                 $ARGS"
 
 if [ "$BEAMLINE" = "i06-1" ]; then
-  ARGS="--nsport 6701 $ARGS"
+  ARGS="--nsport 6701 --debugport=8010                                    $ARGS"
 fi
-if [ "$BEAMLINE" = "i06" ]; then
-  ARGS="--nsport 6700 $ARGS"
-fi
-
 
 echo  $GDA_CORE_SCRIPT $GDA_CORE_SCRIPT_OPTIONS $ARGS
 echo  $GDA_CORE_SCRIPT $GDA_CORE_SCRIPT_OPTIONS $ARGS >> $GDA_CONSOLE_LOG
