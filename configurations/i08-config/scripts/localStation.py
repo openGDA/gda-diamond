0843d6fe6db462674d7f293ce23c52e186c97092
Ramos <silvia.da-graca-ramos@diamond.ac.uk>
2014-04-16 11:50:20 +0100

    Commit after conflicts

--- configurations/i08-config/scripts/localStation.py
+++ configurations/i08-config/scripts/localStation.py
@@ -1,11 +1,56 @@
+#import sys	
+#import os
+#import time
 from gda.configuration.properties import LocalProperties
 from gda.device.scannable import DummyScannable
 from gda.factory import Finder
+from gdascripts.messages import handle_messages
+from gda.jython import InterfaceProvider
+from gda.device.scannable import ScannableBase
 
-print "Initialization Started";
+print "Initialisation Started";
 
 finder = Finder.getInstance()
 
 test = DummyScannable("test")
+try:
+	from gda.device import Scannable
+	from gda.jython.commands.GeneralCommands import ls_names, vararg_alias
+	
+	def ls_scannables():
+		ls_names(Scannable)
 
-print "Initialization Complete";
+
+	from epics_scripts.pv_scannable_utils import createPVScannable, caput, caget
+	alias("createPVScannable")
+	alias("caput")
+	alias("caget")
+
+	from gda.scan.RepeatScan import create_repscan, repscan
+	vararg_alias("repscan")
+
+	from gdascripts.metadata.metadata_commands import setTitle, meta_add, meta_ll, meta_ls, meta_rm
+	alias("setTitle")
+	alias("meta_add")
+	alias("meta_ll")
+	alias("meta_ls")
+	alias("meta_rm")
+	from gda.data.scan.datawriter import NexusDataWriter
+	LocalProperties.set(NexusDataWriter.GDA_NEXUS_METADATAPROVIDER_NAME,"metashop")
+
+	from gdascripts.pd.time_pds import waittimeClass2, showtimeClass, showincrementaltimeClass, actualTimeClass
+	waittime=waittimeClass2('waittime')
+	showtime=showtimeClass('showtime')
+	inctime=showincrementaltimeClass('inctime')
+	actualTime=actualTimeClass("actualTime")
+	
+	run "gda_startup.py"
+	print "Initialisation Complete";
+
+except:
+	exceptionType, exception, traceback = sys.exc_info()
+	handle_messages.log(None, "Error in localStation", exceptionType, exception, traceback, False)
+
+
+
+
