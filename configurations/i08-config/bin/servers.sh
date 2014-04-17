f6ba346b5ff19f4b590d45621d19cc05b48ff1f0
Ramos <silvia.da-graca-ramos@diamond.ac.uk>
2014-04-17 10:42:12 +0100

    Changes in the folder bin of i08 to run i08 dummy gdaservers

--- configurations/i08-config/bin/servers.sh
+++ configurations/i08-config/bin/servers.sh
@@ -1,9 +1,3 @@
-# runs the servers locally
-
-#identify install folder and JAVA_HOME
-source /dls_sw/i08/etc/i08_profile.sh
-
-. /usr/share/Modules/init/bash
 module load java/gda836-64
 echo "JAVA_HOME=$JAVA_HOME"
 
@@ -24,16 +18,16 @@ echo "Starting GDA. Output is being logged to $LOGFILE"
 
 export JAVA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 
-python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/config --restart -v --mode=$GDAMODE nameserver > $LOGFILE  &
+python ${GDAFOLDER}/gda_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/gda/config --restart -v --mode=$GDAMODE nameserver > $LOGFILE  &
 
 
-python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/config --restart -v --mode=$GDAMODE logserver > $LOGFILE  &
+python ${GDAFOLDER}/gda_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=$GDAFOLDER/gda/config --restart -v --mode=$GDAMODE logserver > $LOGFILE  &
 
 
-python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/config --debug -p 8002 --restart -v --mode=$GDAMODE eventserver > $LOGFILE  &
+python ${GDAFOLDER}/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/gda/config --debug -p 8002 --restart -v --mode=$GDAMODE eventserver > $LOGFILE  &
 
 export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 #export JAVA_OPTS="$JAVA_OPTS -javaagent:/dls_sw/dasc/jrebel/5.2/jrebel.jar -Drebel.properties=/dls_sw/dasc/jrebel/5.2/jrebel.properties"
-python ${GDAFOLDER}/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/config --debug -p 8001 --restart -v --mode=$GDAMODE objectserver > $LOGFILE  &
+python ${GDAFOLDER}/gda_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=$GDAFOLDER/gda/config --debug -p 8001 --restart -v --mode=$GDAMODE objectserver > $LOGFILE  &
 echo "Looking for file $SERVER_STARTUP_FILE"
 $GDAFOLDER/config/bin/lookForFile $SERVER_STARTUP_FILE
