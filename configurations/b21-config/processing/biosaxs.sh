94630d02b6df46bb5588041ef866615d619dfee9
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2014-04-28 14:25:35 +0100

    update and add processing bits
    fix untested bssc gui installation
    
    make this run 2

--- configurations/b21-config/processing/biosaxs.sh
+++ configurations/b21-config/processing/biosaxs.sh
@@ -5,43 +5,51 @@
 module load global/cluster
 
 # those two need to be in sync
-DAWN=/dls_sw/apps/DawnDiamond/master/builds-stable/stable-linux64/dawn
-MOML=/home/zjt21856/ncd_model.moml
-
+#DAWN=/dls_sw/apps/DawnDiamond/1.4.1/builds-stable/stable-linux64/dawn
+#DAWN=/dls/b21/data/2014/cm4976-1/tmp/DawnDiamond-1.5.0.v20140314-1320-linux64/dawn
+#MOML=${GDA_CONFIG}/processing/ncd_model.moml
 # those would be found in the environment
 # beamline staff specified
 #PERSISTENCEFILE=/home/zjt21856/persistence_file.nxs
 #NCDREDXML=/home/zjt21856/ncd_configuration.xml
-
 # these should be on the command line
 #DATAFILE=/home/zjt21856/i22-34820.nxs
 #BACKGROUNDFILE=/home/zjt21856/i22-34820.nxs
+#ISPYBUPDATE=$(dirname $0)/updateispyb.py
+
 DATAFILE="$1"
 BACKGROUNDFILE="$2"
+DATACOLLID="$3"
 
+# avoid messages popping up in testing
+export DISPLAY=
 REDUCTIONOUTPUTFILE=
 
+echo edited file
 #is file in visit
 read VISIT RESTOFPATH <<<$( echo $DATAFILE | sed 's,\(/dls/.../data/20../[-a-z0-9]*\)/\(.*\),\1 \2,')
+RESTOFPATH=${RESTOFPATH%.*}
 echo $RESTOFPATH
 if test -d $VISIT ; then
 	echo running reduction in visit based directory under $VISIT
 	if test -w $VISIT ; then
-		REDUCTIONOUTPUTFILE=$(basename ${VISIT}/processed/${RESTOFPATH} nxs).reduced.nxs
+		REDUCTIONOUTPUTFILE=${VISIT}/processed/${RESTOFPATH}.reduced.nxs
 		mkdir -p $(dirname $REDUCTIONOUTPUTFILE)
+		ANALYSISOUTPUT=${VISIT}/processed/${RESTOFPATH}.analysis
 	else 
-		REDUCTIONOUTPUTFILE=$(basename ${VISIT}/processing/${RESTOFPATH} nxs).reduced.nxs
+		REDUCTIONOUTPUTFILE=${VISIT}/processing/${RESTOFPATH}.reduced.nxs
 		mkdir -p $(dirname $REDUCTIONOUTPUTFILE)
 		REDUCTIONOUTPUTFILE=
+		ANALYSISOUTPUT=${VISIT}/processing/${RESTOFPATH}.analysis
 	fi
-	TMPDIR=$(dirname ${VISIT}/tmp/${RESTOFPATH})
-	mkdir -p $TMPDIR
-	TMPDIR=$(mktemp -d ${TMPDIR}.XXX)
+	TMPDIR=${VISIT}/tmp/${RESTOFPATH}.$$
 else
 	echo running reduction outside of a visit
-	TMPDIR=$(mktemp -d ${DATAFILE}.XXX)
+	TMPDIR=${DATAFILE}.$$.reduction
+	ANALYSISOUTPUT=${DATAFILE}.$$.analysis
 fi
 
+mkdir -p $TMPDIR
 cd $TMPDIR
 echo now in $TMPDIR
 
@@ -50,18 +58,23 @@ mkdir $WORKSPACE
 OUTPUTDIR=$TMPDIR/output
 mkdir $OUTPUTDIR
 
-sed "s,bgFile>/dls/i22/data/2013/sm8174-1/i22-107002.nxs</bgFile,bgFile>${BACKGROUNDFILE}</bgFile," < $NCDREDXML > ncd_reduction.xml
+sed "s,bgFile>.*</bgFile,bgFile>${BACKGROUNDFILE}</bgFile," < $NCDREDXML > ncd_reduction.xml
 NCDREDXML=${TMPDIR}/ncd_reduction.xml
 
-mkdir ${WORKSPACE}/reduction/
-WORKSPACEMOML=${WORKSPACE}/reduction/reduction.moml
+mkdir ${WORKSPACE}/workflows/
+WORKSPACEMOML=${WORKSPACE}/workflows/reduction.moml
 ln -s $MOML $WORKSPACEMOML
 
-# /dls_sw/apps/DawnDiamond/master/builds-stable/stable-linux64/dawn -noSplash -application com.isencia.passerelle.workbench.model.launch -data $WORKSPACE -consolelog -os linux -ws gtk -arch $HOSTTYPE -vmargs -Dorg.dawb.workbench.jmx.headless=true -Dcom.isencia.jmx.service.terminate=false -Dmodel=$MODEL -Dxml.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/ncd_configuration.xml -Draw.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/i22-34820.nxs -Dpersistence.path=/scratch/ws/gda836_git/scisoft-ncd.git/uk.ac.diamond.scisoft.ncd.actors/test/uk/ac/diamond/scisoft/ncd/actors/test/persistence_file.nxs -Doutput.path=/scratch/ws/junit-workspace/workflows/output
-#-data /tmp/foo \
+SCRIPT=$TMPDIR/qsub.script
+cat >> $SCRIPT <<EOF
+#! /bin/sh
 
-SCRIPT=$TMPDIR/qsub.script.$$
-cat >> qsub.script.$$ <<EOF
+## set data reduction to started
+$ISPYBUPDATE reduction $DATACOLLID STARTED \"\"
+
+module load java/7-64
+module load python/2.7.2-64
+
 $DAWN -noSplash -application com.isencia.passerelle.workbench.model.launch \
 -data $WORKSPACE \
 -consolelog -os linux -ws gtk -arch $(arch) -vmargs \
@@ -71,16 +84,51 @@ $DAWN -noSplash -application com.isencia.passerelle.workbench.model.launch \
 -Dlog.folder=$TMPDIR \
 -Dxml.path=$NCDREDXML \
 -Draw.path=$DATAFILE \
+-Declipse.pluginCustomization=/dls_sw/b21/software/gda/config/processing/plugin_customisation.ini \
 -Dpersistence.path=$PERSISTENCEFILE \
 -Doutput.path=$OUTPUTDIR
 
+for i in $OUTPUTDIR/results*.nxs ; do
+	GENERATEDFILE=\$i
+	break;
+done
+
+if test -n "\$GENERATEDFILE" && test -r \$GENERATEDFILE ; then
+ : # all fine, but tell ISPyB later
+else 
+	# raise ISPyB error and exit
+	MESSAGE="ERROR cannot find generated reduction file for collection $DATACOLLID in $OUTPUTDIR"
+	$ISPYBUPDATE reduction $DATACOLLID FAILED \$MESSAGE 
+	echo \$MESSAGE  >&2
+	echo ABORTING. >&2
+	exit 1
+fi
 
 if test -n "$REDUCTIONOUTPUTFILE" ; then 
-	for i in $OUTPUTDIR/results*.nxs ; do
-		mv \$i $REDUCTIONOUTPUTFILE
-		break;
-	done
+	ln \$GENERATEDFILE $REDUCTIONOUTPUTFILE
+	REDUCEDFILE="$REDUCTIONOUTPUTFILE"
+else
+	REDUCEDFILE=\$GENERATEDFILE
 fi
+
+# tell ispyb reduction worked and result is in \$REDUCEDFILE
+$ISPYBUPDATE reduction $DATACOLLID COMPLETE \$REDUCEDFILE
+
+#module load edna/sas-local  ## we are on the cluster
+## set analysis status started 
+$ISPYBUPDATE analysis $DATACOLLID STARTED \"\"
+## run edna 
+#run-sas-pipeline.py --data \$REDUCEDFILE --nxsQ '/entry1/detector_result/q' --nxsData '/entry1/detector_result/data' --rMaxStart 50 --rMaxStop 600 --rMaxIntervals 25 --rMaxAbsTol 0.1 --mode fast --threads 10 --columns 10 --symmetry P6 --qmin 0.005 --qmax 0.3 --plotFit
+## update ispyb
+
+#module load edna/sas-local  ## we are on the cluster
+module load numpy/1.6.1
+mkdir $ANALYSISOUTPUT
+$ISPYBUPDATE analysis $DATACOLLID STARTED \"\"
+python $EDNAPYSCRIPT --filename \$REDUCEDFILE --backgroundFilename \$BACKGROUNDFILE --detector detector --dataCollectionId $DATACOLLID --outputFolderName $ANALYSISOUTPUT --threads 4 
+$ISPYBUPDATE analysis $DATACOLLID COMPLETE $ANALYSISOUTPUT
+
 EOF
 
-qsub $SCRIPT
+bash $SCRIPT > ${SCRIPT}.stdout 2> ${SCRIPT}.errout
+#qsub $SCRIPT
---
+++ configurations/b21-config/processing/updateispyb.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python
+
+import sys
+from suds.client import Client
+from socket import *
+
+host = "cs04r-sc-vserv-49"
+# this needs to be set to the machine running GDA server
+# since it is evaluated on the cluster, "localhost" WILL NOT WORK
+controlserver = "localhost"
+
+udpport=9877
+prefix="biosaxs"
+
+URL="http://"+host+":8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl"
+client = Client(URL)
+
+redana=sys.argv[1]
+collid=sys.argv[2]
+state=sys.argv[3]
+messagefilename=sys.argv[4]
+
+if redana == "analysis":
+	client.service.setDataAnalysisStatus(collid, state, messagefilename)
+else:
+	client.service.setDataReductionStatus(collid, state, messagefilename)
+
+#client.service.setDataAnalysisStatus("1985", "COMPLETE", "filename")
+#client.service.setDataAnalysisStatus(collid, state, messagefilename)
+#client.service.setDataReductionStatus(dataCollectionId = "2033", status = "FAILED", filenameOrMessage = "failed message")
+#client.service.setDataReductionStatus(2033, "COMPLETE", "/dls/b21/data/2013/sm999-9/b21-14.nxs")
+
+addr = (controlserver,udpport)
+UDPSock = socket(AF_INET,SOCK_DGRAM)
+UDPSock.sendto(prefix+":"+collid,addr)
+UDPSock.close()
