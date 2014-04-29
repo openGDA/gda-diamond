17f792330aecad527c2f842f15039bd8398e62b5
Tobias Richter <Tobias.Richter@diamond.ac.uk>
2014-04-29 13:39:54 +0100

    update script from ncdsim

--- configurations/b21-config/processing/updateispyb.py
+++ configurations/b21-config/processing/updateispyb.py
@@ -2,18 +2,24 @@
 
 import sys
 from suds.client import Client
+from suds.transport.http import HttpAuthenticated
 from socket import *
 
 host = "cs04r-sc-vserv-49"
 # this needs to be set to the machine running GDA server
 # since it is evaluated on the cluster, "localhost" WILL NOT WORK
-controlserver = "localhost"
+controlserver = "b21-control.diamond.ac.uk"
 
 udpport=9877
 prefix="biosaxs"
 
 URL="http://"+host+":8080/ispyb-ejb3/ispybWS/ToolsForBiosaxsWebService?wsdl"
-client = Client(URL)
+sys.path.append("/dls_sw/dasc/important")
+from ispybbUserInfo import ispybbUser, ispybbPassword
+username = ispybbUser()
+userPassword = ispybbPassword()
+httpAuthenticatedWebService = HttpAuthenticated(username=username, password=userPassword)
+client = Client(URL, transport=httpAuthenticatedWebService)
 
 redana=sys.argv[1]
 collid=sys.argv[2]
