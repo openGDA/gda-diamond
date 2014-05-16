a1cc1bd28a41a841876545e9ab1f8236e1a8d0c9
Charles Mita <charles.mita@diamond.ac.uk>
2014-05-16 10:40:50 +0100

    8.38 Added metashop

--- configurations/b24-config/scripts/localStation.py
+++ configurations/b24-config/scripts/localStation.py
@@ -19,5 +19,26 @@ xyzMetaPaths = [ 'instrument:NXinstrument/XYZ:NXcollection/x',
 xyzUnits = [ 'mm', 'mm', 'mm' ]
 NMD.addScannablePaths( xyz, xyzMetaPaths, xyzUnits )
 
+from gda.configuration.properties import LocalProperties
+
+
+#New "standard" nexus metadata stuff
+#move to mt-config?
+from gdascripts.metadata.metadata_commands import setTitle, getTitle, meta_add, meta_ll, meta_ls, meta_rm
+alias("setTitle")
+alias("getTitle")
+alias("meta_add")
+alias("meta_ll")
+alias("meta_ls")
+alias("meta_rm")
+
+from gda.data.scan.datawriter import NexusDataWriter
+LocalProperties.set( NexusDataWriter.GDA_NEXUS_METADATAPROVIDER_NAME, "metashop" )
+
+
+from gdascripts.scannable.installStandardScannableMetadataCollection import *
+meta.rootNamespaceDict=globals()
+note.rootNamespaceDict=globals()
+
 print "Adding timer devices t, dt, and w, clock"
 from gdascripts.scannable.timerelated import t, dt, w, clock, epoch #@UnusedImport
