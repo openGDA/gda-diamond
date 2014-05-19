92fd82349fdaa2ffc1a8d88ddf5264b1c88a5e8e
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-05-19 11:13:41 +0100

    BLXXJ-264 Create new EnergyDispersiveScan interface for use by EdeScan and the new EdeItScan
    EdeItScan details still to be written

---
+++ plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/EdeItScan.java
@@ -0,0 +1,35 @@
+/*-
+ * Copyright © 2014 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.scan;
+
+import gda.device.Scannable;
+import gda.device.detector.StripDetector;
+import gda.scan.ede.EdeScanType;
+import gda.scan.ede.position.EdeScanPosition;
+import uk.ac.gda.exafs.ui.data.EdeScanParameters;
+
+public class EdeItScan extends EdeScan implements EnergyDispersiveScan {
+
+	public EdeItScan(EdeScanParameters scanParameters, EdeScanPosition motorPositions, EdeScanType scanType,
+			StripDetector theDetector, Integer repetitionNumber, Scannable shutter2) {
+		super(scanParameters, motorPositions, scanType, theDetector, repetitionNumber, shutter2);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/EnergyDispersiveScan.java
@@ -0,0 +1,48 @@
+/*-
+ * Copyright © 2014 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.scan;
+
+import gda.observable.IObserver;
+import gda.scan.ede.EdeScanType;
+
+import java.util.List;
+
+import uk.ac.diamond.scisoft.analysis.dataset.DoubleDataset;
+import uk.ac.gda.exafs.ui.data.EdeScanParameters;
+
+public interface EnergyDispersiveScan extends Scan {
+
+	public void setProgressUpdater(IObserver progressUpdater);
+
+	public List<ScanDataPoint> getData();
+
+	public EdeScanParameters getScanParameters();
+
+	public void setScanParameters(EdeScanParameters scanParameters);
+
+	public EdeScanType getScanType();
+
+	public void setScanType(EdeScanType scanType);
+
+	public DoubleDataset extractEnergyDetectorDataSet();
+
+	public DoubleDataset extractDetectorDataSet(int i);
+
+	public String getHeaderDescription();
+}
