b8b315a843b283d458cab5ca1b639027117ce74e
Peter Holloway <peter.holloway@diamond.ac.uk>
2014-05-30 13:30:15 +0100

    Add option to attach files to feedback email
    As requested on i22/b21 - SCATTER-333

--- uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/feedback/FeedbackDialog.java
+++ uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/feedback/FeedbackDialog.java
@@ -18,8 +18,10 @@
 
 package uk.ac.gda.dls.client.feedback;
 
+import java.io.File;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import gda.configuration.properties.LocalProperties;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -30,18 +32,45 @@ import org.eclipse.jface.layout.GridLayoutFactory;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageLoader;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Layout;
+import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
-import org.springframework.mail.SimpleMailMessage;
+import org.eclipse.ui.PlatformUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import uk.ac.gda.dls.client.Activator;
 
 public class FeedbackDialog extends TitleAreaDialog {
+	private static final Logger logger = LoggerFactory.getLogger(FeedbackDialog.class);
+	private Text nameText;
+	private Text emailText;
+	private Text subjectText;
+	private Text descriptionText;
+	private List attachedFileList;
+	private String[] fileList;
+	private Composite topParent;
+	private boolean hasFiles = false;
+	private boolean screenshot = false;
+	private Group attachments;
 
 	public FeedbackDialog(Shell shell) {
 		super(shell);
@@ -56,14 +85,9 @@ public class FeedbackDialog extends TitleAreaDialog {
 		setMessage("Enter your details below to provide feedback about GDA or the beamline.");
 	}
 	
-	private Text nameText;
-	private Text emailText;
-	private Text subjectText;
-	private Text descriptionText;
-	
 	@Override
 	protected Control createDialogArea(Composite parent) {
-		
+		topParent = parent;
 		GridLayoutFactory.swtDefaults().applyTo(parent);
 		
 		Label nameLabel = new Label(parent, SWT.NONE);
@@ -90,13 +114,29 @@ public class FeedbackDialog extends TitleAreaDialog {
 		descriptionText = new Text(parent, SWT.BORDER | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
 		GridDataFactory.fillDefaults().hint(SWT.DEFAULT, 200).grab(true, true).applyTo(descriptionText);
 		
+		createAttachments(parent);
+		
 		return parent;
 	}
 	
 	@Override
 	protected void createButtonsForButtonBar(Composite parent) {
 		
-		GridLayoutFactory.swtDefaults().numColumns(2).applyTo(parent);
+		GridData data = new GridData(SWT.FILL, SWT.FILL, false, true, 4, 1);
+		GridLayout layout = new GridLayout(5, false);
+		parent.setLayoutData(data);
+		parent.setLayout(layout);
+
+		Button attachButton = new Button(parent, SWT.TOGGLE);
+		attachButton.setText("Attach File(s)");
+		attachButton.setToolTipText("Add files to feedback");
+		
+		final Button screenGrabButton = new Button(parent, SWT.CHECK);
+		screenGrabButton.setText("Include Screenshot");
+		screenGrabButton.setToolTipText("Send screenshot with feedback");
+		
+		Label space = new Label(parent, SWT.NONE);
+		space.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER,true, true));
 		
 		Button sendButton = new Button(parent, SWT.PUSH);
 		sendButton.setText("Send");
@@ -107,11 +147,13 @@ public class FeedbackDialog extends TitleAreaDialog {
 		sendButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+
 				final String name = nameText.getText();
 				final String email = emailText.getText();
 				final String subject = subjectText.getText();
 				final String description = descriptionText.getText();
+				
+				fileList = attachedFileList.getItems();
 				
 				Job job = new Job("Send feedback email") {
 					@Override
@@ -134,15 +176,40 @@ public class FeedbackDialog extends TitleAreaDialog {
 							JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 							mailSender.setHost(smtpHost);
 							
-							SimpleMailMessage message = new SimpleMailMessage();
-							message.setFrom(from);
-							message.setTo(recipients);
-							message.setSubject(mailSubject);
-							message.setText(description);
+							MimeMessage message = mailSender.createMimeMessage();
+							final MimeMessageHelper helper = new MimeMessageHelper(message, FeedbackDialog.this.hasFiles || FeedbackDialog.this.screenshot);
+							helper.setFrom(from);
+							helper.setTo(recipients);
+							helper.setSubject(mailSubject);
+							helper.setText(description);
+							
+							if (FeedbackDialog.this.screenshot) {
+								PlatformUI.getWorkbench().getDisplay().syncExec(new Runnable() {
+									
+									@Override
+									public void run() {
+										String fileName = captureScreen("/tmp/feedbackScreenshot.png");
+										FileSystemResource file = new FileSystemResource(new File(fileName));
+										try {
+											helper.addAttachment(file.getFilename(), file);
+										} catch (MessagingException e) {
+											logger.error("Could not attach screenshot to feedback", e);
+										}
+									}
+								});
+							}
+							
+							if (FeedbackDialog.this.hasFiles) {
+								for (int i = 0; i < fileList.length; i++) {
+									FileSystemResource file = new FileSystemResource(new File(fileList[i]));
+									helper.addAttachment(file.getFilename(), file);
+								}
+							}
 							
 							mailSender.send(message);
 							return Status.OK_STATUS;
 						} catch(Exception ex){
+							logger.error("Could not send feedback", ex);
 							return new Status(IStatus.ERROR, Activator.PLUGIN_ID, 1, "Error sending email", ex);
 						}
 						
@@ -164,6 +231,96 @@ public class FeedbackDialog extends TitleAreaDialog {
 			}
 		});
 		
+		attachButton.addSelectionListener(new SelectionListener() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				hasFiles = !hasFiles;
+				GridData data = ((GridData)attachments.getLayoutData());
+				data.exclude = !hasFiles;
+				attachments.setVisible(hasFiles);
+				topParent.layout();
+			}
+			
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		screenGrabButton.addSelectionListener(new SelectionListener() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				screenshot = ((Button)e.widget).getSelection();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
+		});
 	}
 
+	private void createAttachments(Composite parent) {
+		
+		attachments = new Group(parent, SWT.NONE);
+		attachments.setVisible(false);
+		attachments.setText("Attachments");
+		Layout layout = new GridLayout(2,false);
+		attachments.setLayout(layout);
+
+		GridData data = new GridData();
+		data.exclude = true;
+		data.horizontalAlignment = SWT.FILL;
+		attachments.setLayoutData(data);
+		
+		attachedFileList = new List(attachments, SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI);
+		GridData aFLData = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 2);
+		aFLData.widthHint = parent.getSize().y;
+		attachedFileList.setLayoutData(aFLData);
+		
+		Button addAttachment = new Button(attachments, SWT.PUSH);
+		addAttachment.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+		addAttachment.setText("Add");
+		addAttachment.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fd = new FileDialog(Display.getCurrent().getActiveShell(), SWT.OPEN);
+				fd.setText("Add");
+				fd.setFilterPath(System.getProperty("user.home"));
+				String selected = fd.open();
+				attachedFileList.add(selected);
+			}
+		});
+		
+		Button removeAttachment = new Button(attachments, SWT.PUSH);
+		removeAttachment.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false));
+		removeAttachment.setText("Remove");
+		removeAttachment.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				attachedFileList.remove(attachedFileList.getSelectionIndices());
+			}
+		});
+		
+		
+	}
+	
+	private String captureScreen(final String filename) {
+		final Display parent = PlatformUI.getWorkbench().getDisplay();
+		saveShellImage(parent, filename);
+		return filename;
+	}
+	
+	private void saveShellImage(Display shell, String filename) {
+		Image image = new Image(PlatformUI.getWorkbench().getDisplay(), shell.getClientArea());
+		GC gc = new GC(shell);
+		gc.copyArea(image, 0, 0);
+		gc.dispose();
+		ImageLoader loader = new ImageLoader();
+		ImageData imageData = image.getImageData();
+		loader.data = new ImageData[] { imageData };
+		loader.save(filename, SWT.IMAGE_PNG);
+		image.dispose();
+	}
 }
