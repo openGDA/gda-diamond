a8bd2a34b141a15988a5ce9fc0c8b125ba950ed2
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-06-05 10:18:23 +0100

    B18 config fixed during 8.40 installation testing

---
+++ configurations/b18-config/bin/gda_environment.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This script assumes that $BEAMLINE is set (e.g. i02, i18, b16) if not exit...
+
+if [ ! -n "$BEAMLINE" ]; 
+then
+  echo "Please set BEAMLINE environment variable."
+  exit 1
+fi
+if [ ! -n "$SOFTWAREFOLDER" ]; 
+then
+  echo "Please set SOFTWAREFOLDER environment variable."
+  exit 1
+fi
+unset ANT_HOME
+unset JAVA_HOME
+unset JYTHON_HOME
+unset SVN_HOME
+
+export PATH=/$SOFTWAREFOLDER/$BEAMLINE/software/gda/bin:/$SOFTWAREFOLDER/$BEAMLINE/software/gda_git/gda-diamond.git/dls-config/bin:${PATH}
