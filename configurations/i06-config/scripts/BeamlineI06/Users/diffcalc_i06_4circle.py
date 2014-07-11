57357b0334e87296932f29ab28f5eb7c6f8ad09a
Mark Booth <Mark.Booth@diamond.ac.uk>
2014-07-11 15:30:21 +0100

    BLVI (8.38) Update i06 diffcalc startup file for deployed version of diffcalc.

--- configurations/i06-config/scripts/BeamlineI06/Users/diffcalc_i06_4circle.py
+++ configurations/i06-config/scripts/BeamlineI06/Users/diffcalc_i06_4circle.py
@@ -1,57 +1,45 @@
-# This file's last known position:
-#    /dls_sw/i06-1/software/gda/config/scripts/BeamlineI06/Users/diffcalc_i07_4circle.py
-
 # See notes in: http://wiki.diamond.ac.uk/Wiki/Wiki.jsp?page=I06_Diffcalc
 
 try:
     import diffcalc
 except ImportError:
-    
-    from gda.data.PathConstructor import createFromProperty
+    from gda.configuration.properties import LocalProperties
     import sys
-    diffcalc_path = '/dls_sw/i06-1/software/gda/diffcalc'
-    #diffcalc_path = createFromProperty("gda.root").split('/plugins')[0] + '/diffcalc' 
+    diffcalc_path = LocalProperties.get("gda.install.git.loc") + '/diffcalc.git'
     sys.path = [diffcalc_path] + sys.path
     print diffcalc_path + ' added to GDA Jython path.'
     import diffcalc
-#####
 
-from diffcalc.gdasupport.GdaDiffcalcObjectFactory import createDiffcalcObjects, addObjectsToNamespace
 from gdascripts.pd.dummy_pds import DummyPD
+from diffcalc.gdasupport.factory import create_objects, add_objects_to_namespace
 
-DUMMY_DIFFCALC = False
-
-chi = DummyPD('chi')
-phi = DummyPD('phi')
-
-if DUMMY_DIFFCALC:
+CREATE_DUMMY_AXES = False
+if CREATE_DUMMY_AXES:
     print "!!! Staring dummy diffcalc with tth, th, chi, phi and en." 
     tth = DummyPD('tth')
     th = DummyPD('th')
     en = DummyPD('en')
     en(1500)
-    diffcalcObjects = createDiffcalcObjects(
-        axisScannableList = [tth, th, chi, phi],
-        energyScannable = en,
-        energyScannableMultiplierToGetKeV = .001,
-        geometryPlugin = 'fourc',
-        hklverboseVirtualAnglesToReport=('2theta','Bin','Bout','azimuth')
-    )
-    diffcalcObjects['diffcalcdemo'].commands = []
-    addObjectsToNamespace(diffcalcObjects, globals())
-
+    diffcalc_energy=en
 else:
     print "!!! Staring LIVE diffcalc with th(dd2th), th(ddth), chi(dummy), phi(dummy) and denergy." 
     tth = dd2th
     th = ddth
-    diffcalcObjects = createDiffcalcObjects(
-        axisScannableList = [tth, th, chi, phi],
-        energyScannable = denergy,
-        energyScannableMultiplierToGetKeV = .001,
-        geometryPlugin = 'fourc',
-        hklverboseVirtualAnglesToReport=('2theta','Bin','Bout','azimuth')
-    )
-    diffcalcObjects['diffcalcdemo'].commands = []
-    addObjectsToNamespace(diffcalcObjects, globals())
+    diffcalc_energy=denergy
+
+chi = DummyPD('chi')
+phi = DummyPD('phi')
+
+diffcalcObjects = create_objects(
+    axis_scannable_list = [tth, th, chi, phi],
+    energy_scannable = diffcalc_energy,
+    energy_scannable_multiplier_to_get_KeV = .001,
+    geometry = 'fourc',
+    hklverbose_virtual_angles_to_report=('2theta','Bin','Bout','azimuth')
+)
+
+#demoCommands = []
+#diffcalcObjects['diffcalcdemo'].commands = demoCommands
+add_objects_to_namespace(diffcalcObjects, globals())
 
 hkl.level = 6
