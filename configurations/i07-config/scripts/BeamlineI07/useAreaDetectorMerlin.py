59d8de120898e6b0f568743220a0bf8a3f7b4a0b
Charles Mita <charles.mita@diamond.ac.uk>
2014-07-23 11:39:45 +0100

    BLVII (8.40) Make merlin stats properly update gui.

--- configurations/i07-config/scripts/BeamlineI07/useAreaDetectorMerlin.py
+++ configurations/i07-config/scripts/BeamlineI07/useAreaDetectorMerlin.py
@@ -4,3 +4,7 @@ from Diamond.Analysis.AnalyserNx import AnalyserNxWithRectangularROIClass
 merlinstats = AnalyserNxDetectorClass("merlinstats", merlin, _merlin_for_snaps, [MinMaxSumMeanDeviationProcessor()], panelName="Area Detector")
 merlinroi = AnalyserNxWithRectangularROIClass("merlinroi", merlin, _merlin_for_snaps, [MinMaxSumMeanDeviationProcessor()], panelName="Area Detector")
 merlinroisum = AnalyserNxWithRectangularROIClass("merlinroisum", merlin, _merlin_for_snaps, [SumProcessor()], panelName="Area Detector")
+
+merlinstats.setAlive(True)
+merlinroi.setAlive(True)
+merlinroisum.setAlive(True)
