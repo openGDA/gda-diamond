19e5d7297ba81ad3c64733ae7e81b2844fd07a71
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-07-30 16:56:54 +0100

    Switch default calibrant name from 'Si' to 'CeO2'

--- configurations/i11-1-config/scripts/localStation.py
+++ configurations/i11-1-config/scripts/localStation.py
@@ -230,7 +230,7 @@ try:
     pixium_DetectorTemperature = DisplayEpicsPVClass('pixium_DetectorTemperature', 'BL11J-EA-DET-10:CAM:DetectorTemperature', 'degree', '%.1f') 
     x_pixel_size=ConstantScannable("x_pixel_size", 148.0, unit="um")
     y_pixel_size=ConstantScannable("y_pixel_size", 148.0, unit="um")
-    calibrant_name=CalibrantScannable("calibrant_name", "Si")
+    calibrant_name=CalibrantScannable("calibrant_name", "CeO2")
 except:
     print "cannot create extra pixium scannables"
 
