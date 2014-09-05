39df1b328824f61de7a6d51580547cc55ba6b47b
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-09-05 17:46:55 +0100

    strang fix here due to namespace issue

--- i09-config/scripts/command/analyser_scan.py
+++ i09-config/scripts/command/analyser_scan.py
@@ -42,7 +42,8 @@ def analyserscan(*args):
                 filename=FilenameUtil.convertSeparator(filename)
             controller.update(controller,SequenceFileChangeEvent(filename)) #update client sequence view
             sleep(1.0)
-            while (InterfaceProvider.getJythonServerStatusProvider().isScriptOrScanPaused()):
+            jythonServerStatus=InterfaceProvider.getJythonServerStatusProvider().getJythonServerStatus()
+            while (jythonServerStatus.isScriptOrScanPaused()):
                 sleep(1.0) # wait for user saving dirty file
             arg.setSequenceFilename(filename)
             sequence=arg.loadSequenceData(filename)
