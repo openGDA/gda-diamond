690fdd87a6267501d820405b703298b4733e0f0b
matthew.gerring <matthew.gerring@gmail.com>
2014-10-01 12:07:24 +0100

    Changed location of interface.

--- plugins/uk.ac.gda.beamline.b18/src/uk/ac/gda/beamline/b18/MythenControlView.java
+++ plugins/uk.ac.gda.beamline.b18/src/uk/ac/gda/beamline/b18/MythenControlView.java
@@ -58,7 +58,7 @@ import uk.ac.diamond.scisoft.analysis.plotserver.GuiBean;
 import uk.ac.diamond.scisoft.analysis.plotserver.GuiParameters;
 import uk.ac.diamond.scisoft.analysis.plotserver.GuiPlotMode;
 import uk.ac.diamond.scisoft.analysis.plotserver.GuiUpdate;
-import uk.ac.diamond.scisoft.analysis.rcp.plotting.IGuiInfoManager;
+import uk.ac.diamond.scisoft.analysis.plotserver.IGuiInfoManager;
 import uk.ac.diamond.scisoft.analysis.rcp.plotting.IPlotUI;
 import uk.ac.diamond.scisoft.analysis.rcp.plotting.IUpdateNotificationListener;
 import uk.ac.diamond.scisoft.analysis.rcp.plotting.PlotConsumer;
