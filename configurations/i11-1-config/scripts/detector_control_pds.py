0a2cbbaa5d80a610c2c77398c24f3d64f254aaa5
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2014-10-01 09:58:11 +0100

    BLXI-222: add monitor IE2 control in GDA http://jira.diamond.ac.uk/browse/BLXI-222

--- configurations/i11-1-config/scripts/detector_control_pds.py
+++ configurations/i11-1-config/scripts/detector_control_pds.py
@@ -1,9 +1,9 @@
 from detector_control_class import DetectorControlClass
 pds=[]
 #Ie2 
-llimIe2=DetectorControlClass('llimIe2', 'BL11J-DI-IMON-01:E1:LLIM', 'BL11I-DI-IMON-02:E1:LLIM:RBV', 'mv', '%4.0f'); pds.append(llimIe2)
-ulimIe2=DetectorControlClass('ulimIe2', 'BL11J-DI-IMON-01:E1:ULIM', 'BL11I-DI-IMON-02:E1:ULIM:RBV', 'mv', '%4.0f'); pds.append(ulimIe2)
-pmtIe2=DetectorControlClass('pmtIe2', 'BL11J-DI-IMON-01:E1:CTRL', 'BL11I-DI-IMON-02:E1:CTRL:RBV', 'mv', '%4.0f'); pds.append(pmtIe2)
+llimIe2=DetectorControlClass('llimIe2', 'BL11J-DI-IMON-01:E1:LLIM', 'BL11I-DI-IMON-01:E1:LLIM:RBV', 'mv', '%4.0f'); pds.append(llimIe2)
+ulimIe2=DetectorControlClass('ulimIe2', 'BL11J-DI-IMON-01:E1:ULIM', 'BL11I-DI-IMON-01:E1:ULIM:RBV', 'mv', '%4.0f'); pds.append(ulimIe2)
+pmtIe2=DetectorControlClass('pmtIe2', 'BL11J-DI-IMON-01:E1:CTRL', 'BL11I-DI-IMON-01:E1:CTRL:RBV', 'mv', '%4.0f'); pds.append(pmtIe2)
 
 print "finished ETL Detector object creation"
     
