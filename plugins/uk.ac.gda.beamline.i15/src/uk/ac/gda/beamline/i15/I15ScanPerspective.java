308824c190727e0ade6a4ddcfe9a2245595ac520
Matthew Webber <matthew.webber@diamond.ac.uk>
2014-11-17 16:10:15 +0000

    Remove Beagle log viewer from GDA, it is no longer used.

--- plugins/uk.ac.gda.beamline.i15/src/uk/ac/gda/beamline/i15/I15ScanPerspective.java
+++ plugins/uk.ac.gda.beamline.i15/src/uk/ac/gda/beamline/i15/I15ScanPerspective.java
@@ -43,7 +43,6 @@ public class I15ScanPerspective implements IPerspectiveFactory {
 				IPageLayout.BOTTOM, 0.8f, editorArea);
 		bottomfolder.addView(uk.ac.gda.views.baton.BatonView.ID);
 		bottomfolder.addView("org.dawb.workbench.plotting.views.toolPageView.1D");
-		bottomfolder.addView("ch.qos.logback.beagle.view.BeagleView");
 		bottomfolder.addPlaceholder("org.dawb.passerelle.views.ValueView");
 		bottomfolder.addPlaceholder("org.eclipse.ui.views.ProgressView");
 		bottomfolder.addPlaceholder("org.eclipse.ui.console.ConsoleView");
