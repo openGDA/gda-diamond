4887daa6252e4820306edd4bbc495bbd38546d7f
Richard Woolliscroft <richard.woolliscroft@diamond.ac.uk>
2014-12-11 15:25:31 +0000

    XAS-170 new XesScan object to run I20 XES scans to replace xes_scan.py
    Change-Id: I1c67ac4c0f097fa0ff88023cc4a156f949f041ad

---
+++ plugins/uk.ac.gda.beamline.i20.test/src/uk/ac/gda/server/exafs/scan/XesScanFactoryTest.java
@@ -0,0 +1,154 @@
+/*-
+ * Copyright © 2014 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.server.exafs.scan;
+
+import gda.data.metadata.NXMetaDataProvider;
+import gda.data.scan.datawriter.AsciiDataWriterConfiguration;
+import gda.device.Scannable;
+import gda.jython.scriptcontroller.logging.LoggingScriptController;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.api.mockito.PowerMockito;
+
+public class XesScanFactoryTest {
+
+	private BeamlinePreparer beamlinePreparer;
+	private DetectorPreparer detectorPreparer;
+	private OutputPreparer outputPreparer;
+	private SampleEnvironmentPreparer samplePreparer;
+	private LoggingScriptController loggingScriptController;
+	private AsciiDataWriterConfiguration datawriterconfig;
+	private Scannable energyScannable;
+	private NXMetaDataProvider metashop;
+	private Scannable xesEnergyScannable;
+	private Scannable analyserAngle;
+
+	@Before
+	public void setup() {
+		// mock all the objects which would be used to create the XasScan objects
+
+		// have not mocked ArrayList<AsciiMetadataConfig> original_header
+		beamlinePreparer = PowerMockito.mock(BeamlinePreparer.class);
+		detectorPreparer = PowerMockito.mock(DetectorPreparer.class);
+		samplePreparer = PowerMockito.mock(SampleEnvironmentPreparer.class);
+		outputPreparer = PowerMockito.mock(OutputPreparer.class);
+		loggingScriptController = PowerMockito.mock(LoggingScriptController.class);
+		datawriterconfig = PowerMockito.mock(AsciiDataWriterConfiguration.class);
+		energyScannable = PowerMockito.mock(Scannable.class);
+		xesEnergyScannable = PowerMockito.mock(Scannable.class);
+		analyserAngle = PowerMockito.mock(Scannable.class);
+		metashop = PowerMockito.mock(NXMetaDataProvider.class);
+
+	}
+
+	@Test
+	public void testCanCreateEnergyScan() {
+
+		XesScanFactory theFactory = new XesScanFactory();
+
+		theFactory.setBeamlinePreparer(beamlinePreparer);
+		theFactory.setDetectorPreparer(detectorPreparer);
+		theFactory.setSamplePreparer(samplePreparer);
+		theFactory.setOutputPreparer(outputPreparer);
+		theFactory.setLoggingScriptController(loggingScriptController);
+		theFactory.setDatawriterconfig(datawriterconfig);
+		theFactory.setEnergyScannable(energyScannable);
+		theFactory.setMetashop(metashop);
+		theFactory.setIncludeSampleNameInNexusName(true);
+		theFactory.setScanName("xesscan");
+		theFactory.setXes_energy(xesEnergyScannable);
+		theFactory.setAnalyserAngle(analyserAngle);
+
+		EnergyScan energyScan = theFactory.createEnergyScan();
+
+		if (energyScan == null) {
+			Assert.fail("Null returned from factory");
+		}
+
+		XesScan xesscan = theFactory.createXesScan();
+
+		if (xesscan == null) {
+			Assert.fail("Null returned from factory");
+		}
+
+	}
+
+	@Test
+	public void testXesScanMissingEnergy() {
+		XesScanFactory theFactory = new XesScanFactory();
+
+		theFactory.setBeamlinePreparer(beamlinePreparer);
+		theFactory.setDetectorPreparer(detectorPreparer);
+		theFactory.setSamplePreparer(samplePreparer);
+		theFactory.setOutputPreparer(outputPreparer);
+		theFactory.setLoggingScriptController(loggingScriptController);
+		theFactory.setDatawriterconfig(datawriterconfig);
+		theFactory.setEnergyScannable(energyScannable);
+		theFactory.setMetashop(metashop);
+		theFactory.setIncludeSampleNameInNexusName(true);
+		theFactory.setScanName("xesscan");
+		theFactory.setAnalyserAngle(analyserAngle);
+
+		EnergyScan energyScan = theFactory.createEnergyScan();
+
+		if (energyScan == null) {
+			Assert.fail("Null returned from factory");
+		}
+
+		try {
+			/* XesScan xesscan = */theFactory.createXesScan();
+		} catch (Exception e1) {
+			return;
+		}
+		Assert.fail("Expected exception was not caught.");
+	}
+
+	@Test
+	public void testXesScanMissingAnalyser() {
+		XesScanFactory theFactory = new XesScanFactory();
+
+		theFactory.setBeamlinePreparer(beamlinePreparer);
+		theFactory.setDetectorPreparer(detectorPreparer);
+		theFactory.setSamplePreparer(samplePreparer);
+		theFactory.setOutputPreparer(outputPreparer);
+		theFactory.setLoggingScriptController(loggingScriptController);
+		theFactory.setDatawriterconfig(datawriterconfig);
+		theFactory.setEnergyScannable(energyScannable);
+		theFactory.setMetashop(metashop);
+		theFactory.setIncludeSampleNameInNexusName(true);
+		theFactory.setScanName("xesscan");
+		theFactory.setXes_energy(xesEnergyScannable);
+
+		EnergyScan energyScan = theFactory.createEnergyScan();
+
+		if (energyScan == null) {
+			Assert.fail("Null returned from factory");
+		}
+
+		try {
+			/* XesScan xesscan = */theFactory.createXesScan();
+		} catch (Exception e1) {
+			return;
+		}
+		Assert.fail("Expected exception was not caught.");
+	}
+
+}
---
+++ plugins/uk.ac.gda.beamline.i20/src/uk/ac/gda/server/exafs/scan/XesScan.java
@@ -0,0 +1,188 @@
+/*-
+ * Copyright © 2014 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.server.exafs.scan;
+
+import gda.device.Scannable;
+import gda.device.scannable.TwoDScanPlotter;
+import gda.jython.scriptcontroller.logging.LoggingScriptController;
+
+import java.util.List;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import uk.ac.gda.beans.BeansFactory;
+import uk.ac.gda.beans.exafs.IDetectorConfigurationParameters;
+import uk.ac.gda.beans.exafs.IDetectorParameters;
+import uk.ac.gda.beans.exafs.IOutputParameters;
+import uk.ac.gda.beans.exafs.ISampleParameters;
+import uk.ac.gda.beans.exafs.IScanParameters;
+import uk.ac.gda.beans.exafs.SignalParameters;
+import uk.ac.gda.beans.exafs.XesScanParameters;
+import uk.ac.gda.beans.exafs.i20.I20OutputParameters;
+
+public class XesScan extends XasScanBase implements XasScan {
+
+	private Scannable analyserAngle;
+	private Scannable xes_energy;
+	private Scannable mono_energy;
+	private XasScan xas;
+	private XesScanParameters xesScanParameters;
+	private Object[] xes_args;
+	private TwoDScanPlotter twodplotter = new TwoDScanPlotter();
+	private I20OutputParameters i20OutputParameters;
+
+	public XesScan() {
+	}
+
+	@Override
+	public String getScanType() {
+		return "XES";
+	}
+
+	@Override
+	public void doCollection(ISampleParameters sampleBean, IScanParameters scanBean, IDetectorParameters detectorBean,
+			IOutputParameters outputBean, IDetectorConfigurationParameters detectorConfigurationBean,
+			String experimentFullPath, int numRepetitions) throws Exception {
+		xesScanParameters = (XesScanParameters) scanBean;
+		i20OutputParameters = (I20OutputParameters) outputBean;
+		this.numRepetitions = numRepetitions;
+		this.experimentFullPath = experimentFullPath;
+		this.detectorConfigurationBean = detectorConfigurationBean;
+		this.outputBean = outputBean;
+		this.detectorBean = detectorBean;
+		this.sampleBean = sampleBean;
+		this.scanBean = scanBean;
+
+		scan_unique_id = LoggingScriptController.createUniqueID(getScanType());
+		_runCorrectScanType();
+	}
+
+	private void _runCorrectScanType() throws Exception {
+
+		if (analyserAngle.isBusy()) {
+			analyserAngle.waitWhileBusy();
+		}
+		int innerScanType = xesScanParameters.getScanType();
+		if (innerScanType == XesScanParameters.FIXED_XES_SCAN_XAS || innerScanType == XesScanParameters.FIXED_XES_SCAN_XANES) {
+			_doXASScan();
+			return;
+		}
+
+		try {
+			doCollection();
+		} finally {
+			// make sure the plotter is switched off
+			twodplotter.atScanEnd();
+		}
+
+	}
+
+	// args do run a single concurrentscan
+	@Override
+	protected Object[] createScanArguments(String sampleName, List<String> descriptions) throws Exception {
+
+		int innerScanType = xesScanParameters.getScanType();
+
+		if (innerScanType == XesScanParameters.SCAN_XES_FIXED_MONO) {
+			
+			xes_args = new Object[] { xes_energy, xesScanParameters.getXesInitialEnergy(),
+					xesScanParameters.getXesFinalEnergy(), xesScanParameters.getXesStepSize(), mono_energy,
+					xesScanParameters.getMonoEnergy() };
+		} else if (innerScanType == XesScanParameters.SCAN_XES_SCAN_MONO) {
+			
+			Object[] ef_args = new Object[] { xes_energy, xesScanParameters.getXesInitialEnergy(),
+					xesScanParameters.getXesFinalEnergy(), xesScanParameters.getXesStepSize() };
+			Object[] e0_args = new Object[] { mono_energy, xesScanParameters.getMonoInitialEnergy(),
+					xesScanParameters.getMonoFinalEnergy(), xesScanParameters.getMonoStepSize() };
+			if (xesScanParameters.getLoopChoice() == XesScanParameters.LOOPOPTIONS[0]) {
+				
+				xes_args = ArrayUtils.addAll(ef_args, e0_args);
+				twodplotter.setZ_colName("FFI1");
+				twodplotter.setXArgs(xesScanParameters.getXesInitialEnergy(), xesScanParameters.getXesFinalEnergy(),
+						xesScanParameters.getXesStepSize());
+				twodplotter.setYArgs(xesScanParameters.getMonoInitialEnergy(), xesScanParameters.getMonoFinalEnergy(),
+						xesScanParameters.getMonoStepSize());
+			} else { // innerScanType == XesScanParameters.SCAN_XES_FIXED_MONO
+				
+				xes_args = ArrayUtils.addAll(e0_args, ef_args);
+				twodplotter.setZ_colName("FFI1");
+				twodplotter.setYArgs(xesScanParameters.getXesInitialEnergy(), xesScanParameters.getXesFinalEnergy(),
+						xesScanParameters.getXesStepSize());
+				twodplotter.setXArgs(xesScanParameters.getMonoInitialEnergy(), xesScanParameters.getMonoFinalEnergy(),
+						xesScanParameters.getMonoStepSize());
+			}
+			xes_args = ArrayUtils.add(xes_args, twodplotter);
+		}
+		return xes_args;
+	}
+
+	private void _doXASScan() throws Exception {
+
+		double initialXESEnergy = (Double) xes_energy.getPosition();
+		xes_energy.waitWhileBusy();
+		xes_energy.moveTo(xesScanParameters.getXesEnergy());
+
+		SignalParameters xesEnergySignal = new SignalParameters(xes_energy.getName(), xes_energy.getName(), 2,
+				xes_energy.getName(), xes_energy.getName());
+		i20OutputParameters.addSignal(xesEnergySignal);
+		SignalParameters analyserSignal = new SignalParameters(analyserAngle.getName(), analyserAngle.getName(), 2,
+				xes_energy.getName(), analyserAngle.getName());
+		i20OutputParameters.addSignal(analyserSignal);
+		try {
+			IScanParameters xasScanParams = (IScanParameters) BeansFactory.getBeanObject(experimentFullPath + "/",
+					xesScanParameters.getScanFileName());
+			xas.doCollection(sampleBean, xasScanParams, detectorBean, outputBean, detectorConfigurationBean,
+					experimentFullPath, numRepetitions);
+		} finally {
+			xes_energy.moveTo(initialXESEnergy);
+		}
+	}
+
+	public Scannable getAnalyserAngle() {
+		return analyserAngle;
+	}
+
+	public void setAnalyserAngle(Scannable analyserAngle) {
+		this.analyserAngle = analyserAngle;
+	}
+
+	public Scannable getXes_energy() {
+		return xes_energy;
+	}
+
+	public void setXes_energy(Scannable xes_energy) {
+		this.xes_energy = xes_energy;
+	}
+
+	public Scannable getMono_energy() {
+		return mono_energy;
+	}
+
+	public void setMono_energy(Scannable mono_energy) {
+		this.mono_energy = mono_energy;
+	}
+
+	public XasScan getXas() {
+		return xas;
+	}
+
+	public void setXas(XasScan xas) {
+		this.xas = xas;
+	}
+}
