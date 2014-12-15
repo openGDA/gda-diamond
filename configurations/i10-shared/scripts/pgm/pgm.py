2dd516d28bef09cba852eb4317883f0ae7558ae8
Mark Booth <Mark.Booth@diamond.ac.uk>
2014-12-15 19:23:43 +0000

    BLX-7 (8.40) Updated Peter Bencoks pgm calc script and its tests.

--- configurations/i10-shared/scripts/pgm/pgm.py
+++ configurations/i10-shared/scripts/pgm/pgm.py
@@ -4,22 +4,25 @@ Copy of Peter Bencocks conversion functions: /dls_sw/i10/scripts/Peter/pgm.py
 For use with GDA on I10 at Diamond Light Source
 """
 
-from math import sin, asin # Missing imports
+# Calculates energy for given grating density, grating angle, plane mirror angle, offsets and calibration
+# gd = grating density
+# groff = grating offest
+# pmoff = plane mirror offset
+# ecg = energy calibration gradient
+# ecr = energy calibration reference
 
-# Calculates energy for given grating density, grating angle, plane mirror angle and offsets
-def angles2energy(gd, grang, pmang, groff, pmoff):
-    from math import degrees, radians, sqrt
-    KE=1.239842012e-3
+from math import degrees, radians, sqrt, sin, asin
+
+def angles2energy(gd, grang, pmang, groff, pmoff, ecg, ecr):
+    KE=1.239842e-3
     pmang-=pmoff
     grang-=groff
-    return KE*gd/(sin(radians(2*pmang-grang))-sin(radians(grang)))
+    return (KE*gd/(sin(radians(2*pmang-grang))-sin(radians(grang)))-ecr)*ecg+ecr
 
 # Calculates grating angle for given grating density, energy, cff and grating offset
-def enecff2grating(gd, energy, cff, groff):
-    from math import degrees, radians, sqrt
-    from math import asin
-    KE=1.239842012e-3
-    d=KE*gd/energy
+def enecff2grating(gd, energy, cff, groff, ecg, ecr):
+    KE=1.239842e-3
+    d=KE*gd/((energy-ecr)/ecg+ecr)
     a=1-cff*cff
     b=2*cff*cff*d
     c=cff*cff-cff*cff*d*d-1
@@ -27,10 +30,9 @@ def enecff2grating(gd, energy, cff, groff):
     return degrees(asin((b-e)/(2*a)))+groff
 
 # Calculates plane mirror angle for given grating density, energy, cff and offsets
-def enecff2mirror(gd, energy, cff, groff, pmoff):
-    from math import degrees, radians, sqrt
-    KE=1.239842012e-3
-    d=KE*gd/energy
+def enecff2mirror(gd, energy, cff, groff, pmoff, ecg, ecr):
+    KE=1.239842e-3
+    d=KE*gd/((energy-ecr)/ecg+ecr)
     a=1-cff*cff
     b=2*cff*cff*d
     c=cff*cff-cff*cff*d*d-1
@@ -42,8 +44,7 @@ def enecff2mirror(gd, energy, cff, groff, pmoff):
     return (alfa+beta-groff)/2+pmoff
 
 # Calculates grating angle for given grating density, energy, mirror angle and offsets
-def enemirror2grating(gd, energy, pmang, groff, pmoff):
-    from math import degrees, radians, sqrt
-    KE=1.239842012e-3
+def enemirror2grating(gd, energy, pmang, groff, pmoff, ecg, ecr):
+    KE=1.239842e-3
     pmang-=pmoff
-    return -degrees(asin(KE*gd/(2*energy*sqrt(1-sin(radians(pmang))*sin(radians(pmang))))))+pmang+groff
+    return -degrees(asin(KE*gd/(2*((energy-ecr)/ecg+ecr)*sqrt(1-sin(radians(pmang))*sin(radians(pmang))))))+pmang+groff
--- configurations/i10-shared/scripts/test/pgm/pgmTest.py
+++ configurations/i10-shared/scripts/test/pgm/pgmTest.py
@@ -14,24 +14,29 @@ class PgmTest1(unittest.TestCase):
 
     def setUp(self):
         #self.grating_density        = 1000.      # BL10I-OP-PGM-01:NLINES2
-        self.grating_density        = 400.      # BL10I-OP-PGM-01:NLINES
-        self.cff                    = 2.25      # BL10I-OP-PGM-01:CFF
-        self.grating_offset         = 0.40708   # BL10I-OP-PGM-01:GRTOFFSET
-        self.plane_mirror_offset    = 0.002739  # BL10I-OP-PGM-01:MIROFFSET
+        self.grating_density        = 400.      # BL10I-OP-PGM-01:NLINES        400 line/mm Au
+        self.cff                    = 2.25      # BL10I-OP-PGM-01:CFF            |
+        self.grating_offset         = 0.40708   # BL10I-OP-PGM-01:GRTOFFSET    < / > BL10I-OP-PGM-01:GRT1:GOFF
+        self.plane_mirror_offset    = 0.002739  # BL10I-OP-PGM-01:MIROFFSET    < / > BL10I-OP-PGM-01:GRT1:MOFF
 
-        self.pgm_energy             = 782.000   # BL10I-OP-PGM-01:ENERGY
+        self.pgm_energy             = 712.300   # BL10I-OP-PGM-01:ENERGY
 
-        self.grating_pitch          = 88.1235   # BL10I-OP-PGM-01:GRT:PITCH.RBV
-        self.mirror_pitch           = 88.3536   # BL10I-OP-PGM-01:MIR:PITCH.RBV
+        self.grating_pitch          = 88.0151   # BL10I-OP-PGM-01:GRT:PITCH.RBV
+        self.mirror_pitch           = 88.2753   # BL10I-OP-PGM-01:MIR:PITCH.RBV
 
-        self.decimal_places         = 1
+        self.energy_calibration_gradient    = 1.011 # ?                                > BL10I-OP-PGM-01:GRT1:MX
+        self.energy_calibration_reference   = 430. # ?                                > BL10I-OP-PGM-01:GRT1:REFERENCE
+
+        self.decimal_places         = 2
 
     def testEnergy(self):
         energy = angles2energy(gd       = self.grating_density,
                                grang    = self.grating_pitch,
                                pmang    = self.mirror_pitch,
                                groff    = self.grating_offset,
-                               pmoff    = self.plane_mirror_offset)
+                               pmoff    = self.plane_mirror_offset,
+                               ecg      = self.energy_calibration_gradient,
+                               ecr      = self.energy_calibration_reference)
         
         self.assertAlmostEqual(energy, self.pgm_energy, self.decimal_places)
 
@@ -39,7 +44,9 @@ class PgmTest1(unittest.TestCase):
         grat_pitch=enecff2grating(gd     = self.grating_density,
                                   energy = self.pgm_energy,
                                   cff    = self.cff,
-                                  groff  = self.grating_offset)
+                                  groff  = self.grating_offset,
+                                  ecg    = self.energy_calibration_gradient,
+                                  ecr    = self.energy_calibration_reference)
         
         self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
 
@@ -48,7 +55,9 @@ class PgmTest1(unittest.TestCase):
                                  energy = self.pgm_energy,
                                  cff    = self.cff,
                                  groff  = self.grating_offset,
-                                 pmoff  = self.plane_mirror_offset)
+                                 pmoff  = self.plane_mirror_offset,
+                                 ecg    = self.energy_calibration_gradient,
+                                 ecr    = self.energy_calibration_reference)
         
         self.assertAlmostEqual(mirr_pitch, self.mirror_pitch, self.decimal_places)
 
@@ -57,11 +66,12 @@ class PgmTest1(unittest.TestCase):
                                      energy = self.pgm_energy,
                                      pmang  = self.mirror_pitch,
                                      groff  = self.grating_offset,
-                                     pmoff  = self.plane_mirror_offset)
+                                     pmoff  = self.plane_mirror_offset,
+                                     ecg    = self.energy_calibration_gradient,
+                                     ecr    = self.energy_calibration_reference)
         
         self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
 
-"""
 class PgmTest2(PgmTest1):
 
     def setUp(self):
@@ -74,6 +84,9 @@ class PgmTest2(PgmTest1):
 
         self.grating_pitch          = 87.932    # BL10I-OP-PGM-01:GRT:PITCH.RBV
         self.mirror_pitch           = 88.255    # BL10I-OP-PGM-01:MIR:PITCH.RBV
+
+        self.energy_calibration_gradient    = 1.011 # ?                                > BL10I-OP-PGM-01:GRT1:MX
+        self.energy_calibration_reference   = 430. # ?                                > BL10I-OP-PGM-01:GRT1:REFERENCE
 
         self.decimal_places         = 2
 
@@ -90,6 +103,9 @@ class PgmTest3(PgmTest1):
         self.grating_pitch          = 87.933    # BL10I-OP-PGM-01:GRT:PITCH.RBV
         self.mirror_pitch           = 88.256    # BL10I-OP-PGM-01:MIR:PITCH.RBV
 
+        self.energy_calibration_gradient    = 1.011 # ?                                > BL10I-OP-PGM-01:GRT1:MX
+        self.energy_calibration_reference   = 430. # ?                                > BL10I-OP-PGM-01:GRT1:REFERENCE
+
         self.decimal_places         = 2
 
 class PgmTest4(PgmTest1):
@@ -105,8 +121,10 @@ class PgmTest4(PgmTest1):
         self.grating_pitch          = 87.935    # BL10I-OP-PGM-01:GRT:PITCH.RBV
         self.mirror_pitch           = 88.257    # BL10I-OP-PGM-01:MIR:PITCH.RBV
 
+        self.energy_calibration_gradient    = 1.011 # ?                                > BL10I-OP-PGM-01:GRT1:MX
+        self.energy_calibration_reference   = 430. # ?                                > BL10I-OP-PGM-01:GRT1:REFERENCE
+
         self.decimal_places         = 2
-"""
 
 """
 Finding files... done.
@@ -116,93 +134,85 @@ Importing test modules ... done.
 FAIL: testEnergy (pgmTest.PgmTest1)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 36, in testEnergy
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 41, in testEnergy
     self.assertAlmostEqual(energy, self.pgm_energy, self.decimal_places)
-AssertionError: 778.1475382541757 != 782.0 within 2 places
-
-======================================================================
-FAIL: testGratPitchFromEnergyAndCff (pgmTest.PgmTest1)
-----------------------------------------------------------------------
-Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 44, in testGratPitchFromEnergyAndCff
-    self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 88.1291262169773 != 88.1235 within 2 places
+AssertionError: 712.3106769444129 != 712.3 within 2 places
 
 ======================================================================
 FAIL: testEnergy (pgmTest.PgmTest2)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 36, in testEnergy
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 41, in testEnergy
     self.assertAlmostEqual(energy, self.pgm_energy, self.decimal_places)
-AssertionError: 640.4805993560931 != 696.21 within 2 places
+AssertionError: 642.7958796818319 != 696.21 within 2 places
 
 ======================================================================
 FAIL: testGratPitch (pgmTest.PgmTest2)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 62, in testGratPitch
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 73, in testGratPitch
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99022410049261 != 87.932 within 2 places
+AssertionError: 87.98742861615946 != 87.932 within 2 places
 
 ======================================================================
 FAIL: testGratPitchFromEnergyAndCff (pgmTest.PgmTest2)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 44, in testGratPitchFromEnergyAndCff
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 51, in testGratPitchFromEnergyAndCff
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99284471638843 != 87.932 within 2 places
+AssertionError: 87.98780671618469 != 87.932 within 2 places
 
 ======================================================================
 FAIL: testEnergy (pgmTest.PgmTest3)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 36, in testEnergy
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 41, in testEnergy
     self.assertAlmostEqual(energy, self.pgm_energy, self.decimal_places)
-AssertionError: 640.8471578356111 != 697.2 within 2 places
+AssertionError: 643.1664703010379 != 697.2 within 2 places
 
 ======================================================================
 FAIL: testGratPitch (pgmTest.PgmTest3)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 62, in testGratPitch
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 73, in testGratPitch
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99179184546118 != 87.933 within 2 places
+AssertionError: 87.98899230012374 != 87.933 within 2 places
 
 ======================================================================
 FAIL: testGratPitchFromEnergyAndCff (pgmTest.PgmTest3)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 44, in testGratPitchFromEnergyAndCff
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 51, in testGratPitchFromEnergyAndCff
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99455949406382 != 87.933 within 2 places
+AssertionError: 87.98951348933139 != 87.933 within 2 places
 
 ======================================================================
 FAIL: testEnergy (pgmTest.PgmTest4)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 36, in testEnergy
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 41, in testEnergy
     self.assertAlmostEqual(energy, self.pgm_energy, self.decimal_places)
-AssertionError: 642.096889352862 != 698.21 within 2 places
+AssertionError: 644.4299488527497 != 698.21 within 2 places
 
 ======================================================================
 FAIL: testGratPitch (pgmTest.PgmTest4)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 62, in testGratPitch
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 73, in testGratPitch
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99337667776541 != 87.935 within 2 places
+AssertionError: 87.99057304288202 != 87.935 within 2 places
 
 ======================================================================
 FAIL: testGratPitchFromEnergyAndCff (pgmTest.PgmTest4)
 ----------------------------------------------------------------------
 Traceback (most recent call last):
-  File ".../i10-shared/scripts/test/pgm/pgmTest.py", line 44, in testGratPitchFromEnergyAndCff
+  File "/dls_sw/i10/software/gda_versions/gda_8.40b/workspace_git/gda-mt.git/configurations/i10-shared/scripts/test/pgm/pgmTest.py", line 51, in testGratPitchFromEnergyAndCff
     self.assertAlmostEqual(grat_pitch, self.grating_pitch, self.decimal_places)
-AssertionError: 87.99630515376802 != 87.935 within 2 places
+AssertionError: 87.99125102554004 != 87.935 within 2 places
 
 ----------------------------------------------------------------------
-Ran 16 tests in 0.031s
+Ran 16 tests in 0.027s
 
-FAILED (failures=11)
+FAILED (failures=10)
 
 """
