a7da4696ae6ab4a1d0d98eb1a29d2facda1d4206
Peter Holloway <peter.holloway@diamond.ac.uk>
2015-02-03 16:23:56 +0000

    Add absolute calibration to i22 and ncdsim

--- configurations/i22-config/scripts/localStation.py
+++ configurations/i22-config/scripts/localStation.py
@@ -52,6 +52,7 @@ waxs_distance = DetectorMeta("waxs_distance", ncddetectors, "WAXS", "distance", 
 saxs_distance = DetectorMeta("saxs_distance", ncddetectors, "SAXS", "distance", "m")
 saxs_centre_x = DetectorMeta("saxs_centre_x", ncddetectors, "SAXS", "beam_center_x")
 saxs_centre_y = DetectorMeta("saxs_centre_y", ncddetectors, "SAXS", "beam_center_y")
+saxs_abs_cal = DetectorMeta("saxs_abs_cal", ncddetectors, "SAXS", "scaling_factor")
 
 # preseed listener dispatcher
 print "Pre-seeding listener dispatcher"
