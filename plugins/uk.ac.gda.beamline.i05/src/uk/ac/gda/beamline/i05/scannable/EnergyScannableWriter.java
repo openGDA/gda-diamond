a30f3c596335679be80d12e6398e0cd7224f3bf2
Simon Berriman <simon.berriman@diamond.ac.uk>
2015-02-10 14:47:14 +0000

    Code cleanup prior to fix for new ComponentWriter structure

--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/scannable/EnergyScannableWriter.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/scannable/EnergyScannableWriter.java
@@ -34,84 +34,94 @@ import org.nexusformat.NexusException;
 public class EnergyScannableWriter extends SingleScannableWriter {
 
 	String stokesPath = "instrument:NXinstrument/insertion_device:NXinsertion_device/beam:NXbeam/final_polarisation_stokes";
-	
+
 	protected class DoubleArrayComponentWriter extends DefaultComponentWriter {
 		final int arraylength;
-		public DoubleArrayComponentWriter(int arraylength) {
+
+		public DoubleArrayComponentWriter(final int arraylength) {
 			this.arraylength = arraylength;
 		}
-		
+
 		@Override
-		protected Object getComponentSlab(Object pos) {
+		protected Object getComponentSlab(final Object pos) {
 			return pos;
 		}
+
 		@Override
-		protected int[] makedatadimfordim(int[] dim) {
+		protected int[] makedatadimfordim(final int[] dim) {
 			return ArrayUtils.add(super.makedatadimfordim(dim), arraylength);
 		}
+
 		@Override
-		protected int[] putslabdimfordim(int[] dim) {
+		protected int[] putslabdimfordim(final int[] dim) {
 			return ArrayUtils.add(super.putslabdimfordim(dim), 0);
 		}
+
 		@Override
-		protected int[] nulldimfordim(int[] dim) {
+		protected int[] nulldimfordim(final int[] dim) {
 			return ArrayUtils.add(super.nulldimfordim(dim), 0);
 		}
+
 		@Override
-		protected int[] slabsizedimfordim(int[] dim) {
+		protected int[] slabsizedimfordim(final int[] dim) {
 			return ArrayUtils.add(super.slabsizedimfordim(dim), arraylength);
 		}
 	}
-	
+
 	protected class PolarisationComponentWriter extends StringComponentWriter {
 		ComponentWriter stokesWriter = new DoubleArrayComponentWriter(4);
-		
-		double[] getStokes(String pos) {
-			if (I05Apple.HORIZONTAL.equals(pos)) 
-				return new double[] { 1.0, 1.0, 0.0, 0.0};
-			else if (I05Apple.VERTICAL.equals(pos)) 
-				return new double[] { 1.0, -1.0, 0.0, 0.0};
-			else if (I05Apple.CIRCULAR_RIGHT.equals(pos))
-				return new double[] { 1.0, 0.0, 0.0, 1.0};
-			else if (I05Apple.CIRCULAR_LEFT.equals(pos))
-				return new double[] { 1.0, 0.0, 0.0, -1.0};
+
+		double[] getStokes(final String pos) {
+			if (I05Apple.HORIZONTAL.equals(pos)) {
+				return new double[] { 1.0, 1.0, 0.0, 0.0 };
+			} else if (I05Apple.VERTICAL.equals(pos)) {
+				return new double[] { 1.0, -1.0, 0.0, 0.0 };
+			} else if (I05Apple.CIRCULAR_RIGHT.equals(pos)) {
+				return new double[] { 1.0, 0.0, 0.0, 1.0 };
+			} else if (I05Apple.CIRCULAR_LEFT.equals(pos)) {
+				return new double[] { 1.0, 0.0, 0.0, -1.0 };
+			}
 			return null;
 		}
-		
+
 		@Override
-		protected byte[] getComponentSlab(Object pos) {
-			if (I05Apple.HORIZONTAL.equals(pos)) 
+		protected byte[] getComponentSlab(final Object pos) {
+			if (I05Apple.HORIZONTAL.equals(pos)) {
 				return super.getComponentSlab("linear horiziontal");
-			else if (I05Apple.VERTICAL.equals(pos)) 
+			} else if (I05Apple.VERTICAL.equals(pos)) {
 				return super.getComponentSlab("linear vertical");
-			else if (I05Apple.CIRCULAR_LEFT.equals(pos))
+			} else if (I05Apple.CIRCULAR_LEFT.equals(pos)) {
 				return super.getComponentSlab("left circular");
-			else if (I05Apple.CIRCULAR_RIGHT.equals(pos))
+			} else if (I05Apple.CIRCULAR_RIGHT.equals(pos)) {
 				return super.getComponentSlab("right circular");
+			}
 			return super.getComponentSlab(pos);
 		}
-		
+
 		@Override
-		public Collection<SelfCreatingLink> makeComponent(NeXusFileInterface file, int[] dim, String path,
-				String scannableName, String componentName, Object pos, String unit) throws NexusException {
+		public Collection<SelfCreatingLink> makeComponent(final NeXusFileInterface file, final int[] dim,
+				final String path, final String scannableName, final String componentName, final Object pos,
+				final String unit) throws NexusException {
 			super.makeComponent(file, dim, path, scannableName, componentName, pos, unit);
-			double[] stokes = getStokes(pos.toString());
-			if (stokes == null)
+			final double[] stokes = getStokes(pos.toString());
+			if (stokes == null) {
 				stokesWriter = null;
-			else 
-				stokesWriter.makeComponent(file, dim, stokesPath, scannableName, componentName, stokes, null); 
+			} else {
+				stokesWriter.makeComponent(file, dim, stokesPath, scannableName, componentName, stokes, null);
+			}
 			return new Vector<SelfCreatingLink>();
 		}
-		
+
 		@Override
-		public void writeComponent(NeXusFileInterface file, int[] start, String path, String scannableName,
-				String componentName, Object pos) throws NexusException {
+		public void writeComponent(final NeXusFileInterface file, final int[] start, final String path,
+				final String scannableName, final String componentName, final Object pos) throws NexusException {
 			super.writeComponent(file, start, path, scannableName, componentName, pos);
-			double[] stokes = getStokes(pos.toString());
-			if (stokes == null)
+			final double[] stokes = getStokes(pos.toString());
+			if (stokes == null) {
 				stokesWriter = null;
-			else 
-				stokesWriter.writeComponent(file, start, stokesPath, scannableName, componentName, stokes); 
+			} else {
+				stokesWriter.writeComponent(file, start, stokesPath, scannableName, componentName, stokes);
+			}
 		}
 	}
 
