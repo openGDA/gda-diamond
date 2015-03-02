d2f3fda7e7f0b2f2ec99ee804ea3b6dec003356c
Mark Booth <Mark.Booth@diamond.ac.uk>
2015-03-02 14:27:15 +0000

    BLVI (8.38) Add waveplate 3 motor.

--- configurations/i06-config/scripts/localStation_i06-1.py
+++ configurations/i06-config/scripts/localStation_i06-1.py
@@ -122,6 +122,18 @@ except:
     logger.dump("---> ", exceptionType, exception, traceback)
     localStationErrorCount+=1
 
+print "-------------------------------------------------------------------"
+print "Waveplate 3 (EC2) motor: wp32"
+
+try:
+    from Diamond.PseudoDevices.EpicsMotors import EpicsCallbackMotorClass
+    wp32 = EpicsCallbackMotorClass('wp32', "BL06I-OP-WAVE-32:MTR", '%.4f');
+except:
+    exceptionType, exception, traceback=sys.exc_info();
+    print "XXXXXXXXXX:  wp32 Error "
+    logger.dump("---> ", exceptionType, exception, traceback)
+    localStationErrorCount+=1
+
 # Set up the scan processing wrappers
 print "-------------------------------------------------------------------"
 print "Set up standard scan processing"
