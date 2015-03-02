302cc630632d809a0b218682ec63f04f66a2c972
James Mudd <james.mudd@diamond.ac.uk>
2015-03-02 14:23:34 +0000

    Merge changes from 8.42 into master.

--- configurations/i05-config/scripts/nexusValidationCheck.py
+++ configurations/i05-config/scripts/nexusValidationCheck.py
@@ -7,17 +7,17 @@ nexusValidationTestsDir = "/dls/science/groups/das/NeXusAudit/I05"
 xtn = ".nxs"
 xtnLen = len(xtn)
 
-arpes.APRESRun("/dls_sw/i05/software/gda/config/scripts/nexusValidationFixedScan.arpes").run() # editor.getPath()<-beanfile for swept scan <- variant of 
+arpes.ARPESRun("/dls_sw/i05/software/gda/config/scripts/nexusValidationFixedScan.arpes").run() # editor.getPath()<-beanfile for swept scan <- variant of 
 lastScanFile = lastScanDataPoint().currentFilename                                             # get path of last file
 shutil.copy(lastScanFile, nexusValidationTestsDir+"/"+lastScanFile[:-xtnLen]+"_fixed"+xtn)     # copy resulting nexus file to validation area
 
-arpes.APRESRun("/dls_sw/i05/software/gda/config/scripts/nexusValidationSweptScan.arpes").run() # editor.getPath()<-beanfile for swept scan <- variant of 
+arpes.ARPESRun("/dls_sw/i05/software/gda/config/scripts/nexusValidationSweptScan.arpes").run() # editor.getPath()<-beanfile for swept scan <- variant of 
 lastScanFile = lastScanDataPoint().currentFilename                                             # get path of last file
 shutil.copy(lastScanFile, nexusValidationTestsDir+"/"+lastScanFile[:-xtnLen]+"_swept"+xtn)     # copy resulting nexus file to validation area
 
 # Notes
-# APRESRun.run() ends up calling gda.jython.commands.ScannableCommands.staticscan([self.scienta]) after the scienta analyser is configured
-# so excute APRESRun.run() config-only and then and extended command to exercise the nexus file
+# ARPESRun.run() ends up calling gda.jython.commands.ScannableCommands.staticscan([self.scienta]) after the scienta analyser is configured
+# so excute ARPESRun.run() config-only and then and extended command to exercise the nexus file
 # e.g. scan sapolar -20 24 0.25 say -0.293 0.00428 analyser                                         # with a bit of movement
 # e.g. scan sapolar -20 24 0.25 say -0.293 0.00428 analyser es1 es2                                 # with camera images
 # TBD scan exit_slit 0.2 0.1 -0.01 analyser
