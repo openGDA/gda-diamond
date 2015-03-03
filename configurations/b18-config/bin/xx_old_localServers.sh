02a6c27f3ddddfc6abe7f17a9f0e9815a6243278
Kristian Benning <kristian.benning@diamond.ac.uk>
2015-03-03 10:31:38 +0000

    GDA-6009 switch B18 config to the new standard
    Change-Id: I60684f1282dd81718bacdaa53adb8148addfafe3

---
+++ configurations/b18-config/bin/xx_old_localServers.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr
+/scratch/ws/b18/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda servers --datadir /scratch/users/data  --config  /scratch/ws/b18/config --properties /scratch/ws/b18/config/properties/dummy/java.properties --mode=dummy --debug -p 8001 --restart
