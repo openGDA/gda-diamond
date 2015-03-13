656520d643d7a6bca98c9bf6e3d306d99dbc5574
Charles Mita <charles.mita@diamond.ac.uk>
2015-03-13 17:39:55 +0000

    BLXVII (8.42) - commit beamline staff changes over run

---
+++ configurations/i16-config/scripts/24012015_night.py
@@ -0,0 +1,56 @@
+#First scan #493922
+#started 4:15am 24/01/15
+
+#made a mistake. Went off limits on 0k5 scan! Need to restart on fifth scan.
+#restarted scan #493927
+#another problem on delta limit in start of other domain scans...
+#restarted scan #493930
+
+#[0,0,5]:
+#[0.014, 0.070, 5.008]
+
+pos energy 11.215
+
+pos hkl([0.014,0.07,5.008])
+#scancn hkl [0.005,0.000,0.000] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.000,0.000,0.005] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.005,0.000,-0.005] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.005,0.000,0.005] 401 pil 1 roi2 roi1 checkbeam
+
+#pos hkl([0.014,0.57,5.008])
+#scancn hkl [0.000,0.005,0.000] 201 pil 1 roi2 roi1 checkbeam
+
+#pos hkl([0.014+0.125,0.07+0.375,5.008-0.125])
+#scancn hkl [-0.0025,-0.0075,0.0025] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014-0.125,0.07+0.375,5.008-0.125])
+scancn hkl [0.0025,-0.0075,0.0025] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014+0.125,0.07+0.375,5.008-0.375])
+scancn hkl [-0.0025,-0.0075,0.0075] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014-0.125,0.07+0.125,5.008+0.0417])
+scancn hkl [0.0025,-0.0025,-0.00083] 101 pil 1 roi2 roi1 checkbeam
+
+
+pos energy 11.18
+
+#pos hkl([0.014,0.07,5.008])
+#scancn hkl [0.005,0.000,0.000] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.000,0.000,0.005] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.005,0.000,-0.005] 401 pil 1 roi2 roi1 checkbeam
+#scancn hkl [0.005,0.000,0.005] 401 pil 1 roi2 roi1 checkbeam
+#pos hkl([0.014,0.57,5.008])
+#scancn hkl [0.000,0.005,0.000] 201 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014+0.125,0.07+0.375,5.008-0.125])
+scancn hkl [-0.0025,-0.0075,0.0025] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014-0.125,0.07+0.375,5.008-0.125])
+scancn hkl [0.0025,-0.0075,0.0025] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014+0.125,0.07+0.375,5.008-0.375])
+scancn hkl [-0.0025,-0.0075,0.0075] 101 pil 1 roi2 roi1 checkbeam
+
+pos hkl([0.014-0.125,0.07+0.125,5.008+0.0417])
+scancn hkl [0.0025,-0.0025,-0.00083] 101 pil 1 roi2 roi1 checkbeam
---
+++ configurations/i16-config/scripts/sticky_motors.py
@@ -0,0 +1,25 @@
+def moveaziz(newpos,tol=20):
+	while abs(Aziz()-newpos) >= 20:
+		pos Aziz newpos
+		w(.5)
+	print "Aziz moved"
+
+
+def moveazix(newpos,tol=20):
+	while abs(Azix()-newpos) >= 20:
+		pos Azix newpos
+		w(.5)
+	print "Azix moved"
+
+#>>>showKref()
+
+
+#key       h           k           l          Energy       kphi         Kap         Kth        delta       gamma
+#1        0.0000      0.0000      2.0000     11.1800    104.3300   -134.7559     57.0452      0.0000     26.4100
+#2        0.0000      0.0000      4.0000     11.1800    104.3300   -134.7559     57.0452      0.0000     54.0530
+#>>>showref()
+
+#key       h           k           l          Energy         psi        phi         chi         eta           mu       delta       gamma       alpha        beta
+#1        0.0000      0.0000      2.0000     11.1800     30.6141     47.2848     90.0000     -0.0000     18.4600      0.0000     26.4100    -45.2999     59.9612
+#2        0.0000      0.0000      4.0000     11.1800     30.0625     47.2848     90.0000     -0.0000     32.1600      0.0000     54.0530    -37.2001     60.4675
+#Move completed: cryophi : 0.0000
