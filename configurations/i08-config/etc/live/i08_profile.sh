75e55b6ca0f88fe49e9beac2e02667cf2dc5f0d7
Silvia Ramos <silvia.da-graca-ramos@diamond.ac.uk>
2015-03-30 09:43:40 +0100

    Reconfigure the DLS standard to be able to use gdaservers, gdalog and gdaclient from command line as well as from DLS launcher.
    Change-Id: I12f7189d71b283b8e8da84e2db2f6ac13c231ff4

--- configurations/i08-config/etc/live/i08_profile.sh
+++ configurations/i08-config/etc/live/i08_profile.sh
@@ -11,7 +11,7 @@ fi
 export GDA_INSTANCE_NAME=${BEAMLINE}
 
 export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
-export GDA_INSTANCE_CONFIG_rel=${GDA_WORKSPACE_GIT_NAME}/gda-dls-beamlines-xas.git/i08
+export GDA_INSTANCE_CONFIG=$GDA_WORKSPACE_PARENT/workspace_git/gda-dls-beamlines-xas.git/i08
 
 export PATH=$GDA_INSTANCE_CONFIG/bin:${PATH}
 export GDA_MODE=live
