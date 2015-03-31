7ec5a47a04babf71ccb48de4edddb76445247814
James Mudd <james.mudd@diamond.ac.uk>
2015-03-31 10:20:25 +0100

    ARPES-318 Add s5 xring and xhall remove s5 plus and minus
    Change-Id: I9a82c5abb268373fe3bfdc085f68d13bfe41011e

--- configurations/i05-config/scripts/localStation.py
+++ configurations/i05-config/scripts/localStation.py
@@ -46,11 +46,6 @@ s3xminus=DisplayEpicsPVClass('s3xminus', 'BL05I-AL-SLITS-03:X:MINUS:I', '', '%5.
 s3yplus=DisplayEpicsPVClass('s3yplus', 'BL05I-AL-SLITS-03:Y:PLUS:I', '', '%5.5g')
 s3yminus=DisplayEpicsPVClass('s3yminus', 'BL05I-AL-SLITS-03:Y:MINUS:I', '', '%5.5g')
 
-s5xplus=DisplayEpicsPVClass('s5xplus', 'BL05I-AL-SLITS-05:X:PLUS:I', '', '%5.5g')
-s5xminus=DisplayEpicsPVClass('s5xminus', 'BL05I-AL-SLITS-05:X:MINUS:I', '', '%5.5g')
-s5yplus=DisplayEpicsPVClass('s5yplus', 'BL05I-AL-SLITS-05:Y:PLUS:I', '', '%5.5g')
-s5yminus=DisplayEpicsPVClass('s5yminus', 'BL05I-AL-SLITS-05:Y:MINUS:I', '', '%5.5g')
-
 gauge01=DisplayEpicsPVClass("gauge01","BL05I-VA-GAUGE-01:P","","%.3e")
 gauge03=DisplayEpicsPVClass("gauge03","BL05I-VA-GAUGE-03:P","","%.3e")
 gauge04=DisplayEpicsPVClass("gauge04","BL05I-VA-GAUGE-04:P","","%.3e")
