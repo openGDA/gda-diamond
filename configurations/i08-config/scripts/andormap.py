97518060f32fc32181084661516e502d55d3974a
Silvia Ramos <silvia.da-graca-ramos@diamond.ac.uk>
2015-08-04 18:57:18 +0100

    BLVIII-62, BLVIII-52, BLVIII-61 Needed to do a single commit to solve conflicts
    Change-Id: I0214de748b294b07bdb11e74180094d1641bc1b3

--- configurations/i08-config/scripts/andormap.py
+++ configurations/i08-config/scripts/andormap.py
@@ -1,31 +1,31 @@
 from gda.configuration.properties import LocalProperties
 from gda.scan import ConstantVelocityRasterScan
-from gdascripts.scan import rasterscans
 from gdascripts.scan.rasterscans import RasterScan
-from gdascripts.scan.trajscans import setDefaultScannables
 from gda.epics import CAClient
 import time
 import math
 from plotters import Plotter
-from gda.factory import Finder
+from gda.jython.commands.ScannableCommands import add_default
 
 class AndorMap(RasterScan):
-     
-    def __init__(self,rowScannable,columnScannable,andor):
+
+    def __init__(self, rowScannable, columnScannable, andor):
         RasterScan.__init__(self)
         self.Xsize = 50 # default
         self.Ysize = 50 # default
         self.rowScannable = rowScannable
         self.columnScannable = columnScannable
         self.andor = andor
+        self.ROISetup()
         # setup the Andor trigger to internal for snapshots by default
-        andor.getCollectionStrategy().getAdBase().setTriggerMode(0)
+        self.andor.getCollectionStrategy().getAdBase().setTriggerMode(0)
         self.horizontal_plotter = Plotter("horizontal_plotter",'Horizontal',"Horizontal Gradient")
         self.vertical_plotter = Plotter("vertical_plotter",'Vertical',"Vertical Gradient")
-        self.transmission_plotter = Plotter("transmission_plotter",'transmission_total',"Transmission")   
-        add_default self.horizontal_plotter.getPlotter() self.vertical_plotter.getPlotter() self.transmission_plotter.getPlotter()  
-         
-         
+        self.transmission_plotter = Plotter("transmission_plotter",'transmission_total',"Transmission")
+        add_default(self.horizontal_plotter.getPlotter())
+        add_default(self.vertical_plotter.getPlotter())
+        add_default(self.transmission_plotter.getPlotter())
+
     def __call__(self, *args):
 
         # if one arg, then use that as the map size, else ignore any and all args
@@ -46,41 +46,39 @@ class AndorMap(RasterScan):
     def _createScan(self, args):
 
         # TODO create/ configure a scannable which will oberve data points and broadcast out transmission/phase-contrast to plotting system
-        
         # TODO get scan size from Epics PVs to replace self.map_size
-        
         # TODO check Epics PV holding STXM status as a safety check
 
         #rasterscan row 1 20 1 col 1 20 1 _andorrastor .1 'col'
 #         args = [self.rowScannable, 1, float(self.map_size), 1, self.columnScannable, 1, float(self.map_size), 1, self.andor]
         #self.ROISetup()
         myscan = ConstantVelocityRasterScan(self.scanargs)
-        
+
         # TODO set a PV to tell stxm the scan number
         #scanNumber = scan.getScanNumber()
-        
+
         # TODO tell the stxm the scan number, or the file prefix at this point
 
         return myscan
-    
+
 #configure the ROIs for both the step and raster andor objects
     def ROISetup(self):
-        Xsize = andor.getCollectionStrategy().getAdBase().getArraySizeX_RBV()
-        Ysize = andor.getCollectionStrategy().getAdBase().getArraySizeY_RBV()        
+        Xsize = self.andor.getCollectionStrategy().getAdBase().getArraySizeX_RBV()
+        Ysize = self.andor.getCollectionStrategy().getAdBase().getArraySizeY_RBV()
         XmidSize = Xsize/2
         YmidSize = Ysize/2
-        
+
         print "Setting up Regions of Interest for andor and _andorrastor objects..."
-        andor.roistats1.setRoi(0,0,XmidSize,YmidSize,"quadrant1")
-        andor.roistats2.setRoi(0,YmidSize,XmidSize,YmidSize,"quadrant2")
-        andor.roistats3.setRoi(XmidSize,0,XmidSize,YmidSize,"quadrant3")
-        andor.roistats4.setRoi(XmidSize,YmidSize,XmidSize,YmidSize,"quadrant4")
-        andor.roistats5.setRoi(0,0,Xsize,Ysize,"transmission")
-      
+        self.andor.roistats1.setRoi(0,0,XmidSize,YmidSize,"quadrant1")
+        self.andor.roistats2.setRoi(0,YmidSize,XmidSize,YmidSize,"quadrant2")
+        self.andor.roistats3.setRoi(XmidSize,0,XmidSize,YmidSize,"quadrant3")
+        self.andor.roistats4.setRoi(XmidSize,YmidSize,XmidSize,YmidSize,"quadrant4")
+        self.andor.roistats5.setRoi(0,0,Xsize,Ysize,"transmission")
+
     def OptimizeChunk(self):
-        Xsize = andor.getCollectionStrategy().getAdBase().getArraySizeX_RBV()
-        Ysize = andor.getCollectionStrategy().getAdBase().getArraySizeY_RBV() 
-        dataType = andor.getCollectionStrategy().getAdBase().getDataType()
+        Xsize = self.andor.getCollectionStrategy().getAdBase().getArraySizeX_RBV()
+        Ysize = self.andor.getCollectionStrategy().getAdBase().getArraySizeY_RBV()
+        dataType = self.andor.getCollectionStrategy().getAdBase().getDataType()
         # Each chunk is 1 MByte
         chunkSize = (1024**2)
         #pixel size in bytes
@@ -88,28 +86,27 @@ class AndorMap(RasterScan):
         print "pixelSize:",pixelSize
         framesPerChunk = (chunkSize)/(Xsize*Ysize*pixelSize)
         print "framesperChunk:",framesPerChunk
-        andor.getAdditionalPluginList()[0].setFramesChunks(framesPerChunk)
-        
+        self.andor.getAdditionalPluginList()[0].setFramesChunks(framesPerChunk)
+
     def OpenAndorShutter(self):
         if (LocalProperties.get("gda.mode") == 'live'):
             CAClient().put("BL08I-EA-DET-01:CAM:AndorShutterMode","1")
-     
-    def PrepareForCollection(self,Xsize,Ysize):   
-        andor.getCollectionStrategy().getAdBase().stopAcquiring()
+
+    def PrepareForCollection(self, Xsize, Ysize):   
+        self.andor.getCollectionStrategy().getAdBase().stopAcquiring()
         time.sleep(1)
         self.OpenAndorShutter()
         self.ROISetup()
         self.OptimizeChunk()
         self.resetPlotters(Xsize, Ysize)
-        
+
     def resetPlotters(self, Xsize, Ysize):
         self.vertical_plotter.setAxis(Xsize,Ysize)
         self.horizontal_plotter.setAxis(Xsize,Ysize)
         self.transmission_plotter.setAxis(Xsize,Ysize)
-         
+
 # then create the scan wrapper for map scans
-# col = stxmDummy.stxmDummyX
-# row = stxmDummy.stxmDummyY
-andormap = AndorMap(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor)
-alias andormap
-print "Command andormap(mapSize) created for arming the Andor detector before running STXM maps"
+#_andorrastor = Finder.getInstance().find("_andorrastor")
+#andormap = AndorMap(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor)
+#alias("andormap")
+#print "Command andormap(mapSize) created for arming the Andor detector before running STXM maps"
--- configurations/i08-config/scripts/localStation.py
+++ configurations/i08-config/scripts/localStation.py
@@ -6,13 +6,19 @@ from gda.jython import InterfaceProvider
 from gda.device.scannable import ScannableBase, CoupledScannable
 from gda.device.monitor import EpicsMonitor
 from gdascripts.parameters.beamline_parameters import JythonNameSpaceMapping
+from gda.device.detector.nxdetector.andor.proc import FlatAndDarkFieldPlugin
+from gda.device.detector.nxdetector.andor.proc.FlatAndDarkFieldPlugin import ScanType
+from andormap import AndorMap
+from andormapWithCorrection import AndorMapWithCorrection
+from scanForImageCorrection import ScanForImageCorrection
 #from gdascripts.scannable.beamokay import WaitWhileScannableBelowThreshold, WaitForScannableState
 
 print "Initialisation Started";
 
 
 from gda.device import Scannable
-from gda.jython.commands.GeneralCommands import ls_names, vararg_alias
+from gda.jython.commands.GeneralCommands import ls_names, vararg_alias,run
+from gda.jython.commands.ScannableCommands import add_default
 
 def ls_scannables():
     ls_names(Scannable)
@@ -48,25 +54,24 @@ actualTime=actualTimeClass("actualTime")
 # Use for the calibration of the pgm energy, create a scannable idEnergy
 from idEnergy import my_energy_class1
 
-
-#checkrc = WaitWhileScannableBelowThreshold('checkrc', rc, 190, secondsBetweenChecks=1,secondsToWaitAfterBeamBackUp=5) #@UndefinedVariable
-#checkfe = WaitForScannableState('checkfe', frontend, secondsBetweenChecks=1,secondsToWaitAfterBeamBackUp=60) #@UndefinedVariable
-#checkshtr1 = WaitForScannableState('checkshtr1', shtr1, secondsBetweenChecks=1,secondsToWaitAfterBeamBackUp=60) #@UndefinedVariable
-#checkbeam = ScannableGroup('checkbeam', [checkrc,  checkfe, checkshtr1])
-#checkbeam.configure()
-
-#if (LocalProperties.get("gda.mode") == 'live'): 
+#if (LocalProperties.get("gda.mode") == 'live'):
 #    beamMonitor.configure()
 #    add_default beamMonitor
-#    add_default topupMonitor 
-   
+#    add_default topupMonitor
+
 
 # set up the Andor Area Detector ROIs etc for hardware-driven mapping
 #run "AndorConfiguration"
 # create the command to run STXM mpas which involve andor
+andormap = AndorMap(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor)
 
-run "andormap"
+alias("andormap")
+print "Command andormap(mapSize) created for arming the Andor detector before running STXM maps"
+#To obtain the corrected images just use the collection strategy for step scan
+scanForImageCorrection = ScanForImageCorrection(andor)
+andormapWithCorrection = AndorMapWithCorrection(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor,scanForImageCorrection)
+alias("andormapWithCorrection")
+
 run "xrfmap"
 
-#
 print "Initialisation Complete";
