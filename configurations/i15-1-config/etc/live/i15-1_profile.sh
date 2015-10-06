340f12f08e1ff6fa927e4e77f2466737ebaadfb7
Mark Booth <Mark.Booth@diamond.ac.uk>
2015-10-06 16:47:47 +0100

    BLXV (8.44) Create skeletal i15-1 config.

---
+++ configurations/i15-1-config/etc/live/i15-1_profile.sh
@@ -0,0 +1,9 @@
+# i15-1 beamline profile
+
+if [ ! -n "$BEAMLINE" ]; then
+  echo "ERROR: BEAMLINE not set" 1>&2
+  exit 1
+fi
+
+export PATH=/dls_sw/$BEAMLINE/software/gda/workspace_git/gda-mt.git/configurations/i15-1-config/bin:${PATH}
+export GDA_MODE=live
