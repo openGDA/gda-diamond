3d3e435aabdaafc2bf096f7d9977e513c9d56f01
Mark Booth <Mark.Booth@diamond.ac.uk>
2015-10-16 15:50:35 +0100

    BLXV (8.44) Complete skeletal i15-1 config.

--- configurations/i15-1-config/etc/live/i15-1_profile.sh
+++ configurations/i15-1-config/etc/live/i15-1_profile.sh
@@ -5,5 +5,9 @@ if [ ! -n "$BEAMLINE" ]; then
   exit 1
 fi
 
+# Set up path and mode
 export PATH=/dls_sw/$BEAMLINE/software/gda/workspace_git/gda-mt.git/configurations/i15-1-config/bin:${PATH}
 export GDA_MODE=live
+
+# Set up command completion for the gda command
+source /dls_sw/$BEAMLINE/software/gda/workspace_git/gda-core.git/core-config/bin/gda_core_completion
