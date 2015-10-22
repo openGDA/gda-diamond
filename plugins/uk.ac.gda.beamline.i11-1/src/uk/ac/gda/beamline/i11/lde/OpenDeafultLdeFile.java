7887a0ad86f4424da4365d90e9c9389646f7b064
fajinyuan <fajin.yuan@diamond.ac.uk>
2015-10-22 11:50:47 +0100

    open the default file from workspace, not directly from file system as the later Problems view does not work with editor on validation errors

--- plugins/uk.ac.gda.beamline.i11-1/src/uk/ac/gda/beamline/i11/lde/OpenDeafultLdeFile.java
+++ plugins/uk.ac.gda.beamline.i11-1/src/uk/ac/gda/beamline/i11/lde/OpenDeafultLdeFile.java
@@ -22,6 +22,13 @@ import java.io.File;
 
 import org.eclipse.core.filesystem.EFS;
 import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.IStartup;
@@ -31,38 +38,59 @@ import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.FileStoreEditorInput;
+import org.eclipse.ui.part.FileEditorInput;
 import org.opengda.lde.model.ldeexperiment.presentation.LDEExperimentsEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import gda.data.PathConstructor;
+import gda.rcp.DataProject;
+import uk.ac.gda.preferences.PreferenceConstants;
 
 public class OpenDeafultLdeFile implements IStartup {
 
-	private static final Logger logger=LoggerFactory.getLogger(OpenDeafultLdeFile.class);
+	private static final Logger logger = LoggerFactory.getLogger(OpenDeafultLdeFile.class);
 
 	@Override
 	public void earlyStartup() {
 		final IWorkbench workbench = PlatformUI.getWorkbench();
 
 		workbench.getDisplay().asyncExec(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
 				if (window != null) {
-					IPath path = new Path(PathConstructor.createFromDefaultProperty()+File.separator+"xml"+File.separator+"newsamples.lde");
-					IFileStore fileToBeOpened= EFS.getLocalFileSystem().getStore(path);
-					IWorkbenchPage page = window.getActivePage();
-					FileStoreEditorInput editorInput = new FileStoreEditorInput(fileToBeOpened);
-					
-					try {
-						page.openEditor(editorInput, LDEExperimentsEditor.ID);
-					} catch (PartInitException e) {
-						logger.error("Failed to open default LDE file: "+ path.getDevice(), e);
+					IWorkbenchPage activePage = window.getActivePage();
+					IProject dataProject = DataProject.getDataProjectIfExists();
+					// open if necessary
+					if (dataProject.exists() && !dataProject.isOpen()) {
+						try {
+							dataProject.open(null);
+						} catch (CoreException e1) {
+							logger.error("Error open project " +dataProject.getLocationURI().getPath(), e1);
+						}
 					}
-					//TODO BLXI-321 for some unknown reason, the default perspective does not show views in correct size, so do the following line.
-					page.resetPerspective();
+					IFolder dataFolder = dataProject.getFolder("data");
+					if (dataFolder.exists()) {
+						IFolder xmlFolder = dataFolder.getFolder("xml");
+						if (xmlFolder.exists()) {
+							IFile sampleFile=xmlFolder.getFile("newsamples.lde");
+							if (sampleFile.exists()) {
+							FileEditorInput editorInput=new FileEditorInput(sampleFile);
+							try {
+								activePage.openEditor(editorInput, LDEExperimentsEditor.ID);
+							} catch (PartInitException e) {
+								logger.error("Failed to open default LDE file: " + sampleFile.getLocationURI().getPath(), e);
+							}
+							}
+						}
+						
+					}
+					
+					// TODO BLXI-321 for some unknown reason, the default perspective does not show views in correct
+					// size, so do the following line.
+					activePage.resetPerspective();
 				}
 			}
 		});
