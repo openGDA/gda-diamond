b5e726711aec23766c72cf9e09e59579332493cf
Anthony Hull <anthony.hull@diamond.ac.uk>
2015-10-27 12:10:26 +0000

    Add more configuration, especially to do with live mode

--- configurations/i21-config/etc/live/i21_profile.sh
+++ configurations/i21-config/etc/live/i21_profile.sh
@@ -7,3 +7,6 @@ fi
 
 export PATH=/dls_sw/$BEAMLINE/software/gda/workspace_git/gda-dls-beamlines-i21.git/i21-config/bin:${PATH}
 export GDA_MODE=live
+
+# Set up command completion for the gda command
+source /dls_sw/$BEAMLINE/software/gda/workspace_git/gda-core.git/core-config/bin/gda_core_completion
