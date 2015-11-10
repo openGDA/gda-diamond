639650f7a15d7e2e92c42147e8dce38dd2b65839
Anthony Hull <anthony.hull@diamond.ac.uk>
2015-11-10 13:18:34 +0000

    Replace example config with a very basic configuration
    This replaces the devices inherited from the example config with 5 scannable motors and a scannable "exposure". For the time being, these are dummy in all modes.
    
    This commit also adds missing i21-profile.sh

--- configurations/i21-config/scripts/localStation.py
+++ configurations/i21-config/scripts/localStation.py
@@ -1,60 +1,38 @@
-import sys    
-import os
-import time
-from gda.factory import Finder
-from gda.configuration.properties import LocalProperties
-from gdascripts.messages import handle_messages
-from gda.jython import InterfaceProvider
-from gda.device.scannable import ScannableBase
+import java, sys
 
-        
-def isLive():
-    mode = LocalProperties.get("gda.mode")
-    return mode =="live"
+from gdascripts.messages.handle_messages import simpleLog, log
+
+global run
+
+localStation_exceptions = []
+
+def localStation_exception(exc_info, msg):
+    typ, exception, traceback = exc_info
+    simpleLog("! Failure %s !" % msg)
+    localStation_exceptions.append("    %s" % msg)
+    log(None, "Error %s -  " % msg , typ, exception, traceback, False)
 
 try:
-    from gda.device import Scannable
-    from gda.jython.commands.GeneralCommands import ls_names, vararg_alias
-    
-    def ls_scannables():
-        ls_names(Scannable)
-    
-    alias("ls_scannables")
-
-
-    from epics_scripts.pv_scannable_utils import createPVScannable, caput, caget
-    alias("createPVScannable")
-    alias("caput")
-    alias("caget")
-    
-    from gda.scan.RepeatScan import create_repscan, repscan
-    vararg_alias("repscan")
-
-    #setup tools to create metadata in Nexus files
-    from gdascripts.metadata.metadata_commands import setTitle, meta_add, meta_ll, meta_ls, meta_rm
-    alias("setTitle")
-    alias("meta_add")
-    alias("meta_ll")
-    alias("meta_ls")
-    alias("meta_rm")
-    from gda.data.scan.datawriter import NexusDataWriter
-    LocalProperties.set(NexusDataWriter.GDA_NEXUS_METADATAPROVIDER_NAME,"metashop")
-    
-    #create time scannables
-    from gdascripts.pd.time_pds import waittimeClass2, showtimeClass, showincrementaltimeClass, actualTimeClass
-    waittime=waittimeClass2('waittime')
-    showtime=showtimeClass('showtime')
-    inctime=showincrementaltimeClass('inctime')
-    actualTime=actualTimeClass("actualTime")
-
-    # add ScisoftPy
-    sys.path.append(LocalProperties.getParentGitDir() + "scisoft-core.git/uk.ac.diamond.scisoft.python/src")
-
-    #run user editable startup script 
-    if isLive():
-        run("localStationUser.py")
+    simpleLog("================ INITIALISING I21 GDA ================")
 
 except:
-    exceptionType, exception, traceback = sys.exc_info()
-    handle_messages.log(None, "Error in localStation", exceptionType, exception, traceback, False)
+    localStation_exception(sys.exc_info(), "in localStation")
 
+
+print "*"*80
+print "Attempting to run localStationUser.py from users script directory"
+try:
+    run("localStationUser")
+    print "localStationUser.py completed."
+except java.io.FileNotFoundException, e:
+    print "No localStationUser.py found in user scripts directory"
+except:
+    localStation_exception(sys.exc_info(), "running localStationUser user script")
+
+if len(localStation_exceptions) > 0:
+    simpleLog("=============== %r ERRORS DURING STARTUP ================" % len(localStation_exceptions))
+
+for localStationException in localStation_exceptions:
+    simpleLog(localStationException)
+
+simpleLog("===================== GDA ONLINE =====================")
--- configurations/i21-config/scripts/new_scans.py
+++
@@ -1,2 +0,0 @@
-from init.init_scan_commands_and_processing import *
-scan_processor.rootNamespaceDict=globals()
