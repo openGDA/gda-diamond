d796e3103922c402282ab37a2c4511b1bebef28c
Anthony Hull <anthony.hull@diamond.ac.uk>
2015-11-16 16:41:58 +0000

    Some more changes moving towards Config 1.1

--- configurations/i13-1-config/bin/servers.sh
+++ configurations/i13-1-config/bin/servers.sh
@@ -1,2 +1,3 @@
-export BEAMLINE=i13-1
-. /dls_sw/$BEAMLINE/software/gda/workspace_git/gda-diamond.git/dls-config/bin/remote/servers.sh
+#export BEAMLINE=i13-1
+#. /dls_sw/$BEAMLINE/software/gda/workspace_git/gda-diamond.git/dls-config/bin/remote/servers.sh
+. /dls_sw/i13-1/software/gda/config/etc/live/remotestartupscript.sh
