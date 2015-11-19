f654de9e59dfef18594e696dee2940a6d5237a3d
Anthony Hull <anthony.hull@diamond.ac.uk>
2015-11-19 14:45:15 +0000

    Fixes for dummy & live modes
    - Modify client & server configuration to be closer to 8.40.
    
    - Move live-only devices from common to live config; add dummy equivalents.

--- configurations/i13-config/bin/gda_environment.sh
+++ configurations/i13-config/bin/gda_environment.sh
@@ -4,6 +4,7 @@ export GDAFOLDER=/dls_sw/$BEAMLINE/software/gda
 export GDAVAR=/dls_sw/$BEAMLINE/software/gda_versions/var
 export GDALOGS=/dls_sw/$BEAMLINE/software/logs
 export GDAMODE=live
+export GDA_MODE=live
 
 export PATH=${GDAFOLDER}/config/bin:${GDAFOLDER}/workspace_git/gda-diamond.git/dls-config/bin:${PATH}
 
