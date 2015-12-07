2a3ae02443e7091e5f79bcbd4cf6514f454916cd
Anthony Hull <anthony.hull@diamond.ac.uk>
2015-12-07 11:08:06 +0000

    Modify degas_slit script for new requirements
    See http://confluence.diamond.ac.uk/display/I21/Slits+1 for requirements.
    
    This version has not yet been tested on the beamline.

--- configurations/i21-config/scripts/degas/degas_slit.py
+++ configurations/i21-config/scripts/degas/degas_slit.py
@@ -1,53 +1,121 @@
-#----------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------
 # Script to degas a single slit
-# Move a single slit into position, move the rest out and open the front end
-# If the pressure exceeds a given value, close the front end shutter
-# and wait for the pressure to drop
-#----------------------------------------------------------------------------
+#
+# Move a single slit blade into the beam stepwise, waiting for the pressure
+# to drop below <lowerBound> before advancing it by the next step.
+#
+# If the pressure exceeds <upperBound>, retract the blade completely,
+# wait for the pressure to drop, halve the step size, then advance it as before.
+# Ideally, we should set the initial step size so that this never happens.
+#
+# Constructor arguments:
+#    blade:     the blade to move
+#    startPos:  starting position for the blade.
+#               This should be such that the edge is (just) outside the beam.
+#    endPos:    end position for the blade i.e. when it is fully in the beam
+#    direction: the direction which takes the blade towards the centre of the beam.
+#               Any positive number will be treated as meaning that the position should be incremented,
+#               any negative number that it should be decremented.
+#    gauge:     the pressure gauge to monitor
+#------------------------------------------------------------------------------------------------------------------
 
 from exceptions import KeyboardInterrupt
 from time import sleep
 from gda.jython import ScriptBase
 
 class DegasSlit:
-    def __init__(self, front_end_shutter, gauge, gaugemin = 5e-9, gaugemax = 2.5e-8):
-        self.front_end_shutter = front_end_shutter
+    def __init__(self, blade, startPos, endPos, direction, gauge):
+        self.blade = blade
+        self.startPos = startPos
+        self.endPos = endPos
+        self.setDirection(direction)
         self.gauge = gauge
-        self.gaugemin = gaugemin
-        self.gaugemax = gaugemax
+        
+        # Pressure bounds: see above
+        self.lowerBound = 1e-8
+        self.upperBound = 5e-8
+        
+        # Frequency of monitoring pressure (seconds)
+        self.monitorFreq = 1
+        
+        # Minimum number of monitoring cycles between steps
+        self.minCycles = 10 
+        
+        # Step size (mm)
+        self.stepSize = 0.1
+
+
+    def setDirection(self, direction):
+        if (direction == 0):
+            raise RuntimeError("direction must not be zero")
+        elif (direction > 0):
+            self.direction = 1
+        else:
+            self.direction = -1
 
 
     def run(self):
+        self.blade.moveTo(self.startPos)
+        cyclesBeforeMove = 0
+        finished = False
+
         self.report()
         
         try:
-            while(True):
-                gaugePos = self.gauge.getPosition()
-                shutterPos = self.front_end_shutter.getPosition()
+            while (finished == False):
+                pressure = self.gauge.getPosition()
+                print self.blade, self.gauge
                 
-                print "Pressure = ", self.gauge
-                
-                if (gaugePos > self.gaugemax and shutterPos == 'Open'):
-                    print "Closing front end shutter"
-                    self.front_end_shutter.moveTo('Closed')
-                elif (gaugePos < self.gaugemin and shutterPos == 'Closed'):
-                    print "Opening front end shutter"
-                    self.front_end_shutter.moveTo('Open')
+                if (pressure > self.upperBound):
+                    # Pressure is too high: move blade out and wait for pressure to drop
+                    print "pressure too high: resetting blade"
+                    self.blade.moveTo(self.startPos)
+                    self.stepSize = self.stepSize / 2.0
+                    while (self.gauge.getPosition() > self.lowerBound):
+                        print "waiting for pressure to drop"
+                        print self.blade, self.gauge
+                        sleep(self.monitorFreq)
+                    cyclesBeforeMove = 0
+                        
+                elif (pressure < self.lowerBound):
+                    # Pressure is low: advance the blade, unless we have recently done this,
+                    # or the blade is at its maximum position
+                    if (self.blade.getPosition() >= self.endPos):
+                        print "process finished"
+                        finished = True 
+                    elif (cyclesBeforeMove > 0):
+                        print "pressure low but we cannot move yet: cyclesBeforeMove ", cyclesBeforeMove
+                        cyclesBeforeMove = cyclesBeforeMove - 1
+                    else:
+                        newPos = self.blade.getPosition() + (self.stepSize * self.direction)
+                        print "moving blade to ", newPos
+                        self.blade.moveTo(newPos)
+                        cyclesBeforeMove = self.minCycles
+
+                else:
+                    print "stay steady"
+                    cyclesBeforeMove = cyclesBeforeMove - 1
+
                     
-                # Wait a second before checking again
-                sleep(1)
+                # Wait before checking again
+                sleep(self.monitorFreq)
             
         except KeyboardInterrupt:
-            print "Closing down"
+            print "Process terminated by user"
 
         finally:
+            self.blade.moveTo(self.startPos)
             self.report()
 
         
     def report(self):
         print "--------------- DegasSlit ------------------------"
-        print "front_end_shutter = ", self.front_end_shutter
+        print "blade = ", self.blade
+        print "startPos = ", self.startPos
+        print "endPos = ", self.endPos
+        print "direction = ", self.direction
         print "gauge = ", self.gauge
-        print "gaugemin = ", self.gaugemin
-        print "gaugemax = ", self.gaugemax
+        print "lowerBound = ", self.lowerBound
+        print "upperBound = ", self.upperBound
+        print "stepSize = ", self.stepSize
         print "--------------------------------------------------"
