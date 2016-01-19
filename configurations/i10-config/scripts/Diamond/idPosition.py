8d6a455baffa93e71d252b3ca8755694491a16bc
Mark Booth <Mark.Booth@diamond.ac.uk>
2016-01-19 15:19:21 +0000

    BLX-100 (8.44) Allow angle to control any motor in EnergyScannableLinearArbitrary

--- configurations/i10-config/scripts/Diamond/idPosition.py
+++ configurations/i10-config/scripts/Diamond/idPosition.py
@@ -11,15 +11,29 @@ class IdPosition(list):
         self.rowphase3 = rowphase3
         self.rowphase4 = rowphase4
         self.jawphase = jawphase
-        
+
     def __repr__(self):
         format = "IdPosition(gap=%r, rowphase1=%r, rowphase2=%r, " + \
             "rowphase3=%r, rowphase4=%r, jawphase=%r)"
         return format % (self.gap, self.rowphase1, self.rowphase2,
             self.rowphase3, self.rowphase4, self.jawphase)
-    
+
     def __str__(self):
         format = "gap=%r, rowphase1=%r, rowphase2=%r, " + \
             "rowphase3=%r, rowphase4=%r, jawphase=%r"
         return format % (self.gap, self.rowphase1, self.rowphase2,
             self.rowphase3, self.rowphase4, self.jawphase)
+
+    def merge(self, mergable):
+        assert((self.gap == None) != (mergable.gap == None))
+        assert((self.rowphase1 == None) != (mergable.rowphase1 == None))
+        assert((self.rowphase2 == None) != (mergable.rowphase2 == None))
+        assert((self.rowphase3 == None) != (mergable.rowphase3 == None))
+        assert((self.rowphase4 == None) != (mergable.rowphase4 == None))
+        assert((self.jawphase == None) != (mergable.jawphase == None))
+        return IdPosition(self.gap if mergable.gap == None else mergable.gap,
+                          self.rowphase1 if mergable.rowphase1 == None else mergable.rowphase1,
+                          self.rowphase2 if mergable.rowphase2 == None else mergable.rowphase2,
+                          self.rowphase3 if mergable.rowphase3 == None else mergable.rowphase3,
+                          self.rowphase4 if mergable.rowphase4 == None else mergable.rowphase4,
+                          self.jawphase if mergable.jawphase == None else mergable.jawphase)
