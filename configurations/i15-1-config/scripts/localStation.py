cc5463f2531154d28c5c0cd2f36083a6f7537a1a
Mark Booth <Mark.Booth@diamond.ac.uk>
2016-02-29 11:33:20 +0000

    I15_1-47 (8.51) Make the wrapped detectors the preferred detector scannables.

--- configurations/i15-1-config/scripts/localStation.py
+++ configurations/i15-1-config/scripts/localStation.py
@@ -36,23 +36,23 @@ try:
     from gdascripts.scannable.detector.DetectorDataProcessor import DetectorDataProcessorWithRoi
     from gdascripts.analysis.datasetprocessor.twod.SumMaxPositionAndValue import SumMaxPositionAndValue #@UnusedImport
     from gdascripts.analysis.datasetprocessor.twod.TwodGaussianPeak import TwodGaussianPeak
-    global cam1, cam2, bpm1, bpm2
+    global cam1rawNx, cam2rawNx, bpm1rawNx, bpm2rawNx
 
-    def wrappedDetectorFactory(camdet):
+    def wrappedDetectorFactory(camdet, cam_name):
         try:
-            pdw_name, peak2d_name, max2d_name= camdet.name+"pdw", camdet.name+"Peak2d", camdet.name+"Max2d"
+            pdw_name, peak2d_name, max2d_name= cam_name, cam_name+"Peak2d", cam_name+"Max2d"
             print "Creating %s, %s and %s detector wrappers" % (pdw_name, peak2d_name, max2d_name)
-            cam    = ProcessingDetectorWrapper   (pdw_name, camdet, [], panel_name='GigE Camera', panel_name_rcp='Plot 1')
+            cam    = ProcessingDetectorWrapper   (pdw_name, camdet, [], panel_name_rcp='Plot 1')
             peak2d = DetectorDataProcessorWithRoi(peak2d_name, cam, [TwodGaussianPeak()])
             max2d  = DetectorDataProcessorWithRoi(max2d_name, cam, [SumMaxPositionAndValue()])
             return cam, peak2d, max2d
         except:
             localStation_exception(sys.exc_info(), "creating %s detector wrappers" % camdet.name)
 
-    cam1pdw, cam1Peak2d, cam1Max2d  = wrappedDetectorFactory(cam1)
-    cam2pdw, cam2Peak2d, cam2Max2d2 = wrappedDetectorFactory(cam2)
-    bpm1pdw, bpm1Peak2d, bpm1Max2d3 = wrappedDetectorFactory(bpm1)
-    bpm2pdw, bpm2Peak2d, bpm1Max2d4 = wrappedDetectorFactory(bpm2)
+    cam1, cam1Peak2d, cam1Max2d = wrappedDetectorFactory(cam1rawNx, 'cam1')
+    cam2, cam2Peak2d, cam2Max2d = wrappedDetectorFactory(cam2rawNx, 'cam2')
+    bpm1, bpm1Peak2d, bpm1Max2d = wrappedDetectorFactory(bpm1rawNx, 'bpm1')
+    bpm2, bpm2Peak2d, bpm2Max2d = wrappedDetectorFactory(bpm2rawNx, 'bpm2')
 
 except:
     localStation_exception(sys.exc_info(), "in localStation")
