9ad051763e92f39deaddab24bb878047cca2ab14
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2016-03-14 11:19:01 +0000

    Adjust i18 startup scripts for GDA 9 operation
    Allow the gdaservers script to pass through --debug and change 
    remotestartupsctipt.sh to start the exported gda-server product using 
    SSH_ORIGINAL_COMMAND if required.
    
    Change-Id: I868f12a0fb1890b99aa20c8966ac711b421f9a2c

--- configurations/i18-config/bin/remotestartupscript.sh
+++ configurations/i18-config/bin/remotestartupscript.sh
@@ -1,15 +1,41 @@
+# GDA 9 remote startup script for i18 beamline control machine
+
+# This script is run as a single command by ssh, so we need to set up our environment
 . /usr/share/Modules/init/bash
 
-. /dls_sw/i18/software/gda/workspace_git/gda-diamond.git/dls-config/bin/loadjava.sh
+runningServerPID=$(ps -ef | grep [G]da-server | awk '{ print $2 }')
 
-# JRebel, for development when method signatures may change
-#JAVA_OPTS="-Xms128m -Xmx2024m -XX:MaxPermSize=128m"; export JAVA_OPTS
-#if [ -f "/dls_sw/dasc/jrebel/5.2/jrebel.jar" ]; then
-#	JAVA_OPTS="-javaagent:/dls_sw/dasc/jrebel/5.2/jrebel.jar -Drebel.properties=/dls_sw/dasc/jrebel/5.2/jrebel.properties $JAVA_OPTS"; export JAVA_OPTS
-#fi
+# If the server is already running, give it 10 seconds to properly shut down, then restart 
+if [[ -n "${runningServerPID}" ]]; then
+	$(kill "${runningServerPID}")
+	sleep 10
+fi
 
-python /dls_sw/i18/software/gda/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=/dls_sw/i18/software/gda/config --restart -v --mode=live nameserver &
-python /dls_sw/i18/software/gda/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda  --smart --trace --config=/dls_sw/i18/software/gda/config --restart -v --mode=live logserver &
-python /dls_sw/i18/software/gda/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=/dls_sw/i18/software/gda/config --debug -p 8002 --restart -v --mode=live eventserver &
-export JAVA_OPTS="-Xms128m -Xmx4096m -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
-python /dls_sw/i18/software/gda/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda --smart --trace --config=/dls_sw/i18/software/gda/config --debug -p 8001 --restart -v --mode=live objectserver &
+# There is no user or screen to prompt or display pop-ups
+export GDA_NO_PROMPT=true
+
+# Adjust memory to handle large scans
+export JAVA_OPTS="-Xms128m -Xmx4096m  -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
+
+# Initialise the command string components
+install_root="/dls_sw/i18/software/gda"
+server_binary="$install_root/server/gda-server"
+workspace_loc="$install_root/server/dls-root/workspace"
+config_loc="$install_root/workspace_git/gda-dls-beamlines-xas.git/i18"
+load_java="$install_root/workspace_git/gda-diamond.git/dls-config/bin/loadjava.sh"
+args="-Dgda.mode=live"
+
+for word in "$@"; do
+	if [[ "${word}" == "debug" ]] || [[ ${SSH_ORIGINAL_COMMAND} == "debug" ]]; then
+		args="$args -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8001"
+	fi
+done
+
+# We will need java
+. $load_java
+
+# Assemble the command string
+command="$server_binary -data $workspace_loc -config $config_loc -vmArgs $args"
+
+# and execute it retaining stdin 
+$command &
