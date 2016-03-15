b01bd09c889751e4be4149de2a33a90aa9de66bf
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2016-03-15 10:35:28 +0000

    Finalize remotestartupscript.sh
    Based on discussions with Matthew Webber as to how the application 
    install and startup arrangements will be set out, modify the 
    remote startup script accordingly.
    
    Change-Id: I268d3c7a27e4e429b35b1b9c6c1d68eba97c2a33

--- configurations/i18-config/bin/remotestartupscript.sh
+++ configurations/i18-config/bin/remotestartupscript.sh
@@ -3,39 +3,62 @@
 # This script is run as a single command by ssh, so we need to set up our environment
 . /usr/share/Modules/init/bash
 
-runningServerPID=$(ps -ef | grep [G]da-server | awk '{ print $2 }')
+# Require the BEAMLINE environment variable to have been set
+if [ -z $BEAMLINE ] ; then
+	echo "ERROR: BEAMLINE environment variable not set - GDA server cannot start" 1>&2
+	exit 1
+fi
 
 # If the server is already running, give it 10 seconds to properly shut down, then restart 
-if [[ -n "${runningServerPID}" ]]; then
-	$(kill "${runningServerPID}")
+RUNNING_SERVER_PID=$(ps -ef | grep [G]da-server | awk '{ print $2 }')
+if [[ -n "${RUNNING_SERVER_PID}" ]]; then
+	$(kill "${RUNNING_SERVER_PID}")
 	sleep 10
 fi
 
 # There is no user or screen to prompt or display pop-ups
 export GDA_NO_PROMPT=true
 
-# Adjust memory to handle large scans
+# Adjust memory to handle large scans, -XX:MaxPermSize=128m is retained currently pending further investigation
 export JAVA_OPTS="-Xms128m -Xmx4096m  -XX:MaxPermSize=128m -XX:+DisableExplicitGC"
 
-# Initialise the command string components
-install_root="/dls_sw/i18/software/gda"
-server_binary="$install_root/server/gda-server"
-workspace_loc="$install_root/server/dls-root/workspace"
-config_loc="$install_root/workspace_git/gda-dls-beamlines-xas.git/i18"
-load_java="$install_root/workspace_git/gda-diamond.git/dls-config/bin/loadjava.sh"
-args="-Dgda.mode=live"
+# Ensure directory containing workspaces exists, and is writable by everyone
+USER_WORKSPACE_PARENT=/scratch/gda_server_user_workspaces
+[ -d $USER_WORKSPACE_PARENT ] || mkdir -m 777 $USER_WORKSPACE_PARENT
 
+# Ensure directory containing configs exists, and is writable by everyone
+ECLIPSE_RUNTIME_CONFIG_PARENT=/scratch/gda_server_eclipse_configurations
+[ -d $ECLIPSE_RUNTIME_CONFIG_PARENT ] || mkdir -m 777 $ECLIPSE_RUNTIME_CONFIG_PARENT
+
+# Add the server shared location to the path
+module load gda-server/$BEAMLINE
+
+# Set server application specifics
+SERVER_INSTALL_DIRNAME=$(basename $(readlink -f $(dirname $(which gda-server))))
+SERVER_APPLICATION="gda-server"
+
+# Set user workspace and eclipse runtime configuration location (user and server build specific)
+USER_WORKSPACE=$USER_WORKSPACE_PARENT/$(whoami)
+ECLIPSE_RUNTIME_CONFIG_DIRNAME=$(whoami)-$SERVER_INSTALL_DIRNAME
+ECLIPSE_RUNTIME_CONFIG=$ECLIPSE_RUNTIME_CONFIG_PARENT/$ECLIPSE_RUNTIME_CONFIG_DIRNAME
+
+# Initialise the beamline specific command string components
+BEAMLINE_CONFIG="/dls_sw/$BEAMLINE/software/gda/config"
+
+# Initialise the java startup arguments
+args="-Dgda.mode=live"
 for word in "$@"; do
 	if [[ "${word}" == "debug" ]] || [[ ${SSH_ORIGINAL_COMMAND} == "debug" ]]; then
 		args="$args -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8001"
 	fi
 done
 
-# We will need java
-. $load_java
+# Add java location to the path
+module load java/gda90
+
 
 # Assemble the command string
-command="$server_binary -data $workspace_loc -config $config_loc -vmArgs $args"
+COMMAND="$SERVER_APPLICATION -data $USER_WORKSPACE -configuration $ECLIPSE_RUNTIME_CONFIG -c $BEAMLINE_CONFIG -vmArgs $args"
 
 # and execute it retaining stdin 
-$command &
+$COMMAND &
