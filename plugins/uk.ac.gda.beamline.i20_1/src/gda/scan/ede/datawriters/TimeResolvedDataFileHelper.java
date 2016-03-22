b9aba758a4d35d830d33cb5e4781c9d5a198808c
Peter Chang <peter.chang@diamond.ac.uk>
2016-03-22 09:43:37 +0000

    Refactor to move HDF object code to their own packages
    Change-Id: Ic63fbf295eafeb13bedf01ebbc4d1f42c4b5da52

--- plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/ede/datawriters/TimeResolvedDataFileHelper.java
+++ plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/ede/datawriters/TimeResolvedDataFileHelper.java
@@ -32,12 +32,12 @@ import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
 import org.eclipse.dawnsci.analysis.dataset.impl.DatasetUtils;
 import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
-import org.eclipse.dawnsci.hdf5.H5Utils;
-import org.eclipse.dawnsci.hdf5.HierarchicalDataFactory;
-import org.eclipse.dawnsci.hdf5.HierarchicalDataFileUtils;
-import org.eclipse.dawnsci.hdf5.IHierarchicalDataFile;
-import org.eclipse.dawnsci.hdf5.Nexus;
-import org.eclipse.dawnsci.hdf5.nexus.NexusUtils;
+import org.eclipse.dawnsci.hdf.object.H5Utils;
+import org.eclipse.dawnsci.hdf.object.HierarchicalDataFactory;
+import org.eclipse.dawnsci.hdf.object.HierarchicalDataFileUtils;
+import org.eclipse.dawnsci.hdf.object.IHierarchicalDataFile;
+import org.eclipse.dawnsci.hdf.object.Nexus;
+import org.eclipse.dawnsci.hdf.object.nexus.NexusUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
