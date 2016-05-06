7b05719eca1882f7124e22fd8897fea1f454cc0e
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-05-06 12:01:22 +0100

    I06-70 craete grateing instance with proper status return.

--- configurations/i06-config/scripts/BeamlineI06/usePGM.py
+++ configurations/i06-config/scripts/BeamlineI06/usePGM.py
@@ -5,6 +5,7 @@ print "Enable the Grating Control on PGM";
 
 gratingGetPV = 'BL06I-OP-PGM-01:NLINES.VAL';
 gratingSetPV = 'BL06I-OP-PGM-01:NLINES2.VAL';
-grating = PGM_GratingClass('grating', gratingGetPV, gratingSetPV);
+gratingMoveStatusPV="BL06I-OP-PGM-01:STATUS:MOVING";
+grating = PGM_GratingClass('grating', gratingGetPV, gratingSetPV,gratingMoveStatusPV);
 
 
