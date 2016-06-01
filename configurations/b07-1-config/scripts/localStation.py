1e3feb9b4e0efb809bdadc8e476b18f604f51e83
James Mudd <james.mudd@diamond.ac.uk>
2016-06-01 10:41:33 +0100

    Change to minimal localstation.py
    Change-Id: If3175f9d5ffcbd58b1ac4b07f401b92ff542eac7

--- configurations/b07-1-config/scripts/localStation.py
+++ configurations/b07-1-config/scripts/localStation.py
@@ -1,6 +1,4 @@
 # localStation.py
 # For beamline specific initialisation code.
 #
-from mapping_scan_commands import (
-    mscan, scan_request, submit, step, grid, circ, poly, rect)
-from detector_commands import mandelbrot
+print "Running b07-1 localstation"
