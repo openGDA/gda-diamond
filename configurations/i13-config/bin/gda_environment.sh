75c1763b28def4abd084c3d4069fc0c9b949bb7b
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-06-16 12:02:37 +0100

    Imaging: change environment variables to new format with underscores
    e.g. GDAVAR --> GDA_VAR

--- configurations/i13-config/bin/gda_environment.sh
+++ configurations/i13-config/bin/gda_environment.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 export BEAMLINE=i13
-export GDAFOLDER=/dls_sw/$BEAMLINE/software/gda
-export GDAVAR=/dls_sw/$BEAMLINE/software/gda_versions/var
-export GDALOGS=/dls_sw/$BEAMLINE/software/logs
-export GDAMODE=live
+
+export GDA_FOLDER=/dls_sw/$BEAMLINE/software/gda
+export GDA_VAR=/dls_sw/$BEAMLINE/software/gda_versions/var
+export GDA_LOGS=/dls_sw/$BEAMLINE/software/logs
 export GDA_MODE=live
 
-export PATH=${GDAFOLDER}/config/bin:${GDAFOLDER}/workspace_git/gda-diamond.git/dls-config/bin:${PATH}
-
+export PATH=${GDA_FOLDER}/config/bin:${GDA_FOLDER}/workspace_git/gda-diamond.git/dls-config/bin:${PATH}
