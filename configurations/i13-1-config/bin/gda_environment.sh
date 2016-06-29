c8da295b73de8dafe6cf79a07965e86294053893
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-06-29 10:59:20 +0100

    I13-131 Coherence: delete obsolete environment variables

--- configurations/i13-1-config/bin/gda_environment.sh
+++ configurations/i13-1-config/bin/gda_environment.sh
@@ -7,9 +7,3 @@ export GDA_LOGS=/dls_sw/$BEAMLINE/software/logs
 export GDA_MODE=live
 
 export PATH=${GDA_FOLDER}/config/bin:${GDA_FOLDER}/workspace_git/gda-diamond.git/dls-config/bin:${PATH}
-
-# TODO: delete these obsolete definitions in the next release (9.2)
-export GDAFOLDER=/dls_sw/$BEAMLINE/software/gda
-export GDAVAR=/dls_sw/$BEAMLINE/software/gda_versions/var
-export GDALOGS=/dls_sw/$BEAMLINE/software/logs
-export GDAMODE=live
