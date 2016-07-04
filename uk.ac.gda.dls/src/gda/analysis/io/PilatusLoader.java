e45f4c1a041a0c07831dca953ed7394fd4a5140c
Peter Chang <peter.chang@diamond.ac.uk>
2016-07-04 11:17:31 +0100

    Replace all external users of dataset constructors
    Change-Id: Ic9b7635cc5ee8d2074fede3fe29defb8a35b63f9

--- uk.ac.gda.dls/src/gda/analysis/io/PilatusLoader.java
+++ uk.ac.gda.dls/src/gda/analysis/io/PilatusLoader.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 
 import org.eclipse.dawnsci.analysis.api.io.ScanFileHolderException;
 import org.eclipse.dawnsci.analysis.api.monitor.IMonitor;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.analysis.dataset.impl.IntegerDataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -101,7 +102,7 @@ public class PilatusLoader extends AbstractFileLoader {
 			int size = height * width;
 
 			int[] dims = { height, width };
-			IntegerDataset data = new IntegerDataset(dims);
+			IntegerDataset data = DatasetFactory.zeros(IntegerDataset.class, dims);
 
 			// read in all the data at once for speed.
 			byte[] read = new byte[size * 4];
