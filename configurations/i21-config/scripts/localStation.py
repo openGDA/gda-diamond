5037b1c1173a17f12164c62395bbeda69811003a
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-07-14 12:31:52 +0100

    I21-49 also added standard useful commands

--- configurations/i21-config/scripts/localStation.py
+++ configurations/i21-config/scripts/localStation.py
@@ -9,8 +9,106 @@ from gda.device.scannable.scannablegroup import ScannableGroup
 
 from gdascripts.degas.degas import Degas
 
-global run
+from gda.data import PathConstructor, NumTracker
+from gda.jython.commands.GeneralCommands import alias, run
+from gda.jython.commands.GeneralCommands import pause as enable_pause_or_interrupt
+from gda.jython.commands.ScannableCommands import scan
+import os
 
+global run
+ 
+print "-----------------------------------------------------------------------------------------------------------------"
+print "Set scan returns to the original positions on completion to false (0); default is 0."
+print "   To set scan returns to its start positions on completion please do:"
+print "      >>>scansReturnToOriginalPositions=1"
+scansReturnToOriginalPositions=0;
+print
+
+print "-----------------------------------------------------------------------------------------------------------------"
+print "commands for directory/file operations: "
+print "   >>>pwd - return the current data directory"
+print "   >>>lwf - return the full path of the last working data file"
+print "   >>>nwf - return the full path of the next working data file"
+print "   >>>nfn - return the next data file number to be collected"
+print "   >>>setSubdirectory('test') - change data directory to a sub-directory named 'test', created first if not exist"
+print "   >>>getSubdirectory() - return the current sub-directory setting if exist"
+print "Please note: users can only create sub-directory within their permitted visit data directory via GDA, not themselves."
+print "To create another sub-directory 'child-test' inside a sub-directory 'test', you must specify the full path as 'test/child-test' "
+# set up a nice method for getting the latest file path
+i11NumTracker = NumTracker("i21");
+
+# function to find the working directory
+def pwd():
+    '''return the working directory'''
+    cwd = PathConstructor.createFromDefaultProperty()
+    return cwd
+    
+alias("pwd")
+
+# function to find the last working file path
+def lwf():
+    '''return the last working file path root'''
+    cwd = PathConstructor.createFromDefaultProperty()
+    filenumber = i11NumTracker.getCurrentFileNumber();
+    return os.path.join(cwd,str(filenumber))
+    
+alias("lwf")
+
+# function to find the next working file path
+def nwf():
+    '''query the next working file path root'''
+    cwd = PathConstructor.createFromDefaultProperty()
+    filenumber = i11NumTracker.getCurrentFileNumber();
+    return os.path.join(cwd,str(filenumber+1))
+    
+alias("nwf")
+
+# function to find the next scan number
+def nfn():
+    '''query the next file number or scan number'''
+    filenumber = i11NumTracker.getCurrentFileNumber();
+    return filenumber+1
+    
+alias("nfn")
+
+# the subdirectory parts
+def setSubdirectory(dirname):
+    '''create a new sub-directory for data collection that follows'''
+    finder.find("GDAMetadata").setMetadataValue("subdirectory",dirname)
+    try:
+        os.mkdir(pwd())
+    except :
+        pass
+    
+def getSubdirectory():
+    return finder.find("GDAMetadata").getMetadataValue("subdirectory")
+
+print
+
+from gda.factory import Finder
+from time import sleep  # @UnusedImport
+import java #@UnresolvedImport
+
+
+print
+print "-----------------------------------------------------------------------------------------------------------------"
+print "load EPICS Pseudo Device utilities for creating scannable object from a PV name."
+from gdascripts.pd.epics_pds import * #@UnusedWildImport
+print
+print "-----------------------------------------------------------------------------------------------------------------"
+print "load time utilities for creating timer objects."
+from gdascripts.pd.time_pds import * #@UnusedWildImport
+print
+print "-----------------------------------------------------------------------------------------------------------------"
+print "Load utilities: printJythonEnvironment(), caget(pv), caput(pv,value), attributes(object), "
+print "    iterableprint(iterable), listprint(list), frange(start,end,step)"
+from gdascripts.utils import * #@UnusedWildImport
+print
+print "-----------------------------------------------------------------------------------------------------------------"
+print "load common physical constants"
+from gdascripts.constants import * #@UnusedWildImport
+
+from gdascripts.scan.installStandardScansWithProcessing import * # @UnusedWildImport
 scan_processor.rootNamespaceDict=globals()
 gdascripts.scan.concurrentScanWrapper.ROOT_NAMESPACE_DICT = globals()
 
@@ -28,6 +126,13 @@ else:
     print "Running in dummy mode"
 
 simpleLog("================ INITIALISING I21 GDA ================")
+
+print "create camera total count scannables: d1camtotal, d2camtotal, d3acamtotal, d4camtotal"
+d1camtotal=DisplayEpicsPVClass('d1camtotal', 'BL21I-DI-DCAM-01:STAT:Total_RBV', 'counts', '%10d')
+d2camtotal=DisplayEpicsPVClass('d2camtotal', 'BL21I-DI-DCAM-02:STAT:Total_RBV', 'counts', '%10d')
+d3acamtotal=DisplayEpicsPVClass('d3acamtotal', 'BL21I-DI-DCAM-03:STAT:Total_RBV', 'counts', '%10d')
+d4camtotal=DisplayEpicsPVClass('d4camtotal', 'BL21I-DI-DCAM-04:STAT:Total_RBV', 'counts', '%10d')
+
 
 print "*"*80
 print "Attempting to run localStationUser.py from users script directory"
