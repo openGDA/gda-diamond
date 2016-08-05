4cea879658b7a1d10262777d0eabd08899d5f73c
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-08-05 10:56:26 +0100

    add jython look up table function

---
+++ configurations/i21-config/scripts/LookupTables.py
@@ -0,0 +1,15 @@
+import string
+    
+def readLookupTable(filename): 
+    '''
+    read in the lookup table from a tab-delimited data file. 
+    :param filename:
+    '''
+    f = open(filename, "r")
+    lines = f.readlines()
+    f.close()
+    lines = map(string.split, map(string.strip, lines))
+    lookuptable={}
+    for x in lines[3:]:
+        lookuptable[int(x[0])]=[float(item) for item in x[1:]]
+    return lookuptable
