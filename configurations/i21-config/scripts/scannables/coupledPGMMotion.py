f5ace70cbd978a8867f421f9ff7a6bb41528045c
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-08-30 11:01:14 +0100

    PGM coupled motion - updated by beamline staffs

--- configurations/i21-config/scripts/scannables/coupledPGMMotion.py
+++ configurations/i21-config/scripts/scannables/coupledPGMMotion.py
@@ -6,7 +6,7 @@ Created on 18 Jul 2016
 from gda.device.scannable import ScannableMotionBase
 from gdascripts.utils import caget
 from gdascripts.constants import clight, hPlanck, eV, pi
-from scisoftpy.jython.jymaths import arcsin, arccos, sqrt, cos
+from scisoftpy.jython.jymaths import arcsin, arccos, sqrt, cos, sin
 
 
 PV="BL21I-OP-PGM-01:ENERGY.RBV"
@@ -17,13 +17,12 @@ class CoupledPGMMotion(ScannableMotionBase):
     classdocs
     '''
 
-
-    def __init__(self, name, vpgGroup, pgmMirrorPitch1, pgmGratingPitch1):
+    def __init__(self, name, lineDensity1, pgmMirrorPitch1, pgmGratingPitch1):
         '''
         Constructor
         '''
         self.setName(name)
-        self.vpg=vpgGroup
+        self.lineDensity=lineDensity1
         self.mirrorPitch=pgmMirrorPitch1
         self.gratingPitch=pgmGratingPitch1
         self.setInputNames([name])
@@ -31,24 +30,83 @@ class CoupledPGMMotion(ScannableMotionBase):
         self.setOutputFormat(["%.5f"])
         
     def asynchronousMoveTo(self, cff):
-        B=self.wavelength()*self.vpg/(1-cff*cff)
+        B=self.energy2wavelength(self.energy())*self.lineDensity/(1-cff*cff)
         self.alpha=arcsin(B+sqrt(1+B*B*cff*cff))
         self.beta=arccos(cff*cos(self.alpha))
         theta=(self.alpha+self.beta)/2
-#        print self.alpha*180/pi, self.beta*180/pi, theta*180/pi
+        
+        #print self.alpha*180/pi, self.beta*180/pi, theta*180/pi
         self.mirrorPitch.asynchronousMoveTo(theta*180/pi)
         self.gratingPitch.asynchronousMoveTo(self.beta*180/pi)
     
     def getPosition(self):
-        return float(cos(self.beta)/cos(self.alpha))
+        #return float(cos(self.beta)/cos(self.alpha))
+        return float(cos(float(self.gratingPitch.getPosition())*pi/180)/cos((2*float(self.mirrorPitch.getPosition())-float(self.gratingPitch.getPosition()))*pi/180))
     
     def isBusy(self):
         return self.mirrorPitch.isBusy() or self.gratingPitch.isBusy()
     
-    def wavelength(self):
-        return hPlanck*clight/(energyfrompgm*eV)
+    def energy2wavelength(self, energy):
+        return hPlanck*clight/(energy*eV)
     
+    def wavelength2energy(self, wavelength):
+        return hPlanck*clight/(wavelength*eV)
+    
+    def energy(self):
+        wavelength=(-sin(float(self.gratingPitch.getPosition())*pi/180)+sin((2*float(self.mirrorPitch.getPosition())-float(self.gratingPitch.getPosition()))*pi/180))/self.lineDensity
+        return float(self.wavelength2energy(wavelength))
+    
+class pgmEnergyScannable(ScannableMotionBase):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, name, lineDensity1, pgmMirrorPitch1, pgmGratingPitch1):
+        '''
+        Constructor
+        '''
+        self.setName(name)
+        self.mirrorPitch=pgmMirrorPitch1
+        self.gratingPitch=pgmGratingPitch1
+        self.lineDensity=lineDensity1
+        self.setInputNames([name])
+        self.setExtraNames([])
+        self.setOutputFormat(["%.5f"])
+        
+    def asynchronousMoveTo(self, energy):
+        B=self.lineDensity*self.energy2wavelength(energy)/(1-self.cff()*self.cff())
+        alpha=arcsin(B+sqrt(1+(B*B*self.cff()*self.cff())))
+        beta=arccos(cos(alpha)*self.cff())
+        theta=(alpha+beta)/2
+        
+        #print alpha*180/pi, beta*180/pi, theta*180/pi
+        self.mirrorPitch.asynchronousMoveTo(theta*180/pi)
+        self.gratingPitch.asynchronousMoveTo(beta*180/pi)
+    
+    def getPosition(self):
+        wavelength=(-sin(float(self.gratingPitch.getPosition())*pi/180)+sin((2*float(self.mirrorPitch.getPosition())-float(self.gratingPitch.getPosition()))*pi/180))/self.lineDensity
+        return float(self.wavelength2energy(wavelength))
+    
+    def isBusy(self):
+        return self.mirrorPitch.isBusy() or self.gratingPitch.isBusy()
+    
+    def energy2wavelength(self, energy):
+        return hPlanck*clight/(energy*eV)
+    
+    def wavelength2energy(self, wavelength):
+        return hPlanck*clight/(wavelength*eV)
+    
+    def cff(self):
+        return float(cos(float(self.gratingPitch.getPosition())*pi/180)/cos((2*float(self.mirrorPitch.getPosition())-float(self.gratingPitch.getPosition()))*pi/180))
     
 cff1=CoupledPGMMotion("cff1", 600*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
 cff2=CoupledPGMMotion("cff2", 1000*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
 cff3=CoupledPGMMotion("cff3", 2000*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
+
+energycff1=pgmEnergyScannable("energycff1", 600*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
+energycff2=pgmEnergyScannable("energycff2", 1000*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
+energycff3=pgmEnergyScannable("energycff3", 2000*1000, pgmMirrorPitch, pgmGratingPitch)  # @UndefinedVariable
+
+energy1=BeamEnergy("energy1",idcontroller, idgap, energycff1,lut="IDCalibrationTable.txt")  # @UndefinedVariable
+energy2=BeamEnergy("energy2",idcontroller, idgap, energycff2,lut="IDCalibrationTable.txt")  # @UndefinedVariable
+energy3=BeamEnergy("energy3",idcontroller, idgap, energycff3,lut="IDCalibrationTable.txt")  # @UndefinedVariable
