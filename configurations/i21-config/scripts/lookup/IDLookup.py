fa2d867de0a5f4ae4c5b547ac56a2f61bd753f9c
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-09-05 14:57:55 +0100

    change object name to idlam stands for ID linear angular mode.

--- configurations/i21-config/scripts/lookup/IDLookup.py
+++ configurations/i21-config/scripts/lookup/IDLookup.py
@@ -4,7 +4,9 @@ Created on 30 Aug 2016
 @author: fy65
 '''
 from external import create_function
+#from scisoftpy.external import create_function
 from gda.device.scannable import ScannableBase
+from contextlib import contextmanager
 import time
 
 starttime=time.time()
@@ -12,7 +14,6 @@ starttime=time.time()
 lookup_file='/dls_sw/i21/software/gda/config/lookupTables/LinearAngle.csv'
 #Lookup module
 local_module_path='/dls_sw/i21/software/gda/pythonscript/lookupTable'
-java_pathsubprocess_path='/dls_sw/i21/software/gda_versions/gda_9.1/workspace_git/scisoft-core.git/uk.ac.diamond.scisoft.python/bin'
 
 # To solve the problem that PyDev/ Jython was passing JYTHONPATH as PYTHONPATH to the subprocess.
 # The fix was to load all the environment variables, change the Python Path to the correct spot for Python 2.7 and pass it to Popen through the env argument.
@@ -21,14 +22,17 @@ python_exe="/dls_sw/i21/software/miniconda2/envs/gdaenv/bin/python"
 
 class IDLookupScannable(ScannableBase):
     def __init__(self, name, gap=None, phase=None, lut=lookup_file):
-        self.lookupPolarEnergy=create_function("reverseLookup",module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path,java_pathsubprocess_path],  keep=True)
+        #TODO need to check and fix external process exit on termination so it does not left external process on after GDA stopped.
+        self.lookupPolarEnergy=create_function("reverseLookup",module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path], keep=True)
+        #self.lookupPolarEnergy=create_function("reverseLookup",module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path], dls_module=True, keep=True)
         self.lookupPolarEnergyCreatedInScanStart=False
-        self.lookupGapPhase=create_function("forwardLookup", module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path,java_pathsubprocess_path], keep=True)
+        self.lookupGapPhase=create_function("forwardLookup", module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path], keep=True)
+        #self.lookupGapPhase=create_function("forwardLookup", module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path], dls_module=True, keep=True)
         self.lookupGapPhaseCreatedInScanStart=False
         self.lut=lut
         self.gap=gap
         self.phase=phase
-        
+    
     def atScanStart(self):
         if self.lookupPolarEnergy.proc is None:
             self.lookupPolarEnergy=create_function("reverseLookup",module="Lookup2Dto2D", exe=python_exe, path=[python_path], extra_path=[local_module_path], dls_module=True, keep=True)
@@ -42,10 +46,12 @@ class IDLookupScannable(ScannableBase):
              
     def getEnergyPolarisation(self, gap, phase):
         out=self.lookupGapPhase(gap, phase, filename=self.lut)
+#         return out # used in scisoftpy
         return out.strip('\n')
     
     def getGapPhase(self, energy, polarisation):
         out=self.lookupPolarEnergy(polarisation, energy, filename=self.lut)
+#         return out # used in scisoftpy
         return out.strip('\n')
     
     def getPosition(self):
@@ -76,25 +82,25 @@ class IDLookupScannable(ScannableBase):
         if self.lookupGapPhaseCreatedInScanStart==True:
             self.lookupGapPhase.stop()
         
-energypolar=IDLookupScannable("energypolar", lut=lookup_file)
+idlam=IDLookupScannable("energypolar", lut=lookup_file) #a scannable for ID linear Angular Mode
 endtime=time.time()
-print (endtime-starttime)
+print "time taken for Initialisation: %s" % (endtime-starttime)
 
 def main():
     print
     for gap, phase in [(20, 0), (25, 5), (30, 10), (35, 15), (40, 20), (45, 25), (50, 28), (55, 23), (60, 17), (65, 13), (70, 8)]:
         starttime=time.time()
-        print energypolar.getEnergyPolarisation(gap,phase)
+        print idlam.getEnergyPolarisation(gap,phase)
         endtime= time.time()
-        print (endtime-starttime)
+        print "time taken: %s" % (endtime-starttime)
     
     print 
     
     for energy, polar in [(300, 0), (400, -10), (500, -20), (600, -30), (700, -40), (800, -50), (900, -60), (1000, -70), (1100, -80)]:
         starttime=time.time()
-        print energypolar.getGapPhase(energy, polar)
+        print idlam.getGapPhase(energy, polar)
         endtime= time.time()
-        print (endtime-starttime)
+        print "time taken: %s" % (endtime-starttime)
     
 
 if __name__=="__main__":
