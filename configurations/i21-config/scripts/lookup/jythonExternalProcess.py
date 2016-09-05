1a5ecceae5c1b6866251d214056c5365cd5dde18
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2016-09-05 10:25:30 +0100

    run external python process as command, i.e. it start and stop the external command every call - not perform efficiently, but a good realiable alternative if external function call fails.

--- configurations/i21-config/scripts/lookup/jythonExternalProcess.py
+++ configurations/i21-config/scripts/lookup/jythonExternalProcess.py
@@ -1,5 +1,5 @@
 '''
-This module provides functions to support ID lookup table for Linear Angular mode. It calls an external python process to perform the actual lookup which requires access to scipy and numpy libraries.
+This module provides functions to support ID lookupTable table for Linear Angular mode. It calls an external python process to perform the actual lookupTable which requires access to scipy and numpy libraries.
 A special 'gdaenv' is created as a virtual environment to run this external process at /dls_sw/i21/software/miniconda2/envs/gdaenv/
 Created on 25 Aug 2016
 
@@ -8,6 +8,12 @@ Created on 25 Aug 2016
 import subprocess
 from subprocess import PIPE
 import os
+import time
+
+#The lookupTable table
+lookup_file='/dls_sw/i21/software/gda/config/lookupTables/LinearAngle.csv'
+#Lookup module
+lookup_module_dir='/dls_sw/i21/software/gda/pythonscript'
 
 # To solve the problem that PyDev/ Jython was passing JYTHONPATH as PYTHONPATH to the subprocess.
 # The fix was to load all the environment variables, change the Python Path to the correct spot for Python 2.7 and pass it to Popen through the env argument.
@@ -16,16 +22,41 @@ my_env = os.environ
 my_env["PYTHONPATH"] = python_path
 
 def lookupGapPhase(gap, rowphase):
-    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "gaprowphase", str(gap), str(rowphase)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd='/dls_sw/i21/software/gda/workspace/i21-python/src/lookup', env=my_env).communicate()
+    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "-f", lookup_file, "gaprowphase", str(gap), str(rowphase)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=lookup_module_dir, env=my_env).communicate()
     if stderrdata!='':
         print stderrdata
     return stdoutdata.strip('\n')
 
 def lookupPolarisationEnergy(pol, energy):
-    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "polarisationenergy", str(pol), str(energy)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd='/dls_sw/i21/software/gda/workspace/i21-python/src/lookup', env=my_env).communicate()
+    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "-f", lookup_file, "polarisationenergy", str(pol), str(energy)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=lookup_module_dir, env=my_env).communicate()
     if stderrdata!='':
         print stderrdata
     return stdoutdata.strip('\n')
 
+start=time.time()
 print lookupGapPhase(25, 17)
+end=time.time()
+print "time taken for lookup 1st gap and rowphase: %s" % (end-start)
+start=time.time()
+print lookupGapPhase(60, 20)
+end=time.time()
+print "time taken for lookup 2nd gap and rowphase: %s" % (end-start)
+start=time.time()
+print lookupGapPhase(45, 10)
+end=time.time()
+print "time taken for lookup 3rd gap and rowphase: %s" % (end-start)
+
+print
+
+start=time.time()
 print lookupPolarisationEnergy(-50, 1000)
+end=time.time()
+print "time taken for lookup first polarisation and energy: %s" % (end-start)
+start=time.time()
+print lookupPolarisationEnergy(-30, 800)
+end=time.time()
+print "time taken for lookup 2nd polarisation and energy: %s" % (end-start)
+start=time.time()
+print lookupPolarisationEnergy(-15, 450)
+end=time.time()
+print "time taken for lookup 3rd polarisation and energy: %s" % (end-start)
