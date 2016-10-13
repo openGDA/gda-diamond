490fe26713d6b91d26a4c3aee13e766fb75ebb8c
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-10-13 11:50:03 +0100

    Imaging: add scannables related to metadata & XGI tomography

--- configurations/i13-config/scripts/i13i_utilities.py
+++ configurations/i13-config/scripts/i13i_utilities.py
@@ -13,6 +13,8 @@ from gda.epics import CAClient
 from gda.device.scannable.scannablegroup import ScannableGroup
 from epics_scripts.pv_scannable_utils import createPVScannable, caput, caget, caputStringAsWaveform
 from gda.configuration.properties import LocalProperties
+from gda.device.scannable import EpicsScannable
+from gda.jython import InterfaceProvider
 
 print "Running i13i_utilities.py..."
 
@@ -203,6 +205,51 @@ def clear_meta_data():
     return all_lst, all_kv_lst
 alias("clear_meta_data")
 
+def createScannableFromPV( name, pv, addToNameSpace=True, getAsString=True, hasUnits=False):
+    """
+    Description:
+        Utility function to create a scannable from a given PV
+    Arguments:
+        name - user-specified name of a scannable to be created, e.g. pixium10_DataType
+        pv - EPICS identifier of pv to be used by the scannable, e.g. BL12I-EA-DET-10:CAM:DataType
+        addToNameSpace = if True, the scannable is accessible from the commandline after the call
+        getAsString - If True, output value is a string (useful for enum pv, in which case set getAsString=True, and set hasUnits=False)
+        hasUnits - If False, output value is not converted to units - useful for enum pv with getAsString=True
+    
+    For example,
+        createScannableFromPV("pixium10_DataType", "BL12I-EA-DET-10:CAM:DataType", True, True, False)
+    creates a scannable for pv with the following enums:
+    caget -d 31 BL12I-EA-DET-10:CAM:DataType
+        BL12I-EA-DET-10:CAM:DataType
+        Native data type: DBF_ENUM
+        Request type:     DBR_CTRL_ENUM
+        Element count:    1
+        Value:            UInt32
+        Status:           NO_ALARM
+        Severity:         NO_ALARM
+        Enums:            ( 8)
+                          [ 0] Int8
+                          [ 1] UInt8
+                          [ 2] Int16
+                          [ 3] UInt16
+                          [ 4] Int32
+                          [ 5] UInt32
+                          [ 6] Float32
+                          [ 7] Float64
+    """
+    sc = EpicsScannable()
+    sc.setName(name)
+    sc.setPvName(pv)
+    sc.setUseNameAsInputName(True)
+    sc.setGetAsString(getAsString)
+    sc.setHasUnits(hasUnits)
+    sc.afterPropertiesSet()
+    sc.configure()
+    if addToNameSpace:
+        commandServer = InterfaceProvider.getJythonNamespace()
+        commandServer.placeInJythonNamespace(name,sc)
+    return sc
+
 class StringDisplayEpicsPVClass(ScannableMotionBase):
     '''Create PD to display single EPICS PV which returns a string (label)'''
     def __init__(self, name, pvstring):
@@ -284,14 +331,24 @@ pco_edge_focus = StringDisplayEpicsPVClass("focus_pco_edge", "BL13I-MO-STAGE-02:
 pco_4000_focus_label = StringDisplayEpicsPVClass("focus_pco_4000_label", "BL13I-MO-STAGE-02:FOCUS:MP:RBV:CURPOS")
 pco_4000_focus = StringDisplayEpicsPVClass("focus_pco_4000", "BL13I-MO-STAGE-02:FOCUS.RBV")
 
-filter_stick_1 = StringDisplayEpicsPVClass("filter_stick_1", "BL13I-OP-ATTN-01:STICK1:MP:RBV:CURPOS")
-filter_stick_2 = StringDisplayEpicsPVClass("filter_stick_2", "BL13I-OP-ATTN-01:STICK2:MP:RBV:CURPOS")
-filter_stick_3 = StringDisplayEpicsPVClass("filter_stick_3", "BL13I-OP-ATTN-01:STICK3:MP:RBV:CURPOS")
-filter_stick_4 = StringDisplayEpicsPVClass("filter_stick_4", "BL13I-OP-ATTN-01:STICK4:MP:RBV:CURPOS")
-filter_stick_5 = StringDisplayEpicsPVClass("filter_stick_5", "BL13I-OP-ATTN-01:STICK5:MP:RBV:CURPOS")
+#filter_stick_1 = StringDisplayEpicsPVClass("filter_stick_1", "BL13I-OP-ATTN-01:STICK1:MP:RBV:CURPOS")
+#filter_stick_2 = StringDisplayEpicsPVClass("filter_stick_2", "BL13I-OP-ATTN-01:STICK2:MP:RBV:CURPOS")
+#filter_stick_3 = StringDisplayEpicsPVClass("filter_stick_3", "BL13I-OP-ATTN-01:STICK3:MP:RBV:CURPOS")
+#filter_stick_4 = StringDisplayEpicsPVClass("filter_stick_4", "BL13I-OP-ATTN-01:STICK4:MP:RBV:CURPOS")
+#filter_stick_5 = StringDisplayEpicsPVClass("filter_stick_5", "BL13I-OP-ATTN-01:STICK5:MP:RBV:CURPOS")
+filter_stick_1 = createScannableFromPV("filter_stick_1", "BL13I-OP-ATTN-01:STICK1:MP:RBV:CURPOS", addToNameSpace=True, getAsString=True, hasUnits=False)
+filter_stick_2 = createScannableFromPV("filter_stick_2", "BL13I-OP-ATTN-01:STICK2:MP:RBV:CURPOS", addToNameSpace=True, getAsString=True, hasUnits=False)
+filter_stick_3 = createScannableFromPV("filter_stick_3", "BL13I-OP-ATTN-01:STICK3:MP:RBV:CURPOS", addToNameSpace=True, getAsString=True, hasUnits=False)
+filter_stick_4 = createScannableFromPV("filter_stick_4", "BL13I-OP-ATTN-01:STICK4:MP:RBV:CURPOS", addToNameSpace=True, getAsString=True, hasUnits=False)
+filter_stick_5 = createScannableFromPV("filter_stick_5", "BL13I-OP-ATTN-01:STICK5:MP:RBV:CURPOS", addToNameSpace=True, getAsString=True, hasUnits=False)
 
-xray_mode = StringDisplayEpicsPVClass("xray_mode", "BL13I-OP-DCM-01:MODE:RBV")
+#beamline_xray_mode = StringDisplayEpicsPVClass("beamline_xray_mode", "BL13I-OP-DCM-01:MODE:RBV")
+beamline_xray_mode = createScannableFromPV("beamline_xray_mode", "BL13I-OP-DCM-01:MODE:RBV", addToNameSpace=True, getAsString=True, hasUnits=False)
 
+ionc_A_over_V_gain = createScannableFromPV("ionc_A_over_V_gain", "BL13I-DI-FEMTO-06:GAINHIGHSPEED", addToNameSpace=True, getAsString=True, hasUnits=False)
+ionc_gainmode = createScannableFromPV("ionc_gainmode", "BL13I-DI-FEMTO-06:GAINMODE", addToNameSpace=True, getAsString=True, hasUnits=False)
+ionc_acdc = createScannableFromPV("ionc_acdc", "BL13I-DI-FEMTO-06:ACDC", addToNameSpace=True, getAsString=True, hasUnits=False)
+    
 try:
     pco_edge_agg = ScannableGroup()
     pco_edge_agg.setName("pco_edge_agg")
@@ -335,4 +392,52 @@ def isLive():
     mode = LocalProperties.get("gda.mode")
     return mode =="live" or mode =="live_localhost"
 
+
+
+def stressTest(nScans=100, pathToVisit="/dls/i13/data/2016/cm14467-4", filesys="na"):
+    """
+    Function to run a sequence of tomography fly scans to test the response of the file system. 
+    
+    Args:
+        nScans - initial number of scans, which is then repeated ad infinitum
+        pathToVisit - path to the visit directory
+        filesys - "na" for NetApp and "gpfs" for GPFS01
+         
+    Notes: 
+        (1) all scan files will be saved in the tmp sub-directory of the input visit directory
+        (2) execute reportTomo to see which stages and detectors will be used in these scans
+        (3) it is recommended to use a dummy tomography_translation
+        (4) make sure that Command Queue is not paused! 
+    """
+    print "Executing stressTest..."
+    datadir_saved = LocalProperties.get("gda.data.scan.datawriter.datadir")
+    
+    windowsSubString_dct = {"na": "d:\\i13\\data\\", "gpfs": "t:\\i13\\data\\"}
+    flyScanDetectorNoChunking = finder.find("flyScanDetectorNoChunking")
+    #windowsSubString_saved = flyScanDetectorNoChunking.pluginList[1].ndFileHDF5.file.filePathConverter.getWindowsSubString()
+    #print "windowsSubString_saved = %s" %(windowsSubString_saved)
+    print "Setting windowsSubString to %s..." %(windowsSubString_dct[filesys])
+    flyScanDetectorNoChunking.pluginList[1].ndFileHDF5.file.filePathConverter.setWindowsSubString(windowsSubString_dct[filesys])
+    print "Current windowsSubString = %s" %(flyScanDetectorNoChunking.pluginList[1].ndFileHDF5.file.filePathConverter.getWindowsSubString())
+    
+    title_saved = getTitle()
+    setTitle("stressTest")
+    try:
+        cqp=finder.find("commandQueueProcessor")
+        datadir = os.path.join(pathToVisit, "tmp")
+        LocalProperties.set("gda.data.scan.datawriter.datadir", datadir)    # this does not seem to work from inside this fn as files go to raw, not tmp!
+        LocalProperties.set("gda.data", datadir)                            # this does not seem to work from inside this fn as files go to raw, not tmp!
+        print "All scan files will be saved in %s" %(LocalProperties.get("gda.data.scan.datawriter.datadir"))
+        for i in range(nScans):
+            cmd="tomographyScan.tomoFlyScan(2.3,2.3,exposureTime=.01, start=-90, stop=90., step=0.1, imagesPerDark=0, imagesPerFlat=0, setupForAlignment=False)"
+            cqp.addToTail(JythonCommandCommandProvider(cmd, "StressTest "+`i`,None))
+        cqp.addToTail(JythonCommandCommandProvider("stressTest()", "Add stressTest to the queue",None))
+    except Exception, e:
+        print "Trouble running stressTest: " +str(e)
+    finally:
+        LocalProperties.set("gda.data.scan.datawriter.datadir", datadir_saved)
+        setTitle(title_saved)
+        flyScanDetectorNoChunking.pluginList[1].ndFileHDF5.file.filePathConverter.setWindowsSubString(windowsSubString_dct[filesys])
+    print "Finished executing stressTest - bye!"
+    
 print "Finished running i13i_utilities.py!"
