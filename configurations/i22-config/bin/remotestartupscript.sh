509556c16aea4dabba3595870901fa2f2e795fcd
Peter Holloway <peter.holloway@diamond.ac.uk>
2016-11-18 11:14:56 +0000

    I22-413 Add files/folders for new config

--- configurations/i22-config/bin/remotestartupscript.sh
+++ configurations/i22-config/bin/remotestartupscript.sh
@@ -1,44 +1,27 @@
-#! /bin/bash
+#!/bin/bash
+# This script is only invoked when user gda2 ssh's to the control machine. It is run by an entry in gda's ~/.ssh/authorized_keys
 
-export BEAMLINE=i22
+here_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# This script is run as a single command by ssh, so we need to set up our environment
+# See http://stackoverflow.com/questions/216202/why-does-an-ssh-remote-command-get-fewer-environment-variables-then-when-run-man
 . /usr/share/Modules/init/bash
 
-. /dls_sw/$BEAMLINE/software/gda/workspace_git/gda-diamond.git/dls-config/bin/loadjava.sh
+# There is no user or screen to prompt or display pop-ups
+export GDA_NO_PROMPT=true
 
-mv -f nohup.out nohup.out.0 || true
-touch nohup.out
+# Set an environment variable to indicate we came through the remote startup script, so that we can error if we attempt to do this recursively
+export GDA_IN_REMOTE_STARTUP=true
 
-( 
-umask 2
-
-OBJECT_CONSOLE_LOG_FILE="/dls_sw/$BEAMLINE/logs/gda_server_console.$(date +%F_%T).log"
-
-/dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config --stop logserver || true
-nohup /dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config nameserver >> $OBJECT_CONSOLE_LOG_FILE
-nohup /dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config eventserver >> $OBJECT_CONSOLE_LOG_FILE
-nohup /dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config logserver >> $OBJECT_CONSOLE_LOG_FILE
-nohup /dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config --properties=/dls_sw/$BEAMLINE/software/gda/config/properties/java.properties.clientlogserver logserver >> $OBJECT_CONSOLE_LOG_FILE
-#ssh -o PreferredAuthentications=publickey -i ~/.ssh/id_rsa marccd@i22-mar01 /dls_sw/i22/software/gda/bin/startmar.sh &
-JAVA_OPTS="-Duser.timezone=GMT -Xms1024m -Xmx8192m -XX:PermSize=256m -XX:MaxPermSize=512m" nohup /dls_sw/$BEAMLINE/software/gda/bin/gda --config=/dls_sw/$BEAMLINE/software/gda/config --debug --verbose objectserver >> $OBJECT_CONSOLE_LOG_FILE
-) &
-
-cat >> /dls_sw/$BEAMLINE/logs/gda_server.log <<EOF
-
-gda server restart
-
-EOF
-
-## show log until 'Server initialisation complete' is seen
-PIP=/tmp/`basename $0`-$$
-mknod $PIP p
-tail -n 1 -f /dls_sw/$BEAMLINE/logs/gda_server.log >  $PIP &
-awk '{
-        if (!/DEBUG/) print ;
-        if (/gda.util.ObjectServer - Server initialisation complete.*i22_server_beans/) {
-                print "\nAll done, you can start the client now\n" ;
-                exit ;
-        }
-}' < $PIP
-rm $PIP
-
-sleep 6
+if [[ -n "${SSH_ORIGINAL_COMMAND}" ]]; then
+	if [[ "${SSH_ORIGINAL_COMMAND}" == *"restart"* ]]; then
+		SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/restart/--restart}"
+	elif [[ "${SSH_ORIGINAL_COMMAND}" == *"start"* ]]; then
+		SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/start/--start}"
+	elif [[ "${SSH_ORIGINAL_COMMAND}" == *"stop"* ]]; then
+		SSH_ORIGINAL_COMMAND="${SSH_ORIGINAL_COMMAND/stop/--stop}"
+	fi
+	${here_dir}/gda  ${SSH_ORIGINAL_COMMAND} --mode=live servers
+else
+	${here_dir}/gda --restart --mode=live servers
+fi
