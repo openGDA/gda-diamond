ec1151ad7d4f293bb07bd4327e36aff0f9c92e70
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-11-23 12:39:53 +0000

    Coherence: set motor limits in Diffcalc startup script

--- configurations/i13-1-config/scripts/startup_diffcalc.py
+++ configurations/i13-1-config/scripts/startup_diffcalc.py
@@ -8,3 +8,25 @@ if not 'en' in globals():
 
 print 'Running fivecircle example'
 run('example/fivecircle')
+
+setmax(delta, 24)
+
+setmin(delta, -1)
+
+setmin(gam, 0)
+
+setmax(gam, 15)
+
+setmin(eta, -10)
+
+setmax(eta, 10)
+
+setmin(chi, 75)
+
+setmax(chi, 105)
+
+setmin(phi, -88)
+
+setmax(phi, 88)
+
+hardware()
