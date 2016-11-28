b8f151ef9c973a1e518417d7ac136dbfb8388384
Iain Hall <iain.hall@diamond.ac.uk>
2016-11-28 10:57:06 +0000

    I20-122 : Fix NeXuS file writing in gda 8.58
    NexusGroupData.isDetectorEntryData is now explicitly set to true for detector data in order to have the data written correctly into the Nexus file. 
    A new function 'extractDataFromNexusGroup' has been added to ScanDataHelper, 
    
    Change-Id: Ie8faa87287d2c51633a33da3dcaba6b4f5a681e5

--- plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/ede/datawriters/ScanDataHelper.java
+++ plugins/uk.ac.gda.beamline.i20_1/src/gda/scan/ede/datawriters/ScanDataHelper.java
@@ -21,7 +21,11 @@ package gda.scan.ede.datawriters;
 import java.util.Arrays;
 import java.util.Vector;
 
+import org.eclipse.dawnsci.analysis.dataset.impl.Dataset;
+import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
 import org.eclipse.dawnsci.analysis.dataset.impl.DoubleDataset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gda.data.nexus.extractor.NexusExtractor;
 import gda.data.nexus.extractor.NexusGroupData;
@@ -29,6 +33,8 @@ import gda.device.detector.NXDetectorData;
 import gda.scan.ScanDataPoint;
 
 public class ScanDataHelper {
+	private static final Logger logger = LoggerFactory.getLogger(ScanDataHelper.class);
+
 	public static DoubleDataset extractDetectorDataFromSDP(String detectorName, ScanDataPoint sdp) {
 		return extractDetectorDataFromSDP(detectorName, sdp, false);
 	}
@@ -43,8 +49,21 @@ public class ScanDataHelper {
 		NXDetectorData detData = (NXDetectorData) data.get(detIndex);
 		String dataType = isEnergy? EdeDataConstants.ENERGY_COLUMN_NAME : EdeDataConstants.DATA_COLUMN_NAME;
 		NexusGroupData groupData = detData.getData(detectorName, dataType, NexusExtractor.SDSClassName);
-		double[] originalData = (double[]) groupData.getBuffer();
-		return new DoubleDataset(Arrays.copyOf(originalData, originalData.length), originalData.length);
+		return extractDataFromNexusGroup(groupData);
+	}
+
+	public static DoubleDataset extractDataFromNexusGroup(NexusGroupData groupData) {
+		Dataset dataset = DatasetFactory.createFromObject(groupData.getBuffer());
+		// Generate a warning if there's a problem creating the Dataset or the shape does not match original data
+		if (dataset==null) {
+			logger.warn("Nexus data to could not be converted to Dataset");
+		} else {
+			boolean shapeOk = Arrays.equals(groupData.getDimensions(), dataset.getShape());
+			if (!shapeOk) {
+				logger.warn("Problem converting from Nexus data to Dataset : shape does not match original Nexus data.");
+			}
+		}
+		return (DoubleDataset) dataset;
 	}
 
 	public static int getIndexOfMyDetector(String detectorName, ScanDataPoint scanDataPoint) {
