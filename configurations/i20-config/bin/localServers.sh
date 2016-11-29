5b4459cd96780ca13db74a1e9dbf481620ebfc70
Matthew Webber <matthew.webber@diamond.ac.uk>
2016-11-29 15:21:00 +0000

    [DAQ-395] Remove configuration for the stand-alone GDA Synoptic product
    Change-Id: Ib4b87b98dde325d9e51fd21e03f8338e20f6edbe

--- configurations/i20-config/bin/localServers.sh
+++ configurations/i20-config/bin/localServers.sh
@@ -1,2 +1,2 @@
-export JAVA_HOME=/usr
-/gda_workspaces/master_git/gda-core.git/uk.ac.gda.core/bin/gda servers --datadir /scratch/users/data  --config  /gda_workspaces/master_git/gda-dls-beamlines-xas.git/i20 --properties /gda_workspaces/master_git/gda-dls-beamlines-xas.git/i20/properties/dummy/synoptic.properties --mode=dummy --debug -p 8001 --restart
+xport JAVA_HOME=/usr
+/gda_workspaces/master_git/gda-core.git/uk.ac.gda.core/bin/gda servers --datadir /scratch/users/data  --config  /gda_workspaces/master_git/gda-dls-beamlines-xas.git/i20 --properties --mode=dummy --debug -p 8001 --restart
