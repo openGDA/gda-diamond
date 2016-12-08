26de409fe51dcd3445bc11bba36635f97169f622
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-12-08 13:41:28 +0000

    I08-18 move log panel startup to gda_*_pre_instance; delete old config files
    Change-Id: I3ef3245da0b7d35baf51bd16e350735b0cbd3571

--- configurations/i08-config/bin/old_remotestartupscript.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script is only invoked when user gda2 ssh's to the control machine. It is run by an entry in gda's ~/.ssh/authorized_keys
-$GDA_INSTANCE_CONFIG/etc/live/remotestartupscript.sh
-
-# the /localhome/gda2/.ssh/authorized_keys on i08-control file needs updating in cf_engine to call the above script directly and then delete this script
