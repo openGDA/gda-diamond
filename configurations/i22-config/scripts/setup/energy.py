54e754b7365c14a5d83b82da6b1880d6f69c7a05
Peter Holloway <peter.holloway@diamond.ac.uk>
2016-12-19 15:26:20 +0000

    Remove unused classes in i22 energy.py

--- configurations/i22-config/scripts/setup/energy.py
+++ configurations/i22-config/scripts/setup/energy.py
@@ -1,54 +1,8 @@
-import java
-import gda.device.scannable.ScannableBase
-from math import sin, cos, asin, acos, sqrt, exp, degrees
-from gdascripts.pd.time_pds import tictoc
-from gda.device.scannable import ScannableMotionBase
-from gda.epics import CAClient
-from gda.device import DeviceException
 
-class BraggInkeV(gda.device.scannable.PseudoDevice,gda.observable.IObserver):
 
-	"""
-	    Purpose:       To enable energy in keV to be supplied instead of Bragg
-                           This script controls all the necessary motors including id gap, bragg, perp
-	"""
 
-	def __init__(self, name, bragg):
-		self.name = name
-		self.setInputNames([name])
-		self.bragg = bragg
-		'''
-		self.slope = 1.0006                # April 2013, coefficient changed to slope
-		self.intercept = -0.0383            # April 2013, intercept added
-		self.slope = 0.9990536                # November 2013
-		self.intercept = -0.0083482            # November 2013
-		self.slope = 1.0002945               # September 2014 - Direct calibration of EXAFS features in Bragg angle
-		self.intercept = -0.02577            # September 2014 - Direct calibration of EXAFS features in Bragg angle
-		self.slope = 1.0013725				# Jun 2015 - Direct calibration of EXAFS in Bragg
-		self.intercept = -0.03556			# Jun 2015 - Direct calibration of EXAFS in Bragg
-		'''
-		self.slope = 1.00
-		self.intercept = 0.0
-		self.bragg.addIObserver(self)
 
-	def update(self, observed, change):
-		self.notifyIObservers(self, change)
 
-	def isBusy(self):
-		return self.bragg.isBusy()
-
-	def getPosition(self):
-		""" Return the keV value"""
-		X=float(self.bragg.getPosition())
-		exp = (X-self.intercept) / self.slope 
-		return 12.3985/(6.2695*sin(exp*3.14159265/180)) 
-
-	def asynchronousMoveTo(self,X):
-		""" Moves to the keV value supplied """
-		theta = 180/3.14159265*asin(12.3985/(X*6.2695))
-		thetaExp = self.slope*theta+self.intercept
-		self.bragg.asynchronousMoveTo(thetaExp)
-		
 class Harmonic:
 	""" Constructor method give the initial values
 	Harmonic peak positions from undulator spectra are fitted with a cubic function
@@ -157,74 +111,6 @@ class CalibratedID(gda.device.scannable.PseudoDevice):
 			energy_position = energy_position + step
 		print "Done"
 
-class CalibratedPerp(gda.device.scannable.PseudoDevice):
-	"""
-	    Purpose: To move perp at the right value when changing the energy. THE DCM needs to be calibrated first with foils.
-	"""
-	
-	'''
-	Values from prior to Feb '14
-	intercept = -1.8628
-	slope = 14.7664
-	
-	Values from prior to Nov '14
-	intercept = -0.4829
-	slope = 12.9295
-	'''
-
-	def __init__(self, name, perp):
-		self.name = name
-		self.setInputNames([name])
-		self.perp = perp
-		self.intercept = -0.32111
-		self.slope = 12.78805
-		
-	def isBusy(self):
-		""" This device is busy if perp is moving """
-		return self.perp.isBusy()
-
-	def getPosition(self):
-		""" Return the perp value"""
-		perp_position = float(self.perp.getPosition())
-		return perp_position
-	
-	def asynchronousMoveTo(self,X):
-		""" Moves to the perp value according to the energy supplied """
-		
-		costheta = cos(asin(12.3985/(X*6.2712)))
-		self.perp.asynchronousMoveTo(self.slope/costheta+self.intercept)
-		
-		
-class CalibratedPitch(gda.device.scannable.PseudoDevice):
-	"""
-	    Purpose: To move pitch at the right value when changing the energy. THE DCM needs to be calibrated first with foils.
-	"""
-
-	def __init__(self, name, pitch):
-		self.name = name
-		self.setInputNames([name])
-		self.pitch = pitch
-		self.intercept = 186.1418
-		self.coefficient = 17.0967
-		
-	def isBusy(self):
-		""" This device is busy if pitch is moving """
-		return self.pitch.isBusy()
-
-	def getPosition(self):
-		""" Return the pitch value"""
-		pitch_position = float(self.pitch.getPosition())
-		energy = exp((-pitch_position - self.intercept)/self.coefficient)
-		return energy 
-
-	def asynchronousMoveTo(self,X):
-		""" Moves to the perp value according to the energy supplied """
-		#self.pitch.moveTo(self.pitch.getPosition()-20)
-		self.pitch.moveTo(-1*(self.coefficient*log(X)+self.intercept)+20)
-		sleep(2)
-		self.pitch.asynchronousMoveTo(-1*(self.coefficient*log(X)+self.intercept))
-
-
 class PilatusThreshold(ScannableMotionBase):
 	def __init__(self, name, pvbase):
 		self.setName(name);
@@ -274,11 +160,7 @@ class PilatusThreshold(ScannableMotionBase):
 
 pilthres = PilatusThreshold("pilthres", "BL22I-EA-PILAT-01:CAM")
 pilthresWAXS_L = PilatusThreshold("pilthresWAXS_L", "BL22I-EA-PILAT-03:CAM")
-calibrated_perp = CalibratedPerp("calibrated_perp", dcm_perp)
 calibrated_ID = CalibratedID("calibrated_ID", idgap_mm)
-bkeV = BraggInkeV("bkeV", dcm_bragg)
-bkeV.setProtectionLevel(3)
-energy.setBragg(bkeV)
 energy.clearScannables()
-for i in [calibrated_ID, calibrated_perp, pilthres, pilthresWAXS_L]:
+for i in [calibrated_ID, pilthres, pilthresWAXS_L]:
 	energy.addScannable(i)
