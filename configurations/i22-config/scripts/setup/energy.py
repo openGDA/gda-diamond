9666f7e052b2dedcc78210c7da962ff291b5d076
Peter Holloway <peter.holloway@diamond.ac.uk>
2016-12-19 15:52:10 +0000

    Use new PilatusThreshold to prevent errors when detectors are unavailable

--- configurations/i22-config/scripts/setup/energy.py
+++ configurations/i22-config/scripts/setup/energy.py
@@ -1,7 +1,61 @@
+from java.lang import Double
 
+THRESHOLD_ERROR = 'Cannot access %s: check detector is connected and run ' +\
+		'%s.configure()'
 
+class PilatusThreshold(ScannableMotionBase):
+	def __init__(self, name, pvbase):
+		self.setName(name);
+		self.setInputNames([name])
+		self.setExtraNames([])
+		self.Units=['keV']
+		self.setOutputFormat(['%4.2f'])
+		self.setLevel(7)
+		self.timer=tictoc()
+		self.waittime = 3
+		self.thresholdtolerance = 0.1
+		self.pvbase = pvbase
+		self.configure()
 
+	def configure(self):
+		try:
+			self.thres = CAClient(self.pvbase+":ThresholdEnergy")
+			self.thres.configure()
+			self.thres.caget()
+		except:
+			print THRESHOLD_ERROR % (self.name, self.name)
+			self.thres = DummyThreshold(self.name)
 
+	def rawGetPosition(self):
+		try:
+			return float(self.thres.caget()) * 2.0
+		except:
+			self.thres.clearup()
+			self.thres = DummyThreshold(self.name)
+			return self.thres.caget()
+
+	def rawAsynchronousMoveTo(self,newpos):
+		thres = float(self.thres.caget())
+		if abs((thres * 2.0) - newpos) < newpos * self.thresholdtolerance:
+			# threshold ok
+			pass
+		else:
+			self.thres.caput(newpos / 2.0)
+			self.timer.reset()
+
+	def rawIsBusy(self):
+		return (self.timer()<self.waittime)
+
+class DummyThreshold:
+	def __init__(self, name):
+		self.name = name
+	def caget(self):
+		#when position cannot be accessed return NAN and print error
+		print THRESHOLD_ERROR %(self.name, self.name)
+		return Double.NaN
+	def caput(self, newVal):
+		print THRESHOLD_ERROR %(self.name, self.name)
+		return
 
 class Harmonic:
 	""" Constructor method give the initial values
@@ -110,53 +164,6 @@ class CalibratedID(gda.device.scannable.PseudoDevice):
 				print str(energy_position) + " , " + str(position) +" , "+ str(e)
 			energy_position = energy_position + step
 		print "Done"
-
-class PilatusThreshold(ScannableMotionBase):
-	def __init__(self, name, pvbase):
-		self.setName(name);
-		self.setInputNames([name])
-		self.setExtraNames([])
-		self.Units=['keV']
-		self.setOutputFormat(['%4.2f'])
-		self.setLevel(7)
-		self.timer=tictoc()
-		self.waitUntilTime = 0
-		self.demand = 0.0
-		self.gain = CAClient(pvbase+":GainMenu")
-		self.thres = CAClient(pvbase+":ThresholdEnergy")
-		self.gain.configure()
-		self.thres.configure()
-		self.gainranges = { 0 : [6.5, 19.7], 1 : [4.4, 14.0], 2 : [3.8, 11.4] }
-		self.thresholdtolerance = 0.1
-		self.waittime = 3
-
-	def rawGetPosition(self):
-		return float(self.thres.caget()) * 2.0
-
-	def rawAsynchronousMoveTo(self,newpos):
-		# gain
-		gain = int(self.gain.caget())
-		if newpos >= self.gainranges[gain][0] and newpos <= self.gainranges[gain][1]:
-			# gain ok
-			pass
-		else:
-			for i in self.gainranges.keys():
-				if newpos >= self.gainranges[i][0] and newpos <= self.gainranges[i][1]:
-					self.gain.caput(i)	
-					self.timer.reset()
-					break
-			# raise exception, value out of range
-		# threshold
-		thres = float(self.thres.caget())
-		if abs((thres * 2.0) - newpos) < newpos * self.thresholdtolerance:
-			# threshold ok
-			pass
-		else:
-			self.thres.caput(newpos / 2.0)
-			self.timer.reset()
-	
-	def rawIsBusy(self):
-		return (self.timer()<self.waittime)
 
 pilthres = PilatusThreshold("pilthres", "BL22I-EA-PILAT-01:CAM")
 pilthresWAXS_L = PilatusThreshold("pilthresWAXS_L", "BL22I-EA-PILAT-03:CAM")
