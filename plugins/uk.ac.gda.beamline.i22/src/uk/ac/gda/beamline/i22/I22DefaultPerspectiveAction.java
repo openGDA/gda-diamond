38e2dbaf17844f9c028e1ef857313cef4d2c2756
Peter Holloway <peter.holloway@diamond.ac.uk>
2017-01-17 11:47:24 +0000

    I22-451 Return I22 intro and default perspectives

---
+++ plugins/uk.ac.gda.beamline.i22/src/uk/ac/gda/beamline/i22/I22DefaultPerspectiveAction.java
@@ -0,0 +1,71 @@
+/*-
+ * Copyright © 2017 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.beamline.i22;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.eclipse.ui.intro.IIntroPart;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+import gda.device.FindableObjectHolder;
+import gda.factory.Finder;
+
+public class I22DefaultPerspectiveAction implements IIntroAction {
+
+	@SuppressWarnings("unchecked")
+	public I22DefaultPerspectiveAction() {
+		FindableObjectHolder holder = (FindableObjectHolder)Finder.getInstance().find("clientReferences");
+		List<String> defaults = null;
+		if (holder != null) {
+			Object raw_defaults = holder.get("defaultPerspectives");
+			if (raw_defaults instanceof List<?>) {
+				defaults = (List<String>)raw_defaults;
+			}
+		}
+		if (defaults == null) {
+			defaults = Arrays.asList(new String[] {
+					"gda.rcp.ncd.perspectives.SaxsProcessingPerspective",  // must precede the Saxs & Waxs Perspectives to avoid NPE error in [S|W]axs Data Source View
+					"gda.rcp.ncd.perspectives.SaxsPerspective",
+					"gda.rcp.ncd.perspectives.WaxsPerspective",
+					"gda.rcp.ncd.perspectives.NcdDetectorPerspective",
+					"gda.rcp.ncd.perspectives.SetupPerspective",
+					"uk.ac.gda.client.scripting.JythonPerspective"
+				});
+		}
+		for (String id : defaults) {
+			try {
+				PlatformUI.getWorkbench().showPerspective(id, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+			} catch (WorkbenchException e) {
+				// we see if that fails and it is not the end of the world
+			}
+		}
+	}
+
+	@Override
+	public void run(IIntroSite site, Properties params) {
+		final IIntroPart introPart = PlatformUI.getWorkbench().getIntroManager().getIntro();
+		PlatformUI.getWorkbench().getIntroManager().closeIntro(introPart);
+	}
+
+}
