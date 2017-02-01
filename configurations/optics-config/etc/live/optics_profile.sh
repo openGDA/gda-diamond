df02ee751db5c69e7d5653ff028b15bc7f8c269b
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-02-01 15:15:07 +0000

    set BEAMLINE and GDA_MODE in optics_profile.sh and source it from gda launcher as there is no standard support in IT Linux system to support Lab workstation as beamline workstation, i.e. the profile is not loaded at users login.

--- configurations/optics-config/etc/live/optics_profile.sh
+++ configurations/optics-config/etc/live/optics_profile.sh
@@ -1,13 +1,9 @@
-# i21 beamline profile - to be moved to etc/i06_profile.sh
+# optics beamline profile
 
-if [ ! -n "$BEAMLINE" ]; then
-  echo "ERROR: BEAMLINE not set" 1>&2
-  exit 1
-fi
+export BEAMLINE=optics
 
 # Set up path and mode
 export PATH=/dls_sw/prod/etc/Launcher:/dls_sw/$BEAMLINE/software/gda/config/bin:${PATH}
-#export LD_LIBRARY_PATH=/dls_sw/i21/software/anaconda2/plugins/platforms:${LD_LIBRARY_PATH}
 export GDA_MODE=live
 
 # Set up command completion for the gda command
