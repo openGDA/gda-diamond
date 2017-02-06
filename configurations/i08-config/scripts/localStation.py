8fc58d3555644d01adda9d0ba648cac0f636ddf8
Anthony Hull <anthony.hull@diamond.ac.uk>
2017-02-06 13:43:26 +0000

    I08: update config to reflect current state of beamline
    - Uncomment Andor support and add Andor Malcolm scan
    - Set current values for energy focus scan
    - Replace remote server debug launcher as the old one did not work
    
    Change-Id: Ie5b82ad08bbd12a295bdd8397a594f2d5eea4ef1

--- configurations/i08-config/scripts/localStation.py
+++ configurations/i08-config/scripts/localStation.py
@@ -40,7 +40,7 @@ showtime=showtimeClass('showtime')
 inctime=showincrementaltimeClass('inctime')
 actualTime=actualTimeClass("actualTime")
 
-#if (LocalProperties.get("gda.mode") == 'live'): 
+#if (LocalProperties.get("gda.mode") == 'live'):
 #    beamMonitor.configure()
 #    add_default beamMonitor
 #    add_default topupMonitor
@@ -56,14 +56,14 @@ scanForImageCorrection = ScanForImageCorrection(andor)  # @UndefinedVariable
 andormapWithCorrection = AndorMapWithCorrection(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor,scanForImageCorrection)  # @UndefinedVariable
 alias("andormapWithCorrection")
 
-if (LocalProperties.get("gda.mode") == 'live'): 
-    run(scriptDir+"xrfmap.py")
-    LocalProperties.set("gda.scan.executeAtEnd","/dls_sw/i08/software/gda/config/scripts/I08_NeXus_Fix.sh")
-else:
-    LocalProperties.set("gda.scan.executeAtEnd",None)
+# if (LocalProperties.get("gda.mode") == 'live'):
+#     run(scriptDir+"xrfmap.py")
+#     LocalProperties.set("gda.scan.executeAtEnd","/dls_sw/i08/software/gda/config/scripts/I08_NeXus_Fix.sh")
+# else:
+LocalProperties.set("gda.scan.executeAtEnd",None)
 
-energyStepScan = EnergyStepScan(IDEnergy,xmapMca)  # @UndefinedVariable
-alias("energyStepScan")
+#energyStepScan = EnergyStepScan(IDEnergy,xmapMca)  # @UndefinedVariable
+#alias("energyStepScan")
 
 # Property so that user can drag ROIs in FluorescenceDetector views. imh 11/11/2016
 LocalProperties.set("exafs.editor.overlay.Preference", "True")
@@ -73,6 +73,8 @@ print "Adding mscan mapping scan command. Use help(mscan) to get information on 
 run('mapping_scan_commands.py')
 
 # Allow control of the function that maps energy to zone plate position
-energyFocusFunction = finder.find("energyFocusFunction") 
+energyFocusFunction = finder.find("energyFocusFunction")
+energyFocusFunction.slopeDividend="-12.132765 um"
+energyFocusFunction.interception="25528.85 um"
 
 print "Initialisation Complete";
