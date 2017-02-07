19d1b6267076badbcc04f8aa237afb2fb0a7eeea
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-02-07 17:50:58 +0000

    I06-171 ID is shared by both endstations. Update messages

--- configurations/i06-shared/scripts/i06shared/useID.py
+++ configurations/i06-shared/scripts/i06shared/useID.py
@@ -13,8 +13,17 @@ global denergy0, denergy1, iddpgmenergy, iddrpenergy0, iddrpenergy1
 global uenergy0, uenergy1, idupgmenergy, idurpenergy0, idurpenergy1
 
 ### Polarisation Control on ID;
-print "Enable the Polarisation Control on ID";
-
+print "-"*100
+print "Enable the Polarisation Control on ID, creating objects:"
+print "    'iddpol'      - downstream ID polarisation";
+print "    'denergy'     - downstream ID energy for the current 'iddpol' polarisation with Row Phase & PGM"
+print "    'hdenergy'    - downstream ID energy for the current 'iddpol' polarisation without Row Phase, with PGM "
+print "    'iddrpenergy' - downstream ID energy for the current 'iddpol' polarisation with Row Phase "
+print "    'idupol'      - upstream ID polarisation";
+print "    'uenergy'     - upstream ID energy for the current 'iddpol' polarisation with Row Phase & PGM"
+print "    'huenergy'    - upstream ID energy for the current 'iddpol' polarisation without Row Phase, with PGM "
+print "    'idurpenergy' - upstream ID energy for the current 'iddpol' polarisation with Row Phase "
+print "    'duenergy'    - combined energy - moves 'denergy', 'uenergy', and 'pgmenergy' concurrently"
 #IDD
 iddpolSetPV = 'BL06I-OP-IDD-01:SETPOL';
 iddpolGetPV = 'BL06I-OP-IDD-01:POL';
@@ -40,18 +49,20 @@ Energy.addGroupMember(iddrpenergy);
 
 idupol = ID_PolarisationClass('idupol', idupolSetPV, idupolGetPV, idupolStatusPV, iduEnablePV);
 uenergy = EnergyConsolidationClass('uenergy', idupol, uenergy0, uenergy1, inPositionTolerance = 0.001);
-huenergy = EnergyConsolidationClass('uenergy', idupol, idupgmenergy, uenergy1, inPositionTolerance = 0.001);
+huenergy = EnergyConsolidationClass('huenergy', idupol, idupgmenergy, uenergy1, inPositionTolerance = 0.001);
 idurpenergy = EnergyConsolidationClass('idurpenergy', idupol, idurpenergy0, idurpenergy1, inPositionTolerance = 0.001);
 Energy.addGroupMember(uenergy);
 Energy.addGroupMember(huenergy);
 Energy.addGroupMember(idurpenergy);
 
-
 duenergy = CombinedIDEnergyClass("duenergy", denergy, uenergy, pgmenergy);
 Energy.addGroupMember(duenergy);
 
 ### Enable the Harmonic Control on ID";
-print "Enable the Harmonic Control on ID";
+print "-"*100
+print "Enable the Harmonic Control on ID, creating objects:"
+print "    'iddhar' - downstrean ID harmonic"
+print "    'iduhar' - upstream ID harmonic"
 
 pvHarmonicIDD = "BL06I-OP-IDD-01:HARMONIC";
 iddhar = ID_HarmonicClass("iddhar", pvHarmonicIDD);
