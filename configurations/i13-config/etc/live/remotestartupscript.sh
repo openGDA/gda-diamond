f1e3584742a487c7e5fd4a259396625f9ccdca54
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2017-02-16 15:18:31 +0000

    Stop (hopefully) ssh hanging in live mode
    People have observed the ssh connection used in live mode not returning
    properly in the past which is why it was backgrounded. According to
    StackOverflow this can happen if the program it starts leaves its std
    streams open and the recommended workaround is to redirect them all to
    /dev/null, which this change does. This allows the ssh tunnel to be run
    without the & and thus removes the need for spurious 'read' lines in
    beamline specific scripts to prevent the tunnel being killed before the
    server has had time to start. This supports change 6110 and change 6111.

--- configurations/i13-config/etc/live/remotestartupscript.sh
+++ configurations/i13-config/etc/live/remotestartupscript.sh
@@ -14,7 +14,7 @@ export GDA_NO_PROMPT=true
 export GDA_IN_REMOTE_STARTUP=true
 
 if [[ -n "${SSH_ORIGINAL_COMMAND}" ]]; then 
-    ${here_dir}/../../bin/gda  --${SSH_ORIGINAL_COMMAND} --mode=live servers
+    ${here_dir}/../../bin/gda  --${SSH_ORIGINAL_COMMAND} --mode=live servers < /dev/null > /dev/null 2>&1
 else
-    ${here_dir}/../../bin/gda --restart --mode=live servers
+    ${here_dir}/../../bin/gda --restart --mode=live servers < /dev/null > /dev/null 2>&1
 fi
