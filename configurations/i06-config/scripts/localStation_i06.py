e1292d3f78539bc237e6537eb5e6be2f9c64a99c
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2017-02-23 14:11:19 +0000

    I06-177 removed as the branches are properly separated.

--- configurations/i06-config/scripts/localStation_i06.py
+++
@@ -1,7 +0,0 @@
-#localStation.py
-#For beamline specific initialisation code.
-from Diamond.Utility.BeamlineFunctions import logger
-print "===================================================================";
-print "Performing I06 PEEM line specific initialisation code (localStation_i06.py).";
-print
-import sys
