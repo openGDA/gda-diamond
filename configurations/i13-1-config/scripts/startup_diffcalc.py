7e95b4ac314dee32b8d838d6928ec79a134894ad
Anthony Hull <anthony.hull@diamond.ac.uk>
2017-03-10 11:07:42 +0000

    Coherence: update Diffcalc startup

--- configurations/i13-1-config/scripts/startup_diffcalc.py
+++ configurations/i13-1-config/scripts/startup_diffcalc.py
@@ -1,32 +1,21 @@
 ###
 # Start up Diffcalc for I13
 ###
+from gdascripts.scannable.dummy import SingleInputDummy as Dummy
 
 if not 'en' in globals():
-	en=qcm_energy
-	print "Set 'en' as synonym for 'qcm_energy'"
+	# en=qcm_energy
+	# print "Set 'en' as synonym for 'qcm_energy'"
+	en = Dummy('en')
+	en(9.35)
+	print "Creating Dummy en scannable and setting energy to 9.35 keV"
 
-print 'Running fivecircle example'
-run('example/fivecircle')
+	
 
-setmax(delta, 24)
 
-setmin(delta, -1)
 
-setmin(gam, 0)
+print 'Running startup/i13'
+# For 2.0 branch
+run('startup/i13')
 
-setmax(gam, 15)
 
-setmin(eta, -10)
-
-setmax(eta, 10)
-
-setmin(chi, 75)
-
-setmax(chi, 105)
-
-setmin(phi, -88)
-
-setmax(phi, 88)
-
-hardware()
