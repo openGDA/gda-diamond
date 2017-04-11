fa132530520a7ebf3fb46f458aae178cc4e9e094
Iain Hall <iain.hall@diamond.ac.uk>
2017-04-11 11:59:00 +0100

    B18 : Spring config updates
    - DAServer instances used for 9 and 36 element XSpress2 detectors are 
    now created in the same xml file as the rest of the detector config.
    - Latest server config from beamline (currently using 36 element detector)
    
    Change-Id: I4a6bba2daa96dfd9c5c01c870fd37c07524fa738

--- configurations/b18-config/bin/restart_daserver.sh
+++ configurations/b18-config/bin/restart_daserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## 9 element :
-xterm -e ssh b18detector@b18-xspress0 'pkill -9 da.server; echo killing da.server process will now sleep for 65 seconds for port 1972 to become available; sleep 65; echo starting da.server process on port 1972; cd xspress2_9element/; da.server -port=1972 -log' &
+#xterm -e ssh b18detector@b18-xspress0 'pkill -9 da.server; echo killing da.server process will now sleep for 65 seconds for port 1972 to become available; sleep 65; echo starting da.server process on port 1972; cd xspress2_9element/; da.server -port=1972 -log' &
 
 ## 36 element :
-# xterm -e ssh b18detector@b18-xspress1 'pkill -9 da.server; echo killing da.server process will now sleep for 65 seconds for port 1972 to become available; sleep 65; echo starting da.server process on port 1972; cd xspress2_36element/; da.server -port=1972 -log' &
+xterm -e ssh b18detector@b18-xspress1 'pkill -9 da.server; echo killing da.server process will now sleep for 65 seconds for port 1972 to become available; sleep 65; echo starting da.server process on port 1972; cd xspress2_36element/; da.server -port=1972 -log' &
