f0e7b9b728765459d1fcd6ac6f28742d94568640
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2017-05-02 11:53:20 +0100

    correct variable mis-spell

--- configurations/i21-config/scripts/lookup/jythonExternalProcess.py
+++ configurations/i21-config/scripts/lookup/jythonExternalProcess.py
@@ -22,7 +22,7 @@ my_env = os.environ
 my_env["PYTHONPATH"] = python_path
 
 def lookupGapPhase(gap, rowphase):
-    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "-f", lookup_file, "gaprowphase", str(idgap), str(rowphase)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=lookup_module_dir, env=my_env).communicate()
+    stdoutdata, stderrdata=subprocess.Popen(["./lookup.py", "-f", lookup_file, "gaprowphase", str(gap), str(rowphase)], stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=lookup_module_dir, env=my_env).communicate()
     if stderrdata!='':
         print stderrdata
     return stdoutdata.strip('\n')
