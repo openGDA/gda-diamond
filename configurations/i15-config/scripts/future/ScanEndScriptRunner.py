87bc37a24f71f520ecd6372a28f3157a4f7d85bd
Mark Booth <Mark.Booth@diamond.ac.uk>
2017-06-23 17:22:35 +0100

    I15-237 (9.4) First cut implementation of ScanEndScriptRunner scannable for i15
    This is based on the ScanEndScriptRunner class used by i12.
    
    It should pass the scan nexus filename into the script as the 1st argument,
    but currently passes in .../scan_filename_unknown.nxs instead due to
    differences between i12 and i15.
    
    Change-Id: Ic058bc2e7b7e58f9c044775c1855f09e85dd3ae5

---
+++ configurations/i15-config/scripts/future/ScanEndScriptRunner.py
@@ -0,0 +1,42 @@
+from gda.data import PathConstructor
+from gda.device.scannable import ScannableBase
+from gda.util import OSCommandRunner
+#from gdascripts.parameters import beamline_parameters
+from time import sleep
+
+import os
+
+class ScanEndScriptRunner(ScannableBase):
+    """
+    Class that runs a script at scan end.
+    
+    Example:
+    
+      pixium_redux=ScanEndScriptRunner('pixium_redux', '/dls_sw/apps/dawn_autoprocessing/autoprocess')
+    """
+    def __init__(self, name, exepath, delay_sec=0):
+        self.name = name
+        self.inputNames = [name]
+        self.exepath = exepath
+        self.delay_sec = delay_sec
+
+    def atScanEnd(self):
+        sleep(self.delay_sec)
+        self.run_exe()
+
+    def run_exe(self):
+        path = PathConstructor.createFromDefaultProperty()
+        #filenumber = i12NumTracker.getCurrentFileNumber();
+        filenumber = "scan_filename_unknown"
+        fpath = os.path.join(path, str(filenumber)) + '.nxs'
+        print('Executing script %s for Nexus scan file %s at the end of scan.' %(self.exepath,fpath))
+        OSCommandRunner.runNoWait([self.exepath, fpath], OSCommandRunner.LOGOPTION.ALWAYS, None)
+
+    def isBusy(self):
+        return False
+
+    def rawAsynchronousMoveTo(self,new_position):
+        pass
+
+    def rawGetPosition(self):
+        return 0
