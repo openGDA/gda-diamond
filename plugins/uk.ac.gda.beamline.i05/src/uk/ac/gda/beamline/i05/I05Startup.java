5ab2555e8b10543e15bd3fef4eb64d91d051d80f
James Mudd <james.mudd@diamond.ac.uk>
2017-07-06 14:52:11 +0100

    I05-198 Fix copying of initial .arpes file to scripts/visit dir
    Realised this was being done in a perspective listener which wansn't
    ideal as that code is run every time the perspective is changed so the
    code was moved into the creating the perspective.
    
    The reference .arpes file is now stored in the config root with the file
    name specified by the property gda.analyser.sampleConf
    
    Also refactored out the analyserListener which zero's supplies when the
    perspective is changed and no scan is running.
    
    Change-Id: I2346915a41c59e0b842bf503f25652e1491cc60e

--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/I05Startup.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/I05Startup.java
@@ -18,11 +18,6 @@
 
 package uk.ac.gda.beamline.i05;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.ui.IPerspectiveDescriptor;
@@ -30,38 +25,59 @@ import org.eclipse.ui.IPerspectiveListener;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.WorkbenchException;
-import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import gda.data.PathConstructor;
 import gda.factory.Finder;
 import gda.jython.InterfaceProvider;
 import gda.jython.Jython;
 import uk.ac.gda.devices.vgscienta.IVGScientaAnalyserRMI;
-import uk.ac.gda.util.io.FileUtils;
 
 public class I05Startup implements IStartup {
 	private static final Logger logger = LoggerFactory.getLogger(I05Startup.class);
 
-	private final IVGScientaAnalyserRMI analyser = Finder.getInstance().find("analyser");
+	/**
+	 * This listener will zero supplies on the analyser if the perspective is switched when no scan is running
+	 */
+	private final IPerspectiveListener analyserListener = new IPerspectiveListener() {
+
+		private final IVGScientaAnalyserRMI analyser = Finder.getInstance().find("analyser");
+
+		@Override
+		public void perspectiveChanged(IWorkbenchPage page, IPerspectiveDescriptor perspective, String changeId) {
+			// Note: This is not fired when the user changes perspective! It is fired when the perspective itself
+			// is changed eg reset.
+		}
+
+		@Override
+		public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
+			// Check if a scan is running if not stop the analyser
+			if (InterfaceProvider.getScanStatusHolder().getScanStatus() == Jython.IDLE) {
+				logger.info("Perspective Activated: No scan running: Stop analyser");
+				try {
+					// Stop the analyser and zero supplies
+					analyser.zeroSupplies();
+				} catch (Exception e) {
+					logger.error("Failed to stop analyser on perspective switch", e);
+				}
+			}
+		}
+	};
 
 	@Override
 	public void earlyStartup() {
+		// This is to fix ARPES-253. Create a preference store and then set aspectRatio to false this will make
+		// 2D plots fill the available space by default.
+		IPreferenceStore store = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.plotting");
+		store.setValue("org.dawb.plotting.system.aspectRatio", false);
+
 		// Need to run the startup in the UI thread
 		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
 			@Override
 			public void run() {
-
-				// This is to fix ARPES-253. Create a preference store and then set aspectRatio to false this will make
-				// 2D plots fill the available space by default.
-				IPreferenceStore store = new ScopedPreferenceStore(InstanceScope.INSTANCE, "org.dawnsci.plotting");
-				store.setValue("org.dawb.plotting.system.aspectRatio", false);
-
 				IWorkbench workbench = PlatformUI.getWorkbench();
 
 				logger.info("Creating perspectives");
@@ -75,73 +91,8 @@ public class I05Startup implements IStartup {
 				}
 
 				logger.info("Adding perspective switch listener");
-				workbench.getActiveWorkbenchWindow().addPerspectiveListener(new IPerspectiveListener() {
-
-					@Override
-					public void perspectiveChanged(IWorkbenchPage page, IPerspectiveDescriptor perspective, String changeId) {
-						logger.info("Perspective changed");
-						// Note: This is not fired when the user changes perspective! It is fired when the perspective itself
-						// is changed eg reset.
-					}
-
-					@Override
-					public void perspectiveActivated(IWorkbenchPage page, IPerspectiveDescriptor perspective) {
-
-						logger.info("Perspective Activated: {}", page.getLabel());
-						logger.debug("iwbPage label: {}, perspective label: {}", page.getLabel(), perspective.getLabel());
-						// Check if a scan is running if not stop the analyser
-						if (InterfaceProvider.getScanStatusHolder().getScanStatus() == Jython.IDLE) {
-							logger.info("Perspective Activated: No scan running: Stop analyser");
-							try {
-								// Stop the analyser and zero supplies
-								analyser.zeroSupplies();
-							} catch (Exception e) {
-								logger.error("Failed to stop analyser on perspective switch", e);
-							}
-						}
-
-						// PerspectiveListener to populate the editor with an initial sample .arpes analyser configuration
-						if (perspective.getId().equals("uk.ac.gda.beamline.i05.perspectives.ArpesExperimentPerspective")) {
-
-							// full path to initialExampleAnalyserConfig.arpes
-							String sampFileName = PathConstructor.createFromProperty("gda.analyser.sampleConf");
-							// location that is available in dummy and live and is version controlled
-							String srcDataRootPath = PathConstructor.createFromProperty("gda.analyser.sampleConf.dir");
-							File srcPth = new File(srcDataRootPath, sampFileName);
-
-							String tgtDataRootPath = PathConstructor.createFromProperty("gda.analyser.sampleConf.dir");
-							// location that is available in dummy and live variants and is visitor-specific
-							String cfgTgtPath = PathConstructor.createFromTemplate(tgtDataRootPath + "/$visit$/xml");
-							File tgtXmlDir = new File(cfgTgtPath);
-							File tgtPth = new File(tgtXmlDir, sampFileName);
-
-							// only needs to be invoked once for a each new visit, thereafter workspace caching determines
-							// editor(s) visible, you can delete file in visit xml dir to allow this clause to re-execute
-							if (!tgtPth.exists()) {
-								try {
-									tgtXmlDir.mkdir(); // ensure xml directory exists
-									FileUtils.copy(srcPth, tgtPth);
-									logger.info("Copied sample analyser config file to: {}", tgtPth);
-
-									if (tgtPth.exists() && tgtPth.isFile()) {
-										IFileStore fileStore = EFS.getLocalFileSystem().getStore(tgtPth.toURI());
-										try {
-											IDE.openEditorOnFileStore(page, fileStore);
-										} catch (PartInitException e) {
-											logger.error("Could not open sample analyser config file {}", tgtPth, e);
-										}
-									}
-								} catch (IOException e) {
-									logger.error("Could not copy sample analyser config file from: {} to user dir: {}", srcPth, tgtPth, e);
-								}
-							} else {
-								logger.info("not opening new editor, sample analyser config file {} already exists in user dir", sampFileName);
-							}
-						}
-					}
-				});
+				workbench.getActiveWorkbenchWindow().addPerspectiveListener(analyserListener);
 			}
 		});
 	}
-
 }
--- plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/perspectives/ArpesExperimentPerspective.java
+++ plugins/uk.ac.gda.beamline.i05/src/uk/ac/gda/beamline/i05/perspectives/ArpesExperimentPerspective.java
@@ -18,11 +18,24 @@
 
 package uk.ac.gda.beamline.i05.perspectives;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.ide.IDE;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import gda.configuration.properties.LocalProperties;
+import gda.data.PathConstructor;
 
 public class ArpesExperimentPerspective implements IPerspectiveFactory {
 
@@ -49,6 +62,48 @@ public class ArpesExperimentPerspective implements IPerspectiveFactory {
 		layout.addView("uk.ac.gda.arpes.ui.analyserprogress", IPageLayout.BOTTOM, 0.62f, "uk.ac.gda.arpes.ui.view.samplemetadata");
 		layout.addView("org.eclipse.ui.navigator.ProjectExplorer", IPageLayout.LEFT, 0.35f, IPageLayout.ID_EDITOR_AREA);
 
+		createExampleArpesFileIfRequired();
+
 		logger.info("Finished building ARPES experiment perspective");
+	}
+
+	private void createExampleArpesFileIfRequired() {
+
+		// Find the target location for the example .arpes file
+		final String tgtDataRootPath = PathConstructor.createFromProperty("gda.analyser.sampleConf.dir");
+		final String exampleFileName = LocalProperties.get("gda.analyser.sampleConf");
+		final File targetFile = new File(tgtDataRootPath, exampleFileName);
+		logger.debug("Initial .arpes file target '{}'", targetFile.getAbsolutePath());
+
+		// Find the full path to initialExampleAnalyserConfig.arpes in the config
+		String configDir = LocalProperties.getConfigDir();
+		File exampleFile = new File(configDir, exampleFileName);
+		logger.debug("Initial .arpes file source '{}'", exampleFile.getAbsolutePath());
+
+		// Example file doesn't exist so copy it
+		if (!targetFile.exists()) {
+			try {
+				Files.createDirectories(targetFile.toPath().getParent());
+				logger.info("Created directory '{}'", targetFile.toPath().getParent());
+				Files.copy(exampleFile.toPath(), targetFile.toPath());
+				logger.info("Copied sample analyser config file from '{}' to '{}'", exampleFile, targetFile);
+			} catch (IOException e) {
+				logger.error("Failed copying sample analyser config file from '{}' to '{}'", exampleFile, targetFile, e);
+			}
+		}
+		else {
+			logger.debug("Inital .arpes file already present");
+		}
+
+		// Open the example in the editor
+		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+		IFileStore fileStore = EFS.getLocalFileSystem().getStore(targetFile.toURI());
+		try {
+			IDE.openEditorOnFileStore(page, fileStore);
+			logger.debug("Opened sample analyser config file '{}' in editor", targetFile);
+		} catch (PartInitException e) {
+			logger.error("Could not open sample analyser config file '{}' in editor", targetFile, e);
+		}
+
 	}
 }
