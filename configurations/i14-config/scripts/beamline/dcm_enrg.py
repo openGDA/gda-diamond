f423ce83b788ea77aacd80db9339eb94ba3f8427
Anthony Hull <anthony.hull@diamond.ac.uk>
2017-08-03 10:46:13 +0100

    I14 Update dcm_enrg.py in config to match beamline version
    Change-Id: I25d48fcee713933360ebc88f95aae651980c5381

--- configurations/i14-config/scripts/beamline/dcm_enrg.py
+++ configurations/i14-config/scripts/beamline/dcm_enrg.py
@@ -1,100 +1,27 @@
 
-from math import asin,sin,degrees,radians
+from math import asin,sin,degrees,radians, cos
 from gda.device.scannable import ScannableMotionUnitsBase
 from gda.analysis.datastructure import DataVector
 from gda.analysis.numerical.interpolation import Interpolator
 from gda.jython.commands.ScannableCommands import pos
+from Beamline.Utilities.file_io_tools import *
+from gda.epics import CAClient
 import codecs
 import os.path
-#
-# Temporary script...to be replaced by comboDCM GDA motor at some point
-#
-#
-
-def isNumber(inputvalue):
-    """ Check if a string is a number
-        The method tries to perform a float conversion
-        on the input. If this throws an exception, the exception is
-        caught and the input is identified as not a number.
-        Ugly but effective..
-        Input  : String
-        Output : True or False            
-    """      
-    result=False
-    try:
-        float(inputvalue)
-        result=True
-    except:
-            result=False
-    return result
-    
-def isColumnData(input_string):
-    """
-    tests if string contains numerical data
-    returns true if there are more than 2 floats in the string 
-    and at most one word in the string
-    
-    Input: input_string
-    Output: boolean: Is column data ?
-            number of floating data points in the string
-
-    """
-    isdata = False
-    # Convert string to list of floats and strings
-    floats,strings,ifloats,istrings=stringToFloatList(input_string)
-    # How many floats and strings do we have
-    numberOfFColumns=len(floats)
-    numberOfSColumns=len(strings)
-    # data needs to have at least two columns and more floats than strings
-    if (numberOfFColumns >= 2 and numberOfSColumns<=1):
-        isdata = True
-    return isdata,numberOfFColumns,numberOfSColumns
-
-def stringToFloatList(input_string):
-    """
-    
-    Method takes a string input and splits it up into numbers and strings.
-    The method was for use with file io operations on ascii data.
-    The input string is converted to a list by removing commas and using
-    the python split method. Each element of the list 
-    is checked with isNumber method. The numbers and strings split 
-    into seperate lists and returns
-    
-    Input: string
-    Output : float data list, string data list, index of floats in original 
-    list, index of strings in original list
-    
-    """
-    line=input_string.replace(',', ' ')
-    line=line.strip()
-    # split it up
-    data = line.split()
-    returnData=[]
-    returnStrings=[]
-    indexOfFloats=[]
-    indexOfStrings=[]
-    myindex=0
-    for x in data:
-        if(isNumber(x)):
-            indexOfFloats.append(myindex)
-            returnData.append(float(x))
-        else:
-            indexOfStrings.append(myindex)
-            returnStrings.append(x)
-        myindex=myindex+1
-    return returnData,returnStrings,indexOfFloats,indexOfStrings
 
 
 class DCMpdq(ScannableMotionUnitsBase):
-    def __init__(self, name, dcm_bragg, dcm_perp, id_gap):
+    def __init__(self, name, dcm_bragg, dcm_perp, id_gap,m1_mirror_stripe,m2_mirror_stripe):
             self.setName(name);
             self.setInputNames([name])
             self.setExtraNames([])
             self.setOutputFormat(["%5.5g"])
-            
+
             self.dcm_bragg = dcm_bragg
             self.dcm_perp = dcm_perp
             self.id_gap = id_gap
+            self.m1_mirror_stripe = m1_mirror_stripe
+            self.m2_mirror_stripe = m2_mirror_stripe
 
             # Si 111 spacing at 77K
             self.silicon_d111 = 3.134925
@@ -103,7 +30,7 @@ class DCMpdq(ScannableMotionUnitsBase):
             # DCM offset
             self.myoffset = 35.0
             # Min energy for DCM
-            self.minEnergy = 4.8
+            self.minEnergy = 4.45
             # Max energ yfor DCM
             self.maxEnergy = 26.0
             # Min gap - just above 5mm
@@ -114,12 +41,20 @@ class DCMpdq(ScannableMotionUnitsBase):
             self.currentharmonic=5
             # Position busy
             self.iambusy = 0
+            #self.mirror_stripe_list=[-20.0,0.0,20.0]
             # Variable to control whether to switch harmonics during scans
             # set using get set methods
-            self.disableHarmonicSwitch = 0
-            self.selectHarmonic(self.currentharmonic)
+            self.chan = CAClient()
+            # Default mirror stripe is the Rh stripe
+            self.current_mirror_stripe="blah"
+            self.disableUndulatorHarmonicSwitch = 0
+            self.disableMirrorStripeSwitch = 0
+            self.disablegap=0
+            self.disableDCMFeedback=1
+            self.disableMirrorFeedback=1
+            self.selectUndulatorHarmonic(self.currentharmonic)
             self.setUserUnits("keV")
-            
+
     def rawGetPosition(self):
             """
             Return the energy calculated using the current bragg motor position
@@ -139,33 +74,99 @@ class DCMpdq(ScannableMotionUnitsBase):
             else:
                 # if harmonic switching is off keep the current harmonic and just interpolate the gap
                 # else select the best harmonic for this energy
-                if(self.disableHarmonicSwitch):
+                if(self.disableUndulatorHarmonicSwitch):
                     best_harmonic = self.currentharmonic
                 else:
-                    best_harmonic = self.selectBestHarmonic(position)
+                    best_harmonic = self.selectBestUndulatorHarmonic(position)
+
+                if(self.disableMirrorStripeSwitch):
+                    best_stripe = self.current_mirror_stripe
+                else:
+                    best_stripe = self.selectBestMirrorStripe(position)
+                    self.current_mirror_stripe = self.determine_current_stripe()
+
                 # If you've updated the harmonic read in the correct lookuptable
                 if(best_harmonic != self.currentharmonic):
-                    print "selecting harmonic :",best_harmonic
-                    self.selectHarmonic(best_harmonic)
-                    #self.rolldict = self.read_roll_lookuptable()
-                    #rollmove=True
+                    #print "selecting harmonic :",best_harmonic
+                    self.selectUndulatorHarmonic(best_harmonic)
                     self.currentharmonic=best_harmonic
-                self.iambusy = 1
-                # Look up the bragg, perp and id- linear interpolation of lookuptables
+
+                # If you're moving from Rh or Pt to Si
+                # then move the DCM first then translate stripe
+                # If you're moving from Si to Rh or Pt
+                # move the mirror stripe, then the DCM
+                moveDCMFirst=True
+                moveMirror=False
+                #print best_stripe,self.current_mirror_stripe
+                if(best_stripe != self.current_mirror_stripe):
+                    moveMirror=True
+                    if(best_stripe == "Si"):
+                        moveDCMFirst=True
+                    else:
+                        moveDCMFirst=False
+                else:
+                    moveMirror=False
+
                 bragg,perp=self.calcBraggandPerp(position)
                 newid_gap = self.lookup_gap(position)
-                #newroll =  self.rolldict[self.currentharmonic]
-                #if(rollmove):
-                #    pos dcm_bragg bragg dcm_perp perp id_gap newid_gap dcm_roll newroll
-                #else:
-                #    pos dcm_bragg bragg dcm_perp perp id_gap newid_gap
-                # Moves bragg, perp and gap together to new positions
-                pos(self.dcm_bragg, bragg, self.dcm_perp, perp, self.id_gap, newid_gap)
-                self.iambusy = 0
+                if(moveDCMFirst):
+                    self.iambusy = 1
+                    # Look up the bragg, perp and id- linear interpolation of lookuptables
+                    #print "Moving DCM to :", position
+                    if(self.disablegap):
+                        pos(self.dcm_bragg, bragg, self.dcm_perp, perp)
+                    else:
+                        pos(self.dcm_bragg, bragg, self.dcm_perp, perp, self.id_gap, newid_gap)
+                    #print "DCM move completed"
+                    if(moveMirror):
+                        self.iambusy = 1
+                        # Turn off DCM feedback - we're assuming the DCM crystals are parallel
+                        #self.disableDCMFeedback()
+                        # Move the mirror stripes before a gap change
+                        print "Moving mirror stripe to :",best_stripe
+                        pos(self.m1_mirror_stripe,best_stripe,self.m2_mirror_stripe,best_stripe)
+                        print "Mirror stripe change completed"
+                        self.current_mirror_stripe=best_stripe
+                        #
+                        # Now optimize the mirror angle for any offset
+                        # Turn on mirror feedback, settle, then turn off
+                        #self.runMirrorFeedback()
+                        # Turn off mirror feedback
+                        # Turn on DCM feedback
+                        #self.enableDCMFeedback()
+                    #else:
+                    #    print "Keeping mirror stripe as :",self.current_mirror_stripe
+                    self.iambusy = 0
+                else:
+                    if(moveMirror):
+                        self.iambusy = 1
+                        # Turn off DCM feedback
+                        #self.disableDCMFeedback()
+                        # Move the mirror stripes before a gap change
+                        print "Moving mirror stripe to :",best_stripe
+                        pos(self.m1_mirror_stripe,best_stripe,self.m2_mirror_stripe,best_stripe)
+                        print "Mirror stripe change completed"
+                        self.current_mirror_stripe=best_stripe
+                        #
+                        # Now optimize the mirror angle for any offset as the DCM
+                        # Turn on mirror feedback, settle, then turn off
+                        #self.runMirrorFeedback()
+                        # Turn on DCM feedback
+                        #self.enableDCMFeedback()
+                    else:
+                        print "Keeping mirror stripe as :",self.current_mirror_stripe
+                    # Look up the bragg, perp and id- linear interpolation of lookuptables
+                    #print "Moving DCM to :", position
+                    if(self.disablegap):
+                        pos(self.dcm_bragg, bragg, self.dcm_perp, perp)
+                    else:
+                        pos(self.dcm_bragg, bragg, self.dcm_perp, perp, self.id_gap, newid_gap)
+                    #print "DCM move completed"
+                    self.iambusy = 0
 
     def rawIsBusy(self):
             return self.iambusy
-        
+
     def calcBraggandPerp(self,energy):
         """
         Calculate  the bragg and perp for a given energy (keV)
@@ -174,8 +175,14 @@ class DCMpdq(ScannableMotionUnitsBase):
         bragg_rad  = asin(wavelength/(2*self.silicon_d111))
         bragg_deg  = degrees(bragg_rad)
         perp = (self.myoffset/sin(2.*bragg_rad))*sin(bragg_rad)
+        #perp = (17.533/cos(bragg_rad))-0.1904
+     #   perp = (14.048/cos(bragg_rad))+2.9881
+      #  perp = (19.171/cos(bragg_rad))-2.3326
+
+        #perp = (17.797/cos(bragg_rad))-0.3138
+        #perp = (16.193/cos(bragg_rad))+1.19
         return bragg_deg,perp
-    
+
     def calcEnergyFromCurrentBragg(self):
         """
         Calculate the energy given the current bragg motor position
@@ -189,19 +196,96 @@ class DCMpdq(ScannableMotionUnitsBase):
         """
         Calculate the energy given a bragg position (degrees)
         Just a tool for calculation...
-        
+
         """
         bragg_rad = radians(bragg)
         energy  = self.wavetokeV/(2*self.silicon_d111*sin(bragg_rad))
         return energy
 
-        
+    def determine_current_stripe(self):
+        stripe = "Rh"
+        m2_inpos= float(self.chan.caget("BL14I-OP-MIRR-02:Y:MP:INPOS"))
+        m1_inpos= float(self.chan.caget("BL14I-OP-MIRR-01:Y:MP:INPOS"))
+        # logical and doesn't work ...something odd so rewrite to check == 1 ????
+        #inpos = (m2_inpos and m1_inpos)
+        stripesame = (self.m2_mirror_stripe.getPosition() == self.m1_mirror_stripe.getPosition())
+        if(stripesame and m2_inpos==1.0 and m1_inpos ==1.0):
+                stripe=self.m2_mirror_stripe.getPosition()
+        else:
+             stripe="fixme"
+        return stripe
+
     def disableHarmonicSwitching(self):
-        self.disableHarmonicSwitch = 145
+        self.disableUndulatorHarmonicSwitch = 145
 
     def enableHarmonicSwitching(self):
-        self.disableHarmonicSwitch = 0
-                
+        self.disableUndulatorHarmonicSwitch = 0
+
+    def disableMirrorStripeSwitching(self):
+        self.disableMirrorStripeSwitch = 145
+
+    def enableMirrorStripeSwitching(self):
+        self.disableMirrorStripeSwitch = 0
+
+    def disableSwitching(self):
+        self.disableUndulatorHarmonicSwitch = 145
+        self.disableMirrorStripeSwitch = 145
+
+    def enableSwitching(self):
+        self.disableUndulatorHarmonicSwitch = 0
+        self.disableMirrorStripeSwitch = 0
+        self.disablegap = 0
+
+    def disableUndulator(self):
+        self.disablegap = 1
+
+    def enableUndulator(self):
+        self.disablegap = 0
+
+ #   def enableDCMFeedback(self):
+ #       self.disableDCMFeedback=1
+  #      self.turnonDCMFeedback()
+
+#    def disableDCMFeedback(self):
+#        self.disableDCMFeedback=0
+ #       self.turnoffDCMFeedback()
+
+    def runMirrorFeedback(self):
+        """
+        Enable feedback on M2 mirror piezo and wait until it settles...
+        If the shutters are closed then don't bother switching it on..
+        """
+        if(self.chan.caget("BL14I-PS-SHTR-03:STA")=="Open"):
+            self.chan.caput("BL14I-DI-IONC-01:FB.FBON",1)
+            setpt = float(self.chan.caget("BL14I-DI-IONC-01:FB.VAL"))
+            while(self.chan.caget("BL14I-DI-IONC-01:FB.ERR")>0.001):
+                  sleep(5)
+            self.chan.caput("BL14I-DI-IONC-01:FB.FBON",0)
+
+
+    def turnonDCMFeedback(self):
+        """
+        Turn on the DCM feedback and wait for it to settle
+
+        """
+        self.chan.caput("BL14I-DI-BEST-01:Login:UserPass","admin:WeAreTheBest")
+        # Turn back on the FPGA in case you have been using manual piezo control
+        self.chan.caput("BL14I-DI-BEST-01:PreDAC0:OutMux",1)
+        # Turn back on the FPGA
+        self.chan.caput("BL14I-DI-BEST-01:PID:Enable",1)
+        # Tuned timing to judge how long it takes to settle...or we could read the error off...
+        sleep(10)
+
+    def turnoffDCMFeedback(self):
+        """
+        Turn on the DCM feedback and wait for it to settle
+
+        """
+        self.chan.caput("BL14I-DI-BEST-01:Login:UserPass","admin:WeAreTheBest")
+        # Turn back off the FPGA and enable manual piezo control
+        self.chan.caput("BL14I-DI-BEST-01:PreDAC0:OutMux",1)
+        self.chan.caput("BL14I-DI-BEST-01:PID:Enable",0)
+
 
     def read_lookuptable(self):
         """
@@ -209,7 +293,7 @@ class DCMpdq(ScannableMotionUnitsBase):
         """
         # use codecs as the first line of the files sometimes have
         # extra characters depending on what you edited or saved the harmonic table in...
-        # 
+        #
         f = codecs.open(self.lookuptablefile,"r","utf-8-sig")
         AA=f.readlines()
         f.close()
@@ -219,7 +303,7 @@ class DCMpdq(ScannableMotionUnitsBase):
             self.braggset.add(a[1])
             self.gapset.add(a[2])
 #===============================================================================
-# 
+#
 #     def read_roll_lookuptable(self):
 #         rollfilename = "/dls_sw/i14/scripts/Harmonics/roll_lookup.txt"
 #         f=open(rollfilename)
@@ -245,14 +329,14 @@ class DCMpdq(ScannableMotionUnitsBase):
             print "interpolated gap too high, ", newgap
             return self.maxGap
         else:
-            print 'new interpolated gap',newgap
+            #print 'new interpolated gap',newgap
             return newgap
 
 #    def lookup_pitch(self,bragg):
 #        return Interpolator.polyInterpolatedPoint(self.braggset, self.pitchset, bragg, 3)[0]
 
 #    def lookup_roll(self,bragg):
-#        return Interpolator.polyInterpolatedPoint(self.braggset, self.rollset, bragg, 3)[0]                    
+#        return Interpolator.polyInterpolatedPoint(self.braggset, self.rollset, bragg, 3)[0]
 
     def reset_lookuptable(self):
         """
@@ -270,7 +354,7 @@ class DCMpdq(ScannableMotionUnitsBase):
         self.lookuptablefile=filename
         self.reset_lookuptable()
 
-    def selectBestHarmonic(self,energy):
+    def selectBestUndulatorHarmonic(self,energy):
         """
         For a given energy which harmonic should I use...
         Randomly set by PQ to keep the gaps > 5.8mm for now
@@ -279,9 +363,9 @@ class DCMpdq(ScannableMotionUnitsBase):
             besth = 5
         elif(energy >=7.2 and energy < 10.1):
             besth = 7
-        elif(energy >=10.1 and energy < 13.0):
+        elif(energy >=10.1 and energy < 13.1):
             besth = 9
-        elif(energy >=13.0 and energy < 15.8):
+        elif(energy >=13.1 and energy < 15.8):
             besth = 11
         elif(energy >=15.8 and energy < 18.8):
             besth = 13
@@ -291,17 +375,36 @@ class DCMpdq(ScannableMotionUnitsBase):
             besth = 17
         else:
             besth=5
-        print "Best harmonic for this energy:",besth
-        return besth      
+        #print "Best harmonic for this energy:",besth
+        return besth
 
-    def selectHarmonic(self,harmonic):
+    def selectBestMirrorStripe(self,energy):
+        """
+        For a given energy which harmonic should I use...
+        Randomly set by PQ to keep the gaps > 5.8mm for now
+        """
+        mirror_stripes = ["Si","Rh","Pt"]
+        if(energy >self.minEnergy and energy < 10.5):
+            best_stripe = "Si"
+        elif(energy >=10.5 and energy < 21):
+            best_stripe = "Rh"
+        elif(energy >=21 ):
+            best_stripe = "Pt"
+        else:
+            best_stripe="Rh"
+        #print "Best mirror stripe for this energy:",mirror_stripes[best_stripe]
+        return best_stripe
+
+
+    def selectUndulatorHarmonic(self,harmonic):
         """
         Link to the harmonic files
         These are tab separated lists of energy(keV), bragg(deg), gap(mm)
-        
+
         """
-        print "harmonic selected:",harmonic
-        harmonics_dir = os.path.dirname(os.path.realpath(__file__)) + '/../harmonics/'
+        #print "harmonic selected:",harmonic
+        #harmonics_dir = os.path.dirname(os.path.realpath(__file__)) + '/../harmonics/'
+        harmonics_dir = '/dls_sw/i14/scripts/Beamline/MotionsAndDetectors/harmonics/'
         if(harmonic==5):
             self.setLookupTable(harmonics_dir + 'harmonic5_20160429.txt')
         elif(harmonic==7):
@@ -319,4 +422,3 @@ class DCMpdq(ScannableMotionUnitsBase):
 
         else:
             print 'Cannot find a match for ',harmonic
-        
