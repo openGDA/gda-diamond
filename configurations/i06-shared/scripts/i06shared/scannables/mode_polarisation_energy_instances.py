f35094b505889dce2a281f760710b223e127878d
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-08-23 15:31:09 +0100

    update default ID opening gap when not in use to 100mm

--- configurations/i06-shared/scripts/i06shared/scannables/mode_polarisation_energy_instances.py
+++ configurations/i06-shared/scripts/i06shared/scannables/mode_polarisation_energy_instances.py
@@ -12,7 +12,7 @@ from i06shared.scannables.offsetHarmonic import HarmonicOffset
 import __main__  # @UnresolvedImport
 
 __main__.smode=SourceMode('smode', defaultmode=SourceMode.SOURCE_MODES[0])
-__main__.pol=Polarisation('pol', __main__.iddpol, __main__.iddgap, __main__.idupol, __main__.idugap, __main__.smode,detune=3.0, gap=150.0,defaultPolarisation=Polarisation.POLARISATIONS[0])
+__main__.pol=Polarisation('pol', __main__.iddpol, __main__.iddgap, __main__.idupol, __main__.idugap, __main__.smode,detune=3.0, gap=100.0,defaultPolarisation=Polarisation.POLARISATIONS[0])
 __main__.energy=CombinedEnergy('energy', __main__.denergy, __main__.uenergy, __main__.duenergy, __main__.iddgap, __main__.idugap, __main__.smode, __main__.pol, offhar=0.0, PositionTolerance=0.0001, defaultenergy=400.0)
 __main__.laa=LinearArbitraryAngle('laa', __main__.iddlaangle, __main__.idulaangle, __main__.smode, __main__.pol)
 __main__.haroff=HarmonicOffset('haroff', __main__.energy)
