134867de764080503c81493e5a8acf106e4af03b
Olly King <olly.king@diamond.ac.uk>
2017-10-12 11:28:52 +0100

    DAQ-962 I05-217 Make new script for photon energy dependence measurements
    - Script photonEnergyScan.py added to the i05-config/scripts folder.
    - Entry has also been added to localStation.py to execute the script and
      add the function calculate_hv_scan_values to the Jython namespace.
    - Beamline provides their own function relating hv energy to centre
      energy and this is used as one of the inputs to
      calculate_hv_scan_values
    
    Change-Id: Iffb461af66204fc60f268c83e0419302fb5a1271

--- configurations/i05-config/scripts/localStation.py
+++ configurations/i05-config/scripts/localStation.py
@@ -25,6 +25,8 @@ scan_processor.rootNamespaceDict=globals()
 execfile(gdascripts + "/pd/epics_pds.py");
 
 execfile(gdascripts + "/pd/time_pds.py");
+
+
 class actTimeInInt(actualTimeClass):  # specialise to make displayed time semi-human-readable
    def rawGetPosition(self):
       pad = 10000
@@ -59,6 +61,17 @@ sample_name=metadatatweaks.SampleNameScannable("sample_name","samplename",isgold
 centre_energy=analyser.getCentreEnergyScannable()
 centre_energy.setName("centre_energy")
 centre_energy.setInputNames(["centre_energy"])
+
+energy_group = ScannableGroup() # Make a new ScannableGroup
+energy_group.addGroupMember(energy) # Add members
+energy_group.addGroupMember(centre_energy)
+energy_group.setName('energy_group') # Set the group name
+energy_group.configure() # Configure the group, once all the members are added
+print "Scannable group 'energy_group' created containing 'energy' and 'centre_energy'";
+
+print "Loading Photon and Centre Energy Scan calculator... "
+print "Usage: calculate_hv_scan_values(hv_start, hv_end, hv_step, start_centre_energy, centre_energy_hv_function_name)"
+execfile(gdaScriptDir + "photonCentreEnergyScan.py")
 
 import arpes
 execfile(gdascripts + "scan/pathscanCommand.py");
