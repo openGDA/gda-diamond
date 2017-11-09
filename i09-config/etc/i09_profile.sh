4a66ba34c7975440590b933f2c7e85d6c13498e5
Olly King <olly.king@diamond.ac.uk>
2017-11-09 16:05:01 +0000

    I09-136 Further changes to update GDA to version 9.6
    - Updated i09_profile script in config
    - Changes to fix i09 launcher for GDA 9 and make ID visible

--- i09-config/etc/i09_profile.sh
+++ i09-config/etc/i09_profile.sh
@@ -1,14 +1,14 @@
-# i11 beamline profile
+# i09 beamline profile
 
-export BEAMLINE=i09
-
-if [ -r "/dls_sw/$BEAMLINE/software/gda/config/bin/gda_environment.sh" ]; then
-        . "/dls_sw/$BEAMLINE/software/gda/config/bin/gda_environment.sh"
-else
-
-if [ -r "/dls_sw/$BEAMLINE/etc/gda_environment.sh" ]; then
-        . "/dls_sw/$BEAMLINE/etc/gda_environment.sh"
+if [ ! -n "$BEAMLINE" ]; then
+  echo "ERROR: BEAMLINE not set" 1>&2
+  exit 1
 fi
 
-fi
+export GDA_INSTANCE_NAME=${BEAMLINE}
 
+export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
+export GDA_INSTANCE_CONFIG=$GDA_WORKSPACE_PARENT/workspace_git/gda-dls-beamlines-i09.git/i09-config
+
+export PATH=${GDA_INSTANCE_CONFIG}/bin:${PATH}
+export GDA_MODE=live
