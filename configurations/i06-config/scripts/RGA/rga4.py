7252029bae566230cb3156496a9679c816835aa9
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-11-13 11:18:39 +0000

    add print messages

--- configurations/i06-config/scripts/RGA/rga4.py
+++ configurations/i06-config/scripts/RGA/rga4.py
@@ -1,5 +1,8 @@
 from Diamond.PseudoDevices.EpicsDevices import EpicsMonitorClass
 
+print "-"*100
+print "Enable RGA4 objects: rga4tot, rga4O2, rga4H2O, rga4CO, rga4H2, rga4CO2, rga4CH4, rga4Ar, rgaPeem"
+
 exec("[rga4tot, rga4O2, rga4CO, rga4CO2, rga4CH4, rga4H2O, rga4Ar, rga4H2] = [None, None, None, None, None, None, None, None]")
 rga4tot = EpicsMonitorClass('rga4tot', 'BL06I-VA-RGA-04:TOTP', 'mbar', '%.2e')
 rga4O2 = EpicsMonitorClass('rga4O2', 'BL06I-VA-RGA-04:BAR:M32', 'mbar', '%.2e')
--- configurations/i06-config/scripts/RGA/rga5.py
+++ configurations/i06-config/scripts/RGA/rga5.py
@@ -1,5 +1,8 @@
 from Diamond.PseudoDevices.EpicsDevices import EpicsMonitorClass
 
+print "-"*100
+print "Enable RGA5 objects: rga5tot, rga5O2, rga5CO, rga5CO2, rga5CH4, rga5H2O, rga5Ar, rga5H2, rgaPreparation"
+
 exec("[rga5tot, rga5O2, rga5CO, rga5CO2, rga5CH4, rga5H2O, rga5Ar, rga5H2] = [None, None, None, None, None, None, None, None]")
 rga5tot = EpicsMonitorClass('rga5tot', 'BL06I-VA-RGA-05:TOTP', 'mbar', '%.2e')
 rga5O2 = EpicsMonitorClass('rga5O2', 'BL06I-VA-RGA-05:BAR:M32', 'mbar', '%.2e')
