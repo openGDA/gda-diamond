69d1389fee47534771d1b0919758827c07af4fe1
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-11-23 09:30:35 +0000

    insert scan processor control

--- configurations/i21-config/scripts/calibration/2017-10-24_id_LV.py
+++ configurations/i21-config/scripts/calibration/2017-10-24_id_LV.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Switch on scan processor
 scan_processing_on()
 #pos FPSelection 24.43 # DET3
@@ -35,38 +34,3 @@ wh.close()
                
 #pos FPSelection 18.43 # through-beam
 scan_processing_off()
-=======
-#pos FPSelection 24.43 # DET3
-gap_filename = '/dls_sw/i21/scripts/Polarimeter/LVgap50eV.csv'
-fh = open(gap_filename, 'r')
- 
-energy_gap = []
- 
-for line in fh:
-    e, gap = line.split(',')
-    energy_gap.append((e, gap))
-     
-     
-     
-fh.close()
-
-print(energy_gap)
- 
-write_gap_file = '/dls_sw/i21/scripts/Polarimeter/LVgap50eVReal.csv'
-wh = open(write_gap_file, 'w')
-
-for e, gap in energy_gap:
-    print e, gap
-    dgap=1  
-    pos pgmEnergy e
-    pos idscannable [gap, 'LV', 0]
-   
-    dscan idgap -dgap dgap 21 ca02sr 1
-    peak_val = scan_processor.results['peak'].scn.idgap
-       
-    wh.write(str(e) + ',' + str(peak_val))
-  
-wh.close()
-               
-#pos FPSelection 18.43 # through-beam
->>>>>>> branch 'master' of ssh://dascgitolite@dasc-git.diamond.ac.uk/gda/gda-dls-beamlines-i21.git
