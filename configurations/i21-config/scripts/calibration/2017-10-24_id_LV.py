28781cef9596dd66e8f438d7b35c00c7bbb420a3
fajinyuan <fajin.yuan@diamond.ac.uk>
2017-11-23 09:11:05 +0000

    I21-380 example script for collecting calibration data – reads a .csv file which contains interpolated theoretical gap values to use as starting points for gap scans, then scans the ID gap at each energy and writes the peak value to another .csv file. The calibration script contains a couple of polarimeter things which can be removed and the detector can be changed to a beamline photodiode.

---
+++ configurations/i21-config/scripts/calibration/2017-10-24_id_LV.py
@@ -0,0 +1,33 @@
+#pos FPSelection 24.43 # DET3
+gap_filename = '/dls_sw/i21/scripts/Polarimeter/LVgap50eV.csv'
+fh = open(gap_filename, 'r')
+ 
+energy_gap = []
+ 
+for line in fh:
+    e, gap = line.split(',')
+    energy_gap.append((e, gap))
+     
+     
+     
+fh.close()
+
+print(energy_gap)
+ 
+write_gap_file = '/dls_sw/i21/scripts/Polarimeter/LVgap50eVReal.csv'
+wh = open(write_gap_file, 'w')
+
+for e, gap in energy_gap:
+    print e, gap
+    dgap=1  
+    pos pgmEnergy e
+    pos idscannable [gap, 'LV', 0]
+   
+    dscan idgap -dgap dgap 21 ca02sr 1
+    peak_val = scan_processor.results['peak'].scn.idgap
+       
+    wh.write(str(e) + ',' + str(peak_val))
+  
+wh.close()
+               
+#pos FPSelection 18.43 # through-beam
