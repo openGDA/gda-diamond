8a6cb05bcc2d227b38e101c353c1c31200f2fe7e
Iain Hall <iain.hall@diamond.ac.uk>
2017-11-30 15:46:56 +0000

    B24-18 : Updates from beamline
    Latest updates from currently deployed version of GDA (8.44) :
     - 'X-ray eye' cameras added
     - motor controls for s1 slit added
     - beamlinestaff.xml updated
     - Controls for d6, d7 filters added
     - Latest version of localStation.py and associated scripts

--- configurations/b24-config/scripts/localStation.py
+++ configurations/b24-config/scripts/localStation.py
@@ -34,3 +34,18 @@ note.rootNamespaceDict=globals()
 
 print "Adding timer devices t, dt, and w, clock"
 from gdascripts.scannable.timerelated import t, dt, w, clock, epoch #@UnusedImport
+
+# Function to reset gda motor limits to match Epics motor limits. imh 6/7/2016
+def resetMotorLimits( motorScannable ):
+    lowerMotorLimit = motorScannable.getLowerMotorLimit()
+    upperMotorLimit = motorScannable.getUpperMotorLimit()
+    motorScannable.setLowerGdaLimits( lowerMotorLimit )
+    motorScannable.setUpperGdaLimits( upperMotorLimit )
+
+from b24_utilities import wd, cfn, nfn, pwd, nwd
+alias("wd")
+alias("cfn")
+alias("nfn")
+alias("pwd")
+alias("nwd")
+
