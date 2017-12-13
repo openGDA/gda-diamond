0c37803a287eaaf7222c90cf30005dc1bd33cc39
Peter Holloway <peter.holloway@diamond.ac.uk>
2017-12-13 14:19:07 +0000

    I11-143 Remove deprecated scripts from i11-1/bin

--- configurations/i11-1-config/bin/remotestopscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-umask 002
-export BEAMLINE=i11
-
-. /dls_sw/$BEAMLINE/etc/${BEAMLINE}_profile.sh
-#put stop messages to the same gda output log file as it is started with.
-/dls_sw/$BEAMLINE/software/gda/config/bin/GDA_StopServers >> /dls_sw/$BEAMLINE/logs/gda_output.txt 2>&1 &
