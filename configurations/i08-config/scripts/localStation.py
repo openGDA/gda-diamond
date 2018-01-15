691719f58209bf4538dffd24fe90f1d197be523b
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-01-15 15:41:03 +0000

    I08: remove Andor configuration for old-style scans
    Change-Id: Ib2811a44e4ae17731c82295296b3e411e9010d4c

--- configurations/i08-config/scripts/localStation.py
+++ configurations/i08-config/scripts/localStation.py
@@ -1,15 +1,12 @@
 from gda.configuration.properties import LocalProperties
 
-# Add config/scripts to import search path (why is this not already set in gda9?). 
+# Add config/scripts to import search path (why is this not already set in gda9?).
 # Also, this seems to be different to run search path... imh 11/11/2016
-import sys 
+import sys
 import os
+
 scriptDir=LocalProperties.get("gda.config")+"/scripts/"
 sys.path.append(os.path.abspath(scriptDir))
-
-from andormap import AndorMap
-from andormapWithCorrection import AndorMapWithCorrection
-from scanForImageCorrection import ScanForImageCorrection
 
 print "Initialisation Started";
 from gda.device import Scannable
@@ -39,17 +36,6 @@ showtime=showtimeClass('showtime')
 inctime=showincrementaltimeClass('inctime')
 actualTime=actualTimeClass("actualTime")
 
-# create the command to run STXM mpas which involve andor
-andormap = AndorMap(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor)  # @UndefinedVariable
-
-alias("andormap")
-print "Command andormap(mapSize) created for arming the Andor detector before running STXM maps"
-
-#To obtain the corrected images just use the collection strategy for step scan
-scanForImageCorrection = ScanForImageCorrection(andor)  # @UndefinedVariable
-andormapWithCorrection = AndorMapWithCorrection(stxmDummy.stxmDummyY,stxmDummy.stxmDummyX,_andorrastor,scanForImageCorrection)  # @UndefinedVariable
-alias("andormapWithCorrection")
-
 # Property so that user can drag ROIs in FluorescenceDetector views. imh 11/11/2016
 LocalProperties.set("exafs.editor.overlay.Preference", "True")
 
@@ -61,7 +47,7 @@ run('mapping_scan_commands.py')
 run('energyFocus.py')
 
 # Watchdogs
-print "Adding watchdog commands: enable_watchdogs, disable_watchdogs, list_watchdogs"
+print("Adding watchdog commands: enable_watchdogs, disable_watchdogs, list_watchdogs")
 from watchdogs import enable_watchdogs, disable_watchdogs, list_watchdogs
 alias("enable_watchdogs")
 alias("disable_watchdogs")
