1be94dc9dd8fae46d355b68aed95e58ceb01988a
fajinyuan <fajin.yuan@diamond.ac.uk>
2018-01-22 11:09:05 +0000

    I06-376 add 'rotate' and 'unrotate' commands
    - add temperature for EC3
    - refactor to support dummy running
    - remove UView support (using EPICS PCO now)

--- configurations/i06-config/scripts/localStation.py
+++ configurations/i06-config/scripts/localStation.py
@@ -1,6 +1,5 @@
 #localStation.py
 #For beamline specific initialisation code.
-from i06shared import installation
 
 print "===================================================================";
 print "Performing Beamline I06 specific initialisation code (localStation.py).";
@@ -13,25 +12,22 @@ print "      >>>scansReturnToOriginalPositions=1"
 scansReturnToOriginalPositions=0;
 print
 import sys
+from gdascripts.utils import caput, caget
+from gdascripts.pd.epics_pds import DisplayEpicsPVClass
 from gda.jython.commands.GeneralCommands import alias
 from gda.jython.commands.ScannableCommands import scan
-
-# try:
-#     execfile("/dls_sw/i06/software/gda/i06-shared/scripts/i06shared/localStation.py");
-# except:
-#     exceptionType, exception, traceback=sys.exc_info();
-#     print "Error:  execfile i06-shared 'localStation.py'"
-#     logger.dump("---> ", exceptionType, exception, traceback)
-
+from i06shared import installation
 from i06shared.localStation import *  # @UnusedWildImport
     
 #from scan.fastEnergyScan import zacscan,zacstop,zacmode,fesController,fesData, fastEnergy,uuu,beamlineutil  # @UnusedImport
 
 # customised resources for PEEM line
 from BeamlineI06.beamline import peemline, getTitle,gettitle,getvisit,getVisit,lastscan,setDir,setdir,setTitle,settitle,setVisit,setvisit  # @UnusedImport
+from BeamlineI06.createAlias import closebeam, openbeam  # @UnusedImport
+
+#To eLog the scan
 #from i06shared.setSrsDataFileHeader import fileHeader
 fileHeader.setScanLogger(peemline)
-from BeamlineI06.createAlias import closebeam, openbeam  # @UnusedImport
 
 if installation.isLive():
     from BeamlineI06.U1Scaler8513 import ca51sr,ca52sr,ca53sr,ca54sr,scalar3  # @UnusedImport
@@ -41,24 +37,18 @@ if installation.isLive():
 else:
     print "Running in dummy mode"
 
-#from slits.useS4 import news4xgap, news4ygap  # @UnusedImport
-#from slits.useS4 import s4ygap, s4xgap
-#To eLog the scan
-
-USE_UVIEW=False
-if USE_UVIEW:
-    try:
-        from peem.usePEEM import roi1,roi2,roi3,roi4,uvroi,uvpreview,uvimaging,picture  # @UnusedImport
-        #the following line is only for TCPIP connection in GDA.
-        from peem.uv_leem_reconnect import reconnect  # @UnusedImport
-        from peem.idio_peem_rois import roi1io,roi2io,roi3io,roi4io  # @UnusedImport
-    except:
-        exceptionType, exception, traceback=sys.exc_info();
-        print "Error:  import 'from peem.usePEED'"
-        logger.dump("---> ", exceptionType, exception, traceback)
+# USE_UVIEW=False
+# if USE_UVIEW:
+#     try:
+#         from peem.usePEEM import roi1,roi2,roi3,roi4,uvroi,uvpreview,uvimaging,picture  # @UnusedImport
+#         #the following line is only for TCPIP connection in GDA.
+#         from peem.uv_leem_reconnect import reconnect  # @UnusedImport
+#         from peem.idio_peem_rois import roi1io,roi2io,roi3io,roi4io  # @UnusedImport
+#     except:
+#         exceptionType, exception, traceback=sys.exc_info();
+#         print "Error:  import 'from peem.usePEED'"
+#         logger.dump("---> ", exceptionType, exception, traceback)
     
-
-
 #To add PEEM line device position to the SRS file header
 print "-"*100
 print "Add metadata required by PEEM to file header"
@@ -70,14 +60,32 @@ fileHeader.add([psx, psy]);  # @UndefinedVariable
 m3legs = [m3leg1, m3leg2, m3leg3, m3leg4, m3leg5, m3leg6];  # @UndefinedVariable
 
 #PEEM End Station
-from peem.leem_instances import leem2000, leem_fov, leem_obj, leem_stv, leem_objStigmA, leem_objStigmB, leem_p2alignx  # @UnusedImport
-fileHeader.add([leem_fov, leem_obj, leem_stv, leem_objStigmA, leem_objStigmB])
-from peem.stv_obj_instance import stvobj  # @UnusedImport
-from peem.LEEM2000_scannables_init import leem_rot,leem_temp,objAlignY,objAlignX  # @UnusedImport
+if installation.isLive():
+    from peem.leem_instances import leem2000, leem_fov, leem_obj, leem_stv, leem_objStigmA, leem_objStigmB, leem_p2alignx  # @UnusedImport
+    fileHeader.add([leem_fov, leem_obj, leem_stv, leem_objStigmA, leem_objStigmB])
+    from peem.stv_obj_instance import stvobj  # @UnusedImport
+    from peem.LEEM2000_scannables_init import leem_rot,leem_temp,objAlignY,objAlignX  # @UnusedImport
+else:
+    print "No simulation for LEEM control yet!"
 
 def picture(acqTime):
     scan(t,1,1,1,pcotif,acqTime)  # @UndefinedVariable
 alias("picture")
+
+def unrotate():
+    caput("BL06I-EA-DET-01:ROT:Angle",0)
+alias("unrotate")
+
+def rotate():
+    rot=caget("BL06I-EA-LEEM-01:CALC:ROT:ANGLE")
+    caput("BL06I-EA-DET-01:ROT:Angle",rot)
+alias("rotate")
+    
+# I06-406   
+temp1_EC3=DisplayEpicsPVClass('temp1_EC3','BL06I-EA-EC3-01:TEMP1','C','%f')
+temp2_EC3=DisplayEpicsPVClass('temp2_EC3','BL06I-EA-EC3-01:TEMP2','C','%f')
+temp3_EC3=DisplayEpicsPVClass('temp3_EC3','BL06I-EA-EC3-01:TEMP3','C','%f')
+temp4_EC3=DisplayEpicsPVClass('temp4_EC3','BL06I-EA-EC3-01:TEMP4','C','%f')
 
 from gda.jython.commands.ScannableCommands import add_default
 add_default([fileHeader]);
