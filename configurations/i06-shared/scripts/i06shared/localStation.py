7fddc6fcf82d25d7e797176a23c7889d044e42dd
fajinyuan <fajin.yuan@diamond.ac.uk>
2018-01-30 13:11:39 +0000

    I06-431 i06-1 dummy bean configurations

--- configurations/i06-shared/scripts/i06shared/localStation.py
+++ configurations/i06-shared/scripts/i06shared/localStation.py
@@ -53,8 +53,8 @@ from gdascripts.constants import * #@UnusedWildImport
 
 from gdascripts.scan.installStandardScansWithProcessing import * # @UnusedWildImport
 import gdascripts.scan.concurrentScanWrapper
-scan_processor.rootNamespaceDict=globals()
-gdascripts.scan.concurrentScanWrapper.ROOT_NAMESPACE_DICT = globals()
+scan_processor.rootNamespaceDict=globals()  # @UndefinedVariable
+gdascripts.scan.concurrentScanWrapper.ROOT_NAMESPACE_DICT = globals()  # @UndefinedVariable
 scan_processor_normal_processes = scan_processor.processors
 scan_processor_empty_processes  = []
  
