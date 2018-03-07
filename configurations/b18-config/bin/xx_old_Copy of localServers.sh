43db1c7aea8093d201f2fced6cfa621b91cf1f18
Iain Hall <iain.hall@diamond.ac.uk>
2018-03-07 10:57:59 +0000

    DAQ-850 : Update B18 config for gda 9
     - Local properties tidied up (commented out sections removed)
     - Now using logging config from core - local logging config removed
     - context property placeholder is now used to import local properties
     into spring config.
     - gda, gdaservers startup scripts updated
     - mythenEpics bean commented out - until this object is merged in and
    available in gda 9.
     - Client product files updated to set the necessary directory paths.
    
    Change-Id: I7c4538a899ffc7075898137806f9f5287bc0abc1

--- configurations/b18-config/bin/xx_old_Copy of localServers.sh
+++
@@ -1,2 +0,0 @@
-export JAVA_HOME=/usr
-/gda_workspaces/master_git/gda-core.git/uk.ac.gda.core/bin/gda servers --datadir /scratch/users/data  --config  /gda_workspaces/master_git/gda-dls-beamlines-xas.git/b18 --properties /gda_workspaces/master_git/gda-dls-beamlines-xas.git/b18/properties/dummy/java.properties --mode=dummy --debug -p 8001 --restart
--- configurations/b18-config/bin/xx_old_localServers.sh
+++
@@ -1,2 +0,0 @@
-export JAVA_HOME=/usr
-/scratch/ws/b18/workspace_git/gda-core.git/uk.ac.gda.core/bin/gda servers --datadir /scratch/users/data  --config  /scratch/ws/b18/config --properties /scratch/ws/b18/config/properties/dummy/java.properties --mode=dummy --debug -p 8001 --restart
