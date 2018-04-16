ab14315bfccb7b95d9274dad75427cff700d4bac
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-04-16 18:01:41 +0100

    DASCTEST-501 Fix paths now i14 config has moved
    Change-Id: I3f619546a5a561b638b5fed6ce4606fa9a4bec65

--- configurations/i14-config/etc/live/i14_profile.sh
+++ configurations/i14-config/etc/live/i14_profile.sh
@@ -11,7 +11,7 @@ fi
 export GDA_INSTANCE_NAME=${BEAMLINE}
 
 export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
-export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/workspace_git/gda-dls-beamlines-xas.git/i14
+export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/workspace_git/gda-diamond.git/configurations/i14-config
 
 export PATH=$GDA_INSTANCE_CONFIG/bin:${PATH}
 export GDA_MODE=live
