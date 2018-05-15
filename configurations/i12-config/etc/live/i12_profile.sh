2b31265f71d97cee3248bffd6d08b13d3859d81e
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-05-15 16:13:42 +0100

    [DAQ-1441] I12: update scripts etc. for new config location
    Change-Id: Id38422684370671e85844fb0c99a25dc1a778bcb

--- configurations/i12-config/etc/live/i12_profile.sh
+++ configurations/i12-config/etc/live/i12_profile.sh
@@ -10,7 +10,7 @@ fi
 export GDA_INSTANCE_NAME=${BEAMLINE}
 
 export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
-export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/workspace_git/gda-dls-beamlines-i12.git/i12
+export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/workspace_git/gda-diamond.git/configurations/i12-config
 
 export PATH=$GDA_INSTANCE_CONFIG/bin:${PATH}
 export GDA_MODE=live
@@ -19,7 +19,7 @@ export MATLABPATH=/dls_sw/$BEAMLINE/software/tomoTilt/code/release
 export PATH=${MATLABPATH}:/dls_sw/dasc/bin/iKittenScripts:${PATH}
 
 # If for whatever reason GDA_Launchers failed to be created on user login, this will create it when user open a terminal.
-case $DISPLAY in 
+case $DISPLAY in
 	:0.0|${BEAMLINE}*:0)
 		( cd $HOME/Desktop && test -e "GDA_Launchers" || ln -s /usr/local/etc/"GDA_Launchers" . ) > /dev/null 2>&1
 		( cd $HOME/Desktop && test -e "${BEAMLINE}_Launchers" || ln -s /usr/local/etc/"${BEAMLINE}_Launchers" . ) > /dev/null 2>&1
