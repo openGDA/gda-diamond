46964baca112241cedb9e54dbb8f75de5c6c4721
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2018-05-29 17:13:13 +0100

    remove unused import

--- configurations/i06-shared/scripts/i06shared/scan/idd_fast_energy_scan.py
+++ configurations/i06-shared/scripts/i06shared/scan/idd_fast_energy_scan.py
@@ -22,7 +22,6 @@ from Diamond.Utility.UtilFun import UtilFunctions
 from Diamond.Utility.BeamlineFunctions import BeamlineFunctionClass, logger
 
 from gda.configuration.properties import LocalProperties
-from time import sleep
 
 uuu=UtilFunctions();
 beamline_name = LocalProperties.get(LocalProperties.GDA_BEAMLINE_NAME, "i06")
