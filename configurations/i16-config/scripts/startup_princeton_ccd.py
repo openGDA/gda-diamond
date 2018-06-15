eddebaf29159e82d71d51fc3593da23f0b16dfd7
Mark Booth <Mark.Booth@diamond.ac.uk>
2018-06-15 15:42:33 +0100

    I16-185 (9.8) Update live mode for GDA9
    Remove all Swing panel_name references and/or replace them with RCP
    panel_name_rcp references.
    
    Replace reference to gda_user_scripts with standard beamline scripts.
    
    Restore standard GDA9 gda* commands for the Diamond launcher
    
    Remove old GDA 8 testgda* scripts.

--- configurations/i16-config/scripts/startup_princeton_ccd.py
+++ configurations/i16-config/scripts/startup_princeton_ccd.py
@@ -5,7 +5,7 @@ reload(princeton)
 
 ccddet = princeton.PrincetonDetector('ccddet', x2trig, readout_time=2, filename_fmt='/dls/i16/data/2012/mt7277-1/ccddata/test%i.TIF')
 
-ccd = ProcessingDetectorWrapper('ccd', ccddet, [], root_datadir='/dls/i16/data/2012/mt7277-1/', panel_name='Pilatus 100k', iFileLoader=TIFFImageLoader, fileLoadTimout=24*3600) 
+ccd = ProcessingDetectorWrapper('ccd', ccddet, [], root_datadir='/dls/i16/data/2012/mt7277-1/', panel_name_rcp='Plot 1', iFileLoader=TIFFImageLoader, fileLoadTimout=24*3600) 
 ccd.display_image = False
 
 ccd.processors=[DetectorDataProcessorWithRoi('max', ccd, [SumMaxPositionAndValue()], False)]
