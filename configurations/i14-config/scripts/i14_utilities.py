66e188997105c7950c95573e5169fd7ead184c06
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-07-19 16:11:19 +0100

    [I14-467] Tidy up localStation & other scripts
    - put brackets round print() statements
    - remove unused imports
    - delete (old) Jython modules from config: newer ones are in
    /dls_sw/i14/scripts/*
    - use standard formatting for Jython
    - import shutter definitions from gdaserver.py rather than using finder
    
    Also: more realistic default values for dcm-related motors in dummy mode
    
    Change-Id: I9d37bf113fbd9cdaedfb08dd6e468c2368ba0570

--- configurations/i14-config/scripts/i14_utilities.py
+++ configurations/i14-config/scripts/i14_utilities.py
@@ -2,7 +2,7 @@ from gda.configuration.properties import LocalProperties
 from gda.data import NumTracker
 from gda.factory import Finder
 
-print "Running i14_utilities.py..."
+print("Running i14_utilities.py...")
 
 i14NumTracker = NumTracker("i14")
 finder = Finder.getInstance()
--- configurations/i14-config/scripts/shutter_control.py
+++ configurations/i14-config/scripts/shutter_control.py
@@ -1,15 +1,12 @@
 # Scripts to control shutters
 # These are used by the status view in the client.
 import time
-from gda.factory import Finder
+from gdaserver import oh1_shutter_status, oh2_shutter_status, oh3_shutter_status, eh2_nano_shutter_status
 from gda.jython.commands.ScannableCommands import pos
 
-print "Defining shutter controls"
-
-finder = Finder.getInstance()
+print("Defining shutter controls")
 
 def toggle_oh1_shtr():
-    oh1_shutter_status = finder.find("oh1_shutter_status")
     if oh1_shutter_status() == "Open":
         pos(oh1_shutter_status, "Close")
     else:
@@ -18,7 +15,6 @@ def toggle_oh1_shtr():
         pos(oh1_shutter_status, "Open")
 
 def toggle_oh2_shtr():
-    oh2_shutter_status = finder.find("oh2_shutter_status")
     if oh2_shutter_status() == "Open":
         pos(oh2_shutter_status, "Close")
     else:
@@ -27,7 +23,6 @@ def toggle_oh2_shtr():
         pos(oh2_shutter_status, "Open")
 
 def toggle_oh3_shtr():
-    oh3_shutter_status = finder.find("oh3_shutter_status")
     if oh3_shutter_status() == "Open":
         pos(oh3_shutter_status, "Close")
     else:
@@ -36,7 +31,6 @@ def toggle_oh3_shtr():
         pos(oh3_shutter_status, "Open")
 
 def toggle_eh2_nano_shtr():
-    eh2_nano_shutter_status = finder.find("eh2_nano_shutter_status")
     if eh2_nano_shutter_status() == "Open":
         pos(eh2_nano_shutter_status, "Close")
     else:
