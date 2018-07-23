4b3ca1aa5fc3a0da2ab9c986eb9c60c29f963402
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2018-07-23 14:19:11 +0100

    I10-331 make sure metadata persist betwwen scan and cvscan

--- configurations/i10-config/scripts/localStation.py
+++ configurations/i10-config/scripts/localStation.py
@@ -142,8 +142,21 @@ rmotors=MetaDataPD("rmotors", [tth, th, chi, eta, ttp, thp, py, pz, dsu, dsd, di
 #add_default rmotors
 
 # meta data
-from metadata.metadataItems import *  # @UnusedWildImport
 try:
+    from gda.jython.commands.ScannableCommands import add_default
+    from gdaserver import idd_gap, idd_rowphase1, idd_jawphase,\
+        idd_rowphase3, idd_rowphase4, idd_rowphase2, idd_sepphase, idu_gap,\
+        idu_rowphase1, idu_rowphase2, idu_jawphase, idu_rowphase3, idu_rowphase4,\
+        idu_sepphase, pgm_grat_pitch, pgm_m2_pitch,pgm_energy
+        
+    print '-'*80
+    print "Define metadata list for data collection:"
+    iddmetadatascannables = (idd_gap,idd_rowphase1,idd_rowphase2,idd_rowphase3,idd_rowphase4,idd_jawphase,idd_sepphase)
+    idumetadatascannables = (idu_gap,idu_rowphase1,idu_rowphase2,idu_rowphase3,idu_rowphase4,idu_jawphase,idu_sepphase)
+    pgmmetadatascannables = (pgm_energy, pgm_grat_pitch, pgm_m2_pitch)
+
+    stdmetadatascannables = iddmetadatascannables + idumetadatascannables + pgmmetadatascannables
+
     #SRS file metadata only works when run in localStation.py - see globals()
     print "-"*50
     print "SRS or ASCII file metadata command:"
@@ -156,9 +169,20 @@ try:
     stdmeta()
     print "    Use 'stdmeta' to reset to standard scannables"
     alias('stdmeta')
-    from gda.jython.commands.ScannableCommands import add_default
     add_default(meta)
     meta.quiet = True
+    
+    #Nexus file
+    print "-"*50
+    print "Nexus file metadata commands:"
+    print "    'meta_add' - add a scannable or scannables to the scan metadata"
+    print "    'meta_ll'  - list the items and their values to be put into the scan metadata"
+    print "    'meta_ls'  - list only the items to be put into the scan metadata"
+    print "    'meta_rm'  - remove a scannable or scannables from the scan metadata"
+
+    from metadata.metashop import  *  # @UnusedWildImport
+    for each in stdmetadatascannables:
+        meta_add(each)
 except:
     localStation_exception(sys.exc_info(), "creating SRS file metadata objects")
 
