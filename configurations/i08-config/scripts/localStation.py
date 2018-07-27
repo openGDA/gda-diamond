351cbc4feb83c16eb8cc699275feada0b7ba630d
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-07-27 13:53:07 +0100

    [I08-134] Tidy localStation
    - Use Eclipse to fix formatting and organise imports
    - Call print as a function
    - Suppress warnings about unused imports
    
    Change-Id: I1f9aea473fc92149ebf33c6a918533173d6a227a

--- configurations/i08-config/scripts/localStation.py
+++ configurations/i08-config/scripts/localStation.py
@@ -1,16 +1,22 @@
-from gda.configuration.properties import LocalProperties
-
-import sys
 import os
+import sys
+
+from ScannableInvertedValue import PositionInvertedValue
+from gda.configuration.properties import LocalProperties
+from gda.device import Scannable
+from gda.factory import Finder
+from gda.jython.commands.GeneralCommands import ls_names, run, alias # @UnusedImport
+from gdascripts.pd.time_pds import waittimeClass, showtimeClass, showincrementaltimeClass, actualTimeClass
+from gdascripts.scan.installStandardScansWithProcessing import *  # @UnusedWildImport
+from gdascripts.watchdogs.watchdogs import enable_watchdogs, disable_watchdogs, list_watchdogs # @UnusedImport
+
 
 # Add config/scripts to import search path (why is this not already set in gda9?).
 # Also, this seems to be different to run search path... imh 11/11/2016
-scriptDir=LocalProperties.get("gda.config")+"/scripts/"
+scriptDir = LocalProperties.get("gda.config") + "/scripts/"
 sys.path.append(os.path.abspath(scriptDir))
 
 print("Initialisation Started");
-from gda.device import Scannable
-from gda.jython.commands.GeneralCommands import ls_names, run, alias
 
 def ls_scannables():
     ls_names(Scannable)
@@ -19,11 +25,9 @@ def is_live():
     mode = LocalProperties.get("gda.mode")
     return mode == 'live'
 
-from gda.factory import Finder
 finder = Finder.getInstance()
 
-from ScannableInvertedValue import PositionInvertedValue
-photoDiode1Inverted = PositionInvertedValue("photoDiode1Inverted","photoDiode1")
+photoDiode1Inverted = PositionInvertedValue("photoDiode1Inverted", "photoDiode1")
 
 alias("setTitle")
 alias("meta_add")
@@ -31,20 +35,18 @@ alias("meta_ll")
 alias("meta_ls")
 alias("meta_rm")
 
-from gdascripts.scan.installStandardScansWithProcessing import * #@UnusedWildImport
-scan_processor.rootNamespaceDict=globals()  # @UndefinedVariable
+scan_processor.rootNamespaceDict = globals()  # @UndefinedVariable
 
-from gdascripts.pd.time_pds import waittimeClass, showtimeClass, showincrementaltimeClass, actualTimeClass
-waittime=waittimeClass('waittime')
-showtime=showtimeClass('showtime')
-inctime=showincrementaltimeClass('inctime')
-actualTime=actualTimeClass("actualTime")
+waittime = waittimeClass('waittime')
+showtime = showtimeClass('showtime')
+inctime = showincrementaltimeClass('inctime')
+actualTime = actualTimeClass("actualTime")
 
 # Property so that user can drag ROIs in FluorescenceDetector views. imh 11/11/2016
 LocalProperties.set("exafs.editor.overlay.Preference", "True")
 
 # For access to new mscan mapping scan command. imh 11/11/2016
-print "Adding mscan mapping scan command. Use help(mscan) to get information on how to use it."
+print("Adding mscan mapping scan command. Use help(mscan) to get information on how to use it.")
 run('mapping_scan_commands.py')
 
 # Energy to zone plate position
@@ -56,7 +58,6 @@ run('ioc_control.py')
 
 # Watchdogs
 print("Adding watchdog commands: enable_watchdogs, disable_watchdogs, list_watchdogs")
-from gdascripts.watchdogs.watchdogs import enable_watchdogs, disable_watchdogs, list_watchdogs
 alias("enable_watchdogs")
 alias("disable_watchdogs")
 alias("list_watchdogs")
