73b955154d6a8ac0571e8f9e9b6154db4da47c04
Douglas Winter <douglas.winter@diamond.ac.uk>
2018-08-17 14:09:52 +0100

    i18: set Xspress3 number of images to 1 when enabling array plugin
    The Xspress3 driver doesn't do anything with the ImageMode PV, so this
    is an alternative way of requesting a single image only.
    
    Change-Id: I4bef3b7a6f2fa0ae79ec37c9cb98859c56163928

--- configurations/i18-config/scripts/prepare_for_snapshot.py
+++ configurations/i18-config/scripts/prepare_for_snapshot.py
@@ -7,28 +7,28 @@ def caget(pv):
 	return CAClient.get(pv)
 
 def prep_xsp3():
-	"""We enable callbacks in the array plugin, then acquire a single frame"""
-	from gdaserver import Xspress3A
-	caput(Xspress3A.getNdArray().getBasePVName() + "EnableCallbacks", "Enable")
-	ad_base_pv = Xspress3A.getAdBase().getBasePVName()
+        """We enable callbacks in the array plugin, then acquire a single frame"""
+        from gdaserver import Xspress3A
+        caput(Xspress3A.getNdArray().getBasePVName() + "EnableCallbacks", "Enable")
+        ad_base_pv = Xspress3A.getAdBase().getBasePVName()
 
-	# set trigger mode to software
-	prev_trigger_mode = caget(ad_base_pv + "TriggerMode")
-	trigger_mode_changed = prev_trigger_mode != "Software"
-	if (trigger_mode_changed):
-		caput(ad_base_pv + "TriggerMode", "Software")
+        # set trigger mode to software
+        prev_trigger_mode = caget(ad_base_pv + "TriggerMode")
+        trigger_mode_changed = prev_trigger_mode != "Software"
+        if (trigger_mode_changed):
+                caput(ad_base_pv + "TriggerMode", "Software")
 
-	# set image mode to single
-	prev_img_mode = caget(ad_base_pv + "ImageMode")
-	image_mode_changed = prev_img_mode != "Single"
-	if (image_mode_changed):
-		caput(ad_base_pv + "ImageMode", "Single")
+        # Set number of images to 1
+        prev_num_images = caget(ad_base_pv + "NumImages_RBV")
+        num_images_changed = prev_num_images != 1
+        if num_images_changed:
+                caput(ad_base_pv + "NumImages", 1)
 
-	# acquire single frame
-	caput(ad_base_pv + "Acquire", "Acquire")
+        # acquire single frame
+        caput(ad_base_pv + "Acquire", "Acquire")
 
-	# restore trigger and image mode if necessary
-	if (trigger_mode_changed):
-		caput(ad_base_pv + "TriggerMode", prev_trigger_mode)
-	if (image_mode_changed):
-		caput(ad_base_pv + "ImageMode", prev_img_mode)
+        # restore trigger and image mode if necessary
+        if (trigger_mode_changed):
+                caput(ad_base_pv + "TriggerMode", prev_trigger_mode)
+        if (num_images_changed):
+                caput(ad_base_pv + "NumImages", prev_num_images)
