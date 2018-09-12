e250c4587a5131f66793b4910dce3575171985ba
Iain Hall <iain.hall@diamond.ac.uk>
2018-09-12 13:56:00 +0100

    I20_1-225 : Add scannable to open/close shutter at start/end of scan
    Scannable that simply open a shutter at the start of a scan and closes
    it at the end. Shutter is also closed if scan finishes early for some reason.
    Unlike ShutterChecker, this scannable simply tries to open/close the shutter,
    without complaining if it's not possible e.g. machine day/shutdown or if
    hutch door is open.
    
    Change-Id: Ie3b824ef7bfc07a807688b8e3bbc28395150502a

---
+++ plugins/uk.ac.gda.beamline.i20_1.server/src/gda/device/scannable/ShutterOpenClose.java
@@ -0,0 +1,104 @@
+/*-
+ * Copyright © 2018 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gda.device.scannable;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import gda.device.DeviceException;
+import gda.device.EnumPositioner;
+import gda.device.enumpositioner.ValvePosition;
+import gda.factory.FactoryException;
+
+/**
+ * Scannable that simply open a shutter at the start of a scan and closes it at the end.
+ * Shutter is also closed if scan finishes early for some reason.
+ *
+ */
+public class ShutterOpenClose extends ScannableBase {
+	private static final Logger logger = LoggerFactory.getLogger(ShutterOpenClose.class);
+
+	/** Shutter to be controlled during the scan */
+	private EnumPositioner shutter;
+
+	public ShutterOpenClose() {
+		inputNames = new String[] {};
+		extraNames = new String[] {};
+		outputFormat = new String[] {};
+	}
+
+	@Override
+	public void configure() throws FactoryException {
+		if (shutter == null) {
+			throw new FactoryException("Shutter has not been set");
+		}
+		super.configure();
+	}
+
+	@Override
+	public Object rawGetPosition() throws DeviceException {
+		return null;
+	}
+
+	@Override
+	public void rawAsynchronousMoveTo(Object position) throws DeviceException {
+		// do nothing
+	}
+
+	@Override
+	public void atScanStart() throws DeviceException {
+		moveShutter(ValvePosition.RESET);
+		moveShutter(ValvePosition.OPEN);
+	}
+
+	@Override
+	public void atScanEnd() throws DeviceException {
+		moveShutter(ValvePosition.RESET);
+		moveShutter(ValvePosition.CLOSE);
+	}
+
+	private void moveShutter(String position) throws DeviceException {
+		logger.debug("{} moving to '{}'", shutter.getName(), position);
+		shutter.moveTo(position);
+		logger.debug("{} move to '{}' complete", shutter.getName(), position);
+	}
+
+	@Override
+	public void atCommandFailure() throws DeviceException {
+		atScanEnd();
+	}
+
+	@Override
+	public void stop() throws DeviceException {
+		atScanEnd();
+	}
+
+	@Override
+	public boolean isBusy() throws DeviceException {
+		return shutter.isBusy();
+	}
+
+	public EnumPositioner getShutter() {
+		return shutter;
+	}
+
+	public void setShutter(EnumPositioner shutter) {
+		this.shutter = shutter;
+	}
+}
