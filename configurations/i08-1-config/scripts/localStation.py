2a7dd50c1dba8407dcc5e89c3046dacafbe78dc8
Anthony Hull <anthony.hull@diamond.ac.uk>
2018-10-19 12:15:55 +0100

    [DAQ-1692] Skeleton configuration for I08-1
    As there is no control machine or network drive for I08-1 yet,
    this config is for dummy mode only and contains stage/zone plate motors
    and an andor detector.
    
    It starts up (in dummy mode) and shows a mapping perspective containing
    the above motors & detector
    
    Change-Id: Ia3201773e3bc97e857a86f24c4da4e2804d6ab0c

---
+++ configurations/i08-1-config/scripts/localStation.py
@@ -0,0 +1,21 @@
+from gdascripts.scan.installStandardScansWithProcessing import *  # @UnusedWildImport
+from gdascripts.watchdogs.watchdogs import enable_watchdogs, disable_watchdogs, list_watchdogs  # @UnusedImport
+from gda.jython.commands.GeneralCommands import run
+
+print("Initialisation Started");
+
+run("i08-1_utilities.py")
+
+scan_processor.rootNamespaceDict = globals()  # @UndefinedVariable
+
+# For access to new mscan mapping scan command.
+print("Adding mscan mapping scan command. Use help(mscan) to get information on how to use it.")
+run('mapping_scan_commands.py')
+
+# Watchdogs
+print("Adding watchdog commands: enable_watchdogs, disable_watchdogs, list_watchdogs")
+alias("enable_watchdogs")
+alias("disable_watchdogs")
+alias("list_watchdogs")
+
+print("Initialisation Complete");
