8908ca83c2fe6ec36f6b1922c5e65504a9b0dab0
Douglas Winter <douglas.winter@diamond.ac.uk>
2018-11-05 10:51:55 +0000

    DAQ-1765 Create minimal config project for K11 (DIAD)
    Change-Id: Ic7797a53d80cd46eb925f84e14559a61359bda81

---
+++ configurations/k11-config/etc/live/k11_profile.sh
@@ -0,0 +1,14 @@
+# k11 beamline profile
+
+if [ ! -n "$BEAMLINE" ]; then
+  echo "ERROR: BEAMLINE not set" 1>&2
+  exit 1
+fi
+
+export GDA_INSTANCE_NAME=${BEAMLINE}
+
+export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
+export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/workspace_git/gda-diamond.git/configurations/k11-config
+
+export PATH=$GDA_INSTANCE_CONFIG/bin:${PATH}
+export GDA_MODE=live
