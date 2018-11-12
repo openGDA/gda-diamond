50b78426f6ae8a677dd60d1de5f92d5633030d41
Keith Ralphs <keith.ralphs@diamond.ac.uk>
2018-11-12 13:52:52 +0000

    I07-147 Update scripts to work with GDA9/Diffcalc 2.1
    Scripts updated by Jonathan Rawle to be added to source control
    
    Change-Id: I2627ba38586f656c69f84969034487b13c7a6950

--- configurations/i07-config/scripts/BeamlineI07/Users/restorerois.py
+++ configurations/i07-config/scripts/BeamlineI07/Users/restorerois.py
@@ -1,23 +1,32 @@
 import scisoftpy as dnp
 from gda.data import PathConstructor
 
-def restorerois(scan,detector):
+def restorerois(scan):
     if type(scan) == int:
         f = dnp.io.load(PathConstructor.createFromDefaultProperty()+"/"+ str(scan) + ".dat")
     else:
         f = dnp.io.load(scan)
     roi = 0
+    bean = dnp.plot.getbean(name="Area Detector")
+    oldrois = dnp.plot.getrois(bean)
+    newrois = dnp.plot.roi_list()
     while True:
-        roi += 1
-        if 'roi' + str(roi) + '_X' in f.keys():
-
-            detector.addRoi(f[f.keys().index('roi' + str(roi) + '_X')][0],
-                            f[f.keys().index('roi' + str(roi) + '_Y')][0],
-                            f[f.keys().index('roi' + str(roi) + '_Width')][0],
-                            f[f.keys().index('roi' + str(roi) + '_Height')][0],
-                            f[f.keys().index('roi' + str(roi) + '_Angle')][0],
-                            )
+        r = dnp.plot.roi.rectangle()
+        if 'roi' + str(roi+1) + '_X' in f.keys():
+            roi += 1
+            r.setName('Region ' + str(roi))
+            r.setPoint((f[f.keys().index('roi' + str(roi) + '_X')][0], f[f.keys().index('roi' + str(roi) + '_Y')][0]))
+            r.setLengths((f[f.keys().index('roi' + str(roi) + '_Width')][0], f[f.keys().index('roi' + str(roi) + '_Height')][0]))
+            r.setAngle(f[f.keys().index('roi' + str(roi) + '_Angle')][0])
+            newrois.append(r)
         else:
             break
-    
-    print str(roi-1) + " ROIs added"
+    if roi > 0:
+        # doesn't work unless we set a single ROI first; coords unimportant as this is overwritten anyway
+        roi1 = dnp.jython.jyroi.rectangle()
+        roi1.setName("Region 1")
+        dnp.plot.setroi(bean, roi1)
+
+        dnp.plot.setrois(bean, newrois)
+        dnp.plot.setbean(bean, name="Area Detector")
+    print str(roi) + " ROIs added"
