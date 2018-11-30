b90353277088de107a80c710ebfc02b81478a6e4
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2018-11-30 09:41:36 +0000

    I10-367: 'cvscan' command by default does not with 'egy_g' object
    - extend 'cvscan' to support 'egy_g' and 'egy' using ID energy followers
    - set correct motion controller and ID energy follower for 'egy_g' and 'egy'
    - does not change the default support for 'energy'
    
    Change-Id: Ifefae972af3c923d2c6f0468a9b8d0a46c1c3363
    Task-Url: https://jira.diamond.ac.uk/browse/I10-367

--- configurations/i10-config/scripts/scan/cvscan.py
+++ configurations/i10-config/scripts/scan/cvscan.py
@@ -12,11 +12,20 @@ from gdascripts.scan.installStandardScansWithProcessing import scan_processor
 from gda.jython.commands.GeneralCommands import alias 
 from scannable.waveform_channel.WaveformChannelScannable import WaveformChannelScannable
 from numbers import Number
-from com.sun.org.apache.xpath.internal import Arg
 from scannable.continuous.continuous_energy_scannables import binpointGrtPitch_g,\
-    binpointMirPitch_g, binpointPgmEnergy_g
-from scannable.checkbeanscannables import checkbeamcv,\
-    ZiePassthroughScannableDecorator
+    binpointMirPitch_g, binpointPgmEnergy_g, cemc_g, cemc
+from scannable.checkbeanscannables import ZiePassthroughScannableDecorator
+from scannable.idcontrols.mode_polarisation_energy_instances import smode, pol
+from scannable.idcontrols.sourceModes import SourceMode
+from scannable.idcontrols.polarisation import Polarisation
+from scannable.id_energys.idd_energy_gap import idd_circ_pos_energy_follower,\
+    idd_circ_neg_energy_follower, idd_lin_hor_energy_follower,\
+    idd_lin_ver_energy_follower
+from scannable.id_energys.idd_lin_energy import idd_lin_arbitrary_energy_follower
+from scannable.id_energys.idu_energy_gap import idu_lin_hor3_energy_follower,\
+    idu_circ_pos_energy_follower, idu_circ_neg_energy_follower,\
+    idu_lin_hor_energy_follower, idu_lin_ver_energy_follower
+from scannable.id_energys.idu_lin_energy import idu_lin_arbitrary_energy_follower
 
 class TrajectoryControllerHelper(ScanListener):
     def __init__(self): # motors, maybe also detector to set the delay time
@@ -37,38 +46,91 @@ print "-"*100
 print "Creating I10 GDA 'cvscan' commands: - dwell time must apply to all waveform scannables individually!"
 cvscan_traj=trajscans.CvScan([scan_processor, trajectory_controller_helper]) 
 
+
+def getEnergyFollower4CurrentSmodePolarisation():
+    id_energy_follower=None
+    mode = smode.getPosition()
+    polarisation = pol.getPosition()
+    if mode == SourceMode.SOURCE_MODES[0]:
+        if polarisation == Polarisation.POLARISATIONS[0]:
+            id_energy_follower = idd_circ_pos_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[1]:
+            id_energy_follower = idd_circ_neg_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[2]:
+            id_energy_follower = idd_lin_hor_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[3]:
+            id_energy_follower = idd_lin_ver_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[4]:
+            id_energy_follower = idd_lin_arbitrary_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[5]:
+            raise Exception('Polarisation %s for source mode % is not supported: Energy Calibration is not available!' % (polarisation, mode))
+    elif mode == SourceMode.SOURCE_MODES[1]:
+        if polarisation == Polarisation.POLARISATIONS[0]:
+            id_energy_follower = idu_circ_pos_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[1]:
+            id_energy_follower = idu_circ_neg_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[2]:
+            id_energy_follower = idu_lin_hor_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[3]:
+            id_energy_follower = idu_lin_ver_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[4]:
+            id_energy_follower = idu_lin_arbitrary_energy_follower
+        elif polarisation == Polarisation.POLARISATIONS[5]:
+            id_energy_follower = idu_lin_hor3_energy_follower
+    return id_energy_follower
+
 def cvscan(c_energy, start, stop, step, *args):
-    ''' cvscan that checks if there is enough time to collect data before topup when 'checkbeamcv' is used.
+    ''' cvscan that checks if there is enough time to collect data before topup when 'checkbeamcv' is used,
+    it also supports dynamically motion controller switch for energy object 'egy_g' or 'egy' depending on source mode and polarisation
+    and append correct ID energy follower for  'egy_g' or 'egy'.    
     '''
     wfs=[]
     dwell=[]
     others=[]
     beam_checker=None
     newargs=[c_energy, start, stop, step]
-    for arg in args:
-        if isinstance(arg, WaveformChannelScannable):
-            wfs.append(arg)
-        elif isinstance(arg, Number):
-            dwell.append(arg)
-        elif isinstance(arg, ZiePassthroughScannableDecorator):
-            beam_checker=arg
-        else:
-            others.append(arg)
-    if not checkContentEqual(dwell):
-        raise Exception("dwell time specified must be equal for all detectors!")
-    for each in wfs:
-        newargs.append(each)
-        newargs.append(dwell[0])
-    for other in others:
-        newargs.append(other)
-    if beam_checker is not None:
-        #check if there is enough time for the cvscan before topup
-        scanTime=abs((stop-start)/step*dwell[0])
-        topup_checker=beam_checker.getDelegate().getGroupMember("checktopup_time_cv")
-        topup_checker.minimumThreshold=scanTime
-#         print "topup_checker.minimumThreshold = %r" % (topup_checker.minimumThreshold)
-        newargs.append(beam_checker)
-    cvscan_traj([e for e in newargs])
+    try:
+        for arg in args:
+            if isinstance(arg, WaveformChannelScannable):
+                wfs.append(arg)
+            elif isinstance(arg, Number):
+                dwell.append(arg)
+            elif isinstance(arg, ZiePassthroughScannableDecorator):
+                beam_checker=arg
+            else:
+                others.append(arg)
+        if not checkContentEqual(dwell):
+            raise Exception("dwell time specified must be equal for all detectors!")
+        for each in wfs:
+            newargs.append(each)
+            newargs.append(dwell[0])
+        if c_energy.getName() == "egy_g":
+            for each in wfs:
+                each.setHardwareTriggerProvider(cemc_g) # switch to ContinuousPgmGratingEnergyMoveController
+            id_energy_follower = getEnergyFollower4CurrentSmodePolarisation()
+            if id_energy_follower is not None:
+                newargs.append(id_energy_follower) # add the ID energy follower     
+        elif c_energy.getName() == "egy":
+            for each in wfs:
+                each.setHardwareTriggerProvider(cemc) #switch to ContinuousPgmEnergyMoveController
+            id_energy_follower = getEnergyFollower4CurrentSmodePolarisation()
+            if id_energy_follower is not None:
+                newargs.append(id_energy_follower) # add the ID energy follower     
+        for other in others:
+            newargs.append(other)
+        if beam_checker is not None:
+            #check if there is enough time for the cvscan before topup
+            scanTime=abs((stop-start)/step*dwell[0]+5) # add extra 5 second to wait topup time.
+            topup_checker=beam_checker.getDelegate().getGroupMember("checktopup_time_cv")
+            topup_checker.minimumThreshold=scanTime
+    #         print "topup_checker.minimumThreshold = %r" % (topup_checker.minimumThreshold)
+            newargs.append(beam_checker)
+        cvscan_traj([e for e in newargs])
+    finally:
+        if c_energy.getName() == "egy_g" or c_energy.getName() == "egy":
+            for each in wfs:
+                each.setHardwareTriggerProvider(energycontroller)  # @UndefinedVariable
+        
 
 alias('cvscan')
    
@@ -81,40 +143,57 @@ def cvscan2(c_energy, start, stop, step, *args):
     dwell=[]
     others=[]
     newargs=[c_energy, start, stop, step]
-    for arg in args:
-        if isinstance(arg, WaveformChannelScannable):
-            wfs.append(arg)
-        elif isinstance(arg, Number):
-            dwell.append(arg)
-        elif isinstance(arg, ZiePassthroughScannableDecorator):
-            beam_checker=arg
-        else:
-            others.append(arg)
-    if not checkContentEqual(dwell):
-        raise Exception("dwell time specified must be equal for all detectors!")
-    for each in wfs:
-        newargs.append(each)
-        newargs.append(dwell)
-    if c_energy.getName() == "egy_g" or c_energy.getName() == "egy":
-        #set dwell time to embedded instances of WaveformChannelScannable
-        if binpointGrtPitch_g not in wfs:
-            newargs.append(binpointGrtPitch_g)
+    try:
+        for arg in args:
+            if isinstance(arg, WaveformChannelScannable):
+                wfs.append(arg)
+            elif isinstance(arg, Number):
+                dwell.append(arg)
+            elif isinstance(arg, ZiePassthroughScannableDecorator):
+                beam_checker=arg
+            else:
+                others.append(arg)
+        if not checkContentEqual(dwell):
+            raise Exception("dwell time specified must be equal for all detectors!")
+        for each in wfs:
+            newargs.append(each)
             newargs.append(dwell)
-        if binpointMirPitch_g not in wfs:
-            newargs.append(binpointMirPitch_g)
-            newargs.append(dwell)
-        if binpointPgmEnergy_g not in wfs:
-            newargs.append(binpointPgmEnergy_g)
-            newargs.append(dwell)
-    for other in others:
-        newargs.append(other)
-    if beam_checker is not None:
-        #check if there is enough time for the cvscan before topup
-        scanTime=abs((stop-start)/step*dwell[0])
-        topup_checker=beam_checker.getDelegate().getGroupMember("checktopup_time_cv")
-        topup_checker.minimumThreshold=scanTime
-        newargs.append(beam_checker)
-    cvscan_traj([e for e in newargs])
+        if c_energy.getName() == "egy_g":
+            for each in wfs:
+                each.setHardwareTriggerProvider(cemc_g) # switch to ContinuousPgmGratingEnergyMoveController
+            id_energy_follower = getEnergyFollower4CurrentSmodePolarisation()
+            if id_energy_follower is not None:
+                newargs.append(id_energy_follower) # add the ID energy follower     
+        elif c_energy.getName() == "egy":
+            for each in wfs:
+                each.setHardwareTriggerProvider(cemc) #switch to ContinuousPgmEnergyMoveController
+            id_energy_follower = getEnergyFollower4CurrentSmodePolarisation()
+            if id_energy_follower is not None:
+                newargs.append(id_energy_follower) # add the ID energy follower     
+        if c_energy.getName() == "egy_g" or c_energy.getName() == "egy":
+            #set dwell time to embedded instances of WaveformChannelScannable
+            if binpointGrtPitch_g not in wfs:
+                newargs.append(binpointGrtPitch_g)
+                newargs.append(dwell)
+            if binpointMirPitch_g not in wfs:
+                newargs.append(binpointMirPitch_g)
+                newargs.append(dwell)
+            if binpointPgmEnergy_g not in wfs:
+                newargs.append(binpointPgmEnergy_g)
+                newargs.append(dwell)
+        for other in others:
+            newargs.append(other)
+        if beam_checker is not None:
+            #check if there is enough time for the cvscan before topup
+            scanTime=abs((stop-start)/step*dwell[0])
+            topup_checker=beam_checker.getDelegate().getGroupMember("checktopup_time_cv")
+            topup_checker.minimumThreshold=scanTime+5
+            newargs.append(beam_checker)
+        cvscan_traj([e for e in newargs])
+    finally:
+        if c_energy.getName() == "egy_g" or c_energy.getName() == "egy":
+            for each in wfs:
+                each.setHardwareTriggerProvider(energycontroller)  # @UndefinedVariable
     
 def checkContentEqual(lst):
     return lst[1:] == lst[:-1]
