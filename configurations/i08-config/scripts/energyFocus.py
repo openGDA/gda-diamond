0528199c1179d8d1733855cb7b8f11f496cfb45a
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-01-09 15:46:53 +0000

    [DAQ-1846] Changes to I08 code to use new LinearFunction interface
    Change-Id: I24828e06eaae3405f09033bea8f7adf868736b6a

--- configurations/i08-config/scripts/energyFocus.py
+++ configurations/i08-config/scripts/energyFocus.py
@@ -3,6 +3,7 @@ import json
 from os.path import abspath
 
 from gda.configuration.properties import LocalProperties
+from gda.util import QuantityFactory
 from gdaserver import energyFocusFunction
 
 # Load values of energy/focus function from stash file
@@ -20,14 +21,14 @@ def loadEnergyFocus():
         slopeDivisor = stashedValue.get('slopeDivisor')
     except:
         print("Cannot load stored energy focus values from " + abspath(stashFilePath) + ", using defaults")
-        slopeDividend = "0.0 um"
-        interception = "0.0 um"
-        slopeDivisor = "1.0 eV"
+        slopeDividend = u'0.0 µm'
+        interception = u'0.0 µm'
+        slopeDivisor = '1.0 eV'
 
-    energyFocusFunction.setSlopeDividend(slopeDividend)
-    energyFocusFunction.setInterception(interception)
-    energyFocusFunction.setSlopeDivisor(slopeDivisor)
-    print("energyFocusFunction set to: " + str(energyFocusFunction))
+    energyFocusFunction.setSlopeDividend(QuantityFactory.createFromString(slopeDividend))
+    energyFocusFunction.setInterception(QuantityFactory.createFromString(interception))
+    energyFocusFunction.setSlopeDivisor(QuantityFactory.createFromString(slopeDivisor))
+    print("energyFocusFunction set to: " + energyFocusFunction.toString())
 
 
 # Load values at startup
