6724da61dcd6341e9479cc3e004d4f1e751593ed
Peter Holloway <peter.holloway@diamond.ac.uk>
2019-01-15 11:41:22 +0000

    I22 Beamline staff script changes
    Change-Id: Id0adf59319730caf6e2af9611ca525f09e3145e0

--- configurations/i22-config/scripts/localStationStaff.py
+++ configurations/i22-config/scripts/localStationStaff.py
@@ -8,11 +8,11 @@ gridscan_stage = [base_x, base_y]
 # Uncomment to change which channels and gain settings are used for It and I0 values in nexus files
 # NB Commenting this out again after a reset will not return the settings to default until the servers
 #    are restarted
-#scaler_channels = {'It': NcdChannel(channel=3, scaling=bs2diodegain),
-#                   'I0': NcdChannel(channel=1, scaling=d4d2gain)}
+scaler_channels = {'It': NcdChannel(channel=3, scaling=bs2diodegain),
+                   'I0': NcdChannel(channel=1, scaling=d4d2gain)}
 
 # ===================================================================================
 # Add any commands that needs to be run after each restart/reset below this line
 # ===================================================================================
-diode_y = SingleEpicsPositionerClass('diode_y', 'BL22I-MO-TABLE-05:Y', 'BL22I-MO-TABLE-05:Y.RBV' , 'BL22I-MO-TABLE-05:Y.DMOV' , 'BL22I-MO-TABLE-05:Y.STOP','mm', '%.4f')
-diode_x = SingleEpicsPositionerClass('diode_x', 'BL22I-MO-TABLE-05:X', 'BL22I-MO-TABLE-05:X.RBV' , 'BL22I-MO-TABLE-05:X.DMOV' , 'BL22I-MO-TABLE-05:X.STOP','mm', '%.4f')
+#diode_y = SingleEpicsPositionerClass('diode_y', 'BL22I-MO-TABLE-05:Y', 'BL22I-MO-TABLE-05:Y.RBV' , 'BL22I-MO-TABLE-05:Y.DMOV' , 'BL22I-MO-TABLE-05:Y.STOP','mm', '%.4f')
+#diode_x = SingleEpicsPositionerClass('diode_x', 'BL22I-MO-TABLE-05:X', 'BL22I-MO-TABLE-05:X.RBV' , 'BL22I-MO-TABLE-05:X.DMOV' , 'BL22I-MO-TABLE-05:X.STOP','mm', '%.4f')
