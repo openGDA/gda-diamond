ebdffb9c732927fabec4a32e1059ba0a897da477
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-02-26 16:05:20 +0000

    [DAQ-1840] Improve ConverterTest
    - Refactor as annotation-based test class
    
    - Add individual tests for conversion between eV and PerAngstrom
    (wave vector). These are the only conversions used in GDA code and
    other conversions will be removed in a subsequent change to the
    Converter class.
      - Test with non-zero input values
      - Test return values
    
    Conversion from zero wave vector is unreliable, so the corresponding
    test is ignored. It may work better with JScience4.
    
    Change-Id: Id40f50d98b6f7fe70dbdb46eb264cdcba62bcfa1

--- uk.ac.gda.dls.test/src/gda/gui/exafs/ConverterTest.java
+++ uk.ac.gda.dls.test/src/gda/gui/exafs/ConverterTest.java
@@ -18,30 +18,30 @@
 
 package gda.gui.exafs;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Ignore;
+import org.junit.Test;
 
 /**
  * Test suite for Exafs quantity converter class
  */
-public class ConverterTest extends TestCase {
-	/**
-	 * @param args
-	 *            command line arguments
-	 */
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(ConverterTest.class);
-	}
+public class ConverterTest {
+	// Tolerance for imprecision of conversions
+	private static final double FP_TOLERANCE = 0.00001;
 
-	/**
-	 * Converts a value using previously specified values for the edge energy and twoD.
-	 */
-	public void testConvert() {
-		// double d = Converter.convert(value, convertFromUnit, convertToUnit);
-	}
+	private static final String EV_STRING = "eV";
+	private static final String PERANGSTROM_STRING = "\u00c5\u207b\u00b9";
+
+	private static final double ENERGY_EV = 8932.2489387;
+	private static final double VECTOR_PER_ANGSTROM = 44.4632177;
+
+	private static final double EDGE_ENERGY_KEV = 1.4;
 
 	/**
 	 * Converts a value using temporary values for the edge energy and twoD.
 	 */
+	@Test
 	public void testConvertWithValues() {
 		double twoD = 0.0;
 		double edgeEnergy = 0.0;
@@ -69,4 +69,64 @@ public class ConverterTest extends TestCase {
 
 		Converter.convert(value, convertFromUnit, convertToUnit, edgeEnergy, twoD);
 	}
+
+	//----------------------------------------------------------------------------------------
+	// Convert eV -> PerAngstrom
+	//----------------------------------------------------------------------------------------
+	@Test
+	public void testConvertEvToPerAngstromZero() {
+		Converter.setEdgeEnergy(0);
+		assertEquals(0.0, Converter.convert(0.0, EV_STRING, PERANGSTROM_STRING), FP_TOLERANCE);
+	}
+
+	@Test
+	public void testConvertEvToPerAngstrom() {
+		Converter.setEdgeEnergy(EDGE_ENERGY_KEV);
+		assertEquals(VECTOR_PER_ANGSTROM, Converter.convert(ENERGY_EV, EV_STRING, PERANGSTROM_STRING), FP_TOLERANCE);
+	}
+
+	/*----------------------------------------------------------------------------------------------
+	 * Convert from Per-Angstrom
+	 *
+	 * testConvertPerAngstromToEvZero() is ignored because of unpredictable results
+	 *
+	 * - When run individually, it fails with a ClassCastException, thrown by Quantity.valueOf():
+	 * this is presumably a bug in the JScience Quantity class, which may be fixed in JScience4
+	 *
+	 * - When run as part of the whole class, Quantity.valueOf() successfully creates the
+	 * Quantity, but PhotonEnergy.photonEnergyOf() returns a null photon energy, which then
+	 * causes a NullPointerException. This should probably be fixed in Converter.
+	 * ----------------------------------------------------------------------------------------------*/
+	@Ignore("Not run because of unpredictable results")
+	@Test
+	public void testConvertPerAngstromToEvZero() {
+		Converter.setEdgeEnergy(0);
+		Converter.convert(0.0, PERANGSTROM_STRING, EV_STRING);
+	}
+
+	@Test
+	public void testConvertPerAngstromToEv() {
+		Converter.setEdgeEnergy(EDGE_ENERGY_KEV);
+		assertEquals(ENERGY_EV, Converter.convert(VECTOR_PER_ANGSTROM, PERANGSTROM_STRING, EV_STRING), FP_TOLERANCE);
+	}
+
+	/*---------------------------------------------------------------------------------------------
+	 * Test round-trip conversion
+	 * ----------------------------------------------------------------------------------------------*/
+	@Test
+	public void testRoundTripConversionEv() {
+		Converter.setEdgeEnergy(EDGE_ENERGY_KEV);
+		final double perAngstrom = Converter.convert(ENERGY_EV, EV_STRING, PERANGSTROM_STRING);
+		final double eV = Converter.convert(perAngstrom, PERANGSTROM_STRING, EV_STRING);
+		assertEquals(ENERGY_EV, eV, FP_TOLERANCE);
+	}
+
+	@Test
+	public void testRoundTripConversionPerAngstrom() {
+		Converter.setEdgeEnergy(EDGE_ENERGY_KEV);
+		final double eV = Converter.convert(VECTOR_PER_ANGSTROM, PERANGSTROM_STRING, EV_STRING);
+		final double perAngstrom = Converter.convert(eV, EV_STRING, PERANGSTROM_STRING);
+		assertEquals(VECTOR_PER_ANGSTROM, perAngstrom, FP_TOLERANCE);
+	}
+
 }
