8cd3dbd5d7a11a784dc838bc1914e055573d718a
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-02-28 15:48:43 +0000

    Remove unnecessary command runner beans
    - Refactor RunCommandCompositeFactory (only used in I13 & I13-1) to:
      - make class variables private
      - get a command runner from InterfaceProvider rather than passing it in
      - remove main function
    
    - Delete the CommandRunnerBeans from beamline configs, as they no longer
    need to be passed into RunCommandCompositeFactory
    
    Change-Id: Id463941cda61ead57159eee5e9d39595c2b0bb99

--- uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/views/RunCommandCompositeFactory.java
+++ uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/views/RunCommandCompositeFactory.java
@@ -18,47 +18,25 @@
 
 package uk.ac.gda.dls.client.views;
 
-import java.io.File;
-
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.springframework.beans.factory.InitializingBean;
 
 import gda.jython.ICommandRunner;
-import gda.jython.commandinfo.CommandThreadEvent;
+import gda.jython.InterfaceProvider;
 import gda.rcp.views.CompositeFactory;
-import swing2swt.layout.BorderLayout;
-import uk.ac.gda.ui.utils.SWTUtils;
 
 public class RunCommandCompositeFactory implements CompositeFactory, InitializingBean {
 
-	ICommandRunner commandRunner;
-	String command;
-	String label;
-	String jobTitle;
-	String tooltip="";
-
-	public static Composite createComposite(Composite parent, int style,
-			final ICommandRunner commandRunner, String label, final String command,
-			final String jobTitle, String tooltip) {
-		return new RunCommandComposite(parent, style, commandRunner, label, command,
-				jobTitle, tooltip);
-	}
+	private String command;
+	private String label;
+	private String jobTitle;
+	private String tooltip = "";
 
 	@Override
 	public Composite createComposite(Composite parent, int style) {
+		final ICommandRunner commandRunner = InterfaceProvider.getCommandRunner();
 		return new RunCommandComposite(parent, style, commandRunner, label,
 				command, jobTitle, tooltip);
-	}
-
-	public ICommandRunner getCommandRunner() {
-		return commandRunner;
-	}
-
-	public void setCommandRunner(ICommandRunner commandRunner) {
-		this.commandRunner = commandRunner;
 	}
 
 	public String getCommand() {
@@ -87,62 +65,12 @@ public class RunCommandCompositeFactory implements CompositeFactory, Initializin
 
 	@Override
 	public void afterPropertiesSet() throws Exception {
-		if (commandRunner == null)
-			throw new IllegalArgumentException("commandRunner is null");
 		if (command == null)
 			throw new IllegalArgumentException("command is null");
 		if (label == null)
 			throw new IllegalArgumentException("label is null");
 		if (jobTitle == null)
 			throw new IllegalArgumentException("jobTitle is null");
-	}
-
-	public static void main(String... args) {
-
-		Display display = new Display();
-		Shell shell = new Shell(display);
-		shell.setLayout(new BorderLayout());
-
-		ICommandRunner iCommandRunner = new ICommandRunner() {
-
-			@Override
-			public boolean runsource(String command) {
-				return false;
-			}
-
-			@Override
-			public CommandThreadEvent runScript(File script) {
-				return null;
-			}
-
-			@Override
-			public void runCommand(String command) {
-				try {
-					System.out.println(command);
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-
-			}
-
-			@Override
-			public String locateScript(String scriptToRun) {
-				return null;
-			}
-
-			@Override
-			public String evaluateCommand(String command) {
-				return null;
-			}
-		};
-		final RunCommandComposite comp = new RunCommandComposite(shell, SWT.NONE, iCommandRunner, "My Label",
-				"My Command", "Job Title", "tooltip");
-		comp.setLayoutData(BorderLayout.NORTH);
-		comp.setVisible(true);
-		shell.pack();
-		shell.setSize(400, 400);
-		SWTUtils.showCenteredShell(shell);
 	}
 
 	public String getTooltip() {
