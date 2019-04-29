af2c561f2bf3c5d20e9afc5ce05467231cbe03fd
Anthony Hull <anthony.hull@diamond.ac.uk>
2019-04-29 11:47:36 +0100

    Better checking in dummy XANES scan script
    I have several times forgotten to include dcm_enrg as an outer
    scannable, and the error message you get isn't very helpful.
    
    This puts in an explicit check
    
    Change-Id: I3a5758ae1c7eb917252612f72372b8b60f3c3a21

--- configurations/i14-config/scripts/dummy/xanes_scan.py
+++ configurations/i14-config/scripts/dummy/xanes_scan.py
@@ -5,12 +5,17 @@ def run_xanes_scan_request(scanRequest, xanesEdgeParams):
     print("scanRequest = {0}".format(scanRequest))
     print("xanesEdgeParams = {0}".format(xanesEdgeParams))
 
+    models = scanRequest.getCompoundModel().getModels()
+    if not models.size() > 1:
+        print("Only one scan model found: have you forgotten to define dcm_enrg as an outer scannable?")
+        return;
+
     dcm_enrg_model = scanRequest.getCompoundModel().getModels().get(0)
     print("Model for dcm_enrg: {0}".format(dcm_enrg_model))
     dcm_start = dcm_enrg_model.getStart()
     dcm_stop = dcm_enrg_model.getStop()
     dcm_step = dcm_enrg_model.getStep()
-    num_scans = int(round((dcm_stop - dcm_start) / dcm_step)) + 1 
+    num_scans = int(round((dcm_stop - dcm_start) / dcm_step)) + 1
 
     map_box = scanRequest.getCompoundModel().getModels().get(1).getBoundingBox()
     print("Bounding box for map: {0}".format(map_box))
@@ -26,7 +31,7 @@ def run_xanes_scan_request(scanRequest, xanesEdgeParams):
         map_box.setFastAxisStart(fast_axis_start)
         map_box.setSlowAxisStart(slow_axis_start)
 
-        dcm_enrg_model.setStart(dcm_start)      
+        dcm_enrg_model.setStart(dcm_start)
         dcm_enrg_model.setStop(dcm_start)
 
         scan_name = "XANES scan {0} of {1}".format(i, num_scans)
