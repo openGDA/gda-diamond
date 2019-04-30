a1e47cbdcdc3f1b9324d0f15ec969f23e350f808
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2019-04-30 13:46:16 +0100

    I06-486: PEEM beam stablisation
    Change-Id: I6ad7d9d386814f638ed4d72bbcb032740096a2ee

--- configurations/i06-config/scripts/beam/BeamStablisation.py
+++ configurations/i06-config/scripts/beam/BeamStablisation.py
@@ -11,7 +11,7 @@ roi4 = [1, 1, 250, 1000]  # left
 rois = [roi1, roi2, roi3, roi4]
 
 # use 1st 4 ROI-STAT pair from detector pcotif or pco
-cb = BeamStablisationUsingAreaDetectorRoiStatPair(rois, leem_fov, leem_rot, m4fpitch, m5fpitch, m4fpitchRead, m5fpitchRead, psphi, roistat_index=[1, 2, 3, 4], pvRootName="BL06I-EA-DET-01", detector=pcotif, roi_provider_name="pco_roi")
+#cb = BeamStablisationUsingAreaDetectorRoiStatPair(rois, leem_fov, leem_rot, m4fpitch, m5fpitch, m4fpitchRead, m5fpitchRead, psphi, roistat_index=[1, 2, 3, 4], pvRootName="BL06I-EA-DET-01", detector=pcotif, roi_provider_name="pco_roi")
 # use extra 4 ROI-STAT pair from ROTATED image
 cb = BeamStablisationUsingExtraRoiStatPair(rois, leem_fov, leem_rot, m4fpitch, m5fpitch, m4fpitchRead, m5fpitchRead, psphi, roistat_index=[7, 8, 9, 10], pvRootName="BL06I-EA-DET-01")
 
