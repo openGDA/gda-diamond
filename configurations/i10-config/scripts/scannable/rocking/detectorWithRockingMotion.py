b93fd9c70063660d861e0136ecaed35c4f857884
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2019-08-05 11:42:37 +0100

    I10-419: continuous scan with with CCD
    Change-Id: Ia2950323c9c7397a81b4a873363d8eb7b6360c19

---
+++ configurations/i10-config/scripts/scannable/rocking/detectorWithRockingMotion.py
@@ -0,0 +1,161 @@
+'''
+A wrapper detector that ensures a given motor is moving while detector is exposing
+
+Created on 2 Aug 2019
+
+@author: fy65
+'''
+from gda.device.detector import NXDetector
+import math
+
+ADJUST_MOTOR_SPEED=False
+ROCKING_RANGE_CENTRE=0.0
+
+class NXDetectorWithRockingMotion(NXDetector):
+    '''
+    A wrapper detector that ensures a given motor is moving while detector is exposing
+    '''
+
+
+    def __init__(self, name, motor, detector):
+        '''
+        Constructor
+        @param name: name of this detector
+        @param motor: the motor to be moving during detector collection
+        @param detector: the detector to collect data from   
+        '''
+        self.setName(name)
+        self.motor=motor
+        if not isinstance(detector, NXDetector):
+            raise Exception("The input detector is not NXDetector!")
+        self.detector=detector
+        self.start_motor_angle,self.end_motor_angle = 0.0, 0.0
+        
+    def calculateStartStopPosition(self,changeSpeed):
+        if not changeSpeed:
+            motor_range = self.motor.getSpeed()*self.detector.getCollectionTime()
+            
+            if motor_range > (self.motor.getUpperMotorLimit()-self.motor.getLowerMotorLimit()):
+                raise Exception("motor rocking range is greater than hardware limits permitted")
+            
+            start_motor_angle=ROCKING_RANGE_CENTRE-motor_range/2.0
+            end_motor_angle=ROCKING_RANGE_CENTRE+motor_range/2.0
+            
+            if (start_motor_angle/2.0) < self.motor.getLowerMotorLimit():
+                raise Exception("start rocking angle is outside hardware low limit")
+            if (end_motor_angle/2.0) > self.motor.getUpperMotorLimit():
+                raise Exception("end rocking angle is outside hardware high limit")
+        else:
+            motorspeed=(self.motor.getUpperMotorLimit() - self.motor.getLowerMotorLimit())/self.detector.getCollectionTime()
+            #Need to check if motor speed is valid or not
+            self.motor.setSpeed(motorspeed)
+            start_motor_angle = self.motor.getLowerMotorLimit()
+            end_motor_angle = self.motor.getUpperMotorLimit()
+        return start_motor_angle, end_motor_angle
+
+    def atScanStart(self):
+        self.detector.atScanStart()
+        self.start_motor_angle,self.end_motor_angle = self.calculateStartStopPosition(ADJUST_MOTOR_SPEED)
+        self.motor.moveTo(self.start_motor_angle)
+        
+    def collectData(self):
+        curpos = float(self.motor.getPosition())
+        if math.fabs(curpos - self.start_motor_angle) < math.fabs(curpos - self.end_motor_angle): 
+            self.motor.asynchronousMoveTo(self.end_motor_angle)
+        else:
+            self.motor.asynchronousMoveTo(self.start_motor_angle)
+        self.collectData()
+    
+    def readout(self):
+        self.motor.stop()
+        return self.detector.readout()    
+
+    def stop(self):
+        self.detector.stop()
+        self.motor.stop()
+        
+    def atCommandFailure(self):
+        self.detector.atCommandFailure()
+        self.motor.stop()
+        
+    def afterPropertiesSet(self):
+        self.detector.afterPropertiesSet()
+        
+    def setCollectionStrategy(self,collectionStrategy):
+        self.detector.setCollectionStrategy(collectionStrategy)
+        
+    def setAdditionalPluginList(self,additionalPluginList):
+        self.detector.setAdditionalPluginList(additionalPluginList)
+        
+    def getCollectionStrategy(self):
+        return self.detector.getCollectionStrategy()
+    
+    def getAdditionalPluginList(self):
+        return self.detector.getAdditionalPluginList()
+    
+    def getPluginList(self):
+        return self.detector.getPluginList()
+    
+    def getPluginMap(self):
+        return self.detector.getPluginMap()
+    
+    def __str__(self):
+        return self.detector.__str__()
+    
+    def setInputNames(self, names):
+        self.detector.setInputNames(names)
+        
+    def setExtraNames(self,names):
+        self.detector.setExtraNames(names)
+        
+    def setOutputFormat(self, names):
+        self.detector.setOutputFormat(names)
+        
+    def asynchronousMoveTo(self, collectionTime):
+        self.detector.asynchronousMoveTo(collectionTime)
+        
+    def isBusy(self):
+        return self.detector.isBusy()
+    
+    def getPosition(self):
+        return self.detector.getPosition()
+    
+    def prepareForCollection(self):
+        self.detector.prepareForCollection()
+        
+    def createsOwnFiles(self):
+        return self.detector.createsOwnFiles()
+    
+    def getInputNames(self):
+        return self.detector.getInputNames()
+    
+    def getExtraNames(self):
+        return self.detector.getExtraNames()
+    
+    def getOutputFormat(self):
+        return self.detector.getOutputFormat()
+    
+    def atScanLineStart(self):
+        self.detector.atScanLineStart()
+        
+    def getStatus(self):
+        return self.detector.getStatus()
+    
+    def waitWhileBusy(self):
+        self.detector.waitWhileBusy()
+        
+    def getPositionCallable(self):
+        return self.detector.getPositionCallable()
+    
+    def atScanLineEnd(self):
+        self.detector.atScanLineEnd()
+        
+    def atScanEnd(self):
+        self.detector.atScanEnd()
+        
+    
+    
+
+    
+    
+        
--- configurations/i10-config/scripts/scannable/rocking/rockingMotion_pds.py
+++ configurations/i10-config/scripts/scannable/rocking/rockingMotion_pds.py
@@ -1,8 +1,6 @@
 '''
 Created on 24 Jun 2010
-Please investigate why this file can not be imported from localStation.py
-errors: 1. ds problem
-        2. theta  warning.
+
 @author: fy65
 '''
 from rockingMotion_class import RockingMotion
@@ -11,6 +9,7 @@ from gdaserver import th
 from gda.device.scannable import DummyScannable
 
 ds = DummyScannable("ds")
+rocktheta=RockingMotion("rocktheta", th, -1, 1) 
 rockthetascan=RockingMotion("rockthetascan", th, -1, 1) 
 rockthetacounting=RockingMotorDuringCounting("rockthetacounting", th, -1, 1)
 
