ba2a646b48f721571386c116f54a8375ef37b912
Joe Shannon <joe.shannon@diamond.ac.uk>
2019-09-10 15:19:10 +0100

    I07-210 Add dcam8
    Should be identical to xrayeye config. Is currently used in scans
    to write png files.
    
    Change-Id: I64c123c5ee08393573f849298d940d90137f334d

--- configurations/i07-config/scripts/BeamlineI07/useGigECams.py
+++ configurations/i07-config/scripts/BeamlineI07/useGigECams.py
@@ -4,5 +4,4 @@ print "---------------------------------------------------------"
 print("Use xraycamstats for the Stats plugin of xraycam")
 
 xrayeyestats = NdArrayWithStatPluginDeviceClass('xrayeyestats', xrayeye_det)
-
-
+dcam8stats = NdArrayWithStatPluginDeviceClass('dcam8stats', dcam8_det)
