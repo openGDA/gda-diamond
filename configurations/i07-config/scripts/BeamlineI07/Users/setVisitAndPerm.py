12d4f863305514e373f650114d419f6035e3325b
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-01-13 09:36:07 +0000

    I07 (9.15) Delete print statements from detector classes
    These can be misleading and too verbose. Replace with a summary
    in the setVisit function to report specifically on the detectors that
    I07 care about.
    
    Change-Id: I265e260d9fc1984ae1e203e271dce0a48aa2709b

--- configurations/i07-config/scripts/BeamlineI07/Users/setVisitAndPerm.py
+++ configurations/i07-config/scripts/BeamlineI07/Users/setVisitAndPerm.py
@@ -7,3 +7,7 @@ def setVisit(visit):
 	_setVisit(visit)
 	if not visit.startswith("cm"):
 		i07userperm(visit)
+	print("- Detector filepaths")
+	print("pilatus1: {}".format(pil1.getFilePath()))
+	print("pilatus2: {}".format(pil2.getFilePath()))
+	print("pilatus3: {}".format(pil3.getFilePath()))
