7530acd4782e60e050388eb9b0fc9d4eb7e7fdec
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-01-16 16:23:09 +0000

    I14-775 Capture image for autofocus in monochrome
    Change-Id: Id54b71f415cbd4c756142d4a459a05e91322180b

--- configurations/i14-config/scripts/autofocus/autofocus.py
+++ configurations/i14-config/scripts/autofocus/autofocus.py
@@ -10,6 +10,7 @@ def run_autofocus(detector):
     print('Setting up detector & Python plugin')
     detector_ad_base = detector.getAdBase()
     save_image_mode = detector_ad_base.getImageMode()
+    save_colour_mode = detector_ad_base.getColorMode()
 
     python_plugin = detector.getNdPython()
     python_plugin_base = python_plugin.getPluginBase()
@@ -27,12 +28,13 @@ def run_autofocus(detector):
 
     # Acquire a single image
     detector_ad_base.setImageModeWait(ImageMode.SINGLE, 500)
+    detector_ad_base.setColorMode(0)
     python_plugin_base.enableCallbacks()
     python_plugin.putParam("strips", num_strips)
 
     detector_ad_base.startAcquiringWait()
     detector_ad_base.stopAcquiring()
-    print('Acquired image of size: {}, {}, {}'.format(python_plugin_base.getArraySize0_RBV(), python_plugin_base.getArraySize1_RBV(),python_plugin_base.getArraySize2_RBV()))
+    print('Acquired image of size: {}, {}, {}'.format(python_plugin_base.getArraySize0_RBV(), python_plugin_base.getArraySize1_RBV(), python_plugin_base.getArraySize2_RBV()))
 
     # Get the output (FFT of each strip)
     print('Output: {}'.format(python_plugin.readParam('output', DBRType.INT)))
@@ -40,5 +42,6 @@ def run_autofocus(detector):
     # Restore initial settings
     print('Restoring initial settings')
     detector_ad_base.setImageMode(save_image_mode)
+    detector_ad_base.setColorMode(save_colour_mode)
     if not save_callbacks_enabled:
         python_plugin_base.disableCallbacks()
