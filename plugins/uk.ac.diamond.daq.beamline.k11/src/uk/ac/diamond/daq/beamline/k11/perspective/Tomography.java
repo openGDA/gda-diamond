bc06acad3ec42aba48ee8e048fbd1181a03ecc17
Maurizio Nagni <maurizio.nagni@diamond.ac.uk>
2020-01-23 16:28:56 +0000

    K11-483 - Perspective refactoring to use PerspectiveHelper
    - Tomography
    - PointAndShoot
    
    Change-Id: Icb5c34f483b9d098777fe71c578de8a6f1d2c2b4

--- plugins/uk.ac.diamond.daq.beamline.k11/src/uk/ac/diamond/daq/beamline/k11/perspective/Tomography.java
+++ plugins/uk.ac.diamond.daq.beamline.k11/src/uk/ac/diamond/daq/beamline/k11/perspective/Tomography.java
@@ -18,62 +18,29 @@
 
 package uk.ac.diamond.daq.beamline.k11.perspective;
 
-import org.eclipse.scanning.api.event.EventConstants;
-import org.eclipse.scanning.event.ui.view.StatusQueueView;
-import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.IViewLayout;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import gda.configuration.properties.LocalProperties;
+import uk.ac.gda.perspectives.ThreeColumnPerspectiveLayoutBuilder;
 
 /**
  * @author Maurizio Nagni
  */
 public class Tomography  implements IPerspectiveFactory {
-
 	public static final String ID = "uk.ac.diamond.daq.beamline.k11.perspective.TomographyPerspective";
-
-	private static final String TOMO_MAIN_VIEW = "uk.ac.diamond.daq.beamline.k11.tomography";
-	private static final String CAMERA_CONTROLLER_VIEW = "uk.ac.diamond.daq.client.gui.camera.CameraConfigurationView";
-	private static final String JYTON_CONSOLE_VIEW = "gda.rcp.jythonterminalview";
-
-	private static final Logger logger = LoggerFactory.getLogger(Tomography.class);
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
-		logger.trace("Building K11 Tomography perspective");
-		layout.setEditorAreaVisible(false);
+		ThreeColumnPerspectiveLayoutBuilder helper = new ThreeColumnPerspectiveLayoutBuilder(ID, layout);
 
-		String tomoMain = "tomographyManagement";
-		final IFolderLayout mainFolder = layout.createFolder(tomoMain, IPageLayout.LEFT, 0.20f, editorArea);
-		mainFolder.addView(TOMO_MAIN_VIEW);
-		IViewLayout topViewLayout = layout.getViewLayout(TOMO_MAIN_VIEW);
-		topViewLayout.setCloseable(false);
+		// Left area
+		helper.addViewToLeftFolder(K11DefaultViews.TOMO_MAIN_VIEW, false);
 
-		String tomoTools = "tomographyTools";
-		final IFolderLayout toolsFolder = layout.createFolder(tomoTools, IPageLayout.RIGHT, 0.75f, editorArea);
-		toolsFolder.addView(CAMERA_CONTROLLER_VIEW);
-		IViewLayout topRightLayout = layout.getViewLayout(CAMERA_CONTROLLER_VIEW);
-		topRightLayout.setCloseable(false);
+		// Central area
+		helper.addViewToCentralFolder(K11DefaultViews.CAMERA_CONTROLLER_VIEW, false);
 
-		String tomoMonitor = "tomographyMonitors";
-		final IFolderLayout monitorLayout = layout.createFolder(tomoMonitor, IPageLayout.BOTTOM, 0.75f, tomoTools);
-		monitorLayout.addView(JYTON_CONSOLE_VIEW);
-		String queueViewId = StatusQueueView.createId(LocalProperties.get(LocalProperties.GDA_ACTIVEMQ_BROKER_URI, ""),
-				"org.eclipse.scanning.api",
-				"org.eclipse.scanning.api.event.status.StatusBean",
-				EventConstants.STATUS_TOPIC,
-				EventConstants.SUBMISSION_QUEUE);
-
-
-		queueViewId = queueViewId + "partName=Queue";
-		monitorLayout.addView(queueViewId);
-
-
-		logger.trace("Finished building K11 Tomography perspective");
+		// Right area
+		helper.addViewToRightFolder(K11DefaultViews.JYTON_CONSOLE_VIEW, false);
+		helper.addViewToRightFolder(K11DefaultViews.getQueueId(), false);
 	}
 }
