4853546048ae54bd42bc0861f1d5e31bebc3cc4d
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-01-29 21:23:54 +0000

    DAQ-2693 Replace JScience4 with javax.measure
    Change-Id: Ia9ae164a91988d7c475c848eebc12ce848c51a6b

--- configurations/i18-config/scripts/sampleStageTilt.py
+++ configurations/i18-config/scripts/sampleStageTilt.py
@@ -1,9 +1,9 @@
-from org.jscience.physics.amount import Amount
-from javax.measure.unit import SI
+from tec.units.indriya.quantity import Quantities;
+from tec.units.indriya.unit import MetricPrefix, Units
 from gda.factory import Finder
 
 def setOffset(axis, offset):
-    offset_quantity = Amount.valueOf(offset, SI.MILLI(SI.METER))
+    offset_quantity = Quantities.getQuantity(offset, MetricPrefix.MILLI(Units.METRE))
     Finder.getInstance().find(axis.getName()).setOffset(offset_quantity)
     
 def getOffset(axis):
