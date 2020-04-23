1e375815e3189aa25421a5fb7420f21739720dc5
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-04-23 11:26:19 +0100

    I07-210 Add GigE cams to GDA
    These are all configured the same (copied from the already existing
    dcam8).
    
    Change-Id: Ie60e408d286abc7047c14a92e3bcb6f05e74ffa7

--- configurations/i07-config/scripts/BeamlineI07/useGigECams.py
+++ configurations/i07-config/scripts/BeamlineI07/useGigECams.py
@@ -1,7 +1,18 @@
 from Diamond.AreaDetector.NdArrayPluginDevice import NdArrayWithStatPluginDeviceClass
 
 print "---------------------------------------------------------"
-print("Use xraycamstats for the Stats plugin of xraycam")
+print("Creating GigE cam stats objects")
+
+d1camstats = NdArrayWithStatPluginDeviceClass('d1camstats', d1cam_det)
+d1acamstats = NdArrayWithStatPluginDeviceClass('d1acamstats', d1acam_det)
+d4camstats = NdArrayWithStatPluginDeviceClass('d4camstats', d4cam_det)
+dcam1stats = NdArrayWithStatPluginDeviceClass('dcam1stats', dcam1_det)
+dcam2stats = NdArrayWithStatPluginDeviceClass('dcam2stats', dcam2_det)
+dcam3stats = NdArrayWithStatPluginDeviceClass('dcam3stats', dcam3_det)
+dcam4stats = NdArrayWithStatPluginDeviceClass('dcam4stats', dcam4_det)
+dcam5stats = NdArrayWithStatPluginDeviceClass('dcam5stats', dcam5_det)
+dcam6stats = NdArrayWithStatPluginDeviceClass('dcam6stats', dcam6_det)
+dcam7stats = NdArrayWithStatPluginDeviceClass('dcam7stats', dcam7_det)
+dcam8stats = NdArrayWithStatPluginDeviceClass('dcam8stats', dcam8_det)
 
 xrayeyestats = NdArrayWithStatPluginDeviceClass('xrayeyestats', xrayeye_det)
-dcam8stats = NdArrayWithStatPluginDeviceClass('dcam8stats', dcam8_det)
