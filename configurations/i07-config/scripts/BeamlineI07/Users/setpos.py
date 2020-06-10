7663e51de702aa0b9c1c89d0aa112a512b1ea7ff
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-06-10 16:52:09 +0100

    DAQ-2818 Modify setpos() to call setPosition()
    setPosition() now does the right thing, so it can be called directly
    
    Change-Id: Ife1a7f9426b62c1f901093be121f82a9e766180c

--- configurations/i07-config/scripts/BeamlineI07/Users/setpos.py
+++ configurations/i07-config/scripts/BeamlineI07/Users/setpos.py
@@ -1,15 +1,12 @@
+from gda.device.scannable import ScannableMotor
+from gda.jython.commands.GeneralCommands import alias
+
 def setpos(motor, newpos):
-	if (motor.getClass() != gda.device.scannable.ScannableMotor):
+	if (motor.getClass() != ScannableMotor):
 		raise typeError('Not a scannable')
 	if (type(newpos) != int and type(newpos) != float):
 		raise typeError('Position must be given a value')
 
-	# getPosition seems to get the position, but setPosition sets the offset!
-	# so the only way to increment the offset is to set it to zero first,
-	# otherwise we don't know the original value of the offset
-	motor.getMotor().setPosition(0)	
-	oldpos = motor.getMotor().getPosition()
-	deltaoffset = newpos - oldpos
-	motor.getMotor().setPosition(deltaoffset)
+	motor.setPosition(newpos)
 
 alias("setpos")
