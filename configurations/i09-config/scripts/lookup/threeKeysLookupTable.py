2ccfbd4e9811f1a91935895ea212ba3db3a10231
Dimitrios Ladakis <dimitrios.ladakis@diamond.ac.uk>
2020-07-10 14:53:08 +0100

    I09-285 Move 'jenergy' to use new energy-polarisation as separate scannables
    The new energy-polarisation system were tested on i09-2 soft X-ray
    beamline. This task is to back port it to i09 and replacing exiting
    jenergy implementation.we need to ensure that all the lookup table used
    in i09-2 are up to date with i09 current data.
    
    Change-Id: Ie1e8e6b23e05ac6a15aeaa1bae37a9b2981f8b88

---
+++ configurations/i09-config/scripts/lookup/threeKeysLookupTable.py
@@ -0,0 +1,42 @@
+'''
+Created on 20 Sep 2016
+
+@author: fy65
+'''
+import string
+
+def loadLookupTable(filename):
+    f = open(filename, "r")
+    lines = f.readlines()
+    f.close()
+    lines = map(string.split, map(string.strip, lines))
+    lookuptable={}
+    for line in lines[2:]:
+        #print line
+        lookuptable[(line[0],float(line[1]),float(line[2]))]=[float(item) for item in line[3:]]
+    return lookuptable    
+
+def test():
+    lookuptable=loadLookupTable("/dls_sw/i09-2/software/gda/config/lookupTables/IDEnergy2GapCalibrations.txt")
+    formatstring="%12s\t%12s\t%12s\t%12s\t%12s\t%12s\t%12s\t%12s"
+    print (formatstring % ("Mode", "Min Energy", "Max Energy", "Coefficent0", "Coefficent1", "Coefficent2", "Coefficent3", "Coefficent4"))
+    for key, value in sorted(lookuptable.iteritems()):
+        print (formatstring % (key[0],key[1],key[2],value[0],value[1],value[2],value[3],value[4]))    
+    print
+    print lookuptable[("LH",1200,1400)]
+    print lookuptable.keys()
+    lowEnergies=sorted([e[1] for e in lookuptable.keys() if e[0]=="LH"])
+    highEnergies=sorted([e[2] for e in lookuptable.keys() if e[0]=="LH"])
+    print lowEnergies
+    print highEnergies
+    minEnergy=min(lowEnergies)
+    maxEnergy=max(highEnergies)
+    print minEnergy, maxEnergy    
+    limits=zip(lowEnergies, highEnergies)
+    for low, high in limits:
+        print low, high
+    from calibration.energy_polarisation_class import getFittingCoefficents
+    print getFittingCoefficents("LH", 800.1, lookuptable)
+
+if __name__ == "__main__":
+    test()
