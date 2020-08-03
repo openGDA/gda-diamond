880e1c58e04329e392f1bb1d089b72dae0a750fd
Joe Shannon <joe.shannon@diamond.ac.uk>
2020-08-03 14:42:41 +0100

    I07 Get PV strings from existing EpicsMotors in useMotors
    To reduce duplication of the PV strings.
    
    Change-Id: Id269468d91dbb1da416e46fbc83fb436303e69e7

--- configurations/i07-config/scripts/BeamlineI07/useMotors.py
+++ configurations/i07-config/scripts/BeamlineI07/useMotors.py
@@ -1,24 +1,25 @@
 
 from Diamond.PseudoDevices.EpicsMotors import EpicsMotorClass, EpicsCallbackMotorClass, EpicsMotorOffsetClass;
 
-pvMotor = "BL07I-MO-DIFF-01:DELTA"
+from gdaserver import MotorDELTA_DIFF1
+from gdaserver import MotorGAMMA_DIFF1
+from gdaserver import MotorOMEGA_DIFF1
+from gdaserver import MotorALPHA_DIFF1
+from gdaserver import MotorTHETA_DIFF1
+from gdaserver import MotorCHI_DIFF1
+
 #diff1delta = EpicsMotorClass('diff1delta',pvMotor, '%.6f');
-diff1vdeltaoffset = EpicsMotorOffsetClass('diff1vdeltaoffset',pvMotor, '%.6f');
+diff1vdeltaoffset = EpicsMotorOffsetClass('diff1vdeltaoffset', MotorDELTA_DIFF1.getPvName(), '%.6f');
 #diff1hgammaoffset = EpicsMotorOffsetClass('diff1hgammaoffset',pvMotor, '%.6f');
 
-pvMotor = "BL07I-MO-DIFF-01:GAMMA"
-diff1vgammaoffset = EpicsMotorOffsetClass('diff1vgammaoffset',pvMotor, '%.6f');
+diff1vgammaoffset = EpicsMotorOffsetClass('diff1vgammaoffset', MotorGAMMA_DIFF1.getPvName(), '%.6f');
 #diff1hdeltaoffset = EpicsMotorOffsetClass('diff1hdeltaoffset',pvMotor, '%.6f');
 
-pvMotor = "BL07I-MO-DIFF-01:OMEGA"
-diff1vomegaoffset = EpicsMotorOffsetClass('diff1vomegaoffset',pvMotor, '%.6f');
+diff1vomegaoffset = EpicsMotorOffsetClass('diff1vomegaoffset', MotorOMEGA_DIFF1.getPvName(), '%.6f');
 
-pvMotor = "BL07I-MO-DIFF-01:ALPHA"
-diff1valphaoffset = EpicsMotorOffsetClass('diff1valphaoffset',pvMotor, '%.6f');
+diff1valphaoffset = EpicsMotorOffsetClass('diff1valphaoffset', MotorALPHA_DIFF1.getPvName(), '%.6f');
 
-pvMotor = "BL07I-MO-DIFF-03:THETA"
-diff1homegaoffset = EpicsMotorOffsetClass('diff1homegaoffset',pvMotor, '%.6f');
+diff1homegaoffset = EpicsMotorOffsetClass('diff1homegaoffset', MotorTHETA_DIFF1.getPvName(), '%.6f');
 
-pvMotor = "BL07I-MO-DIFF-01:CHI"
-diff1halphaoffset = EpicsMotorOffsetClass('diff1halphaoffset',pvMotor, '%.6f');
+diff1halphaoffset = EpicsMotorOffsetClass('diff1halphaoffset', MotorCHI_DIFF1.getPvName(), '%.6f');
 
