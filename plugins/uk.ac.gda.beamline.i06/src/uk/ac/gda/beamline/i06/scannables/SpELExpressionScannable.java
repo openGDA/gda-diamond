4933571e1ba4d764bb87a04b24bd6847064cef97
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2020-09-04 11:52:19 +0100

    I06-805 Scannable handles Spring SpEL expressions
    - move register function to configure method
    - using Spring bean to handle fully qualified name to Class conversion
    - remove { and } in javadoc as it cannot be parsed when they are inside
    XML tags
    
    Change-Id: I4648fbdfbaa56cb7796bfbaa3d79aed2fb5c5ef3

--- plugins/uk.ac.gda.beamline.i06/src/uk/ac/gda/beamline/i06/scannables/SpELExpressionScannable.java
+++ plugins/uk.ac.gda.beamline.i06/src/uk/ac/gda/beamline/i06/scannables/SpELExpressionScannable.java
@@ -19,19 +19,9 @@
 package uk.ac.gda.beamline.i06.scannables;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.expression.AccessException;
 import org.springframework.expression.BeanResolver;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
@@ -41,11 +31,9 @@ import com.google.common.base.Strings;
 import gda.device.DeviceException;
 import gda.device.Scannable;
 import gda.device.scannable.PVScannable;
-import gda.device.scannable.ScannablePositionChangeEvent;
 import gda.factory.FactoryException;
 import gda.factory.Finder;
 import uk.ac.gda.beamline.i06.spring.spel.SpELUtils;
-import uk.ac.gda.beamline.i06.utils.Either;
 
 /**
  * A {@link Scannable} that applies the given expressions {@link #getMoveToExpression()} in {@link #asynchronousMoveTo(Object)} and
@@ -64,71 +52,87 @@ import uk.ac.gda.beamline.i06.utils.Either;
  * method used to extending SpEL must be defined in {@link SpELUtils} for this class. If the function/method takes formal parameters, both function name and
  * parameter types have to be specified as Java Reflection is used to return the register function.
  * <p>
- * All expressions, including function name and funtion parameter types, can be defined in bean definition.
+ * All expressions, including function name and function parameter types, can be defined in bean definition.
  * <p>
  * For more information on SpEL, @see <a href="https://docs.spring.io/spring/docs/4.3.10.RELEASE/spring-framework-reference/html/expressions.html">Spring
  * Expression Language (SpEL)</a>.
  *<p>
- * Example bean definition:
- * <pre>
+ *
+ * Example using function for complex algorithm that cannot be represented by one statement
+ *<pre>
  * {@code
- * 	<bean id="medipixAcquireTime" class="uk.ac.gda.beamline.i06.scannables.SpELExpressionScannable">
- * 		<property name="pvName" value="BL06I-EA-DET-02:CAM:AcquireTime"/>
- * 		<property name="outputFormat">
- * 			<list>
- * 				<value>%4.3f</value>
- * 			</list>
- * 		</property>
- * 		<property name="functionNameToBeCalledInAsynchronousMoveTo" value="updateAcquirePeriodWhenAcquireTimeChanges"/>
- * 		<property name="functionParameterTypesTobeCalledInAsynchronousMoveTo">
- * 			<list>
- * 				<value>gda.device.Scannable</value>
- * 				<value>gda.device.Scannable</value>
- *				<value>java.lang.Double</value>
- *			</list>
- *		</property>
- *		<property name="moveToExpression" value="#updateAcquirePeriodWhenAcquireTimeChanges(@medipixExposureTime, @medipixAcquirePeriod, #input)"/>
- *	</bean>
- *	<bean id="medipixIdleTime" class="uk.ac.gda.beamline.i06.scannables.SpELExpressionScannable">
- *		<property name="pvName" value="BL06I-EA-DET-02:CAM:AcquirePeriod"/>
- *		<property name="outputFormat">
- *			<list>
- *				<value>%4.3f</value>
- *			</list>
- *		</property>
- *		<property name="moveToExpression" value="#input > 0.002 ? @medipixExposureTime.getPosition() + #input : @medipixExposureTime.getPosition() + 0.002"/>
- *		<property name="getPositionExpression" value="(#output - @medipixExposureTime.getPosition())>0.002 : #output - @medipixExposureTime.getPosition() : 0.002"/>
- *	</bean>
+ * <bean id="medipixAcquireTime" class="uk.ac.gda.beamline.i06.scannables.SpELExpressionScannable">
+ * 	<property name="pvName" value="BL06I-EA-DET-02:CAM:AcquireTime"/>
+ * 	<property name="outputFormat">
+ * 		<list>
+ * 			<value>%4.3f</value>
+ * 		</list>
+ * 	</property>
+ * 	<property name="functionNameToBeCalledInAsynchronousMoveTo" value="updateAcquirePeriodWhenAcquireTimeChanges"/>
+ * 	<property name="functionParameterTypesTobeCalledInAsynchronousMoveTo" value="'gda.device.Scannable','gda.device.Scannable','java.lang.Double'"/>
+ *	<property name="moveToExpression" value="#updateAcquirePeriodWhenAcquireTimeChanges(@medipixExposureTime, @medipixAcquirePeriod, #input)"/>
+ * </bean>
+ * }
+ *</pre>
+ *
+ *<p>
+ * Example for one liner expression
+ * <pre>
+ *{@code
+ *<bean id="medipixIdleTime" class="uk.ac.gda.beamline.i06.scannables.SpELExpressionScannable">
+ *	<property name="pvName" value="BL06I-EA-DET-02:CAM:AcquirePeriod"/>
+ *	<property name="outputFormat">
+ *		<list>
+ *			<value>%4.3f</value>
+ *		</list>
+ *	</property>
+ *	<property name="moveToExpression" value="#input > 0.002 ? @medipixExposureTime.getPosition() + #input : @medipixExposureTime.getPosition() + 0.002"/>
+ *	<property name="getPositionExpression" value="(#output - @medipixExposureTime.getPosition())>0.002 : #output - @medipixExposureTime.getPosition() : 0.002"/>
+ *</bean>
  *}
- * </pre>
+ *</pre>
  *
  * For a full example @see /i06-config/clients/main/live/medipix_live_controls.xml
  * @since 9.16
  */
 public class SpELExpressionScannable extends PVScannable {
 
-	private static final Logger logger = LoggerFactory.getLogger(SpELExpressionScannable.class);
-
 	private String moveToExpression;
 	private String getPositionExpression;
 	private ExpressionParser parser;
 	private StandardEvaluationContext context;
 	private String functionNameToBeCalledInAsynchronousMoveTo;
-	private List<String> functionParameterTypesTobeCalledInAsynchronousMoveTo;
+	private List<Class<?>> functionParameterTypesToBeCalledInAsynchronousMoveTo;
 	private String functionNameToBeCalledInGetPosition;
-	private List<String> functionParameterTypesToBeCalledInGetPosition;
+	private List<Class<?>> functionParameterTypesToBeCalledInGetPosition;
 
 	@Override
 	public void configure() throws FactoryException {
 		parser = new SpelExpressionParser();
 		context = new StandardEvaluationContext();
-		context.setBeanResolver(new BeanResolver() {
-
-			@Override
-			public Object resolve(EvaluationContext context, String beanName) throws AccessException {
-				return Finder.find(beanName);
+		context.setBeanResolver((ec,name) -> Finder.find(name));
+		if (!Strings.isNullOrEmpty(getFunctionNameToBeCalledInAsynchronousMoveTo())) {
+			// register function in SpELUtils class to be used inside asynchronuousMoveTo method of this class
+			Class<?>[] parameterTypes = new Class[functionParameterTypesToBeCalledInAsynchronousMoveTo.size()];
+			functionParameterTypesToBeCalledInAsynchronousMoveTo.toArray(parameterTypes);
+			try {
+				context.registerFunction(getFunctionNameToBeCalledInAsynchronousMoveTo(),
+						SpELUtils.class.getDeclaredMethod(getFunctionNameToBeCalledInAsynchronousMoveTo(), parameterTypes));
+			} catch (NoSuchMethodException | SecurityException e) {
+				throw new FactoryException(e.getMessage(), e);
 			}
-		});
+		}
+		if (!Strings.isNullOrEmpty(getFunctionNameToBeCalledInGetPosition())) {
+			// register function in SpELUtils class to be used inside getPosition method of this class
+			Class<?>[] parameterTypes = new Class[functionParameterTypesToBeCalledInGetPosition.size()];
+			functionParameterTypesToBeCalledInGetPosition.toArray(parameterTypes);
+			try {
+				context.registerFunction(getFunctionNameToBeCalledInGetPosition(),
+						SpELUtils.class.getDeclaredMethod(getFunctionNameToBeCalledInGetPosition(), parameterTypes));
+			} catch (NoSuchMethodException | SecurityException e) {
+				throw new FactoryException(e.getMessage(), e);
+			}
+		}
 		super.configure();
 	}
 
@@ -142,23 +146,12 @@ public class SpELExpressionScannable extends PVScannable {
 		if (Strings.isNullOrEmpty(getMoveToExpression())) {
 			super.asynchronousMoveTo(value);
 		} else {
-			context.setVariable("input", Double.valueOf(value.toString()).doubleValue());
+			context.setVariable("input", Double.parseDouble(value.toString()));
 			if (Strings.isNullOrEmpty(getFunctionNameToBeCalledInAsynchronousMoveTo())) {
-				// support simple SpEL expression
-				Expression exp = parser.parseExpression(getMoveToExpression());
-				double newValue = exp.getValue(context, Double.class);
-				super.asynchronousMoveTo(newValue);
+				super.asynchronousMoveTo(parser.parseExpression(getMoveToExpression()).getValue(context, Double.class));
 			} else {
-				// support complex algorithm implemented as method in SpELUtils class
-				Class<?>[] parameterTypes = getParameterTypes(functionParameterTypesTobeCalledInAsynchronousMoveTo);
-				try {
-					context.registerFunction(getFunctionNameToBeCalledInAsynchronousMoveTo(),
-							SpELUtils.class.getDeclaredMethod(getFunctionNameToBeCalledInAsynchronousMoveTo(), parameterTypes));
-					parser.parseExpression(getMoveToExpression()).getValue(context);
-				} catch (NoSuchMethodException | SecurityException e) {
-					logger.error("Cannot find the given method {}", getFunctionNameToBeCalledInAsynchronousMoveTo(), e);
-					throw new DeviceException(e.getMessage(), e);
-				}
+				// move call is done inside registered function
+				parser.parseExpression(getMoveToExpression()).getValue(context);
 			}
 		}
 	}
@@ -170,66 +163,23 @@ public class SpELExpressionScannable extends PVScannable {
 	 */
 	@Override
 	public Object getPosition() throws DeviceException {
-		return calculateNewPosition(Double.valueOf(super.getPosition().toString()).doubleValue());
+		return calculateNewPosition(super.getPosition());
 	}
 
 	@Override
 	protected void notifyObserversOfNewPosition(Serializable newPosition) {
-		try {
-			this.notifyIObservers(this, new ScannablePositionChangeEvent(calculateNewPosition(Double.valueOf(newPosition.toString()).doubleValue())));
-		} catch (NumberFormatException | DeviceException e) {
-			logger.error("Exception throw in notifyObserversOfNewPosition(Serializable)", e);
-		}
+		Double calculateNewPosition = calculateNewPosition(newPosition);
+		super.notifyObserversOfNewPosition(calculateNewPosition);
 	}
 
-	private double calculateNewPosition(double newPosition) throws DeviceException {
+	private Double calculateNewPosition(Object newPosition) {
+		Double position = Double.parseDouble(newPosition.toString());
 		if (Strings.isNullOrEmpty(getGetPositionExpression())) {
-			return newPosition;
+			return position;
 		} else {
-			double value;
-			context.setVariable("output", newPosition);
-			if (Strings.isNullOrEmpty(getFunctionNameToBeCalledInGetPosition())) {
-				// support simple SpEL expression
-				Expression exp = parser.parseExpression(getGetPositionExpression());
-				value = exp.getValue(context, Double.class);
-
-			} else {
-				// support complex algorithm implemented as method in SpELUtils class
-				Class<?>[] parameterTypes = getParameterTypes(functionParameterTypesToBeCalledInGetPosition);
-				try {
-					context.registerFunction(getFunctionNameToBeCalledInGetPosition(),
-							SpELUtils.class.getDeclaredMethod(getFunctionNameToBeCalledInGetPosition(), parameterTypes));
-				} catch (NoSuchMethodException | SecurityException e) {
-					logger.error("Cannot find the given method {}", getFunctionNameToBeCalledInGetPosition(), e);
-				}
-				value = parser.parseExpression(getGetPositionExpression()).getValue(context, Double.class);
-			}
-			return value;
+			context.setVariable("output", position);
+			return parser.parseExpression(getGetPositionExpression()).getValue(context, Double.class);
 		}
-	}
-
-	private Class<?> getClassForName(String clazz) throws ClassNotFoundException {
-		return Class.forName(clazz);
-	}
-
-	private void logError(Exception e) {
-		logger.error(e.getMessage(), e);
-	}
-
-	private Class<?>[] getParameterTypes(List<String> functionParameterTypes) throws DeviceException {
-		Class<?>[] parameterTypes;
-		Supplier<Stream<Either<Exception, Class<?>>>> streamSupplier = () -> Optional.ofNullable(functionParameterTypes).map(Collection::stream).orElseGet(Stream::empty)
-				.map(Either.lift(this::getClassForName));
-		if (streamSupplier.get().anyMatch(e -> e.isLeft())) {
-			// handle any exception thrown inside stream
-			String message = streamSupplier.get().filter(e -> e.isLeft()).map(e -> e.getLeft().get()).peek(this::logError).map(e -> e.getMessage())
-					.collect(Collectors.joining(","));
-			throw new DeviceException(message);
-		} else {
-			// No Exception in the stream
-			parameterTypes = streamSupplier.get().map(e -> e.getRight()).filter(e -> e.isPresent()).map(e -> e.get()).toArray(Class[]::new);
-		}
-		return parameterTypes;
 	}
 
 	public String getMoveToExpression() {
@@ -264,19 +214,19 @@ public class SpELExpressionScannable extends PVScannable {
 		this.functionNameToBeCalledInGetPosition = functionNameToBeCalledInGetPosition;
 	}
 
-	public List<String> getFunctionParameterTypesTobeCalledInAsynchronousMoveTo() {
-		return functionParameterTypesTobeCalledInAsynchronousMoveTo;
+	public List<Class<?>> getFunctionParameterTypesTobeCalledInAsynchronousMoveTo() {
+		return functionParameterTypesToBeCalledInAsynchronousMoveTo;
 	}
 
-	public void setFunctionParameterTypesTobeCalledInAsynchronousMoveTo(List<String> functionParameterTypesTobeCalledInAsynchronousMoveTo) {
-		this.functionParameterTypesTobeCalledInAsynchronousMoveTo = functionParameterTypesTobeCalledInAsynchronousMoveTo;
+	public void setFunctionParameterTypesTobeCalledInAsynchronousMoveTo(List<Class<?>> functionParameterTypesTobeCalledInAsynchronousMoveTo) {
+		this.functionParameterTypesToBeCalledInAsynchronousMoveTo = functionParameterTypesTobeCalledInAsynchronousMoveTo;
 	}
 
-	public List<String> getFunctionParameterTypesToBeCalledInGetPosition() {
+	public List<Class<?>> getFunctionParameterTypesToBeCalledInGetPosition() {
 		return functionParameterTypesToBeCalledInGetPosition;
 	}
 
-	public void setFunctionParameterTypesToBeCalledInGetPosition(List<String> functionParameterTypesToBeCalledInGetPosition) {
+	public void setFunctionParameterTypesToBeCalledInGetPosition(List<Class<?>> functionParameterTypesToBeCalledInGetPosition) {
 		this.functionParameterTypesToBeCalledInGetPosition = functionParameterTypesToBeCalledInGetPosition;
 	}
 
