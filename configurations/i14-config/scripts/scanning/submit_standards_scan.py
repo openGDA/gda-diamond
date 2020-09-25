ebaa9f4e438f36083ca7698bbfcf7b73991acaa6
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-09-25 15:41:22 +0100

    I14-823 Restore parsing of parameters in standards scan script
    Change-Id: I4689b6c0d095543a99d63cc3defcc80dd4565ad9

--- configurations/i14-config/scripts/scanning/submit_standards_scan.py
+++ configurations/i14-config/scripts/scanning/submit_standards_scan.py
@@ -1,7 +1,18 @@
 # Script to be called by the Standards scan GUI
-# It passes the contents of the energy and exposure time text boxs to standards_scan()
-# The values to be passed must have been set as named values in the script service as
-# "scanPath" and "exposureTime" respectively.
+# It splits up the arguments in the energy text box and calls standards_scan()
+# The GUI sets the contents of the energy text box as a named value in the script service as "scanPath"
+# and the exposure time as "exposureTime".
+
+import re
+
+# An energy range specifies <start> <stop> <step> as a string such as: "5 20 0.001"
+# Split the string and convert to floating point numbers
+def split_and_convert_energy_range(energy_range):
+    split_range = energy_range.split(' ')
+    return [float(i) for i in split_range]
 
 print("Running submit_standards_scan.py")
-standards_scan(scanPath, exposureTime)
+
+energy_ranges = scanPath.split(';')
+path = [split_and_convert_energy_range(i) for i in energy_ranges]
+standards_scan(path, float(exposureTime))
