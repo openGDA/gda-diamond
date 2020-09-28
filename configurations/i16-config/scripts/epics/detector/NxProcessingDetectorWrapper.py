c7a86def02181f3073bd9f3d6deda5a49ce39e9c
Mark Booth <Mark.Booth@diamond.ac.uk>
2020-09-28 12:48:17 +0100

    Revert "I16-350 (9.15) Enable pil3 hdf5 writing"
    This reverts commit 8e7b96143a51ace19903ca877db83901ff6610f0.
    
    Change-Id: Ibc6a4f3fcd1f80eb0b848fba3ce947588c7fe7d5

--- configurations/i16-config/scripts/epics/detector/NxProcessingDetectorWrapper.py
+++ configurations/i16-config/scripts/epics/detector/NxProcessingDetectorWrapper.py
@@ -1,4 +1,7 @@
-from gdascripts.scannable.detector.ProcessingDetectorWrapper import SwitchableHardwareTriggerableProcessingDetectorWrapper
+import gda.device.detector.addetector.filewriter.MultipleImagesPerHDF5FileWriter
+from gdascripts.scannable.detector.ProcessingDetectorWrapper import ProcessingDetectorWrapper, SwitchableHardwareTriggerableProcessingDetectorWrapper
+from scisoftpy.external import create_function
+from gda.configuration.properties import LocalProperties
 from gda.device import DeviceException
 
 class NxProcessingDetectorWrapper(SwitchableHardwareTriggerableProcessingDetectorWrapper):
@@ -39,12 +42,33 @@ class NxProcessingDetectorWrapper(SwitchableHardwareTriggerableProcessingDetecto
                 return_performance_metrics,
                 array_monitor_for_hardware_triggering )
 
+        self.linkFunction = create_function("detectorLinkInserter", "nexusHDFLink", dls_module='python/ana')
         self.lastReadout = None
+
 
     def atScanEnd(self):
         SwitchableHardwareTriggerableProcessingDetectorWrapper.atScanEnd(self)
+        plugins = self.det.getAdditionalPluginList()
+        writers = [ wr for wr in plugins if isinstance(wr, gda.device.detector.nxdetector.plugin.areadetector.filewriter.MultipleImagesPerHDF5FileWriter) ]
+        if len(writers) == 0:
+            return
+        if not LocalProperties.get("gda.scan.endscan.neworder", "False").lower() == "true":
+            print "Warning: gda.scan.endscan.neworder must be True to create hdf links"
+            return
+        writer = writers[0]
+        ndfile = writer.getNdFile()
+        detectorFileName = ndfile.getFileTemplate_RBV() % (ndfile.getFilePath_RBV(), ndfile.getFileName_RBV(), ndfile.getFileNumber_RBV())
+        nexusPaths = ["/entry1/instrument/%s" % self.name, "/entry1/%s" % self.name]
+        datadirectory = LocalProperties.get("gda.data.scan.datawriter.datadir")
+        nexusFileName = "%s/%d.nxs" % (datadirectory, ndfile.getFileNumber_RBV())
+        detectorPath = "/entry/instrument/detector/data"
+        detectorFileName = detectorFileName.replace(datadirectory, "")
+        if detectorFileName[0] == '/':
+            detectorFileName = detectorFileName.split('/', 1)[1]
+        print "Creating HDF Links"
+        self.linkFunction(nexusFileName, detectorFileName, nexusPaths, detectorPath)
 
-    def getExtraNames(self): # Replace ExtraName 't' with 'count_time'
+    def getExtraNames(self):
         return ['count_time'] + SwitchableHardwareTriggerableProcessingDetectorWrapper.getExtraNames(self)[1:]
 
     def _readout(self):
@@ -60,3 +84,4 @@ class NxProcessingDetectorWrapper(SwitchableHardwareTriggerableProcessingDetecto
             if self.lastReadout == None:
                 raise #this is getting silly
             return self.lastReadout
+
