daa2c3e0a23b90f59ef74397c3fa736c2d2f9a5e
Anthony Hull <anthony.hull@diamond.ac.uk>
2020-10-20 15:59:36 +0100

    Add watchdog functionality to P99
    - Define watchdogs, but disable them by default
    - Add beans for beam & port shutter. These have to be dummy, as the
    beamline is not connected to the ring
    
    Change-Id: I6843458698bfd33051cba6de25088146e76c97be

--- configurations/p99-config/scripts/localStation.py
+++ configurations/p99-config/scripts/localStation.py
@@ -1,9 +1,17 @@
 # localStation.py
 # For beamline specific initialisation code.
 #
+from gda.jython.commands.GeneralCommands import alias
 from mapping_scan_commands import (
     mscan, detector, scan_request, submit, step, grid, circ, poly, rect)
 
 print "Installing standard scans with processing"
 from gdascripts.scan.installStandardScansWithProcessing import * #@UnusedWildImport
 scan_processor.rootNamespaceDict = globals()
+
+print("Importing watchdog commands")
+from gdascripts.watchdogs.watchdogs import enable_watchdogs, disable_watchdogs, list_watchdogs, set_watchdog_enabled, is_watchdog_enabled
+alias("enable_watchdogs")
+alias("disable_watchdogs")
+alias("list_watchdogs")
+disable_watchdogs()
