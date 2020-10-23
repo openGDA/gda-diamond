cf4c3b5da664ff2833f887ed3c0161806b0f531d
Iain Hall <iain.hall@diamond.ac.uk>
2020-10-23 11:42:05 +0100

    I20_1-326 : Power calculation updates
    Updates for power calculation :
    - Correct scannables are monitored so that power calculation is run each
    time a relevant motor is moved.
    - ME2Scannable now notifies observers when a change in one of the
    underlying scannables takes place.
    - Filter and flux files added (so power calculation now works in dummy
    mode works) - these were copied from
    /dls_sw/i20-1/software/gda_var/powerCalculationData
    
    Change-Id: I55d0164b5245a3fff9f730994fba74396c94e4cf

--- plugins/uk.ac.gda.beamline.i20_1.server/src/gda/device/scannable/ME2Positioner.java
+++ plugins/uk.ac.gda.beamline.i20_1.server/src/gda/device/scannable/ME2Positioner.java
@@ -18,15 +18,27 @@
 
 package gda.device.scannable;
 
+import static gda.device.EnumPositionerStatus.IDLE;
+import static gda.device.EnumPositionerStatus.MOVING;
+
 import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import gda.device.DeviceException;
 import gda.device.EnumPositioner;
+import gda.device.EnumPositionerStatus;
 import gda.device.enumpositioner.EnumPositionerBase;
+import uk.ac.diamond.daq.concurrent.Async;
 import uk.ac.gda.api.remoting.ServiceInterface;
 
 @ServiceInterface(EnumPositioner.class)
 public class ME2Positioner extends EnumPositionerBase implements EnumPositioner {
+
+	private static final Logger logger = LoggerFactory.getLogger(ME2Positioner.class);
+	private EnumPositioner stripeScannable;
+	private EnumPositioner yScannable;
 
 	public enum Positions {
 		RHODIUM("Rhodium"), SILICON("Silicon"), OUT("Out of beam");
@@ -65,34 +77,58 @@ public class ME2Positioner extends EnumPositionerBase implements EnumPositioner 
 		}
 	}
 
-	private EnumPositioner stripeScannable;
-	private EnumPositioner yScannable;
-
 	public ME2Positioner() {
 		setPositionsInternal(Arrays.asList(Positions.RHODIUM.toString(),
 				Positions.SILICON.toString(), Positions.OUT.toString()));
 	}
 
 	@Override
-	public boolean isBusy() throws DeviceException {
-		return stripeScannable.isBusy() || yScannable.isBusy();
+	public void rawAsynchronousMoveTo(Object position) throws DeviceException {
+		setStatus(MOVING);
+		Async.execute(() -> {
+			try {
+				moveTo(position);
+			} catch (DeviceException e) {
+				logger.error("Error moving {} to position {}", getName(), position, e);
+			} finally {
+				setStatus(IDLE);
+			}
+		});
 	}
 
 	@Override
-	public void rawAsynchronousMoveTo(Object position) throws DeviceException {
+	public void moveTo(Object position) throws DeviceException {
+		setStatus(EnumPositionerStatus.MOVING);
 		Positions targetPosition = Positions.findByUserName(position.toString());
+		if (targetPosition == null) {
+			logger.warn("Could not move {} to position {}", getName(), position);
+			setStatus(EnumPositionerStatus.IDLE);
+			return;
+		}
+
+		logger.debug("Moving {} to {}", getName(), targetPosition);
 		switch (targetPosition) {
-		case RHODIUM:
-			stripeScannable.asynchronousMoveTo(Positions.RHODIUM.toString());
-			yScannable.asynchronousMoveTo(Me2yPosition.IN.toString());
-			break;
-		case SILICON:
-			stripeScannable.asynchronousMoveTo(Positions.SILICON.toString());
-			yScannable.asynchronousMoveTo(Me2yPosition.IN.toString());
-			break;
-		case OUT:
-			yScannable.asynchronousMoveTo(Me2yPosition.OUT.toString());
-			break;
+			case RHODIUM:
+				stripeScannable.asynchronousMoveTo(Positions.RHODIUM.toString());
+				yScannable.asynchronousMoveTo(Me2yPosition.IN.toString());
+				break;
+			case SILICON:
+				stripeScannable.asynchronousMoveTo(Positions.SILICON.toString());
+				yScannable.asynchronousMoveTo(Me2yPosition.IN.toString());
+				break;
+			case OUT:
+				yScannable.asynchronousMoveTo(Me2yPosition.OUT.toString());
+				break;
+		}
+
+		try {
+			stripeScannable.waitWhileBusy();
+			yScannable.waitWhileBusy();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new DeviceException(e);
+		} finally {
+			setStatus(EnumPositionerStatus.IDLE);
 		}
 	}
 
@@ -104,6 +140,13 @@ public class ME2Positioner extends EnumPositionerBase implements EnumPositioner 
 			return Positions.OUT.toString();
 		}
 		return "unknown";
+	}
+
+	private void setStatus(EnumPositionerStatus status) {
+		if (status != getPositionerStatus()) {
+			setPositionerStatus(status);
+			notifyIObservers(this, status);
+		}
 	}
 
 	public EnumPositioner getStripeScannable() {
