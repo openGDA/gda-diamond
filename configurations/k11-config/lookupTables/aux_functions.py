0f0047c0834b37e8880bad97490bab2103858f98
Douglas Winter <douglas.winter@diamond.ac.uk>
2021-01-29 13:17:10 +0000

    K11: Update diffraction mono energy workflow chain
    - chain reconfigured
    - all scannables apart from eh_shutter swapped to live
    
    Change-Id: Ieae3f7d9da17d293710a7422dc5f2f6ed46fa31c

--- configurations/k11-config/lookupTables/aux_functions.py
+++ configurations/k11-config/lookupTables/aux_functions.py
@@ -6,17 +6,23 @@ Created on Tue Nov  3 13:20:16 2020
 
 # Hans: better give these functions distinct names!
 
-def beam_selector_diff_pos(eneregy):
-    return 45
+def beam_selector_monoDiff_pos(eneregy):
+    return 'Diffraction beam' # 45
 
     
     
     
-def beam_selector_diff_pos(eneregy):
-    return 225
+def beam_selector_monoImg_pos(eneregy):
+    return 'Mono imaging beam' # 225
 
 
 
 
-def beam_selector_diff_pos(eneregy):
-    return 135
+def beam_selector_noBeam_pos(eneregy):
+    return 'No beam' # 135
+  
+def beam_selector_allBeams_pos(energy):
+    return 'All beams through'
+
+def beam_selector_pinkImg_pos(energy):
+    return 'Pink imaging beam'
