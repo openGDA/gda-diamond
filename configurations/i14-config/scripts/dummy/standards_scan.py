ec7b221ded70d2c81a530bfffbf174ec76e13aa5
Anthony Hull <anthony.hull@diamond.ac.uk>
2021-03-02 16:46:34 +0000

    DAQ-833 Add "reverse scan" check box to Standards Scan view
    Pass parameters to Jython as a StandardsScanParams object rather
    than individually, for scalability.
    
    Change-Id: I3294758ed3dd784fd89d7cc3df2c39562123164a

--- configurations/i14-config/scripts/dummy/standards_scan.py
+++ configurations/i14-config/scripts/dummy/standards_scan.py
@@ -3,15 +3,18 @@
 import sys
 from time import sleep
 
-def standards_scan(*args):
+def standards_scan(energy_ranges, exposure_time, reverse_scan):
     try:
-        run_standards_scan(args)
+        run_standards_scan(energy_ranges, exposure_time, reverse_scan)
     except (KeyboardInterrupt):
         print("Standards scan interrupted by user")
     except:
         print("Standards scan terminated abnormally: {0}".format(sys.exc_info()[0]))
 
-def run_standards_scan(*args):
-    print("Running standards scan with arguments: {}".format(args))
+def run_standards_scan(energy_ranges, exposure_time, reverse_scan):
+    print("Running standards scan")
+    print("Energy_ranges: {}".format(energy_ranges))
+    print("Exposure time: {}".format(exposure_time))
+    print("Reverse scan: {}".format(str(reverse_scan)))
     sleep(5)
     print("Finished running standards scan")
--- configurations/i14-config/scripts/scanning/submit_standards_scan.py
+++ configurations/i14-config/scripts/scanning/submit_standards_scan.py
@@ -3,7 +3,9 @@
 # The GUI sets the contents of the energy text box as a named value in the script service as "scanPath"
 # and the exposure time as "exposureTime".
 
-import re
+from gda.util.osgi import OsgiJythonHelper
+from org.eclipse.dawnsci.analysis.api.persistence import IMarshallerService
+from uk.ac.diamond.daq.mapping.api import StandardsScanParams
 
 # An energy range specifies <start> <stop> <step> as a string such as: "5 20 0.001"
 # Split the string and convert to floating point numbers
@@ -13,6 +15,9 @@ def split_and_convert_energy_range(energy_range):
 
 print("Running submit_standards_scan.py")
 
-energy_ranges = scanPath.split(';')
+marshaller_service = OsgiJythonHelper.getService(IMarshallerService)
+scanParams = marshaller_service.unmarshal(standardsScanParamsJson, StandardsScanParams)
+
+energy_ranges = scanParams.getScanPath().split(';')
 path = [split_and_convert_energy_range(i) for i in energy_ranges]
-standards_scan(path, float(exposureTime))
+standards_scan(path, scanParams.getExposureTime(), scanParams.isReverseScan())
