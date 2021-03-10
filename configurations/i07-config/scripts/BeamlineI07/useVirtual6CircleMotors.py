d0800a0c00792f42b621554c197683b776f1a921
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-03-10 19:30:23 +0000

    I07-366 Rationalise DIFF1 motor names
    Change-Id: Iaf15d6b6fe57f174c8780c61d0a90792556c8c2c

--- configurations/i07-config/scripts/BeamlineI07/useVirtual6CircleMotors.py
+++ configurations/i07-config/scripts/BeamlineI07/useVirtual6CircleMotors.py
@@ -1,5 +1,5 @@
 from BeamlineI07.devices.virtual6c import Virtual6CircleCompositeMotor
-from gdaserver import diff1halpha, diff1valpha, diff1vgamma, diff1vdelta
+from gdaserver import diff1chi, diff1alpha, diff1gamma, diff1delta, diff2alpha, diff2gamma, diff2delta
 from gda.configuration.properties import LocalProperties
 
 
@@ -7,11 +7,11 @@ from gda.configuration.properties import LocalProperties
 diffmode = LocalProperties.get("gda.active.diffractometer.mode")
 
 if diffmode == "eh1h":
-	sgam = Virtual6CircleCompositeMotor('sgam', diff1halpha, diff1vgamma,  diff1vdelta, 'horizontalVirtualGamma')
-	sdel = Virtual6CircleCompositeMotor('sdel', diff1halpha, diff1vgamma,  diff1vdelta, 'horizontalVirtualDelta')
+	sgam = Virtual6CircleCompositeMotor('sgam', diff1chi, diff1gamma,  diff1delta, 'horizontalVirtualGamma')
+	sdel = Virtual6CircleCompositeMotor('sdel', diff1chi, diff1gamma,  diff1delta, 'horizontalVirtualDelta')
 elif diffmode == "eh1v":
-	sgam = Virtual6CircleCompositeMotor('sgam', diff1valpha, diff1vgamma,  diff1vdelta, 'verticalVirtualGamma')
-	sdel = Virtual6CircleCompositeMotor('sdel', diff1valpha, diff1vgamma,  diff1vdelta, 'verticalVirtualDelta')
+	sgam = Virtual6CircleCompositeMotor('sgam', diff1alpha, diff1gamma,  diff1delta, 'verticalVirtualGamma')
+	sdel = Virtual6CircleCompositeMotor('sdel', diff1alpha, diff1gamma,  diff1delta, 'verticalVirtualDelta')
 elif diffmode == "eh2":
 	sgam = Virtual6CircleCompositeMotor('sgam', diff2alpha, diff2gamma,  diff2delta, 'verticalVirtualGamma')
 	sdel = Virtual6CircleCompositeMotor('sdel', diff2alpha, diff2gamma,  diff2delta, 'verticalVirtualDelta')
