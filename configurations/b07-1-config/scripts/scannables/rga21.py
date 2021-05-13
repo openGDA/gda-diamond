7bad1f2f74a1726eca8b0188f0d0d4ffb49c9ea8
Tom Richardson <tom.richardson@diamond.ac.uk>
2021-05-13 17:11:19 +0100

    B07-574 Fix incorrect PV names for RGA21 and RGA24 scannables
    Change-Id: I5b9e5e382dd75283c3973749d0debad862b0eff0

--- configurations/b07-1-config/scripts/scannables/rga21.py
+++ configurations/b07-1-config/scripts/scannables/rga21.py
@@ -11,12 +11,12 @@ rga21HE = EpicsMonitorClass('rga21HE', 'BL07C-VA-RGA-21:BAR:M4', 'mbar', '%.2e')
 rga21CH2 = EpicsMonitorClass('rga21CH2', 'BL07C-VA-RGA-21:BAR:M14', 'mbar', '%.2e')
 rga21CH3 = EpicsMonitorClass('rga21CH3', 'BL07C-VA-RGA-21:BAR:M15', 'mbar', '%.2e')
 rga21CH4 = EpicsMonitorClass('rga21CH4', 'BL07C-VA-RGA-21:BAR:M16', 'mbar', '%.2e')
-rga21H2O = EpicsMonitorClass('rga21H2O', 'BL06I-VA-RGA-21:BAR:M18', 'mbar', '%.2e')
-rga21CO = EpicsMonitorClass('rga21CO', 'BL06I-VA-RGA-21:BAR:M28', 'mbar', '%.2e')
-rga21O2 = EpicsMonitorClass('rga21O2', 'BL06I-VA-RGA-21:BAR:M32', 'mbar', '%.2e')
-rga21AR = EpicsMonitorClass('rga21AR', 'BL06I-VA-RGA-21:BAR:M40', 'mbar', '%.2e')
-rga21CO2 = EpicsMonitorClass('rga21CO2', 'BL06I-VA-RGA-21:BAR:M44', 'mbar', '%.2e')
-rga21CF3 = EpicsMonitorClass('rga21CF3', 'BL06I-VA-RGA-21:BAR:M69', 'mbar', '%.2e')
+rga21H2O = EpicsMonitorClass('rga21H2O', 'BL07C-VA-RGA-21:BAR:M18', 'mbar', '%.2e')
+rga21CO = EpicsMonitorClass('rga21CO', 'BL07C-VA-RGA-21:BAR:M28', 'mbar', '%.2e')
+rga21O2 = EpicsMonitorClass('rga21O2', 'BL07C-VA-RGA-21:BAR:M32', 'mbar', '%.2e')
+rga21AR = EpicsMonitorClass('rga21AR', 'BL07C-VA-RGA-21:BAR:M40', 'mbar', '%.2e')
+rga21CO2 = EpicsMonitorClass('rga21CO2', 'BL07C-VA-RGA-21:BAR:M44', 'mbar', '%.2e')
+rga21CF3 = EpicsMonitorClass('rga21CF3', 'BL07C-VA-RGA-21:BAR:M69', 'mbar', '%.2e')
 
 rga21= [rga21tot, rga21sumofpeaks, rga21H2, rga21HE, rga21CH2, rga21CH3, rga21CH4, rga21H2O,rga21CO, rga21O2, rga21AR, rga21CO2, rga21CF3]
 
--- configurations/b07-1-config/scripts/scannables/rga24.py
+++ configurations/b07-1-config/scripts/scannables/rga24.py
@@ -11,12 +11,12 @@ rga24HE = EpicsMonitorClass('rga24HE', 'BL07C-VA-RGA-24:BAR:M4', 'mbar', '%.2e')
 rga24CH2 = EpicsMonitorClass('rga24CH2', 'BL07C-VA-RGA-24:BAR:M14', 'mbar', '%.2e')
 rga24CH3 = EpicsMonitorClass('rga24CH3', 'BL07C-VA-RGA-24:BAR:M15', 'mbar', '%.2e')
 rga24CH4 = EpicsMonitorClass('rga24CH4', 'BL07C-VA-RGA-24:BAR:M16', 'mbar', '%.2e')
-rga24H2O = EpicsMonitorClass('rga24H2O', 'BL06I-VA-RGA-24:BAR:M18', 'mbar', '%.2e')
-rga24CO = EpicsMonitorClass('rga24CO', 'BL06I-VA-RGA-24:BAR:M28', 'mbar', '%.2e')
-rga24O2 = EpicsMonitorClass('rga24O2', 'BL06I-VA-RGA-24:BAR:M32', 'mbar', '%.2e')
-rga24AR = EpicsMonitorClass('rga24AR', 'BL06I-VA-RGA-24:BAR:M40', 'mbar', '%.2e')
-rga24CO2 = EpicsMonitorClass('rga24CO2', 'BL06I-VA-RGA-24:BAR:M44', 'mbar', '%.2e')
-rga24CF3 = EpicsMonitorClass('rga24CF3', 'BL06I-VA-RGA-24:BAR:M69', 'mbar', '%.2e')
+rga24H2O = EpicsMonitorClass('rga24H2O', 'BL07C-VA-RGA-24:BAR:M18', 'mbar', '%.2e')
+rga24CO = EpicsMonitorClass('rga24CO', 'BL07C-VA-RGA-24:BAR:M28', 'mbar', '%.2e')
+rga24O2 = EpicsMonitorClass('rga24O2', 'BL07C-VA-RGA-24:BAR:M32', 'mbar', '%.2e')
+rga24AR = EpicsMonitorClass('rga24AR', 'BL07C-VA-RGA-24:BAR:M40', 'mbar', '%.2e')
+rga24CO2 = EpicsMonitorClass('rga24CO2', 'BL07C-VA-RGA-24:BAR:M44', 'mbar', '%.2e')
+rga24CF3 = EpicsMonitorClass('rga24CF3', 'BL07C-VA-RGA-24:BAR:M69', 'mbar', '%.2e')
 
 rga24= [rga24tot, rga24sumofpeaks, rga24H2, rga24HE, rga24CH2, rga24CH3, rga24CH4, rga24H2O,rga24CO, rga24O2, rga24AR, rga24CO2, rga24CF3]
 
