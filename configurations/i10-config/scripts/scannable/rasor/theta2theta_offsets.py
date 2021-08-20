9d0025841a71e21eb0ad0231bc0f0b4e23a5c5d7
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2021-08-20 17:21:58 +0100

    I10-521 refactor theta-two theta offsets to support dummy for Rasor
    Change-Id: I843e87cb9da8d9fda324249c0e6978983fc754e6

---
+++ configurations/i10-config/scripts/scannable/rasor/theta2theta_offsets.py
@@ -0,0 +1,21 @@
+'''
+Created on Aug 19, 2021
+
+@author: fy65
+'''
+import installation
+from gdascripts.pd.epics_pds import EpicsReadWritePVClass
+from gdascripts.pd.dummy_pds import DummyEpicsReadWritePVClass
+from utils.ExceptionLogs import localStation_exception
+import sys
+
+if installation.isLive():
+    try:
+        th_off = EpicsReadWritePVClass('th_off', 'ME01D-MO-DIFF-01:THETA.OFF', 'deg', '%.6f')
+        tth_off = EpicsReadWritePVClass('tth_off', 'ME01D-MO-DIFF-01:TWOTHETA.OFF', 'deg', '%.6f')
+    except:
+        localStation_exception(sys.exc_info(), "creating th & tth offset and encoder offset scannables")
+else:
+    th_off = DummyEpicsReadWritePVClass('th_off', 90.0, 'deg', '%.6f')
+    tth_off = DummyEpicsReadWritePVClass('tth_off', 90.0, 'deg', '%.6f')
+        
