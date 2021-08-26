924fd8744786cb5a2c44b2724de7c0cb62c7fcc0
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2021-08-26 15:09:30 +0100

    I10-509 format error in EM control of the magnetic field
    Change-Id: I36cedddba95738b8b7b02652fb770d46255b7fed

--- configurations/i10-config/scripts/scannable/positions/magnet_instances.py
+++ configurations/i10-config/scripts/scannable/positions/magnet_instances.py
@@ -4,5 +4,5 @@ Created on Jan 5, 2021
 @author: fy65
 '''
 from scannable.positions.positionCompare import PositionCompareClass
-magnetField = PositionCompareClass("magnetField", "BL10J-EA-MAGC-01:FIELD", "BL10J-EA-MAGC-01:FIELD:RBV", 0.1, "T", "3.3f")
-magnetCurrent = PositionCompareClass("magnetCurrent", "BL10J-EA-MAGC-01:DMD", "BL10J-EA-MAGC-01:I:RBV", 1, "A", "3.3f")
+magnetField = PositionCompareClass("magnetField", "BL10J-EA-MAGC-01:FIELD", "BL10J-EA-MAGC-01:FIELD:RBV", 0.1, "T", "%3.3f")
+magnetCurrent = PositionCompareClass("magnetCurrent", "BL10J-EA-MAGC-01:DMD", "BL10J-EA-MAGC-01:I:RBV", 1, "A", "%3.3f")
