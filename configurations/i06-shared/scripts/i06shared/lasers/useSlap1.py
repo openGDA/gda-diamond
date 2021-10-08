e0ce88a600ed21638898d4887d6c8671f8ba5ecc
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2021-10-08 10:26:10 +0100

    update from i06-1 beamline GDA
    Change-Id: Iacd4c739d9017c630ebee4933586cdd3b0d55fc4

--- configurations/i06-shared/scripts/i06shared/lasers/useSlap1.py
+++ configurations/i06-shared/scripts/i06shared/lasers/useSlap1.py
@@ -1,13 +1,13 @@
 from Diamond.Slap.SlapWebServiceClient import SlapClass, JavaSlapClass, LaserPhaseDeviceClass, LaserDelayDeviceClass, LaserLockingStateDeviceClass
 
 #I06 rack mounted Windows server i06-ws011
-LabViewWebServiceURL = 'http://i06-ws011.diamond.ac.uk:8080/SlapWebServices'
+LabViewWebServiceURL = 'http://i06-ws020.diamond.ac.uk:8080/SlapWebServices'
 
 #laser=SlapClass(LabViewWebServiceURL);
 laser1=JavaSlapClass(LabViewWebServiceURL);
 laser1.phase2delay_ce=[-0.0058976070255, 1.31253880142145E-4, -2.26326432895618E-8, 3.53663784002266E-12, -2.63010486113614E-16, 1.04042843159956E-20, -2.16666613588553E-25, 2.26190976227321E-30, -9.58629545249244E-36, 2.90678349766489E-42]
 laser1.delay2phase_ce=[-88.36914666723533, 12855.32380237554, 824.10592216745135, -1946.0535305088042, 664.20982780807435, -115.31870988361487, 11.61543948306145, -0.68333494362544, 0.02173576685147, -2.87621826209067E-4]
-laser1.setHost("i06-ws011.diamond.ac.uk");
+laser1.setHost("i06-ws020.diamond.ac.uk");
 laser1.setOffset(20000);
 #laser1.setMaxStep(100):
 
@@ -16,4 +16,4 @@ laser1phase=LaserPhaseDeviceClass("laser1phase", laser1)
 laser1delay=LaserDelayDeviceClass("laser1delay", laser1)
 laser1locking=LaserLockingStateDeviceClass("laser1locking", laser1)
 
-#The value readback service for testing: http://i06-ws011.diamond.ac.uk:8080/SlapWebServices/ReadOnlyService
+#The value readback service for testing: http://i06-ws020.diamond.ac.uk:8080/SlapWebServices/ReadOnlyService
--- configurations/i06-shared/scripts/i06shared/lasers/useSlap2.py
+++ configurations/i06-shared/scripts/i06shared/lasers/useSlap2.py
@@ -1,13 +1,13 @@
 from Diamond.Slap.SlapWebServiceClient import SlapClass, JavaSlapClass, LaserPhaseDeviceClass, LaserDelayDeviceClass, LaserLockingStateDeviceClass
 
 #I06 rack mounted Windows server i06-ws014
-LabViewWebService2URL = 'http://i06-ws014.diamond.ac.uk:8080/SlapWebServices'
+LabViewWebService2URL = 'http://i06-ws021.diamond.ac.uk:8080/SlapWebServices'
 
 #laser2=SlapClass(LabViewWebServiceURL);
 laser2=JavaSlapClass(LabViewWebService2URL);
 laser2.phase2delay_ce=[-23.9209961982287, 0.142095153813483, -2.32220343219571E-05, 3.20115386459306E-09, -2.14561959061541E-13, 7.62576731446377E-18, 1.35627681022786E-22, 9.97611012094939E-28, 4.20630921302906E-34, -2.84482027189109E-38]
 laser2.delay2phase_ce=[250.936271279245, 13.6034842059384, 0.000656983157377759, -2.08799828953862E-06, 7.56552965866636E-10, -1.38088907300248E-13, 1.45660372818405E-17, -8.96012702104993E-22, 2.98195847300446E-26, -4.14229055816086E-31]
-laser2.setHost("i06-ws014.diamond.ac.uk");
+laser2.setHost("i06-ws021.diamond.ac.uk");
 laser2.setOffset(20000);
 #laser2.setMaxStep(100):
 
@@ -17,4 +17,4 @@ laser2delay=LaserDelayDeviceClass("laser2delay", laser2)
 laser2locking=LaserLockingStateDeviceClass("laser2locking", laser2)
 
 #To Test, open a browser from any beamline workstation, type the following address in LOcation field.
-#laser2URL='http://i06-ws014.diamond.ac.uk:8080/SlapWebServices/ReadOnlyService'
+#laser2URL='http://i06-ws021.diamond.ac.uk:8080/SlapWebServices/ReadOnlyService'
