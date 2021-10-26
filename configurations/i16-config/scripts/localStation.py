5bea6906bd7e392dcd53886eb00de3b266319030
Mark Booth <Mark.Booth@diamond.ac.uk>
2021-10-26 20:01:18 +0100

    I16-524 (9.21) Fix processing of pil3_100ks scans
    Change-Id: I0e31d3a8bd4df1df9dd9910e857e7a05ecf93376

--- configurations/i16-config/scripts/localStation.py
+++ configurations/i16-config/scripts/localStation.py
@@ -992,11 +992,38 @@ if USE_PIL3:
 			fileLoadTimout=60,
 			returnPathAsImageNumberOnly=True,
 			array_monitor_for_hardware_triggering = _pilatus3_counter_monitor)
-		pil3_100k.processors=[DetectorDataProcessorWithRoi('max', pil3_100k, [SumMaxPositionAndValue()], False)]
-		pil3_100k.printNfsTimes = False
-		pil3_100ks = DetectorWithShutter(pil3_100k, x1, X1_DELAY, nameSuffix="")
+
+		#pil3_100ks = DetectorWithShutter(pil3_100k, x1, X1_DELAY, nameSuffix="")
+		# With ^ the Nexus file ends up with a pil3_100ks node but the link writer inside NxProcessingDetectorWrapper fails with
+		#   KeyError: "Unable to open object (object 'pil3_100k' doesn't exist)"
+		# as pil3_100k has no way to know that it's name is being overridden. Instead clone the NxProcessingDetectorWrapper with
+		# it's new name baked in.
+		pil3_100ksNPDW = NxProcessingDetectorWrapper('pil3_100ks',
+				pil3_100k.detector,
+				pil3_100k.hardware_triggered_detector,
+				pil3_100k.detector_for_snaps,
+				pil3_100k.processors,
+				pil3_100k.panel_name,
+				pil3_100k.toreplace,
+				pil3_100k.replacement,
+				pil3_100k.iFileLoader,
+				pil3_100k.root_datadir,
+				pil3_100k.fileLoadTimout,
+				pil3_100k.printNfsTimes,
+				pil3_100k.returnPathAsImageNumberOnly,
+				pil3_100k.panel_name_rcp,
+				pil3_100k.return_performance_metrics,
+				pil3_100k.array_monitor_for_hardware_triggering)
+		pil3_100ks = DetectorWithShutter(pil3_100ksNPDW, x1, X1_DELAY, nameSuffix="")
+
+		pil3_100k.processors      = [DetectorDataProcessorWithRoi('max', pil3_100k,  [SumMaxPositionAndValue()], False)]
+		pil3_100ksNPDW.processors = [DetectorDataProcessorWithRoi('max', pil3_100ks, [SumMaxPositionAndValue()], False)]
+		pil3_100k.printNfsTimes      = False
+		pil3_100ksNPDW.printNfsTimes = False
 		pil3 = pil3_100k
+		pil  = pil3_100k
 		pil3s = pil3_100ks
+		pils  = pil3_100ks
 
 		pil3_100kthresh = PilatusThreshold('pil3_100kthresh', pil3_100k.hardware_triggered_detector.driver.getAdDriverPilatus())
 		pil3_100kgain =        PilatusGain('pil3_100kgain',   pil3_100k.hardware_triggered_detector.driver.getAdDriverPilatus())
