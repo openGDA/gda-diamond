5e7efa9997359001be75102b6959675410c59e1d
Douglas Winter <douglas.winter@diamond.ac.uk>
2021-12-06 16:03:20 +0000

    I18-509 Add scannable configurations in ascii header
    These are per-scan monitors which are explicitly configured by the
    user so must be included as metadata.
    
    Change-Id: I595e376d4ce9cabb86b1369f897c1021be77c2a0

--- plugins/uk.ac.diamond.daq.beamline.i18.controls/src/uk/ac/gda/server/exafs/scan/preparers/I18OutputPreparer.java
+++ plugins/uk.ac.diamond.daq.beamline.i18.controls/src/uk/ac/gda/server/exafs/scan/preparers/I18OutputPreparer.java
@@ -3,15 +3,27 @@ package uk.ac.gda.server.exafs.scan.preparers;
 import gda.data.metadata.NXMetaDataProvider;
 import gda.data.scan.datawriter.AsciiDataWriterConfiguration;
 import gda.data.scan.datawriter.AsciiMetadataConfig;
+import gda.device.DeviceException;
+import uk.ac.gda.beans.exafs.IDetectorParameters;
+import uk.ac.gda.beans.exafs.IOutputParameters;
+import uk.ac.gda.beans.exafs.ISampleParameters;
 import uk.ac.gda.beans.exafs.IScanParameters;
-import uk.ac.gda.beans.exafs.QEXAFSParameters;
-import uk.ac.gda.beans.exafs.XanesScanParameters;
-import uk.ac.gda.beans.exafs.XasScanParameters;
+import uk.ac.gda.beans.exafs.ScannableConfiguration;
+import uk.ac.gda.beans.exafs.i18.I18SampleParameters;
 
 public class I18OutputPreparer extends OutputPreparerBase {
 
+	private I18SampleParameters sampleParameters;
+
 	public I18OutputPreparer(AsciiDataWriterConfiguration datawriterconfig, NXMetaDataProvider metashop) {
 		super(datawriterconfig, metashop);
+	}
+
+	@Override
+	public void configure(IOutputParameters outputParameters, IScanParameters scanBean,
+			IDetectorParameters detectorBean, ISampleParameters sampleParameters) throws DeviceException {
+		super.configure(outputParameters, scanBean, detectorBean, sampleParameters);
+		this.sampleParameters = (I18SampleParameters) sampleParameters;
 	}
 
 	@Override
@@ -19,22 +31,16 @@ public class I18OutputPreparer extends OutputPreparerBase {
 		// get the basic one from super
 		AsciiDataWriterConfiguration datawriterconfig = super.getAsciiDataWriterConfig(scanBean).clone();
 
-		// if its xas, xanes, quick-xanes, then add the sample stage motor positions to the header
-		if (scanBean instanceof XasScanParameters || scanBean instanceof XanesScanParameters || scanBean instanceof QEXAFSParameters){
-			var xPosition = new AsciiMetadataConfig();
-			xPosition.setLabel("X stage");
-			datawriterconfig.getHeader().add(xPosition);
-
-			var yPosition = new AsciiMetadataConfig();
-			yPosition.setLabel("Y stage");
-			datawriterconfig.getHeader().add(yPosition);
-		}
-
-		// always add the Z position
-		var zPosition = new AsciiMetadataConfig();
-		zPosition.setLabel("Z stage");
-		datawriterconfig.getHeader().add(zPosition);
+		// print scannable configurations
+		sampleParameters.getScannableConfigurations().stream()
+			.map(this::toAsciiMetadataConfig).forEach(datawriterconfig.getHeader()::add);
 
 		return datawriterconfig;
+	}
+
+	private AsciiMetadataConfig toAsciiMetadataConfig(ScannableConfiguration configuration) {
+		var metadata = new AsciiMetadataConfig();
+		metadata.setLabel(configuration.getScannableName() + ": " + configuration.getPosition());
+		return metadata;
 	}
 }
