4474aa11e3572338f6661e01df33003ad87ab6ca
Joe Shannon <joe.shannon@diamond.ac.uk>
2021-12-13 13:39:29 +0000

    DAQ-2682 Delete uk.ac.gda.client.sixd
    Move used components into uk.ac.gda.beamline.i07.
    
    Change-Id: Ifc2eacb507eaf363565a13c077857dffe8e209df

---
+++ plugins/uk.ac.gda.beamline.i07/src/uk/ac/gda/sixd/views/I07DatasetPart.java
@@ -0,0 +1,37 @@
+/*-
+ * Copyright © 2021 Diamond Light Source Ltd.
+ *
+ * This file is part of GDA.
+ *
+ * GDA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License version 3 as published by the Free
+ * Software Foundation.
+ *
+ * GDA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GDA. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package uk.ac.gda.sixd.views;
+
+import org.dawnsci.datavis.view.parts.DatasetPart;
+
+public class I07DatasetPart extends DatasetPart {
+
+	private static final String ID = "uk.ac.gda.sixd.views.I07DatasetPart";
+
+	public static String getId() {
+		return ID;
+	}
+
+	@Override
+	protected String getLoadedFileSelectionPartID() {
+
+		return I07LoadedFilePart.getId();
+	}
+
+}
