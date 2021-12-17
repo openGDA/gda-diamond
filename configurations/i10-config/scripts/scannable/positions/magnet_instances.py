8e3b8ba4c4f4b2b4df9be2f48a444236703784b8
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2021-12-17 14:30:17 +0000

    I10-585 magnetCurrent readback not correct
    - use demand as readback as requested by bemline
    
    Change-Id: I2a5486ad8912497e21006648c6e9c6f2ce1e87b2

--- configurations/i10-config/scripts/scannable/positions/magnet_instances.py
+++ configurations/i10-config/scripts/scannable/positions/magnet_instances.py
@@ -5,4 +5,4 @@ Created on Jan 5, 2021
 '''
 from scannable.positions.positionCompare import PositionCompareClass
 magnetField = PositionCompareClass("magnetField", "BL10J-EA-MAGC-01:FIELD", "BL10J-EA-MAGC-01:FIELD:RBV", 0.1, "T", "%3.3f")
-magnetCurrent = PositionCompareClass("magnetCurrent", "BL10J-EA-MAGC-01:DMD", "BL10J-EA-MAGC-01:I:RBV", 1, "A", "%3.3f")
+magnetCurrent = PositionCompareClass("magnetCurrent", "BL10J-EA-MAGC-01:DMD", "BL10J-EA-MAGC-01:DMD", 1, "A", "%3.3f")
