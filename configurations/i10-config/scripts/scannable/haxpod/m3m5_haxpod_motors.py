a2635aeaf466c95946018960ba091ab7a11adb15
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2022-01-25 14:57:58 +0000

    I10-602 change deadband from 0.001 to 0.1
    Change-Id: I4c42f2426676ba8f76f60eca8edcd0865668428a

--- configurations/i10-config/scripts/scannable/haxpod/m3m5_haxpod_motors.py
+++ configurations/i10-config/scripts/scannable/haxpod/m3m5_haxpod_motors.py
@@ -22,7 +22,7 @@ if installation.isLive():
         m3m5_yaw = PositionCompareMotorClass("m3m5_yaw", "BL10I-OP-SWTCH-01:YAW.VAL", "BL10I-OP-SWTCH-01:YAW.RBV", "BL10I-OP-SWTCH-01:YAW.STOP", 0.002, "urad", "%.3f")
         m3m5_pitch = PositionCompareMotorClass("m3m5_pitch", "BL10I-OP-SWTCH-01:PITCH.VAL", "BL10I-OP-SWTCH-01:PITCH.RBV", "BL10I-OP-SWTCH-01:PITCH.STOP", 0.002, "urad", "%.3f")
         m3m5_roll = PositionCompareMotorClass("m3m5_roll", "BL10I-OP-SWTCH-01:ROLL.VAL", "BL10I-OP-SWTCH-01:ROLL.RBV", "BL10I-OP-SWTCH-01:ROLL.STOP", 0.002, "urad", "%.3f")
-        m3m5fpitch = SingleEpicsPositionerNoStatusClassDeadbandOrStop('m3m5fpitch', 'BL10I-OP-SWTCH-01:FPITCH:DMD:AO', 'BL10I-OP-SWTCH-01:FPITCH:RBV:AI', 'V', '%.3f', 0.001)
+        m3m5fpitch = SingleEpicsPositionerNoStatusClassDeadbandOrStop('m3m5fpitch', 'BL10I-OP-SWTCH-01:FPITCH:DMD:AO', 'BL10I-OP-SWTCH-01:FPITCH:RBV:AI', 'V', '%.3f', 0.1)
         M3M5=ScannableGroup("M3M5", [m3m5_x, m3m5_y, m3m5_z, m3m5_yaw, m3m5_pitch, m3m5_roll, m3m5fpitch])
     except:
         localStation_exception(sys.exc_info(), "initialising m3m5 hexapod and fpitch scannables")
--- configurations/i10-config/scripts/scannable/haxpod/m6_haxpod_motors.py
+++ configurations/i10-config/scripts/scannable/haxpod/m6_haxpod_motors.py
@@ -22,7 +22,7 @@ if installation.isLive():
         m6_yaw = PositionCompareMotorClass("m6_yaw", "BL10J-OP-FOCA-01:YAW.VAL", "BL10J-OP-FOCA-01:YAW.RBV", "BL10J-OP-FOCA-01:YAW.STOP", 0.002, "urad", "%.3f")
         m6_pitch = PositionCompareMotorClass("m6_pitch", "BL10J-OP-FOCA-01:PITCH.VAL", "BL10J-OP-FOCA-01:PITCH.RBV", "BL10J-OP-FOCA-01:PITCH.STOP", 0.002, "urad", "%.3f")
         m6_roll = PositionCompareMotorClass("m6_roll", "BL10J-OP-FOCA-01:ROLL.VAL", "BL10J-OP-FOCA-01:ROLL.RBV", "BL10J-OP-FOCA-01:ROLL.STOP", 0.002, "urad", "%.3f")
-        m6fpitch = SingleEpicsPositionerNoStatusClassDeadbandOrStop('m6fpitch', 'BL10J-OP-FOCA-01:FPITCH:DMD:AO', 'BL10J-OP-FOCA-01:FPITCH:RBV:AI', 'V', '%.3f', 0.001)
+        m6fpitch = SingleEpicsPositionerNoStatusClassDeadbandOrStop('m6fpitch', 'BL10J-OP-FOCA-01:FPITCH:DMD:AO', 'BL10J-OP-FOCA-01:FPITCH:RBV:AI', 'V', '%.3f', 0.1)
         M6=ScannableGroup("M6", [m6_x, m6_y, m6_z, m6_yaw, m6_pitch, m6_roll, m6fpitch])
     except:
         localStation_exception(sys.exc_info(), "initialising m6 hexapod and fpitch scannables")
