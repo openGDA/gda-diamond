f1aa7f9a452fca2ff6db0a09ab9620a400a13601
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2022-03-08 15:14:26 +0000

    fix print statement format.
    Change-Id: Iae1b2962eb55a3f3610bffccdf60b88067aa03d0

--- configurations/i10-config/scripts/i10commands/snapshot.py
+++ configurations/i10-config/scripts/i10commands/snapshot.py
@@ -4,15 +4,15 @@ Created on 16 Apr 2018
 @author: fy65
 '''
 from gda.device.scannable import DummyScannable
-print "-"*100
-print "creating 'dummy' scannable"
+print("-"*100)
+print("creating 'dummy' scannable")
 dummy = DummyScannable("dummy")
 
 from gda.jython.commands.ScannableCommands import scan
 from gda.jython.commands.GeneralCommands import alias
-print "-"*100
-print "creating 'snap' command for capturing a snapshot off a detector:"
-print "    Usage example: >>>snap pimte 6.0"
+print("-"*100)
+print("creating 'snap' command for capturing a snapshot off a detector:")
+print("    Usage example: >>>snap pimte 6.0")
 def snap(*args):
     newargs=[dummy, 1,1,1]
     for arg in args:
