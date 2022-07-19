317c215477d1f19f3e08eff0b7dece9faf496798
Mark Booth <Mark.Booth@diamond.ac.uk>
2022-07-19 12:05:40 +0100

    I15_1-1175 (9.26) Update single point scan test to use arc not pe detector
    Change-Id: I7858e72c24a8eff024d5c0dbfb7d64c11c401c02

--- configurations/i15-1-config/tests/test_single_point_scan.py
+++ configurations/i15-1-config/tests/test_single_point_scan.py
@@ -1,7 +1,8 @@
 from gda.configuration.properties import LocalProperties
+from gdascripts.parameters import beamline_parameters
 from i15x.j15 import beamline as j15
-#from rigs.j15.arc_scanrequests import do_arc_datacollections
-from rigs.j15.hybrid_scanrequests import do_hybrid_datacollections
+from rigs.j15.arc_scanrequests import do_arc_datacollections
+#from rigs.j15.hybrid_scanrequests import do_hybrid_datacollections
 from rigs.ispyb.discovery import showMeTheSamples
 
 # Test based on testOldScanning() in /dls_sw/i15-1/scripts/Visits/arc_testing_01.py
@@ -13,5 +14,8 @@ def test_single_point_scan_is_not_refused():
 	j15.setVisit(LocalProperties.get('gda.defVisit'))
 	# Any sample should do, so use the first valid sample
 	first_valid_sample=showMeTheSamples().keys()[0]
-	do_hybrid_datacollections(first_valid_sample, frames=1,
-		pe1_time=10, pe2_time=None, samx=None, blocking=True, comment="test")
+	jythonNameMap = beamline_parameters.JythonNameSpaceMapping()
+	arc = jythonNameMap['arc']
+	arc.filename = "/dls_sw/i15-1/scripts/Xpdf/ProcessingFiles/arc_sim_1_frame.hdf5"
+	do_arc_datacollections(first_valid_sample, frames=1,
+		arc_time=10, tths=[0.0], samx=None, blocking=True, comment="test")
