1d282d8a54273b823c488eaf368af4a3ef7f4bcb
Mark Booth <Mark.Booth@diamond.ac.uk>
2022-08-31 19:37:08 +0100

    I15-1 (9.27) Add pytest.marks annotations to email tests
    Test functionality already in master, but not in gda-9.27 release using
    a cherry pick of change 36289 on gda-core.
    
    Change-Id: Ib5441eac5fab0f08775da18b95deee7cd3225f8a

--- configurations/i15-1-config/tests/test_email.py
+++ configurations/i15-1-config/tests/test_email.py
@@ -1,11 +1,17 @@
 from gda.util import Email
+import pytest
 
+# Note, support for marks was cherry picked from change/36289/1 onto the
+#       GDA 9.27 release branch for i15-1, to enable testing.
+
+@pytest.mark.email
 def test_email_send():
 	Email().to("mark.booth@diamond.ac.uk") \
 		.subject("test_email_send") \
 		.message("test message") \
 		.send()
 
+@pytest.mark.email
 def test_email_sendAsync():
 	Email().to("mark.booth@diamond.ac.uk") \
 		.subject("test_email_sendAsync") \
