8d728def9a93db2c15c88ede2aa140b30fb86d4c
Paula Martin <paula.martin-gomez@diamond.ac.uk>
2022-11-24 09:38:17 +0000

    I14 Remove ptychography parameters
    Change-Id: I2d05f81464d2c0ed09836946e062f9e88d95bac0

--- configurations/i14-config/scripts/scanning/submit_ptychography_scan.py
+++ configurations/i14-config/scripts/scanning/submit_ptychography_scan.py
@@ -5,12 +5,11 @@
 from gda.util.osgi import OsgiJythonHelper
 from org.eclipse.dawnsci.analysis.api.persistence import IMarshallerService
 from org.eclipse.scanning.api.event.scan import ScanRequest
-from uk.ac.diamond.daq.mapping.api import PtychographyParams
 
 print("Running submit_ptychography_scan.py")
 
 marshaller_service = OsgiJythonHelper.getService(IMarshallerService)
 
 scanRequest = marshaller_service.unmarshal(scanRequestJson, ScanRequest)
-ptychoParams = marshaller_service.unmarshal(ptychoParamsJson, PtychographyParams)
-run_ptychography_scan_request(scanRequest, ptychoParams)
+
+run_ptychography_scan_request(scanRequest)
