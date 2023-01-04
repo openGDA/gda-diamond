a6f6a4b71f740298bac55c9406703ce2091f901e
Paula Martin <paula.martin-gomez@diamond.ac.uk>
2023-01-04 15:13:32 +0000

    I08-374 Refactor polarisation functions
    - Allows to call functions to set the polarisation mode without
    submitting a scan.
    
    - idgap is moved to 50
    
    - phase scannable is moved depending on the edge and whether it is right
    (negative) or left (positive) polarisation
    
    - caput according to some coefficients in csv files
    
    Change-Id: I3173a88f32ca768a938046e2ee54240b326648f9

--- configurations/i08-shared/scripts/polarisation/id_energy_gap_mapping.py
+++ configurations/i08-shared/scripts/polarisation/id_energy_gap_mapping.py
@@ -1,44 +1,23 @@
 # Functions to change the mappings between ID energy & gap
-import sys
 import csv
 from gdascripts.utils import caput
 from i08_shared_utilities import is_live
-from gda.configuration.properties import LocalProperties
 
-print("Running id_energy_gap_mapping.py")
-
-gda_var_dir = LocalProperties.get("gda.var")
-circular_coefficients_filename = gda_var_dir + "/circular.csv"
-linear_coefficients_filename = gda_var_dir + "/linear.csv"
-energy_to_gap_col_name= "eV to mm"
-gap_to_energy_col_name = "mm to eV"
 
 def get_coefficients(file_path):
-    polarisation_type = file_path.split("/")[-1].split(".")[0]
-          
     filename = open(file_path, 'r')
-    file = csv.DictReader(filename)
+    csv_file = csv.DictReader(filename)
     
     energy_to_gap = []
     gap_to_energy = []
-    
-    for col in file:
-        energy_to_gap.append(float(col[energy_to_gap_col_name]))
-        gap_to_energy.append(float(col[gap_to_energy_col_name]))
+    for row in csv_file:
+        energy_to_gap.append(float(row["EnergyToGap"]))
+        gap_to_energy.append(float(row["GapToEnergy"]))
         
-    print("Energy to gap for {} polarisation coefficients: {}".format(polarisation_type, energy_to_gap))
-    print("Gap to energy for {} polarisation coefficients: {}".format(polarisation_type, gap_to_energy))
-    
-    return energy_to_gap, gap_to_energy
+    return energy_to_gap, gap_to_energy  
 
-def set_mappings_for_linear_polarisation():
-    print("Setting mappings for linear polarisation")
-    energy_to_gap, gap_to_energy = get_coefficients(linear_coefficients_filename)
-    set_mappings(energy_to_gap, gap_to_energy)
-
-def set_mappings_for_circular_polarisation():
-    print("Setting mappings for circular polarisation")
-    energy_to_gap, gap_to_energy = get_coefficients(circular_coefficients_filename)
+def set_mappings_for_polarisation(file_name):
+    energy_to_gap, gap_to_energy = get_coefficients(file_name)
     set_mappings(energy_to_gap, gap_to_energy)
 
 def set_mappings(energy_to_gap, gap_to_energy):
@@ -48,9 +27,12 @@ def set_mappings(energy_to_gap, gap_to_energy):
     for x in range(len(gap_to_energy)):
         pv = "BL08I-OP-ID-01:BVT:C{}".format(x + 1)
         set_mapping(pv, gap_to_energy[x])
+    print("Polarisation setting complete")
 
 def set_mapping(pv, value):
     if is_live():
         caput(pv, value)
     else:
         print("caput({}, {:e})".format(pv, value))
+        
+        
--- configurations/i08-shared/scripts/polarisation/polarisation_functions.py
+++ configurations/i08-shared/scripts/polarisation/polarisation_functions.py
@@ -1,89 +1,63 @@
 from gda.factory import Finder
-from java.util import ArrayList
-from org.eclipse.scanning.api.event.scan import ScanRequest
-from org.eclipse.scanning.api.script import ScriptLanguage
-from org.eclipse.scanning.api.script import ScriptRequest
-from uk.ac.diamond.daq.mapping.api import PolarisationScanParameters
-from mapping_scan_commands import submit
-from i08_shared_utilities import is_live
-from id_energy_gap_mapping import set_mappings_for_linear_polarisation, set_mappings_for_circular_polarisation
+from gda.configuration.properties import LocalProperties
+from id_energy_gap_mapping import set_mappings_for_polarisation
 
-# Functions to perform polarisation scans
+# Name of the files that contain the mappings coefficients 
 
-# The scannables we need to move to set up the scan
+coefficients_directory = LocalProperties.get("gda.var") + "/polarisationCoefficients"
+circular_left_filename = coefficients_directory + "/circular_left.csv"
+circular_right_filename = coefficients_directory + "/circular_right.csv"
+linear_horizontal_filename = coefficients_directory + "/linear_horizontal.csv"
+linear_vertical_filename = coefficients_directory + "/linear_vertical.csv"
+
+# The scannables we need to set a polarisation mode
 id_gap_scannable = Finder.find("idgap")
-phase_scannable = Finder.find("phase")
+phase_scannable  = Finder.find("phase")
 
-# Module-level variables to store the original phase motor positions and those required for this scan
+# Module-level variables to store the original phase motor positions
+polarisation_idgap_position = 50
 original_phase_position = None
 polarisation_phase_position = None
-polarisation_idgap_position = 50
 
-left_polarisation_script = "set_left_polarisation.py"
-right_polarisation_script = "set_right_polarisation.py"
-
-print("Running polarisation_functions.py")
-
-def run_polarisation_scan(scanRequest, polarisationScanParameters):
+def set_circular_left_polarisation(edge):
+    polarisation_phase_position = get_phase_position_from_edge(edge)
     
-    global original_phase_position
-    global polarisation_phase_position
-    global id_gap_scannable
-    global phase_scannable
-    
-    print("Polarisation scan parameters: {}".format(polarisationScanParameters))
-    
-    original_phase_position = phase_scannable.getPosition()
-    print("Original phase position {}".format(original_phase_position))
-    
-    polarisation_phase_position = polarisationScanParameters.getPhasePosition()
-    print("Polarisation motor positions {}".format(polarisation_phase_position))
-    
-    print("Moving id gap to: {}".format(polarisation_idgap_position))
     id_gap_scannable.moveTo(polarisation_idgap_position)
+    phase_scannable.moveTo(0);
+    phase_scannable.moveTo(polarisation_phase_position)
     
-    if polarisationScanParameters.getRunFirst() == PolarisationScanParameters.Polarisation.LEFT:
-        submit_polarisation_scan("left", left_polarisation_script, scanRequest)
-        submit_polarisation_scan("right", right_polarisation_script, scanRequest)
+    set_mappings_for_polarisation(circular_left_filename)
+    
+    
+def set_circular_right_polarisation(edge):
+    polarisation_phase_position = get_phase_position_from_edge(edge)
+    
+    id_gap_scannable.moveTo(polarisation_idgap_position)
+    phase_scannable.moveTo(0);
+    phase_scannable.moveTo(-polarisation_phase_position);
+    
+    set_mappings_for_polarisation(circular_right_filename)
+    
+    
+def set_linear_horizontal_polarisation():
+    set_mappings_for_polarisation(linear_horizontal_filename)
+    
+def set_linear_vertical_polarisation():
+    set_mappings_for_polarisation(linear_vertical_filename)
+    
+def get_phase_position_from_edge(edge):
+    if edge == 'O-K':
+        return 17.8
+    elif edge == 'Fe-L':
+        return 18.2
+    elif edge == 'Co-L':
+        return 18.4
+    elif edge == 'Ni-L':
+        return 18.6
+    elif edge == 'Cu-L':
+        return 18.8
     else:
-        submit_polarisation_scan("right", left_polarisation_script, scanRequest)
-        submit_polarisation_scan("left", right_polarisation_script, scanRequest)
-
-
-def set_polarisation(negative):
-    global polarisation_phase_position
-    target_phase = polarisation_phase_position
-    if negative:
-        target_phase = -target_phase
+        raise ValueError("Edge not valid")
     
-    move_phase(target_phase)
-    set_mappings_for_circular_polarisation()
-
-def move_phase(position):
-    global phase_scannable
-    print("Moving phase motors to: {}".format(position))
-    phase_scannable.moveTo(0)
-    phase_scannable.moveTo(position)
     
-def restore_polarisation():
-    global original_phase_position
-    move_phase(original_phase_position)
-    set_mappings_for_linear_polarisation()
-
-def submit_polarisation_scan(scanName, beforeScript, scanRequest):
-    scanRequest.setBeforeScript(create_script_request(beforeScript))
-    scanRequest.setAfterScript(create_script_request("restore_polarisation.py"))
-    scanRequest.setAlwaysRunAfterScript(True)
-
-    # Add scanName to the description defined in the GUI
-    submitName = scanRequest.getScanMetadata()[0].getFieldValue("name") + " - " + scanName
-
-    print("Polarisation scan, name: {}, request: {}".format(submitName, scanRequest))
-    submit(scanRequest, block = False, name = submitName)
     
-# Convert a script name to a ScriptRequest object
-def create_script_request(scriptName):
-    scriptRequest = ScriptRequest()
-    scriptRequest.setLanguage(ScriptLanguage.SPEC_PASTICHE)
-    scriptRequest.setFile("polarisation/" + scriptName)
-    return scriptRequest
