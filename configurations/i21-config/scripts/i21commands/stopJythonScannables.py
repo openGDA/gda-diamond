1be32d7b68b79a4963ae0e48e066489411961f2b
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2023-04-28 16:34:21 +0100

    I21-1045 clcik 'Stop All' button should not stop 's5cam' acquiring
    - fix the way scannables in STOP_ALL_EXCLUSIONS list be accessed.
    
    Change-Id: I9eb04304cd9528cb40630d5363fa45d40ce48ff4

--- configurations/i21-config/scripts/i21commands/stopJythonScannables.py
+++ configurations/i21-config/scripts/i21commands/stopJythonScannables.py
@@ -3,27 +3,29 @@ Created on 5 Feb 2018
 
 @author: fy65
 '''
+from gda.jython import InterfaceProvider
+
 def stopJythonScannablesExceptExcluded():
     print("!!! Stopping Jython scannables")
     from gda.factory import Finder
     from gda.device.scannable import ScannableMotionBase, ScannableBase
     from gda.device import Scannable
-    commandServer = Finder.find("command_server")
-    scannables = commandServer.getNamesForAllObjectsOfType(Scannable)
+    commandserver = Finder.find("command_server")
+    scannables = commandserver.getNamesForAllObjectsOfType(Scannable)
     #     print scannables
-    excluded_scannables = scannables["STOP_ALL_EXCLUSIONS"]
+    excluded_scannables = InterfaceProvider.getJythonNamespace().getFromJythonNamespace("STOP_ALL_EXCLUSIONS")
     dontuse = None
     for dontuse in scannables.values():
         if isinstance(dontuse, (ScannableMotionBase, ScannableBase)):
             if excluded_scannables is not None and dontuse not in excluded_scannables:
                 try:
-                    print '    Stopping ' + dontuse.getName()
+                    print('    Stopping ' + dontuse.getName())
                     dontuse.stop()
                 except:
-                    print '    problem stopping ' + dontuse.getName()
+                    print('    problem stopping ' + dontuse.getName())
             else:
-                print '    ' + dontuse.getName() +' is excluded from Stop All.'
+                print('    ' + dontuse.getName() +' is excluded from Stop All.')
     
     del dontuse
-    print
+    print()
     
