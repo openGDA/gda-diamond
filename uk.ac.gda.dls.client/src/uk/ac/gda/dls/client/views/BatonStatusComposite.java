f958c9c3d7124daddff3fa4475cc7222bb5f67ea
Paul Coe <paul.coe@diamond.ac.uk>
2023-07-10 17:41:29 +0100

    DAQ-4375 rearranges Baton Status methods
    * makes most methods in Baton Status Composite
       static - where possible
    
    * removes unnecessary local fields
    
    * removes unnecessary holding of state ( colour )
       inside UI object ( Composite ), instead
        delegating state holding to BatonStateProvider
    
    Change-Id: If05a8f8e58109e2d8f08a51aac4875fa0192a28b

--- uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/views/BatonStatusComposite.java
+++ uk.ac.gda.dls.client/src/uk/ac/gda/dls/client/views/BatonStatusComposite.java
@@ -22,6 +22,8 @@ import static uk.ac.gda.dls.client.views.BatonStatusPopupMenuBuilder.showView;
 
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.layout.GridDataFactory;
@@ -32,7 +34,6 @@ import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
@@ -42,9 +43,10 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.ui.PlatformUI;
+import org.eclipse.swt.widgets.Menu;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import com.swtdesigner.SWTResourceManager;
 
@@ -64,176 +66,54 @@ final class BatonStatusComposite extends Composite {
 	private static final Color BATON_HELD_UDC_COLOR = Display.getDefault().getSystemColor(SWT.COLOR_BLUE);
 	private static final Color BATON_NOT_HELD_COLOR = Display.getDefault().getSystemColor(SWT.COLOR_RED);
 
-	private static final int MARGIN = 4;
-	private static final int GRID_HINT = 40;
+	private static final String BATON_HELD_TOOL_TIP = """
+			Baton held!
+			Right click open menu
+			Left click open manager""";
 
-	private static final String BATON_HELD_TOOL_TIP = "Baton held!\nRight click open menu\nLeft click open manager";
-	private static final String BATON_HELD_UDC_TOOL_TIP = "Baton held by automated client!\nRight click open menu\nLeft click open manager";
-	private static final String BATON_NOT_HELD_TOOL_TIP = "Baton not held!\nRight click open menu\nLeft click open manager";
+	private static final String BATON_HELD_UDC_TOOL_TIP = """
+			Baton held by automated client!
+			Right click open menu
+			Left click open manager""";
 
-	private static final String CANNOT_CONNECT_TO_GDA_SERVER = "Cannot Connect to GDA Server";
-	private static final String GDA_SERVER_IS_NOT_RESPONDING = "The GDA Server is not responding.\n\nPlease contact your GDA support engineer.";
+	private static final String BATON_NOT_HELD_TOOL_TIP = """
+			Baton not held!
+			Right click open menu
+			Left click open manager""";
+
+	private static final String CANNOT_CONNECT_TO_GDA_SERVER = "Cannot Connect to GDA Server.";
+
+	private static final String GDA_SERVER_IS_NOT_RESPONDING = """
+		The GDA Server is not responding.
+		Please contact your GDA support engineer.""";
+
 	private static final String PROP_BATON_BANNER = "gda.beamline.baton.banner";
 
-	private static final Display DEFAULT_DISPLAY = Display.getDefault();
-	private static final UserInterfaceAsynchronously DEFAULT_DISPLAY_ASYNCH =
-		DEFAULT_DISPLAY::asyncExec;
-
-	private final IBatonStateProvider batonStateProvider;
-	private final UserInterfaceAsynchronously displayAsynch;
-
-	private AtomicColour currentColor;
-
-	private Optional<Runnable> maybeDisposeFont = Optional.empty();
-
-	private Canvas batonCanvas;
-
-	private Font boldFont;
-	private Label lblBanner;
+	private final Optional<Label> maybeLabelBanner;
+	private final BatonStatusCanvas canvas;
 
 	public BatonStatusComposite(Composite parent, int style, IBatonStateProvider batonStateProvision, String label) {
 		super(parent, style);
-		batonStateProvider = batonStateProvision;
-
+		var group = prepareGroup(this, style, label);
 		var ui = parent.getDisplay();
-		displayAsynch = ui::asyncExec;
-		var grp = prepareGroup(style, label);
-		var stateColor = representBatonStateByColour(batonStateProvider);
-		currentColor = new AtomicColour(stateColor);
-
-		batonCanvas = new Canvas(grp, SWT.NONE);
-		GridData gridData = new GridData(GridData.VERTICAL_ALIGN_FILL);
-		gridData.widthHint = GRID_HINT;
-		gridData.heightHint = GRID_HINT;
-		batonCanvas.setLayoutData(gridData);
-
-		addCanvasPaintListener(batonCanvas);
-		var initialTooltip = getBatonStateTooltip(batonStateProvider);
-		batonCanvas.setToolTipText(initialTooltip);
-		BatonStatusPopupMenuBuilder.preparePopUpMenu(this, batonCanvas::setMenu, batonStateProvider);
-
-		IObserver serverObserver = createServerObserver();
-
-		try {
-			var jsfObserver = InterfaceProvider.getJSFObserver();
-			addObserver(jsfObserver,serverObserver);
-		} catch (Exception ne) {
-			DEFAULT_DISPLAY_ASYNCH.asynchronouslyExecuteOnUiThread( () -> {
-				var activeShell = DEFAULT_DISPLAY.getActiveShell();
-				MessageDialog.openWarning(activeShell, CANNOT_CONNECT_TO_GDA_SERVER, GDA_SERVER_IS_NOT_RESPONDING);
-			});
-		}
-
-		batonCanvas.addMouseListener(new MouseAdapter() {
-
-			@Override
-			public void mouseUp(MouseEvent event) {
-				if (event.button == 1) {
-					try {
-						PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView(uk.ac.gda.views.baton.BatonView.ID);
-					} catch (Exception ex) {
-						logger.error("Cannot open baton manager", ex);
-					}
-				}
-			}
-		});
-
-		String banner = LocalProperties.get(PROP_BATON_BANNER, " ");
-		if (!(null==banner || banner.isEmpty()) ) {
-			lblBanner = new Label(this, SWT.CENTER);
-			boldFont = FontDescriptor.createFrom(lblBanner.getFont())
-				.setStyle( SWT.BOLD )
-				.createFont( lblBanner.getDisplay() );
-			maybeDisposeFont = Optional.of(boldFont::dispose);
-			lblBanner.setFont(boldFont);
-			lblBanner.setText(banner);
-			GridDataFactory.swtDefaults().align(SWT.CENTER, SWT.FILL).applyTo(lblBanner);
-		}
-	}
-
-	@Override
-	public void dispose() {
-		super.dispose();
-		maybeDisposeFont.ifPresent(Runnable::run);
+		maybeLabelBanner = maybeExtractBannerText().map(this::createBannerLabel);
+		canvas = new BatonStatusCanvas(group, batonStateProvision, ui::asyncExec);
+		BatonStatusPopupMenuBuilder.preparePopUpMenu(this, canvas::setMenu, batonStateProvision);
 	}
 
 	public void setBannerProvider(IObservable provider) {
 		IObserver iObserver = (source, arg) -> {
-			if (arg instanceof String text) {
-				lblBanner.setText(text);
+			if (arg instanceof String argAsText) {
+				Predicate<Label> excludeDisposedLabel = Predicate.not(Label::isDisposed);
+				maybeLabelBanner.filter(excludeDisposedLabel)
+								.ifPresent(label -> label.setText(argAsText));
 			}
 		};
-		addObserver(provider, iObserver);
+		addObserverWithDisposalHandling(provider, iObserver);
 	}
 
-	private Group prepareGroup(int style, String label) {
-		GridDataFactory.fillDefaults().applyTo(this);
-		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(this);
-		Group grp = new Group(this, style);
-		GridDataFactory.fillDefaults().applyTo(grp);
-		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(grp);
-		grp.setBackground(SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT));
-		grp.setText(label);
-		GridLayoutFactory.swtDefaults().numColumns(1).applyTo(this);
-		GridDataFactory.fillDefaults().applyTo(this);
-		return grp;
-	}
+	private void addObserverWithDisposalHandling(IObservable iObservable, IObserver iObserver) {
 
-	private void updateBatonCanvas(String toolTipText) {
-		displayAsynch.asynchronouslyExecuteOnUiThread( () -> {
-			if (batonCanvas != null && !batonCanvas.isDisposed()) {
-				batonCanvas.setToolTipText(toolTipText);
-				batonCanvas.redraw();
-				batonCanvas.update();
-			}
-		});
-	}
-
-	private IObserver createServerObserver() {
-		return (source, arg) ->
-			DEFAULT_DISPLAY_ASYNCH.asynchronouslyExecuteOnUiThread( () -> whenUpdateReceivedFromServer(arg) );
-	}
-
-	private void whenUpdateReceivedFromServer(Object arg) {
-		if (arg instanceof gda.jython.batoncontrol.BatonChanged) {
-			whenBatonChanges(batonStateProvider);
-		}
-		if (arg instanceof UserMessage) {
-			ensureMessagesViewIsOpen();
-		}
-	}
-
-	private void ensureMessagesViewIsOpen() {
-		// Message received - ensure "Messages" view is open
-		try {
-			showView(MessageView.ID);
-		} catch (Exception e) {
-			logger.warn("Unable to open Messages view", e);
-		}
-	}
-
-	private void whenBatonChanges(IBatonStateProvider batonStateProvider) {
-		var stateColor = representBatonStateByColour(batonStateProvider);
-		currentColor.writeCurrentColour(stateColor);
-		var toolTipText = getBatonStateTooltip(batonStateProvider);
-		updateBatonCanvas(toolTipText);
-	}
-
-	private void addCanvasPaintListener(Canvas canvas) {
-		canvas.addPaintListener( paintEvent -> {
-			GC gc = paintEvent.gc;
-			gc.setAntialias(SWT.ON);
-			var latestColor = currentColor.readCurrentColour();
-			gc.setBackground(latestColor);
-			gc.setLineWidth(1);
-			Point topLeft = new Point(MARGIN, MARGIN);
-			Point size = insideMarginsOf(canvas, MARGIN);
-			gc.fillOval(topLeft.x, topLeft.y, size.x, size.y);
-			gc.drawOval(topLeft.x, topLeft.y, size.x, size.y);
-		});
-	}
-
-	private void addObserver(IObservable iObservable, IObserver iObserver) {
 		if (Objects.isNull(iObservable) || Objects.isNull(iObserver)) {
 			return;
 		}
@@ -242,10 +122,45 @@ final class BatonStatusComposite extends Composite {
 		this.addDisposeListener(releaseObserver);
 	}
 
-	private static Point insideMarginsOf(Canvas canvas, int margin) {
-		Rectangle clientArea = canvas.getClientArea();
-		var delta = 2 * margin;
-		return new Point(clientArea.width - delta, clientArea.height - delta);
+	private Label createBannerLabel(String bannerText) {
+		var banner = new Label(this, SWT.CENTER);
+		var font = banner.getFont();
+		var display = banner.getDisplay();
+		var boldFont = FontDescriptor.createFrom(font)
+									.setStyle(SWT.BOLD)
+									.createFont(display);
+		banner.addDisposeListener(e -> boldFont.dispose());
+		banner.setFont(boldFont);
+		banner.setText(bannerText);
+		GridDataFactory.swtDefaults()
+						.align(SWT.CENTER, SWT.FILL)
+						.applyTo(banner);
+		return banner;
+	}
+
+	private static Optional<String> maybeExtractBannerText() {
+		var textFromProperty = LocalProperties.get(PROP_BATON_BANNER, " ");
+		return Optional.of(textFromProperty)
+						.filter(StringUtils::hasLength);
+	}
+
+	private static Group prepareGroup(Composite parent, int style, String label) {
+		prepareAsSingleColumnWithDefaultFill(parent);
+		var group = new Group(parent, style);
+		prepareAsSingleColumnWithDefaultFill(group);
+		var transparent = SWTResourceManager.getColor(SWT.COLOR_TRANSPARENT);
+		group.setBackground(transparent);
+		group.setText(label);
+		return group;
+	}
+
+	private static void prepareAsSingleColumnWithDefaultFill(Composite composite) {
+		Consumer<Composite> applyFill =
+			GridDataFactory.fillDefaults()::applyTo;
+		Consumer<Composite> setSingleColumn  =
+			GridLayoutFactory.swtDefaults()
+							.numColumns(1)::applyTo;
+		applyFill.andThen(setSingleColumn).accept(composite);
 	}
 
 	private static String getBatonStateTooltip(IBatonStateProvider batonStateProvider) {
@@ -265,6 +180,142 @@ final class BatonStatusComposite extends Composite {
 	private static boolean isBatonHeldByUdc(IBatonStateProvider batonStateProvider) {
 		var batonHolder = batonStateProvider.getBatonHolder();
 		return batonHolder != null && batonHolder.isAutomatedUser();
+	}
+
+	private final class BatonStatusCanvas {
+
+		private static final int OUTLINE_LINE_WIDTH = 1;
+		private static final Display DEFAULT_DISPLAY = Display.getDefault();
+		private static final UiThreadAsynchExecution DEFAULT_DISPLAY_ASYNCH =
+			DEFAULT_DISPLAY::asyncExec;
+
+		private static final int PRIMARY_MOUSE_BUTTON = 1;
+		private static final int CANVAS_MARGIN = 4;
+		private static final int GRID_HINT = 40;
+
+		private static final String CANNOT_OPEN_BATON_MANAGER = "Cannot open baton manager.";
+		private static final String UNABLE_TO_OPEN_MESSAGES_VIEW = "Unable to open Messages view";
+
+		private final IBatonStateProvider batonStateProvider;
+		private final AtomicColour atomicColourCache;
+		private final Canvas batonCanvas;
+		private final UiThreadAsynchExecution uiAsynchExec;
+
+		private BatonStatusCanvas(Group group, IBatonStateProvider batonStateProvision, UiThreadAsynchExecution displayAsynch) {
+			batonStateProvider = batonStateProvision;
+			var initialStatusColor = representBatonStateByColour(batonStateProvider);
+			atomicColourCache = new AtomicColour(initialStatusColor);
+
+			uiAsynchExec = displayAsynch;
+			batonCanvas = new Canvas(group, SWT.NONE);
+
+			GridData gridData = new GridData(GridData.VERTICAL_ALIGN_FILL);
+			gridData.widthHint = GRID_HINT;
+			gridData.heightHint = GRID_HINT;
+			batonCanvas.setLayoutData(gridData);
+
+			addCanvasPaintListener();
+			addMouseListener();
+			whenBatonChanges(batonStateProvision);  // initialise baton canvas state
+			var serverObserver = createServerObserver();
+			addServerObserverWithDisposalHandling(serverObserver);
+		}
+
+		void setMenu(Menu menu) {
+			batonCanvas.setMenu(menu);
+		}
+
+		private void addServerObserverWithDisposalHandling(IObserver serverObserver) {
+			try {
+				var jsfObserver = InterfaceProvider.getJSFObserver();
+				addObserverWithDisposalHandling(jsfObserver, serverObserver);
+			} catch (Exception ne) {
+				DEFAULT_DISPLAY_ASYNCH.asynchronouslyExecuteOnUiThread( () -> {
+					var activeShell = DEFAULT_DISPLAY.getActiveShell();
+					MessageDialog.openWarning(activeShell, CANNOT_CONNECT_TO_GDA_SERVER, GDA_SERVER_IS_NOT_RESPONDING);
+				});
+			}
+		}
+
+		private void whenBatonChanges(IBatonStateProvider batonStateProvider) {
+			var stateColor = representBatonStateByColour(batonStateProvider);
+			atomicColourCache.writeCurrentColour(stateColor);
+			var toolTipText = getBatonStateTooltip(batonStateProvider);
+			updateBatonCanvas(toolTipText);
+		}
+
+		private IObserver createServerObserver() {
+			return (source, arg) ->
+				DEFAULT_DISPLAY_ASYNCH.asynchronouslyExecuteOnUiThread( () -> whenUpdateReceivedFromServer(arg) );
+		}
+
+		private void whenUpdateReceivedFromServer(Object arg) {
+			if (arg instanceof gda.jython.batoncontrol.BatonChanged) {
+				whenBatonChanges(batonStateProvider);
+			}
+			if (arg instanceof UserMessage) {
+				ensureMessagesViewIsOpen();
+			}
+		}
+
+		private void ensureMessagesViewIsOpen() {
+			// Message received - ensure "Messages" view is open
+			try {
+				showView(MessageView.ID);
+			} catch (Exception e) {
+				logger.warn(UNABLE_TO_OPEN_MESSAGES_VIEW, e);
+			}
+		}
+
+		private void updateBatonCanvas(String toolTipText) {
+			uiAsynchExec.asynchronouslyExecuteOnUiThread( () -> {
+				if (!batonCanvas.isDisposed()) {
+					batonCanvas.setToolTipText(toolTipText);
+					batonCanvas.redraw();
+					batonCanvas.update();
+				}
+			});
+		}
+
+		private void addMouseListener() {
+			batonCanvas.addMouseListener(new MouseAdapter() {
+
+				@Override
+				public void mouseUp(MouseEvent event) {
+					if(PRIMARY_MOUSE_BUTTON != event.button) {
+						return;
+					}
+					var identifiedBatonView = uk.ac.gda.views.baton.BatonView.ID;
+					try {
+						showView(identifiedBatonView);
+					} catch (Exception exception) {
+						logger.error(CANNOT_OPEN_BATON_MANAGER, exception);
+					}
+				}
+			});
+		}
+
+		private void addCanvasPaintListener() {
+			batonCanvas.addPaintListener( paintEvent -> {
+				GC gc = paintEvent.gc;
+				gc.setAntialias(SWT.ON);
+				var latestColor = atomicColourCache.readCurrentColour();
+				gc.setBackground(latestColor);
+				gc.setLineWidth(OUTLINE_LINE_WIDTH);
+				Point topLeft = new Point(CANVAS_MARGIN, CANVAS_MARGIN);
+				Point size = insideMarginsOf(batonCanvas, CANVAS_MARGIN);
+				gc.fillOval(topLeft.x, topLeft.y, size.x, size.y);
+				gc.drawOval(topLeft.x, topLeft.y, size.x, size.y);
+			});
+		}
+
+		private static Point insideMarginsOf(Canvas canvas, int margin) {
+			Rectangle clientArea = canvas.getClientArea();
+			var delta = 2 * margin;
+			var marginX = clientArea.width - delta;
+			var marginY = clientArea.height - delta;
+			return new Point(marginX, marginY);
+		}
 	}
 
 	private static final class AtomicColour {
@@ -291,7 +342,7 @@ final class BatonStatusComposite extends Composite {
 	}
 
 	@FunctionalInterface
-	interface UserInterfaceAsynchronously {
+	interface UiThreadAsynchExecution {
 		void asynchronouslyExecuteOnUiThread(Runnable r);
 	}
 }
