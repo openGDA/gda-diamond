69ee12de33e5903c06025a044e54250e6453e2bf
Mark Booth <Mark.Booth@diamond.ac.uk>
2023-07-21 14:12:30 +0100

    I15_1-1223 Add an arc.calibrationFilePath to scan tests
    This is required to create the processing request, even if we don't run
    one when when we do a scan.
    
    Change-Id: I95f227cebb75302f810741836a97312a2ddc278d

--- configurations/i15-1-config/tests/test_single_point_scan.py
+++ configurations/i15-1-config/tests/test_single_point_scan.py
@@ -13,6 +13,12 @@ def test_single_point_scan_is_not_refused():
 	arc = jythonNameMap['arc']
 	arc.dummy_mode = True # Do not try to actually run the live detector
 	arc.filename = "/dls_sw/i15-1/scripts/Xpdf/ProcessingFiles/arc_sim_1_frame.hdf5"
+	arc.calibrationFilePath = u'/dls_sw/i15-1/scripts/notebooks/arc_40keV.json'
+	# Tests on beamline (2023-06-27) didn't need an arc.calibrationFilePath since it was accidentally
+	# generating processing for the pe2AD detector.
+	# Tests in dummy mode did, so checked the current live values:
+	#   arc.filename = '/dls/i15-1/data/2022/cm31137-3/processing/testSaveSim.hdf5' # 2023-07-20
+	#   arc.calibrationFilePath = u'/dls_sw/i15-1/scripts/notebooks/arc_40keV.json'
 
 	collect_arc(sampleid=first_valid_sample, exposure_time=10, frames=1, monitorsPerScan=[],
 				monitorsPerPoint=[],comment="",f2="auto",preSleep=None,blocking=True,samX=None)
