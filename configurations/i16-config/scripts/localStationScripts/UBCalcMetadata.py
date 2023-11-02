17e34d58ff26f9b19d981774d8a523609ddc012f
Mark Booth <Mark.Booth@diamond.ac.uk>
2023-11-02 16:18:49 +0000

    I16-635 (9.33) Add surface vector normal to sample node
    Change-Id: Icd9a300946bba8c5be78bc6b83e388603e17f0ca

--- configurations/i16-config/scripts/localStationScripts/UBCalcMetadata.py
+++ configurations/i16-config/scripts/localStationScripts/UBCalcMetadata.py
@@ -1,5 +1,4 @@
 from gda.device.scannable import ScannableBase
-from __builtin__ import False
 try:
 	import json
 except ImportError:
@@ -10,11 +9,21 @@ from diffcalc.ub.calcstate import UBCalcStateEncoder
 class UBCalcMetadata (ScannableBase):
 	def __init__(self, name, ubcalc):
 		self.name = name
+		self.setInputNames([])
+		self.setExtraNames(['value', 'n_hkl'])
+		self.setOutputFormat(['%s', '%6.4f'])
 		self.ubcalc = ubcalc
 
 	def getPosition(self):
 		if self.ubcalc is not None or self.ubcalc._state is not None:
-			return json.dumps(self.ubcalc._state, cls=UBCalcStateEncoder)
+			return json.dumps(self.ubcalc._state, cls=UBCalcStateEncoder), self.getSurfaceVector()
+		return None, self.getSurfaceVector()
+
+	def getSurfaceVector(self):
+		if self.ubcalc is not None and self.ubcalc.n_hkl is not None:
+			ref_matrix = self.ubcalc.n_hkl # See I16-635
+			surface_vector = ref_matrix.T.tolist()[0]
+			return surface_vector
 		return None
 
 	def rawAsynchronousMoveTo(self, position):
