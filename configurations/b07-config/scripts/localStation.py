ef7677bbba9d85b7d53b20844277bb4bf32f94d0
eir17846 <victor.rogalev@diamond.ac.uk>
2023-11-30 16:08:26 +0000

    B07-940 Add patch panel scannables + metadata
    Change-Id: I941bb61515c379f3cda91acc6c9eeba8df262c20

--- configurations/b07-config/scripts/localStation.py
+++ configurations/b07-config/scripts/localStation.py
@@ -91,8 +91,10 @@ es1_list = [ES1_temperature_51,ES1_temperature_52]  # @UndefinedVariable
 mfc_list = [mfc1, mfc2, mfc3, mfc4, mfc5, mfc6, mfc7, mfc8] # @UndefinedVariable
 metadata_group4 = d21b_list + sm21b_list + sm52b_list + ring_list + pgm_list + shutter_list + other_list + es1_list
 meta_data_list = metadata_group1 + metadata_group2 + metadata_group3 + metadata_group4
-for each in meta_data_list:
-    meta_add(each)
+
+[meta_add(each) for each in meta_data_list]
+[meta_add(each) for each in patch_panel_ES1.getGroupMembersAsArray()]
+[meta_add(each) for each in patch_panel_ES2.getGroupMembersAsArray()]
 
 from scan.miscan import miscan  # @UnusedImport
 print("-"*100)
