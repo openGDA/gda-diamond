2e5c407526dbe5ab03679b1fdf34c00e97d2055f
eir17846 <victor.rogalev@diamond.ac.uk>
2023-12-08 16:24:11 +0000

    I05-642 checkForBeamlineProblems() - new function
    Change-Id: I57e613714f63de4eb2490a8ef5ab50670d29a289

--- configurations/i05-config/scripts/localStation.py
+++ configurations/i05-config/scripts/localStation.py
@@ -123,8 +123,10 @@ print "Adding ring_current as a default_scannable"
 add_default(ring_current)
 
 from plottings.configScanPlot import setYFieldVisibleInScanPlot,getYFieldVisibleInScanPlot,setXFieldInScanPlot,useSeparateYAxes,useSingleYAxis, getXFieldInScanPlot  # @UnusedImport
-
 setXFieldInScanPlot(0)
+
+from checkBeamlineHealth import *
+checkForBeamlineProblems()
 
 print "==================================================================="
 if LocalProperties.get("gda.mode")=="live":  # don't execute in squish tests
