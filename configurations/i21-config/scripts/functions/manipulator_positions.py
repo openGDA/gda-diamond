5082425f137c9ec8d43777085248d20529820b94
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2024-01-12 09:32:24 +0000

    I21-1106 save motor values for sample position and carbon tape position
    - support capture multiple named positions
    
    Change-Id: I5fb893ddc2e4c9816e3d487aacf4191adf765cd4

--- configurations/i21-config/scripts/functions/manipulator_positions.py
+++ configurations/i21-config/scripts/functions/manipulator_positions.py
@@ -8,66 +8,61 @@ Created on Sep 20, 2022
 from gdaserver import x,y,z,th,phi,chi  # @UnresolvedImport
 from gda.jython.commands.GeneralCommands import alias
 
-sample_pos = {"x":0, "y":0, "z":0, "th":0, "phi":0, "chi":0}
-carbon_tape_pos = {"x":0, "y":0, "z":0, "th":0, "phi":0, "chi":0}
+# define dictionary to store positions
+sample_pos = {"sample": {"x":0, "y":0, "z":0, "phi":0, "chi":0}}
+carbon_tape_pos = {"carbon": {"x":0, "y":0, "z":0, "phi":0, "chi":0}}
 
-def save_sample_positions():
+def save_sample_positions(name = "sample"):
     global sample_pos
-    sample_pos["x"] = float(x.getPosition())
-    sample_pos["y"] = float(y.getPosition())
-    sample_pos["z"] = float(z.getPosition())
-    sample_pos["th"] = float(th.getPosition())
-    sample_pos["phi"] = float(phi.getPosition())
-    sample_pos["chi"] = float(chi.getPosition())
-    print("sample positions are saved.")
+    sample_pos[name]["x"] = float(x.getPosition())
+    sample_pos[name]["y"] = float(y.getPosition())
+    sample_pos[name]["z"] = float(z.getPosition())
+    sample_pos[name]["phi"] = float(phi.getPosition())
+    sample_pos[name]["chi"] = float(chi.getPosition())
+    print("sample positions are saved with key %s." % name)
     
 alias("save_sample_positions")
 
-def save_carbon_tape_positions():
+def save_carbon_tape_positions(name = "carbon"):
     global carbon_tape_pos
-    carbon_tape_pos["x"] = float(x.getPosition())
-    carbon_tape_pos["y"] = float(y.getPosition())
-    carbon_tape_pos["z"] = float(z.getPosition())
-    carbon_tape_pos["th"] = float(th.getPosition())
-    carbon_tape_pos["phi"] = float(phi.getPosition())
-    carbon_tape_pos["chi"] = float(chi.getPosition())
+    carbon_tape_pos[name]["x"] = float(x.getPosition())
+    carbon_tape_pos[name]["y"] = float(y.getPosition())
+    carbon_tape_pos[name]["z"] = float(z.getPosition())
+    carbon_tape_pos[name]["phi"] = float(phi.getPosition())
+    carbon_tape_pos[name]["chi"] = float(chi.getPosition())
     print("carbon tape positions are saved.")
 
 alias("save_carbon_tape_positions")
     
-def move_to_sample_positions():
+def move_to_sample_positions(name = "sample"):
     global sample_pos
-    x.asynchronousMoveTo(sample_pos["x"])
-    y.asynchronousMoveTo(sample_pos["y"])
-    z.asynchronousMoveTo(sample_pos["z"])
-    th.asynchronousMoveTo(sample_pos["th"])
-    phi.asynchronousMoveTo(sample_pos["phi"])
-    chi.asynchronousMoveTo(sample_pos["chi"])
+    x.asynchronousMoveTo(sample_pos[name]["x"])
+    y.asynchronousMoveTo(sample_pos[name]["y"])
+    z.asynchronousMoveTo(sample_pos[name]["z"])
+    phi.asynchronousMoveTo(sample_pos[name]["phi"])
+    chi.asynchronousMoveTo(sample_pos[name]["chi"])
     x.waitWhileBusy()
     y.waitWhileBusy()
     z.waitWhileBusy()
-    th.waitWhileBusy()
     phi.waitWhileBusy()
     chi.waitWhileBusy()
-    print("move to sample positions completed.")
+    print("move to sample positions to '%s' completed." % name)
     
 alias("move_to_sample_positions")
 
-def move_to_carbon_tape_positions():
+def move_to_carbon_tape_positions(name = "carbon"):
     global carbon_tape_pos
-    x.asynchronousMoveTo(carbon_tape_pos["x"])
-    y.asynchronousMoveTo(carbon_tape_pos["y"])
-    z.asynchronousMoveTo(carbon_tape_pos["z"])
-    th.asynchronousMoveTo(carbon_tape_pos["th"])
-    phi.asynchronousMoveTo(carbon_tape_pos["phi"])
-    chi.asynchronousMoveTo(carbon_tape_pos["chi"])
+    x.asynchronousMoveTo(carbon_tape_pos[name]["x"])
+    y.asynchronousMoveTo(carbon_tape_pos[name]["y"])
+    z.asynchronousMoveTo(carbon_tape_pos[name]["z"])
+    phi.asynchronousMoveTo(carbon_tape_pos[name]["phi"])
+    chi.asynchronousMoveTo(carbon_tape_pos[name]["chi"])
     x.waitWhileBusy()
     y.waitWhileBusy()
     z.waitWhileBusy()
-    th.waitWhileBusy()
     phi.waitWhileBusy()
     chi.waitWhileBusy()
-    print("move to carbon tape positions completed.")
+    print("move to carbon tape positions '%s' completed." % name)
     
 alias("move_to_carbon_tape_positions")
 
