bdf57eef415b417ecaa189ef0103ee4bc44fc1d5
Paula <paula.martin-gomez@diamond.ac.uk>
2024-02-27 12:43:47 +0000

    I14 Remove XREYE detector references
    Change-Id: Ib68d1a7e66730bee9b1de22bfd5b8250e8ddf92b

--- configurations/i14-config/scripts/initialise_detectors.py
+++ configurations/i14-config/scripts/initialise_detectors.py
@@ -5,13 +5,11 @@ from i14_utilities import isLive
 from gdascripts.detectors.initialise_detector import initialise_detector
 
 def initialise_detectors():
-	from gdaserver import xsp3_addetector, xreye_addetector, merlin_addetector
+	from gdaserver import xsp3_addetector, merlin_addetector
 	print('Initialising detectors...')
 	if isLive():
-		initialise_detector("Excalibur", "BL14I-EA-EXCBR-01:CONFIG:ACQUIRE", "BL14I-EA-EXCBR-01:MASTER:ARR", "Internal", "Single")
 		initialise_detector("Merlin", merlin_addetector.getAdBase().getBasePVName(), merlin_addetector.getNdArray().getBasePVName(), "Software", "Single")
 		initialise_detector("Xspress3", xsp3_addetector.getAdBase().getBasePVName(), xsp3_addetector.getNdArray().getBasePVName(), "Software", "Single")
-		initialise_detector("Xray Eye", xreye_addetector.getAdBase().getBasePVName(), xreye_addetector.getNdArray().getBasePVName(), "Internal", "Fixed")
 	else:
 		initialise_detector("Simulator", xreye_addetector.getAdBase().getBasePVName(), xreye_addetector.getNdArray().getBasePVName(), "Internal", "Single")
 	print('Detector initialisation complete')
