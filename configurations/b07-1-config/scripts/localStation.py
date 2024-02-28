04588a8c485f64b8ae46fc2f6188c48aabb0c0d2
eir17846 <victor.rogalev@diamond.ac.uk>
2024-02-28 09:48:56 +0000

    B07-966 Add real time scannable (B07-1)
    Change-Id: Ia55ac2f756b8916d823793c0155ff105c7ac6ac8

--- configurations/b07-1-config/scripts/localStation.py
+++ configurations/b07-1-config/scripts/localStation.py
@@ -13,21 +13,21 @@ print("=========================================================================
 print
 
 print("Load EPICS pseudo device utilities for creating scannable object from a PV name.")
-from gdascripts.pd.epics_pds import * #@UnusedWildImport
+from gdascripts.pd.epics_pds import *  # @UnusedWildImport
 
 print("Load time utilities.")
-from gdascripts.pd.time_pds import * #@UnusedWildImport
-# Make time scannable 
+from gdascripts.pd.time_pds import *  # @UnusedWildImport
+# Make time scannable
 # Example: scan timeScannable 0 3600 30 analyser - Make a scan starting now, for 1 hour, recording the analyser every 30 secs
-from gdascripts.scannable.timerelated import TimeSinceScanStart
+from gdascripts.scannable.timerelated import TimeSinceScanStart, clock, epoch  # @UnusedImport
 timeScannable = TimeSinceScanStart('timeScannable')
 
 print("Load utilities: caget(pv), caput(pv,value), attributes(object), iterableprint(iterable), listprint(list), frange(start,end,step)")
-from gdascripts.utils import * #@UnusedWildImport
+from gdascripts.utils import *  # @UnusedWildImport
 
 print("Installing standard scans with processing")
-from gdascripts.scan.installStandardScansWithProcessing import * #@UnusedWildImport
-scan_processor.rootNamespaceDict=globals()
+from gdascripts.scan.installStandardScansWithProcessing import *  # @UnusedWildImport
+scan_processor.rootNamespaceDict = globals()
 
 print("Installing regional scan")
 from gdascripts.scan.RegionalScan import RegionalScanClass
@@ -47,31 +47,31 @@ from scannables.rga24 import rga24, rga24AR, rga24CF3, rga24CH2, rga24CH3, rga24
 
 from scannables.PLV1000 import plv1, plv2  # @UnusedImport
 
-#check beam scannables
+# check beam scannables
 from scannables.checkbeanscannables import checkbeam, checkfe, checkrc, checktopup_time  # @UnusedImport
 
 print("-"*100)
 print("setup meta-data provider commands: meta_add, meta_ll, meta_ls, meta_rm ")
-from metadata.metashop import meta_add,meta_ll,meta_ls, meta_rm  # @UnusedImport
+from metadata.metashop import meta_add, meta_ll, meta_ls, meta_rm  # @UnusedImport
 import metadata.metashop as metashop  # @UnusedImport
 
 print("-"*100)
 import installation
-meta_data_list = [] #other common metadata can be added here if required!
+meta_data_list = []  # other common metadata can be added here if required!
 if installation.isLive():
-    meta_data_list = meta_data_list +[rga]  # @UndefinedVariable
+    meta_data_list = meta_data_list + [rga]  # @UndefinedVariable
     end_station_configuration = int(caget("BL07C-EA-ENDST-01:CFG:HW_RBV"))
-    if end_station_configuration == 1: #TPOT
+    if end_station_configuration == 1:  # TPOT
         print("add TPOT scannables to metadata list ...")
         try:
-            meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation] #@UndefinedVariable
+            meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
         except Exception as e:
             print("adding TPOT to metadata failed.")
             localStation_exception(sys.exc_info(), "adding TPOT to metadata error")
-    if end_station_configuration == 2: #TCUP
+    if end_station_configuration == 2:  # TCUP
         print("add TCUP metadata scannables to be captured in data files.")
         try:
-            meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp] #@UndefinedVariable
+            meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
         except Exception as e:
             print("adding TCUP to metadata failed.")
             localStation_exception(sys.exc_info(), "adding TCUP to metadata error")
@@ -80,13 +80,13 @@ else:
     spring_profiles = System.getProperty("gda.spring.profiles.active")
     if "TPOT" in spring_profiles:
         print("add TPOT metadata scannables to be captured in data files.")
-        meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation] #@UndefinedVariable
+        meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
     if "TCUP" in spring_profiles:
         print("add TCUP metadata scannables to be captured in data files.")
-        meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp] #@UndefinedVariable
+        meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
 
-patch_panel_list = [ppc_temp_1,ppc_temp_2,ppc_temp_3,ppc_temp_4,ppc_ai_1,ppc_ai_2,ppc_ai_3,ppc_ai_4,ppc_ao_1,ppc_ao_2,ppc_ao_3,ppc_ao_4,ppc_di_1,ppc_di_2,ppc_di_3,ppc_di_4,ppc_do_5,ppc_do_6,ppc_do_7,ppc_do_8]  # @UndefinedVariable
-crystal_monochromator_list = [ccmc_x, ccmc_y, ccmc_z] # @UndefinedVariable
+patch_panel_list = [ppc_temp_1, ppc_temp_2, ppc_temp_3, ppc_temp_4, ppc_ai_1, ppc_ai_2, ppc_ai_3, ppc_ai_4, ppc_ao_1, ppc_ao_2, ppc_ao_3, ppc_ao_4, ppc_di_1, ppc_di_2, ppc_di_3, ppc_di_4, ppc_do_5, ppc_do_6, ppc_do_7, ppc_do_8]  # @UndefinedVariable
+crystal_monochromator_list = [ccmc_x, ccmc_y, ccmc_z]  # @UndefinedVariable
 meta_data_list = meta_data_list + patch_panel_list + crystal_monochromator_list
 
 for each in meta_data_list:
