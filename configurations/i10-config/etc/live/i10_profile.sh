fc3f7c9d40e8058977094ed72c34e4e138ab4669
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2024-03-20 16:42:07 +0000

    update bash scripts that wrap gda launcher
    - support Spring profile for end station specific GDA server and client
    - load new gda_launcher in environment when user login
    - chain log panel launcher so user would know when server start
    completed!
    - update diamond launcher applications file
    
    Change-Id: I1889eb26ed06f27b2ffde20bb25bda93a33be34b

--- configurations/i10-config/etc/live/i10_profile.sh
+++ configurations/i10-config/etc/live/i10_profile.sh
@@ -1,5 +1,4 @@
 # i10 beamline profile
-#This script is sourced from /dls_sw/b24/etc/i14_profile.sh
 #KrisB 17/09/14 - Commenting out $BEAMLINE as always set on beamline workstations via /etc/profile.d/beamline.sh
 
 
@@ -8,10 +7,7 @@ if [ ! -n "$BEAMLINE" ]; then
   exit 1
 fi
 
-#export GDA_INSTANCE_NAME=${BEAMLINE}
-
-#export GDA_WORKSPACE_PARENT=/dls_sw/$BEAMLINE/software/gda
-#export GDA_INSTANCE_CONFIG=${GDA_WORKSPACE_PARENT}/config
+module load gda_launcher
 
 export PATH=/dls_sw/$BEAMLINE/software/gda/config/bin:${PATH}
 export LD_LIBRARY_PATH=/dls_sw/i21/software/anaconda2/plugins/platforms:${LD_LIBRARY_PATH}
