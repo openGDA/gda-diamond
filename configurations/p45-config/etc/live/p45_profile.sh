a803f40dc5e9518b141dc6cc904448f0c6c6eb99
Joe Shannon <joe.shannon@diamond.ac.uk>
2024-03-28 14:53:25 +0000

    p45: Add new lanucher to PATH in beamline profile
    This is added to the end of the path to ensure that the old gda script
    in config/bin takes priority if the version is switched back to 9.32.
    
    Change-Id: I24c7f77f96a5b2a350efcd5a1da2b617afee9045

--- configurations/p45-config/etc/live/p45_profile.sh
+++ configurations/p45-config/etc/live/p45_profile.sh
@@ -8,7 +8,12 @@ if [ ! -n "$BEAMLINE" ]; then
 fi
 
 # Set up path and mode
+# Prepend legacy config/bin
 export PATH=/dls_sw/$BEAMLINE/software/gda/workspace_git/gda-diamond.git/configurations/p45-config/bin:${PATH}
+
+# Postpend new gda_launcher
+export PATH=${PATH}:/dls_sw/apps/gda_launcher/nightly
+
 export GDA_MODE=live
 
 # Set up command completion for the gda command
