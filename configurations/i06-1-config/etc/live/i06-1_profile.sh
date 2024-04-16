6d26c4e67e0f8e3c8c9105d3c5a88410457a73ab
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2024-04-16 09:52:01 +0100

    I06-1300 update GDA launchers for i06, i06-1,and i06-2 following core gda_launcher changes
    - add profiles support to i06-1 config.toml
    - replace 'gda.group.dir' property
    - chain client launching with server start
    - update launcher in etc folder to symlinks to support roll back
    
    Change-Id: Ifce2a2e4c9c8d06b00ecb3f7f626309270bd040b

--- configurations/i06-1-config/etc/live/i06-1_profile.sh
+++ configurations/i06-1-config/etc/live/i06-1_profile.sh
@@ -5,7 +5,7 @@ if [[ $GDA_ROOT_DIR == *"8.38"* ]]; then
 	. /dls_sw/i06-1/software/gda/workspace_git/gda-mt.git/configurations/i06-config/bin/gda_environment.sh
 else
 	# Set up path and mode
-	export PATH=/dls_sw/prod/etc/Launcher:/dls_sw/$BEAMLINE/software/gda/config/bin:${PATH}
+	export PATH=/dls_sw/prod/etc/Launcher:/dls_sw/$BEAMLINE/software/gda/config/bin:/dls_sw/apps/gda_launcher/stable:${PATH}
 	export GDA_MODE=live
 
 	# Set up command completion for the gda command
--- configurations/i06-config/etc/live/i06_profile.sh
+++ configurations/i06-config/etc/live/i06_profile.sh
@@ -1,7 +1,7 @@
 # i06 beamline profile
 
 # Set up path and mode
-export PATH=/dls_sw/prod/etc/Launcher:/dls_sw/$BEAMLINE/software/gda/config/bin:${PATH}
+export PATH=/dls_sw/prod/etc/Launcher:/dls_sw/$BEAMLINE/software/gda/config/bin:/dls_sw/apps/gda_launcher/stable:${PATH}
 export GDA_MODE=live
 
 # Set up command completion for the gda command
