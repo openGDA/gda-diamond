44903698f31ef0efa6a61aea3481a9a420a3a715
eir17846 <victor.rogalev@diamond.ac.uk>
2024-04-19 17:11:27 +0100

    I05-668 fix bug with scan_group globals() fail for imported modules
    Need either to execfile() instead of import or use __main__
    
    Change-Id: Ifdb62d746a252692144b546487ca2c7b3a48e3cf

--- configurations/i05-config/scripts/localStation.py
+++ configurations/i05-config/scripts/localStation.py
@@ -34,14 +34,14 @@ from gdascripts.constants import * #@UnusedWildImport
 
 
 class actTimeInInt(actualTimeClass):  # specialise to make displayed time semi-human-readable
-   def rawGetPosition(self):
-      pad = 10000
-      t = localtime(time.time())      # t = localtime(super(actualTimeClass, self).rawGetPosition())
-      tInInt = ((((((t[0]*100+t[1])*100+t[2])*pad+t[3])*100+t[4])*pad)+t[5])
-      return tInInt                   # actual wall clock date & time
+	def rawGetPosition(self):
+			pad = 10000
+			t = localtime(time.time())      # t = localtime(super(actualTimeClass, self).rawGetPosition())
+			tInInt = ((((((t[0]*100+t[1])*100+t[2])*pad+t[3])*100+t[4])*pad)+t[5])
+			return tInInt                   # actual wall clock date & time
 actTime = actTimeInInt("actTime")
 
-# Make time scannable 
+# Make time scannable
 # Example: scan timeScannable 0 3600 30 analyser - Make a scan starting now, for 1 hour, recording the analyser every 30 secs
 timeScannable = TimeSinceScanStart('timeScannable')
 
@@ -57,7 +57,7 @@ alias("getVisit")
 setVisit = metadatatweaks.setVisit
 alias("setVisit")
 def isgold():
-   return saz.getPosition() < -18
+	return saz.getPosition() < -18
 sample_name=metadatatweaks.SampleNameScannable("sample_name","samplename",isgoldpost=isgold)
 
 energy_group = ScannableGroup() # Make a new ScannableGroup
@@ -88,12 +88,12 @@ alias("W_F")
 #print "Added work function calculator - Usage: W_F(photon_energy)"
 #binding_function = analyser.toBindingEnergy
 def B_E(photon_energy):
-    """Since analyser.toBindingEnergy(scannable) returns an array,
-    this function extracts the value from the array if that is the case"""
-    result = binding_function(photon_energy)
-    if isinstance(result, array.array):
-        return result[0]
-    return result
+		"""Since analyser.toBindingEnergy(scannable) returns an array,
+		this function extracts the value from the array if that is the case"""
+		result = binding_function(photon_energy)
+		if isinstance(result, array.array):
+				return result[0]
+		return result
 alias("B_E")
 print "Added binding energy calculator - Usage: B_E(kinetic_energy)"
 
@@ -122,19 +122,19 @@ from i05Shared.resolutionEstimator import *
 estimateResolution()
 
 def set_analyser_slit(slit):
-    """ Set analyser entrance slit by integer value """
-    analyser.getEntranceSlitInformationProvider().setCurrentSlitByValue(slit)
-    print "Analyser slit is set to: "
-    print get_analyser_slit()
-    
+		""" Set analyser entrance slit by integer value """
+		analyser.getEntranceSlitInformationProvider().setCurrentSlitByValue(slit)
+		print "Analyser slit is set to: "
+		print get_analyser_slit()
+
 def get_analyser_slit():
-    """ Get current analyser entrance slit parameters """
-    return analyser.getEntranceSlitInformationProvider().getCurrentSlit()
+		""" Get current analyser entrance slit parameters """
+		return analyser.getEntranceSlitInformationProvider().getCurrentSlit()
 
 print "="*20
 if LocalProperties.get("gda.mode")=="live":  # don't execute in squish tests
-   print "Running i05 scripts."
-   # module location is /dls_sw/ixx/scripts/
-   from beamline.master import *
-   # run "beamline/master.py"
+	print "Running i05 scripts."
+	# module location is /dls_sw/ixx/scripts/
+	from beamline.master import *
+	# run "beamline/master.py"
 print "="*20
