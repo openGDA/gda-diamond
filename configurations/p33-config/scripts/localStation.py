753e1c296df1c8ea8b83a757becb18eaf3da27ca
Douglas Winter <douglas.winter@diamond.ac.uk>
2024-05-31 08:59:38 +0100

    p33: config changes from live deployment
    - Fixed photodiode pv
    - Added timerScannable
    - Enable SRS file writing
    - Allow exceptions on startup
    - Specify default visit
    
    Change-Id: Ia499c4cb886a93f1c43e398d8350fe30c8feb470

--- configurations/p33-config/scripts/localStation.py
+++ configurations/p33-config/scripts/localStation.py
@@ -1,2 +1,5 @@
 from gdascripts.scan.installStandardScansWithProcessing import * #@UnusedWildImport
 scan_processor.rootNamespaceDict = globals()
+
+from gdascripts.scannable.timerelated import TimeSinceScanStart
+timerScannable = TimerSinceScanStart("timerScannable")
