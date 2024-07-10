473663851d67ea78df94fd696bc7f1c9e77a7056
Iain Hall <iain.hall@diamond.ac.uk>
2024-07-10 17:22:09 +0100

    I18-634 : Updates for running pump-probe laser experiment
    Updates for running pump-probe experiment using the Tfg.
    The measurement on the Tfg works by accumulating several frames of data
    over a number of cycles. Each cycle is started by a trigger from the
    laser, while each frame within a cycle uses internal triggering.
    The Tfg measurement is repeated across a range of different incident
    beam energies, by doing a 2-dimensional scan (using tfg scan as inner
    loop). The scan over energies can be repeated a number of times to
    improve statistics. After each scan, the data is processed to find
    average, start-end frame count differences etc. and results added to the
    Nexus file.
    
    Script updates :
    - pump_probe_tfg_setup.py - runs the data collection using the Tfg.
    This script is derived from old pump-probe scripts, but now uses the
    scan mechanism to read out and collect data rather than manually reading
    scaler data from the Tfg and writing it to several Ascii files.
    - qexafs_scans.py - updated to include new TfgScanRunner class, which
    makes running Tfg scans using different trigger options much easier.
    - laser_exp_processor.py - new ProcessLaserData class to process the
    data collected during laser experiment.
    
    Change-Id: Iadab8c6007dd15139c53b6f324f0b7aa0acc9e0e

---
+++ configurations/i18-config/scripts/laser_exp_processor.py
@@ -0,0 +1,129 @@
+from os.path import dirname, basename
+from org.eclipse.dawnsci.hdf5.nexus import NexusFileHDF5
+from org.eclipse.january.dataset import DatasetFactory
+
+def path_is_valid(file, path):
+    f = None
+    try :
+        f = NexusFileHDF5.openNexusFileReadOnly(file)
+        return f.isPathValid(path)
+    finally :
+        if f is not None :
+            f.close()
+
+def remove_node(file, fullpath) :
+    if not path_is_valid(file, fullpath) :
+        return 
+
+    f = None
+    try :
+        dataname = basename(fullpath)
+        grouppath = dirname(fullpath)
+        f = NexusFileHDF5.openNexusFile(file)
+        f.removeNode(grouppath, dataname)
+    finally :
+        if f is not None :
+            f.close()
+    
+def load_dataset(file, fullpath, get_link_target=False):
+
+    dataname = basename(fullpath)
+    grouppath = dirname(fullpath)
+    print("Loading %s/%s from %s" % (grouppath, dataname, file))
+    f = None
+    try:
+        f = NexusFileHDF5.openNexusFileReadOnly(file)
+        group = f.getGroup(grouppath, False)
+        if get_link_target : 
+            return group.getNode(dataname).getAttributeNames()
+        else : 
+            datanode = f.getData(group, dataname)
+            return dnp.array(datanode.getDataset().getSlice(None))
+    finally:
+        # print("Finally called")
+        if f is not None:
+            f.close()
+
+def add_dataset(file, fullpath, data) :
+    dataname = basename(fullpath)
+    grouppath = dirname(fullpath)
+    print("Adding dataset to %s in %s" % (fullpath, file))
+        
+    dataset = DatasetFactory.createFromObject(data)
+    dataset.setName(dataname)
+    
+    f = None
+    try :
+        f = NexusFileHDF5.openNexusFile(file)
+        # 1 = LZW compression
+        # True = create path
+        f.createData(grouppath, dataset, True)
+    finally :
+        if f is not None :
+            f.close()
+
+from org.slf4j import LoggerFactory
+import traceback
+import scisoftpy as dnp
+
+class ProcessLaserData :
+
+    def __init__(self):
+        self.logger = LoggerFactory.getLogger("ProcessLaserData")
+        
+        self.detector_name = "qexafs_counterTimer01"
+        self.processed_path = "/entry1/processed/"
+        self.end_frame_range = 50,60
+        self.start_frame_range = 0,10
+
+    def process_data(self, filename):
+        self.logger.info("Computing processed datasets at end of scan")
+        
+        i0_data = load_dataset(filename, '/entry1/'+self.detector_name+"/I0")
+        iapd_data = load_dataset(filename, '/entry1/'+self.detector_name+"/Iother")
+        iapd_norm = iapd_data/i0_data
+        num_time_frames=iapd_norm.shape[1]
+        
+        #average of the last few frames for wach row of iapd_norm data
+        end_range = self.end_frame_range[1]-self.end_frame_range[0]
+        end_timeframe_data = [sum(d)/end_range for d in iapd_norm[:, self.end_frame_range[0]:self.end_frame_range[1]]]
+        
+        start_range = self.start_frame_range[1]-self.start_frame_range[0]
+        start_timeframe_data = [sum(d)/start_range for d in iapd_norm[:, self.start_frame_range[0]:self.start_frame_range[1]]]
+
+        start_end_diff = [start - end for start, end in zip(start_timeframe_data, end_timeframe_data) ]
+        
+        #subtract last_timeframe_data from each row of normalised apd_normdata 
+        iapd_norm_sub = [a-b for a, b in zip(iapd_norm, end_timeframe_data)]
+        
+        return i0_data, iapd_data, iapd_norm, start_timeframe_data, end_timeframe_data, start_end_diff, iapd_norm_sub
+    
+    def add_processed_data(self, filename, overwrite=False):
+        try :
+            self.add_datasets(filename, overwrite)
+        except :
+            stacktrace=traceback.format_exc()
+            print("Problem adding processed datasets : "+stacktrace)
+            self.logger.warn("Problem adding processed datasets to {} : {}", filename, stacktrace)
+            
+    def add_datasets(self, filename, overwrite=False):
+        i0_data, iapd_data, iapd_norm, start_timeframe_data, end_timeframe_data, start_end_diff, iapd_norm_sub = self.process_data(filename)
+        
+        data_to_add = {"Inorm" : iapd_norm.tolist(), "InormSub" : self.to_list(iapd_norm_sub),
+                       "EndAvg" : end_timeframe_data, "StartAvg" : start_timeframe_data,
+                       "StartEndDiff" : start_end_diff}
+        
+        self.logger.info("Adding processed datasets to {}", filename)
+        for node_name, dataset in data_to_add.items() :
+            node_path = self.processed_path+node_name
+            if overwrite :
+                remove_node(filename, node_path)
+            self.logger.info("Adding {}", node_path)
+            add_dataset(filename, node_path, dataset)
+        self.logger.info("Finished")
+
+    def to_list(self, two_d_arr) :
+        return [list(row) for row in two_d_arr]
+    
+
+laser_exp_processor = ProcessLaserData()
--- configurations/i18-config/scripts/qexafs_scans.py
+++ configurations/i18-config/scripts/qexafs_scans.py
@@ -5,6 +5,7 @@ import math
 
 # Import cvscan to make it easier to run ContinuousScans
 from gda.jython.commands.ScannableCommands import cv as cvscan
+from pickle import FALSE
 vararg_alias("cvscan")
 
 # Class for I18 to allow them to reuse the B18 position based continuous scannable classes :
@@ -99,21 +100,142 @@ cont_scannable.setDelegateScannable(dummy_scannable)
 cont_scannable.setMaxMotorSpeed(1000)
 cont_scannable.configure()
 
+from gda.device import ContinuousParameters
 from gda.scan import ContinuousScan
 
-def run_tfg_continuous_scan(num_points, scan_time=5, internal_trigger=False) :
+def generate_continuous_params(start, stop, num_points, total_time) :
+    params = ContinuousParameters()
+    params.setStartPosition(start)
+    params.setEndPosition(stop)
+    params.setNumberDataPoints(num_points)
+    params.setTotalTime(total_time)
+    params.setContinuouslyScannableName(cont_scannable.getName())
+    return params
+
+def run_tfg_continuous_scan(num_points, scan_time=5, external_trigger=False) :
     """
         Run a continuous scan using Tfg (qexafs_counterTimer01) and a 'dummy' motor.
         Parameters : 
             num_points - number of frames of data to collect
             scan_time  - how long the scan should take to run (seconds). Optional, default = 5 seconds)
-            internal_trigger - whether each frame is triggered internally. Optional, default = False.
-                               If set to False, Tfg will wait for external trigger before collecting each frame.
+            external_trigger - whether each frame is triggered internally. Optional, default = False.
+                               If set to True, Tfg will wait for external trigger before collecting each frame.
                                (Use qexafs_counterTimer01.setTtlSocket(..) to set the Tfg TTL port to use for triggers)
     """
-    qexafs_counterTimer01.setUseInternalTriggeredFrames(internal_trigger)
+    qexafs_counterTimer01.setUseExternalTriggers(external_trigger)
     sc=ContinuousScan(cont_scannable, 0, num_points, num_points, scan_time, [qexafs_counterTimer01])
     sc.runScan()
   
-print("Adding 'run_tfg_continuous_scan' function. Use help(run_tfg_continuous_scan) for more info. ")
 
+
+daServer = qexafs_counterTimer01.getScaler().getDaServer()
+
+from gda.data.scan.datawriter import DefaultDataWriterFactory
+
+class TfgScanRunner :
+    def __init__(self) :
+        self.buffered_scaler = None
+        self.frame_dead_time = 0.0
+        self.external_trigger_frames = False
+        self.external_trigger_start = False
+        self.initial_group_command = ""
+        self.num_cycles = 1
+        self.extra_scannables = None 
+
+        self.nexus_name_template = "" 
+        self.ascii_name_template = "" 
+
+    def apply_settings(self) :
+        self.buffered_scaler.setExternalTriggeredFrames(self.external_trigger_frames)
+        self.buffered_scaler.setExternalTriggerStart(self.external_trigger_start)
+        self.buffered_scaler.setFrameDeadTime(self.frame_dead_time)
+        self.buffered_scaler.setGroupInitialCommand(self.initial_group_command);   
+        self.buffered_scaler.setNumCycles(self.num_cycles)
+        if self.num_cycles > 1 :
+            self.buffered_scaler.setFrameCountDuringCycles(False)
+        else :
+            self.buffered_scaler.setFrameCountDuringCycles(True)        
+        
+    def show_settings(self) :
+        print("Settings in tfgScanRunner : ")
+        for k, v in self.__dict__.items() :
+            val = "\"%s\""%(v) if isinstance(v, str) else v
+            print("\t%s = %s"%(k, val))
+
+    def create_datawriter(self) :
+        dw=DefaultDataWriterFactory.createDataWriterFromFactory()
+        if len(self.nexus_name_template) > 0 :
+            dw.setNexusFileNameTemplate(self.nexus_name_template)
+        if len(self.ascii_name_template) > 0 :
+            dw.setAsciiFileNameTemplate(self.ascii_name_template)
+        # set the ascii and file
+        return dw
+    
+    # Add scan parameters as metadata
+    def setup_metadata(self, num_points, time_per_point) :
+        meta_add("tfg_num_cycles", self.num_cycles)
+        meta_add("tfg_time_per_point", time_per_point)
+        meta_add("tfg_num_points", num_points)
+
+    # Remove the custom metadata values
+    def clear_metadata(self):
+        metashop=Finder.find("metashop")
+        metashop.remove("tfg_num_cycles")
+        metashop.remove("tfg_time_per_point")
+        metashop.remove("tfg_num_points")
+    
+    def show_message(self, message):
+        InterfaceProvider.getTerminalPrinter().print(message)
+        
+    def run_scan(self, num_points, time_per_point) :
+        # run the scan
+        try :
+            sc = self.generate_scan(num_points, time_per_point)
+            sc.runScan()
+        finally : 
+            self.show_message("Clearing Tfg metadata after scan")
+            self.clear_metadata()
+
+    def generate_scan(self, num_points, time_per_point):
+        
+        msg = "Running Tfg scan : %d points, %.5g sec per point, %d cycles"%(num_points, time_per_point, self.num_cycles)
+        self.show_message(msg)
+        
+        # apply class settings to buffered scaler
+        self.apply_settings()
+        
+        # generate scan object
+        scan_time = time_per_point*num_points
+        # self.buffered_scaler.stop()
+        sc=ContinuousScan(cont_scannable, 0, num_points, num_points, scan_time, [self.buffered_scaler])
+
+        if self.extra_scannables is not None and len(self.extra_scannables) > 0 :
+            sc.getAllScannables().addAll(self.extra_scannables)
+
+        # setup the metadata
+        self.setup_metadata(num_points, time_per_point)
+
+        # inject datawriter        
+        datawriter = self.create_datawriter()
+        sc.setDataWriter(datawriter)
+        
+        return sc
+
+   
+# Scan runner for laser experiment     
+tfgScanRunner= TfgScanRunner()
+tfgScanRunner.buffered_scaler = qexafs_counterTimer01
+
+output_directory = ""
+filename_template = ""
+
+if len(filename_template) == 0 :
+    filename_template = "%d"
+
+tfgScanRunner.ascii_name_template = "%s/ascii/%s.dat"%(output_directory, filename_template)
+tfgScanRunner.nexus_name_template = "%s/nexus/%s.nxs"%(output_directory, filename_template)
+
+delay_after_trigger = 50e-9
+tfgScanRunner.initial_group_command = "1 %.5g 0 0 0 8 0"%(delay_after_trigger)
+
+print("Adding 'run_tfg_continuous_scan' and 'tfgScanRunner'.\nUse help(run_tfg_continuous_scan), for more info. ")
