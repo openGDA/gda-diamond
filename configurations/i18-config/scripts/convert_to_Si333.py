e5e28d294ff3260916a2f336a3f6e7d3c404eb98
Iain Hall <iain.hall@diamond.ac.uk>
2024-08-02 13:56:51 +0100

    I18 : Updates from beamline
    - Latest version localStation.py with misc. updates
    - Updates to convert_to_Si333.py script - now sets the harmonic range
    correctly, and adjusts the converter list for Si111 and Si333.
    - Detector template updated to make Xspress3Odin the default detector
    - Xspress3MappingMonitor has been removed - not needed for Xspress3Odin
    
    Change-Id: Ia740cd080b61f8f8a120423c4abfa1a5e9b48878

--- configurations/i18-config/scripts/convert_to_Si333.py
+++ configurations/i18-config/scripts/convert_to_Si333.py
@@ -6,6 +6,9 @@ eV_Deg_converter_Si111=Finder.find("eV_Deg_converter_Si111")
 eV_Deg_converter_Si333=Finder.find("eV_Deg_converter_Si333")
 eV_Deg_converter_Si311=Finder.find("eV_Deg_converter_Si311")
 
+lookup_name_provider_Si111=Finder.find("lookup_name_provider_Si111")
+
+harmonic1_Si333=Finder.find("harmonic1_Si333")
 harmonic1_Si111=Finder.find("harmonic1_Si111")
 harmonic3_Si111=Finder.find("harmonic3_Si111")
 
@@ -13,18 +16,40 @@ harmonic3_Si111=Finder.find("harmonic3_Si111")
 si_333_range=[51.000, 50.999]
 si_111_range=[56.187, 56.187]
 
+def get_converter_list(include_333=False) :
+    str_format="harmonic%d_Si111"
+    si_333="harmonic1_Si333"
+    converters = []
+    for i in range(1, 24, 2) :
+        if include_333 and i == 1 :
+            converters.append(si_333)
+        else :
+            converters.append(str_format%(i))
+    return [Finder.find(name) for name in converters]
+        
+def show_names(findable_list) :
+    print [f.getName() for f in findable_list]
+
+def setup_converters(include_333=False) :
+    print("Setting up converter list in lookup_name_provider_Si111 :")
+    converters_list = get_converter_list(include_333)
+    show_names(converters_list)
+    lookup_name_provider_Si111.setConverters(converters_list)
 
 def setup_for_Si333() :
-    print("Setting up harmonic1, 3 converters for Si333 :")
-    adjust_harmonic_range(si_333_range)
+    print("Setting up harmonic1 and harmonic13 converters for Si333 :")
+    setup_converters(include_333=True)
+    adjust_harmonic_range(si_333_range, include_333=True)
     adjust_energy_scannables(eV_Deg_converter_Si333)
-    
+    print("Done!\n")
     
 def setup_for_Si111() :
-    print("Setting up harmonic1, 3 converters for Si111 :")
-    adjust_harmonic_range(si_111_range)
+    print("Setting up harmonic1 and harmonic1 converters for Si111 :")
+    setup_converters(include_333=False)
+    adjust_harmonic_range(si_111_range, include_333=False)
     adjust_energy_scannables(eV_Deg_converter_Si111)
-    
+    print("Done!\n")
+
 ## Make the Si111 energy scannables using the given (Si333 or Si111) converter 
 def adjust_energy_scannables(converter) :
     print("Setting energy scannables to use %s converter :"%(converter.getName()))
@@ -38,12 +63,14 @@ def adjust_energy_scannables(converter) :
     zebraBraggEnergy.setDelegateConverters(delegates)
     
 ## Adjust the harmonic energy range (end of harmonic1, start of harmonic3
-def adjust_harmonic_range(end_start_vals) :
+def adjust_harmonic_range(end_start_vals, include_333) :
     harm1_end=end_start_vals[0]
     harm3_start=end_start_vals[1]
-    print("\tSetting %s end to %f"%(harmonic1_Si111.getName(), harm1_end))
-    harmonic1_Si111.setRangeStop(harm1_end)
+    harmonic1 = harmonic1_Si333 if include_333 else harmonic1_Si111
+    print("\tSetting %s end to %f"%(harmonic1.getName(), harm1_end))
+    harmonic1.setRangeStop(harm1_end)
     
-    print("\tSetting %s start to %f"%(harmonic1_Si111.getName(), harm3_start))
-    harmonic1_Si111.setRangeStart(harm3_start)
+    print("\tSetting %s start to %f"%(harmonic3_Si111.getName(), harm3_start))
+    harmonic3_Si111.setRangeStart(harm3_start)
+
 
