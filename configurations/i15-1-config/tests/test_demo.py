d90712eee7215bc9149eed9e859f27fc99615086
Mark Booth <Mark.Booth@diamond.ac.uk>
2024-08-30 14:01:49 +0100

    I15-1 (9.35) Add plotting tests and update i22 example tests
    Change-Id: I8c1f09b27cfb4abeedd7b735d28fe5b7ddfaf8cb

--- configurations/i15-1-config/tests/test_demo.py
+++ configurations/i15-1-config/tests/test_demo.py
@@ -1,9 +1,11 @@
-"""
-from datetime import datetime, timedelta
+""" Copied from /i22-config/tests/test_demo.py at commit ebdc189
+import pytest
+from array import array
 
 from uk.ac.diamond.scisoft.analysis.io import LoaderFactory
 
-DT_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
+# Use java time parsing because jython can't handle time zones
+from java.time import OffsetDateTime, Duration
 
 def test_set_subdirectory(meta):
     meta['subdirectory'] = 'testing'
@@ -28,13 +30,13 @@ def test_expected_metadata_present(meta):
     assert 'sample_name' in meta
 
 @pytest.mark.parametrize("start,stop,step", [(0,4,0.1), (4,0,0.1)])
-def test_1d_scan(start, stop, step, main, meta, gaussian_pair):
+def test_1d_scan(scan_command, start, stop, step, main, meta, gaussian_pair):
     meta['subdirectory'] = 'testing'
     meta['title'] = 'test_1d_scan'
     meta['sample_name'] = 'test_sample_one'
     main.sample_thickness(0.42)
     gx, gy = gaussian_pair
-    main.scan(gx, start, stop, step, gy)
+    scan_command(gx, start, stop, step, gy)
     sdp = lastScanDataPoint()
     command = 'scan gx {} {} {} gy'.format(start, stop, step)
     assert sdp.getCommand() == command
@@ -53,21 +55,50 @@ def test_1d_scan(start, stop, step, main, meta, gaussian_pair):
     entry1 = scan_tree.getGroupNode('entry1')
     assert entry1.getDataNode('scan_command').getString() == command
     assert entry1.getDataNode('title').getString() == 'test_1d_scan'
-    start = str(entry1.getDataNode('start_time').getString())
+    start_time = str(entry1.getDataNode('start_time').getString())
     end = str(entry1.getDataNode('end_time').getString())
     # Scan should be quick with dummy scannables
-    assert datetime.strptime(end, DT_FORMAT) - datetime.strptime(start, DT_FORMAT) < timedelta(seconds=5)
+    assert _duration(start_time, end) < 5
 
     sample = entry1.getGroupNode('sample')
     assert sample.getDataNode('name').getString() == 'test_sample_one'
     assert sample.getDataNode('thickness').getDataset().getSlice(None).getData()[0] == 0.42
 
     instrument = entry1.getGroupNode('instrument')
-    assert instrument.getDataNode('gx').shape == [41]
-    assert instrument.getDataNode('gy').shape == [41]
+    assert instrument.getGroupNode('gx').getDataNode('gx').dataset.shape == array('i', [41])
+    assert instrument.getGroupNode('gy').getDataNode('gy').dataset.shape == array('i', [41])
 
 def test_single_xray_frame(main):
     from setup import tfgsetup
     tfgsetup.setupTfg(1, 800, 200)
     staticscan(main.ncddetectors)
+
+@pytest.mark.mode('live', reason="Relies on shared file system permissions")
+def test_permissions(config):
+    from uk.ac.diamond.daq.linux import UserUtilities
+    from os import path
+    assert UserUtilities.groupCanWrite('i22_staff', path.join(config, 'xml', 'permissions.xml'))
+    assert UserUtilities.groupCanWrite('i22_staff', path.join(config, 'scripts', 'beamlineScripts'))
+    assert UserUtilities.groupCanWrite('i22_staff', path.join(config, 'scripts', 'sampleEnvironment'))
+    assert UserUtilities.groupCanWrite('i22_staff', path.join(config, 'scripts', 'templatesRepository'))
+    assert UserUtilities.groupCanWrite('i22_staff', path.join(config, 'etc', 'i22.cfg'))
+
+def test_2d_scan(scan_command, main, meta, gaussian_2d):
+    x,y,g = gaussian_2d
+    meta['subdirectory'] == 'testing'
+    meta['title'] == 'test_2d_scan'
+    meta['sample_name'] == "test_sample_two"
+
+    scan_command(x, -1, 1, 0.2, y, -1, 1, 0.4, g)
+    sdp = lastScanDataPoint()
+    command = 'scan x -1 1 0.2 y -1 1 0.4 g'
+    assert sdp.command == command
+    assert sdp.numberOfPoints == 66
+    assert sdp.instrument == 'i22'
+
+def _duration(start, end):
+    start = OffsetDateTime.parse(start)
+    end = OffsetDateTime.parse(end)
+    return Duration.between(start, end).seconds
+
 """
