f25aac0a375e335537a1da16aed2cf2418acb78b
Oli Wenman <Oliver.Wenman@diamond.ac.uk>
2024-10-01 16:29:30 +0100

    I09-579 - Updated nixswr_count to nixswr_repeat
    Updated nixswr_count to nixswr_repeat as requested by PBS. Fixed
    springbean order for live and dummy so server can start
    
    Change-Id: I2cb026b31a9cdc9408705321ea106a8c5ea0d778

--- configurations/i09-config/scripts/localStation.py
+++ configurations/i09-config/scripts/localStation.py
@@ -260,10 +260,10 @@ print("keithley2600 control objects:\nGeneral operation: keithley_a, keithley_b\
 # from scan.MultiRegionScan import mrscan, ALWAYS_COLLECT_AT_STOP_POINT, NUMBER_OF_DECIMAL_PLACES  # @UnusedImport
 from i09shared.scan.miscan import miscan  # @UnusedImport
 
-from gdaserver import nixswr_count, nixswr_time # @UnresolvedImport @UnusedImport
+from gdaserver import nixswr_repeat, nixswr_time # @UnresolvedImport @UnusedImport
 print("-"*100)
-print("Installed object nixswr_count, measure with nixswr camera that takes input the number of frames to acquire.")
-print("\tSynatx: scan scannable 1 2 1 nixswr_count 10")
+print("Installed object nixswr_repeat, measure with nixswr camera that takes input the number of frames to acquire.")
+print("\tSynatx: scan scannable 1 2 1 nixswr_repeat 10")
 print("Installed object nixswr_time, measure with nixswr camera that takes input the amount of time to acquire over (in seconds).")
 print("\tSynatx: scan scannable 1 2 1 nixswr_time 0.001")
 
