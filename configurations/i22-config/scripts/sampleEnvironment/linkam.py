b2387d7113380cf3c79f53b433d1158fe4abcbd7
Mark Booth <Mark.Booth@diamond.ac.uk>
2024-10-09 15:27:20 +0100

    I22-1332 (9.35) Improve linkam logging
    Change-Id: Ia2c74268f1bf629346a1ab27262ac5d2e9c3c88a

--- configurations/i22-config/scripts/sampleEnvironment/linkam.py
+++ configurations/i22-config/scripts/sampleEnvironment/linkam.py
@@ -208,13 +208,18 @@ class Linkam(ScannableBase):
         self._logger.debug('Setting running state to {}', run_state)
         self._pv.set(self.START, 1 if run_state else 0)
 
-    def start(self):
+    def start(self): # This function does not appear to be used
+        self._logger.trace("start() running was {}", self.running)
         self.running = True
+        if (abs(self.temp - self.setpoint) > self.tolerance):
+            self._logger.warn('start() Current temperature {} is not within tolerance {} of current setpoint {}', self.temp, self.tolerance, self.setpoint)
+            print('Current temperature is not within tolerance of current setpoint!')
 
     def stop(self):
         # self.running = False
         self.setpoint = self.temp
         if self._ramp_future is not None:
+            self.logger.debug("Stopping ramp future")
             self._ramp_future.cancel(True)
             self._ramp_future = None
 
@@ -232,10 +237,14 @@ class Linkam(ScannableBase):
             return self.temp,
 
     def isBusy(self):
+        self._logger.trace("isBusy moving {} or {} (_ramping_thread={} and _ramp_future={} and _ramp_future.isDone())",
+                self.moving, "false" if self._ramp_future is None else self._ramp_future.isDone(), self._ramping_thread.get(), self._ramp_future)
         return (not self._ramping_thread.get() and self._ramp_future is not None and not self._ramp_future.isDone()) or self.moving
     
     @property
     def moving(self):
+        self._logger.trace("moving: running {} and {} (abs(temp {} - setpoint {})={} > tolerance {})", self.running,
+               abs(self.temp - self.setpoint) > self.tolerance, self.temp, self.setpoint, abs(self.temp - self.setpoint), self.tolerance)
         return abs(self.temp - self.setpoint) > self.tolerance and self.running
 
     def _temp_update(self, src, temp):
@@ -277,10 +286,15 @@ class Linkam(ScannableBase):
                 time += float(abs(curr[0] - prev[0])) * 60/curr[1]
         print('Expected run time ~%f' %time)
         steps = int(time // self.interval) + 2 # +1 to counter truncating to int, +1 to include end point
+        self._logger.trace("ramps({}) with interval {} requires {} steps with time {} where _ramp_future was {}",
+            ramps, self.interval, steps, time, self._ramp_future)
         self._ramp_future = Async.submit(lambda: self.run_ramps(*ramps[1:]))
+        self._logger.trace("ramps({}) with interval {} requires {} steps with time {} where _ramp_future now {}",
+            ramps, self.interval, steps, time, self._ramp_future)
         return ScanPositionProviderFactory.create([i*self.interval for i in range(steps)])
 
-    def run_ramps(self, *ramps):
+    def run_ramps(self, *ramps): # Note that as this is run Async, errors in here will be swallowed & the function will stop running
+        self._logger.trace("run_ramps({})", ramps)
         self._logger.info("Starting to run {} ramps", len(ramps))
         self._ramping_thread.set(True) # override the isBusy check if we're the ones being busy
         ramps = ((self.temp,),) + ramps
@@ -289,13 +303,15 @@ class Linkam(ScannableBase):
                 raise ValueError('Linkam is already busy')
             for prev, curr in zip(ramps, ramps[1:]):
                 self._logger.info('Starting ramp {}', curr)
+                self._logger.trace('run_ramps: prev={} curr={}', prev, curr)
                 if abs(curr[0] - prev[0]) < self.tolerance: # hold
                     self._logger.info("Holding at current temp for {}", curr[1])
                     sleep(curr[1])
                 else: # ramp
                     self._logger.info('Moving to {} at {} C/s', curr[0], curr[1])
                     self.ramp_rate = curr[1]
-                    self(curr[0])
+                    self(curr[0]) # This calls self.setpoint(curr[0])
+                # We should probably check if we should be exiting early at some point
         except:
             self._logger.error("Failed to run ramps", exc_info=True)
 
