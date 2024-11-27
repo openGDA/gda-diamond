e32deda5054a4ee01c3303f469ca854dc64232d3
David Garman <david.garman@diamond.ac.uk>
2024-11-27 12:44:40 +0000

    I16-839 add 4th (actually 0th) value to stokes parameters
    Change originally made by Dan Porter as part of nexus upgrade
    
    Change-Id: I5ec9a9ae705bfeb4d6002b972167268841cd8c7d

--- configurations/i16-config/scripts/pd_polarisation_simulator.py
+++ configurations/i16-config/scripts/pd_polarisation_simulator.py
@@ -15,7 +15,7 @@ magSpin_scattering = SampleMagSpin('magSpin_scattering', moment, stokes_pars, pa
 Usage:
   pos moment [0, 0, 3]  # moment of 3ub along c-axis
   mu, mv, mw, mx, my, mz = moment()  # returns moment in crystal and lab axes
-  pos stokes_pars [1, 0, 0]  # define incident beam Stokes parameters P1,P2,P3
+  pos stokes_pars [1, 1, 0, 0]  # define incident beam Stokes parameters P0,P1,P2,P3
   j_00, j_01, j_10, j_11, Isim = pa_jones()  # uses current analyser position
   j_00, j_01, j_10, j_11, Itot, Iana = charge_scattering()
   j_00, j_01, j_10, j_11, Itot, Iana = magE1E1_scattering()
@@ -24,7 +24,7 @@ Usage:
 Simulation:
   con gam 0 mu 0 psi 0
   pos moment [2, 0, 0]
-  pos stokes_pars [1, 0, 0]
+  pos stokes_pars [1, 1, 0, 0]
   pos pol 90
   scan psic -180 180 10 hkl [0,0,6] magE1E1_scattering
   pos psic 0
@@ -569,34 +569,44 @@ class StokesParameters(ScannableMotionBase):
     """
     Define Stokes Parameters of incident beam
 
+    The Stokes parameters are four components labelled I,Q,U,V or p0, p1, p2, p3
+
+    I (S_0) is the beam intensity (often normalized to 1).
+    Q (S_1) is linearly polarized along the x axis (Q > 0) or y axis (Q < 0).
+    U (S_2) is linearly polarized along the x==y axis (U > 0) or the -x==y axis (U < 0).
+    V (S_3) is circularly polarized. V > 0 when the electric field vector rotates clockwise
+               at the sample with respect to time when observed from the source;
+               V < 0 indicates the opposite rotation.
+
     Usage:
         stokes_pars = StokesParameters('stokes_pars')
-        pos stokes_pars [1, 0, 0]  # linear horizontal
-        pos stokes_pars [-1, 0, 0]  # linear vertical
-        pos stokes_pars [0, 1, 0]  # Linearly polarized (+45Deg)
-        pos stokes_pars [0, -1, 0]  # Linearly polarized (-45Deg)
-        pos stokes_pars [0, 0, 1]  # circular right
-        pos stokes_pars [0, 0, -1]  # circular left
+        pos stokes_pars [1, 1, 0, 0]  # linear horizontal
+        pos stokes_pars [1, -1, 0, 0]  # linear vertical
+        pos stokes_pars [1, 0, 1, 0]  # Linearly polarized (+45Deg)
+        pos stokes_pars [1, 0, -1, 0]  # Linearly polarized (-45Deg)
+        pos stokes_pars [1, 0, 0, 1]  # circular right
+        pos stokes_pars [1, 0, 0, -1]  # circular left
     """
 
     def __init__(self, name):
         self.setName(name)
-        self.setInputNames(['p1', 'p2', 'p3'])
+        self.setInputNames(["stokes_parameters"])  # 22/11/2024 by DP I16-839
         self.setExtraNames([])
-        self.setOutputFormat(['%.4f','%.4f','%.4f'])
+        self.setOutputFormat(['%.4f'])
         self.setLevel(3)
+        self.p0 = 1.
         self.p1 = 1.
         self.p2 = 0.
         self.p3 = 0
 
     def getPosition(self):
-        return self.p1, self.p2, self.p3
+        return [[self.p0, self.p1, self.p2, self.p3]]
 
     def isBusy(self):
         return 0
 
     def asynchronousMoveTo(self, stokes_parameters):
-        self.p1, self.p2, self.p3 = stokes_parameters
+        self.p0, self.p1, self.p2, self.p3 = stokes_parameters
 
     def setStokes(self, phi=0, chi=0):
         """
