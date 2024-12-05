23e895ad3e0383f10ae9d2cdaeeb11844c335ba6
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2024-12-05 17:12:17 +0000

    I10-725 XAS templates for i10 3 end stations
    - install xasscan with i10/i10-1 specific template file name
    configuration
    - install SpEL expression process function
    - in stall xasmode scannable
    - include  copy of NXxas definition this development based on for future
    reference.
    
    Change-Id: I5f8eeec198fb986bc927af206c017eaf078d80ba

---
+++ configurations/i10-shared/scripts/scannables/XAS_Mode.py
@@ -0,0 +1,56 @@
+'''
+2 Scannable classes - one for XAS mode,  the other for mapping XAS mode to nexus link path.
+
+Created on 5 Dec 2024
+
+@author: fy65
+'''
+from gda.device.scannable import ScannableMotionBase
+
+XAS_MODES = ['TEY', 'TFY', 'TFY_front', 'TFY_side', 'PEY', 'PFY'] # this is different from i06.
+
+class XASMode(ScannableMotionBase):
+    '''
+    Scannable that allows to set XAS measurement mode - i.e. the default measurement PV or channel for the absorbed beam defined in NXxas.nxdl.xml 
+    '''
+
+    def __init__(self, name, mode = 'TEY'):
+        '''
+        Constructor
+        '''
+        self.setName(name)
+        self.setInputNames([name])
+        self.mode = mode
+
+    def getPosition(self):
+        return self.mode
+
+    def asynchronousMoveTo(self, m):
+        if m not in XAS_MODES:
+            raise ValueError("%s is not a supported measurement mode. Supported mode must be one of %r." % (m, XAS_MODES))
+        self.mode = m
+
+    def isBusy(self):
+        return False
+
+
+class XASModePathMapper(ScannableMotionBase):
+    ''' Scannable that return linked path for a given XAS mode.
+    '''
+    def __init__(self, name, mode, dic):
+        self.setName(name)
+        self.setInputNames([name])
+        self.dict = dic
+        self.mode = mode
+
+    def getPosition(self):
+        return self.dict[self.mode.getPosition()]
+
+    def asynchronousMoveTo(self, pos):
+        raise RuntimeError(self.getName() + " is a READ-ONLY scannable object")
+
+    def isBusy(self):
+        return False
+
+
+
