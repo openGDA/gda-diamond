abbddd08e883207d7aaaa894d4856f50c80ce608
eir17846 <victor.rogalev@diamond.ac.uk>
2025-02-10 11:38:08 +0000

    B07-994 Configure new beamline branch B07-2
    Change-Id: I6671f603dc61d0312525a235c8bd341651b302fb

---
+++ configurations/b07-2-config/scripts/detector/Xspress3MiniSetup.py
@@ -0,0 +1,20 @@
+'''
+Created on Jan 24, 2023
+
+@author: fy65
+'''
+from gdascripts.utils import caput
+
+XSPRESS3_PV_ROOT = "BL07B-EA-XSP3-01:"
+
+def proc4xspress3_num_frames(num_int, pv_root = XSPRESS3_PV_ROOT):
+    caput(pv_root + "PROC:NumFilter", num_int)
+    caput(pv_root + "NumImages", num_int)
+    caput(pv_root + "PROC:ResetFilter", 1)
+    caput(pv_root + "ArrayCounter", 0)
+
+
+def proc4xspress3_setup_ports(pv_root = XSPRESS3_PV_ROOT):
+    caput(pv_root + "PROC:NDArrayPort", "XSP3.DTC")
+    caput(pv_root + "ROI1:NDArrayPort", "XSP3.DTC")
+    caput(pv_root + "ROISUM1:NDArrayPort", "XSP3.PROC")
