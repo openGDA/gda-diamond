e2b10a22d67eacb21d6dbbb600d1af58052a8b67
eir17846 <victor.rogalev@diamond.ac.uk>
2025-03-03 13:10:06 +0000

    I09-614 Add beamline control in GDA
    Change-Id: Ic232f91a3612bd0cac680155c7076d7c22b6277c

--- plugins/uk.ac.gda.beamline.i09-2/src/uk/ac/gda/beamline/i09_2/perspectives/DLDPerspective.java
+++ plugins/uk.ac.gda.beamline.i09-2/src/uk/ac/gda/beamline/i09_2/perspectives/DLDPerspective.java
@@ -33,7 +33,9 @@ public class DLDPerspective implements IPerspectiveFactory {
 	private static final String STATUS_VIEW_ID = "uk.ac.gda.beamline.i09-2.statusView";
 
 	private static final String TOOLPAGE_FOLDER = "toolpageFolder";
+	private static final String DLD_CONTROL_FOLDER="dldcontrol";
 	private static final String LIVE_CONTROL_FOLDER="livecontrol";
+	private static final String DASHBOARD_FOLDER = "dashboard";
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
@@ -45,6 +47,7 @@ public class DLDPerspective implements IPerspectiveFactory {
 
 	private void activateAddons() {
 		ResetMovingSumAddon resetMovingSumAddon = new ResetMovingSumAddon();
+		resetMovingSumAddon.configure();
 	}
 
 	private void defineLayout(IPageLayout layout) {
@@ -57,13 +60,17 @@ public class DLDPerspective implements IPerspectiveFactory {
 		left.addPlaceholder(GDA_NAVIGATOR_VIEW_ID);
 		left.addPlaceholder("uk.ac.diamond.sda.navigator.views.FileView");
 
-		IFolderLayout liveControlFolder=layout.createFolder(LIVE_CONTROL_FOLDER, IPageLayout.TOP, (float)0.2, PROJ_FOLDER); //$NON-NLS-1$
-		liveControlFolder.addView(LiveControlsView.ID + ":dld_count_rate");
-		liveControlFolder.addView(LiveControlsView.ID + ":pgm_control_set");
-		liveControlFolder.addPlaceholder("uk.ac.gda.rcp.views.dashboardView");
-		liveControlFolder.addPlaceholder(LiveControlsView.ID + ":*");
-		liveControlFolder.addPlaceholder(LiveControlsView.ID);
-		liveControlFolder.addPlaceholder(IPageLayout.ID_OUTLINE);
+		IFolderLayout liveBeamlineControlFolder=layout.createFolder(LIVE_CONTROL_FOLDER, IPageLayout.TOP, (float)0.7, PROJ_FOLDER); //$NON-NLS-1$
+		liveBeamlineControlFolder.addView(LiveControlsView.ID + ":beamline_control_set");
+		liveBeamlineControlFolder.addPlaceholder(LiveControlsView.ID + ":*");
+		liveBeamlineControlFolder.addPlaceholder(LiveControlsView.ID);
+		liveBeamlineControlFolder.addPlaceholder(IPageLayout.ID_OUTLINE);
+
+		IFolderLayout dashboardFolder=layout.createFolder(DASHBOARD_FOLDER, IPageLayout.TOP, (float)0.2, LIVE_CONTROL_FOLDER); //$NON-NLS-1$
+		dashboardFolder.addView("uk.ac.gda.rcp.views.dashboardView");
+
+		IFolderLayout dldLiveControlFolder=layout.createFolder(DLD_CONTROL_FOLDER, IPageLayout.TOP, (float)0.2, DASHBOARD_FOLDER); //$NON-NLS-1$
+		dldLiveControlFolder.addView(LiveControlsView.ID + ":dld_count_rate");
 
 		// middle row
 		IFolderLayout statusFolder =  layout.createFolder(STATUS_FOLDER, IPageLayout.LEFT, (float)0.4, editorArea);
@@ -72,18 +79,18 @@ public class DLDPerspective implements IPerspectiveFactory {
 		statusFolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
 		statusFolder.addPlaceholder("org.eclipse.ui.console.ConsoleView");
 
-        IFolderLayout terminalfolder= layout.createFolder(TERMINAL_FOLDER, IPageLayout.BOTTOM, (float)0.4, STATUS_FOLDER); //$NON-NLS-1$
-        terminalfolder.addView(JythonTerminalView.ID);
-        terminalfolder.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
-        terminalfolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
-        terminalfolder.addPlaceholder(NewSearchUI.SEARCH_VIEW_ID);
-        terminalfolder.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+		IFolderLayout terminalfolder= layout.createFolder(TERMINAL_FOLDER, IPageLayout.BOTTOM, (float)0.4, STATUS_FOLDER); //$NON-NLS-1$
+		terminalfolder.addView(JythonTerminalView.ID);
+		terminalfolder.addPlaceholder(IPageLayout.ID_PROBLEM_VIEW);
+		terminalfolder.addPlaceholder(IProgressConstants.PROGRESS_VIEW_ID);
+		terminalfolder.addPlaceholder(NewSearchUI.SEARCH_VIEW_ID);
+		terminalfolder.addPlaceholder(IPageLayout.ID_BOOKMARKS);
 
 		IFolderLayout scanPlotFolder=layout.createFolder(PLOT_1D_FOLDER, IPageLayout.TOP, (float)0.4, TERMINAL_FOLDER); //$NON-NLS-1$
-        scanPlotFolder.addView(LivePlotView.ID);
-        scanPlotFolder.addPlaceholder("org.dawnsci.mapping.ui.spectrumview");
+		scanPlotFolder.addView(LivePlotView.ID);
+		scanPlotFolder.addPlaceholder("org.dawnsci.mapping.ui.spectrumview");
 
-        // third row
+		// third row
 		IFolderLayout detectorPlotFolder=layout.createFolder(PLOT_2D_FOLDER, IPageLayout.RIGHT, (float)0.3, editorArea); //$NON-NLS-1$
 		detectorPlotFolder.addView("uk.ac.gda.beamline.i09-2.dld.live.stream.view.LiveImageXY:dld_liveimagexy#EPICS_ARRAY");
 		detectorPlotFolder.addPlaceholder(LiveStreamView.ID+":*");
@@ -96,27 +103,27 @@ public class DLDPerspective implements IPerspectiveFactory {
 	}
 
 	private void defineActions(IPageLayout layout) {
-        layout.addPerspectiveShortcut(DLDPerspective.ID);
-        layout.addPerspectiveShortcut(JythonPerspective.ID);
+		layout.addPerspectiveShortcut(DLDPerspective.ID);
+		layout.addPerspectiveShortcut(JythonPerspective.ID);
 
-        layout.addNewWizardShortcut(PythonProjectWizard.WIZARD_ID); //$NON-NLS-1$
-        layout.addNewWizardShortcut(PythonSourceFolderWizard.WIZARD_ID); //$NON-NLS-1$
-        layout.addNewWizardShortcut(PythonPackageWizard.WIZARD_ID); //$NON-NLS-1$
-        layout.addNewWizardShortcut(PythonModuleWizard.WIZARD_ID); //$NON-NLS-1$
-        layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
-        layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
-        layout.addNewWizardShortcut("org.eclipse.ui.editors.wizards.UntitledTextFileWizard");//$NON-NLS-1$
+		layout.addNewWizardShortcut(PythonProjectWizard.WIZARD_ID); //$NON-NLS-1$
+		layout.addNewWizardShortcut(PythonSourceFolderWizard.WIZARD_ID); //$NON-NLS-1$
+		layout.addNewWizardShortcut(PythonPackageWizard.WIZARD_ID); //$NON-NLS-1$
+		layout.addNewWizardShortcut(PythonModuleWizard.WIZARD_ID); //$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");//$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");//$NON-NLS-1$
+		layout.addNewWizardShortcut("org.eclipse.ui.editors.wizards.UntitledTextFileWizard");//$NON-NLS-1$
 
-        layout.addShowViewShortcut(LivePlotView.ID);
-        layout.addShowViewShortcut(JythonTerminalView.ID);
-        layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
-        layout.addShowViewShortcut(NewSearchUI.SEARCH_VIEW_ID);
-        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
-        layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-        layout.addShowViewShortcut(GDA_NAVIGATOR_VIEW_ID);
-        layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+		layout.addShowViewShortcut(LivePlotView.ID);
+		layout.addShowViewShortcut(JythonTerminalView.ID);
+		layout.addShowViewShortcut(IPageLayout.ID_PROJECT_EXPLORER);
+		layout.addShowViewShortcut(NewSearchUI.SEARCH_VIEW_ID);
+		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+		layout.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+		layout.addShowViewShortcut(GDA_NAVIGATOR_VIEW_ID);
+		layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
 
-        layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
-    }
+		layout.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET);
+	}
 
 }
