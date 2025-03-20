875b3c5c60fdfc76b3e5802d4a493db78549f959
eir17846 <victor.rogalev@diamond.ac.uk>
2025-03-20 12:48:32 +0000

    B07-1082 unify GDA meta data of B and C Branch
    Change-Id: I0df5065f398519af8d638929256aaafc781cb449

--- configurations/b07-1-config/scripts/localStation.py
+++ configurations/b07-1-config/scripts/localStation.py
@@ -6,6 +6,7 @@ from gda.configuration.properties import LocalProperties
 from uk.ac.diamond.daq.configuration import ConfigUtils
 from utils.ExceptionLogs import localStation_exceptions, localStation_exception
 from gdascripts.messages.handle_messages import simpleLog
+from gdascripts import installation
 
 print("=================================================================================================================")
 print("Performing beamline specific initialisation code (b07-1).")
@@ -53,36 +54,113 @@ from metadata.metashop import meta_add, meta_ll, meta_ls, meta_rm  # @UnusedImpo
 import metadata.metashop as metashop  # @UnusedImport
 
 print("-"*100)
-import installation
-meta_data_list = []  # other common metadata can be added here if required!
+meta_data_list = [endstation, ring_current, analyser_sledge] # @UndefinedVariable
+# add sample positions depending on endstation in use
 if installation.isLive():
-		meta_data_list = meta_data_list + [rga]  # @UndefinedVariable
-		if ConfigUtils.profileActive("TPOT"):
-				print("add TPOT scannables to metadata list ...")
-				try:
-						meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
-				except Exception as e:
-						print("adding TPOT to metadata failed.")
-						localStation_exception(sys.exc_info(), "adding TPOT to metadata error")
-		if ConfigUtils.profileActive("TCUP"):
-				print("add TCUP metadata scannables to be captured in data files.")
-				try:
-						meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
-				except Exception as e:
-						print("adding TCUP to metadata failed.")
-						localStation_exception(sys.exc_info(), "adding TCUP to metadata error")
+	if ConfigUtils.profileActive("TPOT"):
+		print("add TPOT scannables to metadata list ...")
+		try:
+			meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
+		except Exception as e:
+			print("adding TPOT to metadata failed.")
+			localStation_exception(sys.exc_info(), "adding TPOT to metadata error")
+	if ConfigUtils.profileActive("TCUP"):
+		print("add TCUP metadata scannables to be captured in data files.")
+		try:
+			meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
+		except Exception as e:
+			print("adding TCUP to metadata failed.")
+			localStation_exception(sys.exc_info(), "adding TCUP to metadata error")
 else:
-		if ConfigUtils.profileActive("TPOT"):
-				print("add TPOT metadata scannables to be captured in data files.")
-				meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
-		if ConfigUtils.profileActive("TCUP"):
-				print("add TCUP metadata scannables to be captured in data files.")
-				meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
+	if ConfigUtils.profileActive("TPOT"):
+		print("add TPOT metadata scannables to be captured in data files.")
+		meta_data_list = meta_data_list + [sm_xp, sm_yp, sm_zp, sm_polar_rotation, sm_azimuth_rotation]  # @UndefinedVariable
+	if ConfigUtils.profileActive("TCUP"):
+		print("add TCUP metadata scannables to be captured in data files.")
+		meta_data_list = meta_data_list + [sm2_xp, sm2_yp, sm2_zp]  # @UndefinedVariable
 
-patch_panel_list = [ppc_temp_1, ppc_temp_2, ppc_temp_3, ppc_temp_4, ppc_ai_1, ppc_ai_2, ppc_ai_3, ppc_ai_4, ppc_ao_1, ppc_ao_2, ppc_ao_3, ppc_ao_4, ppc_di_1, ppc_di_2, ppc_di_3, ppc_di_4, ppc_do_5, ppc_do_6, ppc_do_7, ppc_do_8]  # @UndefinedVariable
-crystal_monochromator_list = [ccmc_x, ccmc_y, ccmc_z]  # @UndefinedVariable
-diagnosis_list = [d9c_y_positioner, d9c_y]  # @UndefinedVariable
-meta_data_list = meta_data_list + patch_panel_list + crystal_monochromator_list + diagnosis_list
+meta_shutter_list = [expt_shutter,fast_shutter] # @UndefinedVariable
+patch_panel_list = [ppc_temp_1, ppc_temp_2, ppc_temp_3, ppc_temp_4, # @UndefinedVariable
+					ppc_ai_1, ppc_ai_2, ppc_ai_3, ppc_ai_4,  # @UndefinedVariable
+					ppc_ao_1, ppc_ao_2, ppc_ao_3, ppc_ao_4,  # @UndefinedVariable
+					ppc_di_1, ppc_di_2, ppc_di_3, ppc_di_4,  # @UndefinedVariable
+					ppc_do_5, ppc_do_6, ppc_do_7, ppc_do_8]  # @UndefinedVariable
+crystal_monochromator_list = [ccmc_x, ccmc_y, ccmc_z, ccmc_named_position]  # @UndefinedVariable
+diagnosis_list = [d9c_y_positioner, d9c_y,d7c_y,d21c_positioner,d21c_x]  # @UndefinedVariable
+meta_pgm_list = [pgm_energy, pgm_cff, pgm_mirror_pitch, pgm_mirror_x, pgm_grating_pitch, pgm_grating_x,pgm_grating,pgm_mirror,pgm_override] # @UndefinedVariable
+meta_mirrors_list = [m1b_mask_temp,	m1b_mirror_temp, # @UndefinedVariable
+					m1c_x,m1c_y,m1c_pitch,m1c_roll,	m1c_yaw,m1c_mask_temp,m1c_mirror_temp, # @UndefinedVariable
+					m3c_x,m3c_y,m3c_pitch,m3c_roll,m3c_yaw, # @UndefinedVariable
+					m4c_x,m4c_y,m4c_pitch,m4c_roll,m4c_yaw, # @UndefinedVariable
+					m5c_x,m5c_y,m5c_pitch,m5c_roll,	m5c_yaw]# @UndefinedVariable
+meta_slit_list = [s1c_xcentre,s1c_xsize,s1c_xpos,s1c_xneg,s1c_ycentre,s1c_ysize,s1c_ypos,s1c_yneg,s2c_xcentre,s2c_xsize, # @UndefinedVariable
+					s2c_ycentre,s2c_ysize,s3c_xcentre,s3c_xsize,s3c_ycentre,s3c_ysize,s4c_xgap,s4c_xgapsize,s4c_ygap,s4c_ygapsize,s4c_z]# @UndefinedVariable
+meta_temperature_list = [es_01_temp_output,es_01_temp_temperature,es_01_temp_setpoint,es_01_temp_ramprate]# @UndefinedVariable
+meta_stanfords_list = [ca31c_sensitivity, # @UndefinedVariable
+						ca31c_sensitivity_units, # @UndefinedVariable
+						ca31c_bias_voltage_status,# @UndefinedVariable
+						ca31c_bias_voltage,# @UndefinedVariable
+						ca31c_filter_type,# @UndefinedVariable
+						ca31c_filter_highpass,# @UndefinedVariable
+						ca31c_filter_lowpass,# @UndefinedVariable
+						ca31c_gain_mode,# @UndefinedVariable
+						ca31c_offset,# @UndefinedVariable
+						ca31c_offset_units,# @UndefinedVariable
+						ca31c_offset_current,# @UndefinedVariable
+						ca31c_inverted,# @UndefinedVariable
+						ca31c_current,# @UndefinedVariable
+						ca31c_average_current,# @UndefinedVariable
+						ca32c_sensitivity,# @UndefinedVariable
+						ca32c_sensitivity_units,# @UndefinedVariable
+						ca32c_bias_voltage_status,# @UndefinedVariable
+						ca32c_bias_voltage,# @UndefinedVariable
+						ca32c_filter_type,# @UndefinedVariable
+						ca32c_filter_highpass,# @UndefinedVariable
+						ca32c_filter_lowpass,# @UndefinedVariable
+						ca32c_gain_mode,# @UndefinedVariable
+						ca32c_offset,# @UndefinedVariable
+						ca32c_offset_units,# @UndefinedVariable
+						ca32c_offset_current,# @UndefinedVariable
+						ca32c_inverted,# @UndefinedVariable
+						ca32c_current,# @UndefinedVariable
+						ca32c_average_current,# @UndefinedVariable
+						ca33c_sensitivity,# @UndefinedVariable
+						ca33c_sensitivity_units,# @UndefinedVariable
+						ca33c_bias_voltage_status,# @UndefinedVariable
+						ca33c_bias_voltage,# @UndefinedVariable
+						ca33c_filter_type,# @UndefinedVariable
+						ca33c_filter_highpass,# @UndefinedVariable
+						ca33c_filter_lowpass,# @UndefinedVariable
+						ca33c_gain_mode,# @UndefinedVariable
+						ca33c_offset,# @UndefinedVariable
+						ca33c_offset_units,# @UndefinedVariable#
+						ca33c_offset_current,# @UndefinedVariable
+						ca33c_inverted,# @UndefinedVariable
+						ca33c_current,# @UndefinedVariable
+						ca33c_average_current,# @UndefinedVariable
+						ca34c_sensitivity,# @UndefinedVariable
+						ca34c_sensitivity_units,# @UndefinedVariable
+						ca34c_bias_voltage_status,# @UndefinedVariable
+						ca34c_bias_voltage,# @UndefinedVariable
+						ca34c_filter_type,# @UndefinedVariable
+						ca34c_filter_highpass,# @UndefinedVariable
+						ca34c_filter_lowpass,# @UndefinedVariable
+						ca34c_gain_mode,# @UndefinedVariable
+						ca34c_offset,# @UndefinedVariable
+						ca34c_offset_units,# @UndefinedVariable
+						ca34c_offset_current,# @UndefinedVariable
+						ca34c_inverted,# @UndefinedVariable
+						ca34c_current,# @UndefinedVariable
+						ca34c_average_current,# @UndefinedVariable
+						ca99c_sensitivity,# @UndefinedVariable
+						ca99c_time_constant,# @UndefinedVariable
+						ca99c_channel_one,# @UndefinedVariable
+						ca99c_channel_two]# @UndefinedVariable
+
+meta_vacuum_list = [cap03c, cap04c, pir82c, pir83c, # @UndefinedVariable
+					img01c,	img02c, img03c, img04c, img05c,	img06c,	img07c,	img08c, img09c, img10c, img11c] # @UndefinedVariable
+
+meta_data_list += patch_panel_list+crystal_monochromator_list+diagnosis_list+meta_temperature_list+meta_pgm_list+meta_vacuum_list+meta_mirrors_list+meta_slit_list+meta_stanfords_list+meta_shutter_list
 
 for each in meta_data_list:
 		meta_add(each)
--- configurations/b07-config/scripts/localStation.py
+++ configurations/b07-config/scripts/localStation.py
@@ -1,9 +1,9 @@
 # localStation.py
 # For beamline specific initialisation code.
 #
-from gda.configuration.properties import LocalProperties
-from gda.device.scannable import DummyScannable
-import installation
+from gda.configuration.properties import LocalProperties #@UnusedImport
+from gda.device.scannable import DummyScannable #@UnusedImport
+from gdascripts import installation #@UnusedImport
 
 print "=================================================================================================================";
 print "Performing beamline specific initialisation code (b07).";
@@ -53,10 +53,10 @@ from metadata.metashop import meta_add,meta_ll,meta_ls, meta_rm  # @UnusedImport
 import metadata.metashop  # @UnusedImport
 print("Can now add meta data items to be captured in data files.")
 meta_data_list = [] #other common metadata can be added here if required!
-s1b_list = [s1b_xcentre,s1b_xsize,s1b_ysize,s1b_xpos,s1b_xneg] # @UndefinedVariable
+s1b_list = [s1b_xcentre,s1b_ycentre,s1b_xsize,s1b_ysize,s1b_xpos,s1b_ypos,s1b_xneg,s1b_yneg] # @UndefinedVariable
 s2b_list = [s2b_xsize,s2b_ysize,s2b_xcentre,s2b_ycentre] # @UndefinedVariable
 s3b_list = [s3b_xsize,s3b_ysize,s3b_xcentre,s3b_ycentre] # @UndefinedVariable
-s4b_list = [s4b_xgapsize,s4b_ygapsize,s4b_z] # @UndefinedVariable
+s4b_list = [s4b_xgap, s4b_ygap,s4b_xgapsize, s4b_ygapsize,s4b_z] # @UndefinedVariable
 metadata_group1 = s1b_list + s2b_list + s3b_list + s4b_list
 
 m1b_list = [m1b_x,m1b_y,m1b_pitch,m1b_roll,m1b_yaw,m1b_mask_temp,m1b_mirror_temp,m1b_chiller] # @UndefinedVariable
@@ -83,16 +83,60 @@ sm21b_list = [sm21b_x,sm21b_y,sm21b_z,sm21b_roty] # @UndefinedVariable
 sm52b_list = [sm52b_x,sm52b_y,sm52b_z,sm52b_roty,sm52b_rotz,sm52b_xp,sm52b_yp,sm52b_zp] # @UndefinedVariable
 ring_list = [ring_current] # @UndefinedVariable
 pgm_list = [pgm_energy,pgm_cff,pgm_grating_pitch,pgm_grating_x,pgm_mirror_pitch,pgm_mirror_x,pgm_grating,pgm_mirror,pgm_override] # @UndefinedVariable
-shutter_list = [expt_shutter,pss_shutter1_con,pss_shutter2_con,fast_shutter_con] # @UndefinedVariable
-other_list = [img22b,img23b,cap21b,cap22b,_es2_temp_temperature_monitor,_es2_temp_output_monitor, _es2_temp_setpoint_monitor] # @UndefinedVariable
-es1_list = [_es1_temp_51_temperature_monitor, _es1_temp_51_setpoint_monitor, _es1_temp_51_output_monitor, _es1_temp_52_temperature_monitor, _es1_temp_52_setpoint_monitor, _es1_temp_52_output_monitor]  # @UndefinedVariable
+shutter_list = [expt_shutter,pss_shutter1_con,pss_shutter2_con,fast_shutter_con, GV13] # @UndefinedVariable
+pressures_list = [img22b,img23b,cap21b,cap22b,img10b,img51b,img53b,img60b] # @UndefinedVariable
+es2_temperatures_list = [es2_21_temp_temperature,es2_21_temp_output, es2_21_temp_setpoint] # @UndefinedVariable
+es1_temperatures_list = [es1_51_temp_temperature, es1_51_temp_setpoint, es1_51_temp_output, es1_52_temp_temperature, es1_52_temp_setpoint, es1_52_temp_output]  # @UndefinedVariable
 mfc_list = [mfc1, mfc2, mfc3, mfc4, mfc5, mfc6, mfc7, mfc8] # @UndefinedVariable
-metadata_group4 = d21b_list + sm21b_list + sm52b_list + ring_list + pgm_list + shutter_list + other_list + es1_list
+metadata_group4 = d21b_list + sm21b_list + sm52b_list + ring_list + pgm_list + shutter_list + pressures_list + es1_temperatures_list + es2_temperatures_list
 
-stanfords_list = [ca18b, stanford_amplifiers]# @UndefinedVariable
-metadata_group5 = stanfords_list
+metadata_group5 = [	ca18b_current,# @UndefinedVariable
+				ca18b_gain,# @UndefinedVariable
+				ca18b_average_current, # @UndefinedVariable
+				ca16b_sensitivity ,# @UndefinedVariable
+				ca16b_sensitivity_units ,# @UndefinedVariable
+				ca16b_bias_voltage_status ,# @UndefinedVariable
+				ca16b_bias_voltage ,# @UndefinedVariable
+				ca16b_filter_type ,# @UndefinedVariable
+				ca16b_filter_highpass ,# @UndefinedVariable
+				ca16b_filter_lowpass ,# @UndefinedVariable
+				ca16b_gain_mode ,# @UndefinedVariable
+				ca16b_offset ,# @UndefinedVariable
+				ca16b_offset_units ,# @UndefinedVariable
+				ca16b_offset_current ,# @UndefinedVariable
+				ca16b_inverted ,# @UndefinedVariable
+				ca16b_current ,# @UndefinedVariable
+				ca16b_average_current ,# @UndefinedVariable
+				ca35b_sensitivity ,# @UndefinedVariable
+				ca35b_sensitivity_units ,# @UndefinedVariable
+				ca35b_bias_voltage_status ,# @UndefinedVariable
+				ca35b_bias_voltage ,# @UndefinedVariable
+				ca35b_filter_type ,# @UndefinedVariable
+				ca35b_filter_highpass ,# @UndefinedVariable
+				ca35b_filter_lowpass ,# @UndefinedVariable
+				ca35b_gain_mode ,# @UndefinedVariable
+				ca35b_offset ,# @UndefinedVariable
+				ca35b_offset_units ,# @UndefinedVariable
+				ca35b_offset_current ,# @UndefinedVariable
+				ca35b_inverted ,# @UndefinedVariable
+				ca35b_current ,# @UndefinedVariable
+				ca35b_average_current ,# @UndefinedVariable
+				ca36b_sensitivity ,# @UndefinedVariable
+				ca36b_sensitivity_units ,# @UndefinedVariable
+				ca36b_bias_voltage_status ,# @UndefinedVariable
+				ca36b_bias_voltage ,# @UndefinedVariable
+				ca36b_filter_type ,# @UndefinedVariable
+				ca36b_filter_highpass ,# @UndefinedVariable
+				ca36b_filter_lowpass ,# @UndefinedVariable
+				ca36b_gain_mode ,# @UndefinedVariable
+				ca36b_offset ,# @UndefinedVariable
+				ca36b_offset_units ,# @UndefinedVariable
+				ca36b_offset_current ,# @UndefinedVariable
+				ca36b_inverted ,# @UndefinedVariable
+				ca36b_current ,# @UndefinedVariable
+				ca36b_average_current]# @UndefinedVariable
 
-meta_data_list = metadata_group1 + metadata_group2 + metadata_group3 + metadata_group4 + metadata_group5
+meta_data_list = metadata_group1 + metadata_group2 + metadata_group3 + metadata_group4 + metadata_group5+[endstation]# @UndefinedVariable
 
 [meta_add(each) for each in meta_data_list]
 [meta_add(each) for each in patch_panel_ES1.getGroupMembersAsArray()]# @UndefinedVariable
