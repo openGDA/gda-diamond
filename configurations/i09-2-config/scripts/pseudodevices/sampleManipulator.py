21c125ca84bc679c1d80013ce5d5e250ee8fd55d
eir17846 <victor.rogalev@diamond.ac.uk>
2025-03-26 14:44:16 +0000

    I09-617 Fix piezo motors synchronization in client
    Change-Id: Idba4f7e392ba38c9bdaec6368a86540934b61dba

--- configurations/i09-2-config/scripts/pseudodevices/sampleManipulator.py
+++ configurations/i09-2-config/scripts/pseudodevices/sampleManipulator.py
@@ -3,26 +3,28 @@ Created on 30 Nov 2023
 
 @author: eir17846
 '''
-from gdascripts.pd.epics_pds import SingleEpicsPositionerNoStatusClassDeadband
+from gdascripts.pd.epics_pds import SingleEpicsPositionerNoStatusClassDeadbandSyncInput
 from gdascripts import installation as installation
 
 if installation.isLive():
-	sx1 = SingleEpicsPositionerNoStatusClassDeadband("sx1","BL09K-MO-SM-01:PI:X1:MOV:WR","BL09K-MO-SM-01:PI:X1:POS:RD","BL09K-MO-SM-01:PI:X1:SRG:RD","BL09K-MO-SM-01:PI:X1:HLT:WR.PROC","mm","%f",0.01)
-	sx2 = SingleEpicsPositionerNoStatusClassDeadband("sx2","BL09K-MO-SM-01:PI:X2:MOV:WR","BL09K-MO-SM-01:PI:X2:POS:RD","BL09K-MO-SM-01:PI:X2:SRG:RD","BL09K-MO-SM-01:PI:X2:HLT:WR.PROC","mm","%f",0.01)
-	sx3 = SingleEpicsPositionerNoStatusClassDeadband("sx3","BL09K-MO-SM-01:PI:X3:MOV:WR","BL09K-MO-SM-01:PI:X3:POS:RD","BL09K-MO-SM-01:PI:X3:SRG:RD","BL09K-MO-SM-01:PI:X3:HLT:WR.PROC","mm","%f",0.01)
-	sy = SingleEpicsPositionerNoStatusClassDeadband("sy","BL09K-MO-SM-01:PI:Y:MOV:WR","BL09K-MO-SM-01:PI:Y:POS:RD","BL09K-MO-SM-01:PI:Y:SRG:RD","BL09K-MO-SM-01:PI:Y:HLT:WR.PROC","mm","%f",0.01)
-	sz1 = SingleEpicsPositionerNoStatusClassDeadband("sz1","BL09K-MO-SM-01:PI:Z1:MOV:WR","BL09K-MO-SM-01:PI:Z1:POS:RD","BL09K-MO-SM-01:PI:Z1:SRG:RD","BL09K-MO-SM-01:PI:Z1:HLT:WR.PROC","mm","%f",0.01)
-	sz2 = SingleEpicsPositionerNoStatusClassDeadband("sz2","BL09K-MO-SM-01:PI:Z2:MOV:WR","BL09K-MO-SM-01:PI:Z2:POS:RD","BL09K-MO-SM-01:PI:Z2:SRG:RD","BL09K-MO-SM-01:PI:Z2:HLT:WR.PROC","mm","%f",0.01)
+	base_pv = "BL09K-MO-SM-01:PI:"
 	"""if defined as ScannableMotor then every pos there is error "target request is missed"!"""
-	sxc = SingleEpicsPositionerNoStatusClassDeadband("sxc","BL09K-MO-SM-01:PI:X","BL09K-MO-SM-01:PI:X.RBV","BL09K-MO-SM-01:PI:X.MOVN","BL09K-MO-SM-01:PI:X.STOP","mm","%f",0.01)
-	szc = SingleEpicsPositionerNoStatusClassDeadband("szc","BL09K-MO-SM-01:PI:Z","BL09K-MO-SM-01:PI:Z.RBV","BL09K-MO-SM-01:PI:Z.MOVN","BL09K-MO-SM-01:PI:Z.STOP","mm","%f",0.01)
+	# sxc = SingleEpicsPositionerNoStatusClassDeadbandCheckInput("sxc","BL09K-MO-SM-01:PI:X","BL09K-MO-SM-01:PI:X.RBV","BL09K-MO-SM-01:PI:X.MOVN","BL09K-MO-SM-01:PI:X.STOP","mm","%f",0.01)
+	# szc = SingleEpicsPositionerNoStatusClassDeadbandCheckInput("szc","BL09K-MO-SM-01:PI:Z","BL09K-MO-SM-01:PI:Z.RBV","BL09K-MO-SM-01:PI:Z.MOVN","BL09K-MO-SM-01:PI:Z.STOP","mm","%f",0.01)
+
+	sx1 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx1",base_pv+"X1:MOV:WR","BL09K-MO-SM-01:PI:X1:POS:RD","BL09K-MO-SM-01:PI:X1:ONT:RD","BL09K-MO-SM-01:PI:X1:HLT:WR.PROC","mm","%f",0.01)
+	sx2 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx2","BL09K-MO-SM-01:PI:X2:MOV:WR","BL09K-MO-SM-01:PI:X2:POS:RD","BL09K-MO-SM-01:PI:X2:ONT:RD","BL09K-MO-SM-01:PI:X2:HLT:WR.PROC","mm","%f",0.01)
+	sx3 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx3","BL09K-MO-SM-01:PI:X3:MOV:WR","BL09K-MO-SM-01:PI:X3:POS:RD","BL09K-MO-SM-01:PI:X3:ONT:RD","BL09K-MO-SM-01:PI:X3:HLT:WR.PROC","mm","%f",0.01)
+	sz1 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sz1","BL09K-MO-SM-01:PI:Z1:MOV:WR","BL09K-MO-SM-01:PI:Z1:POS:RD","BL09K-MO-SM-01:PI:Z1:ONT:RD","BL09K-MO-SM-01:PI:Z1:HLT:WR.PROC","mm","%f",0.01)
+	sz2 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sz2","BL09K-MO-SM-01:PI:Z2:MOV:WR","BL09K-MO-SM-01:PI:Z2:POS:RD","BL09K-MO-SM-01:PI:Z2:ONT:RD","BL09K-MO-SM-01:PI:Z2:HLT:WR.PROC","mm","%f",0.01)
+	sy  = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sy","BL09K-MO-SM-01:PI:Y:MOV:WR","BL09K-MO-SM-01:PI:Y:POS:RD","BL09K-MO-SM-01:PI:Y:ONT:RD","BL09K-MO-SM-01:PI:Y:HLT:WR.PROC","mm","%f",0.01)
+
 else:
 	from gda.device.scannable import DummyScannable #@UnresolvedImport
-	sx1 = DummyScannable("sx1")
-	sx2 = DummyScannable("sx2")
-	sx3 = DummyScannable("sx3")
-	sy  = DummyScannable("sy")
-	sz1 = DummyScannable("sz1")
+	base_pv = "ws410-MO-SIM-01:"
+	sx1 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx1",base_pv+"M1",base_pv+"M1.RBV",base_pv+"M1.DMOV",base_pv+"M1.STOP","mm","%f",0.01)
+	sx2 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx2",base_pv+"M2",base_pv+"M2.RBV",base_pv+"M2.DMOV",base_pv+"M2.STOP","mm","%f",0.01)
+	sx3 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sx3",base_pv+"M3",base_pv+"M3.RBV",base_pv+"M3.DMOV",base_pv+"M3.STOP","mm","%f",0.01)
+	sy 	= SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sy", base_pv+"M4",base_pv+"M4.RBV",base_pv+"M4.DMOV",base_pv+"M4.STOP","mm","%f",0.01)
+	sz1 = SingleEpicsPositionerNoStatusClassDeadbandSyncInput("sz1",base_pv+"M5",base_pv+"M5.RBV",base_pv+"M5.DMOV",base_pv+"M5.STOP","mm","%f",0.01)
 	sz2 = DummyScannable("sz2")
-	sxc = DummyScannable("sxc")
-	szc = DummyScannable("szc")
