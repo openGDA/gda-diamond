7f22b14080170bca317110850eaa079a49a1e727
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2025-04-02 14:09:34 +0100

    I21-1226 a beam loss caused script to crash with an error instead of pausing
    - adding check for insertion device access control
    
    Change-Id: I2a06e99fdb1e54a5f11424cff381033d1a795dfa

--- configurations/i10-shared/scripts/scannables/checkbeanscannables.py
+++ configurations/i10-shared/scripts/scannables/checkbeanscannables.py
@@ -1,4 +1,6 @@
 '''
+add ID gap scannables to capture and restore ID gaps when beam dump occurrs.
+
 Created on 10 Apr 2018
 
 @author: fy65
@@ -8,7 +10,7 @@ Created on 10 Apr 2018
 ###############################################################################
 from utils.ExceptionLogs import localStation_exception
 import sys
-from gdaserver import rc, topup_time, frontend, idd_gap, idu_gap  # @UnresolvedImport
+from gdaserver import rc, topup_time, frontend, idd_gap, idu_gap, idblena_id1, idblena_id2 # @UnresolvedImport
 print("-"*100)
 try:
     print("Creating checkbeam and checkbeamcv devices composed of 3 conditions:")
@@ -22,7 +24,7 @@ try:
     from gdascripts.scannable.beamokay import WaitWhileScannableBelowThreshold, WaitForScannableState
     from gda.device.scannable.scannablegroup import ScannableGroup
 
-    checkrc = WaitWhileScannableBelowThreshold('checkrc', rc, 190, secondsBetweenChecks = 1, secondsToWaitAfterBeamBackUp = 5, id1gap = idd_gap, id2gap = idu_gap)
+    checkrc = WaitWhileScannableBelowThreshold('checkrc', rc, 190, secondsBetweenChecks = 1, secondsToWaitAfterBeamBackUp = 5, id1gap = idd_gap, id2gap = idu_gap, accesscontrol4id1 = idblena_id1, accesscontrol4id2 = idblena_id2)
     checktopup_time = WaitWhileScannableBelowThreshold('checktopup_time', topup_time, 5, secondsBetweenChecks = 1, secondsToWaitAfterBeamBackUp = 5)
     checkfe = WaitForScannableState('checkfe', frontend, secondsBetweenChecks = 1, secondsToWaitAfterBeamBackUp = 60)
     checkbeam = ScannableGroup('checkbeam', [checkrc, checkfe, checktopup_time])
