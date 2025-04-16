7922b9b55c7457c3f2d59918276d56378f46cbf8
Mark Booth <Mark.Booth@diamond.ac.uk>
2025-04-16 16:29:50 +0100

    I15 (9.37) Add help for plotting test fixtures
    Change-Id: I8040c4067c8a0302b9065d1c3e9b34b117836d10

--- configurations/i15-config/tests/conftest.py
+++ configurations/i15-config/tests/conftest.py
@@ -4,3 +4,5 @@ def pytest_configure(config):
     # register markers to avoid warnings
     config.addinivalue_line("markers",
         "email: mark test as sending email, (deselect with 'marks=\"not email\"')")
+    config.addinivalue_line("markers",
+        "plotting: mark test as plotting demo, (select with 'marks=\"plotting\"')")
