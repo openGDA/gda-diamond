866280ac0d3e02c1d081feae79074f9705bf8fd8
Paula <paula.martin-gomez@diamond.ac.uk>
2025-05-07 12:32:17 +0100

    I14-1099 Print out estimated time
    Change-Id: Ieeb2addf152b03867884f2675b60ec39e0a8b481

--- configurations/i14-config/scripts/scanning/submit_tomography_scan.py
+++ configurations/i14-config/scripts/scanning/submit_tomography_scan.py
@@ -2,8 +2,48 @@ from gda.util.osgi import OsgiJythonHelper #@Unresolvedimport
 from org.eclipse.dawnsci.analysis.api.persistence import IMarshallerService #@Unresolvedimport
 from org.eclipse.scanning.api.event.scan import ScanRequest #@Unresolvedimport
 from uk.ac.diamond.daq.mapping.api import TomographyCalibrationData #@Unresolvedimport
-    
+
+import math
+from java.lang import System
+from java.util import Date
+from java.text import SimpleDateFormat
+
 marshaller_service = OsgiJythonHelper.getService(IMarshallerService)
+
+
+def format_time(total_estimated_time):
+    hours = math.floor(total_estimated_time / 3600.0)
+    minutes = math.floor((total_estimated_time % 3600.0) / 60.0)
+    seconds = total_estimated_time % 60.0
+    return "Mapping scan duration: {:02.0f}h {:02.0f}m {:02.0f}s".format(hours, minutes, seconds)
+
+def format_total_time(total_estimated_time):
+    hours = math.floor(total_estimated_time / 3600.0)
+    minutes = math.floor((total_estimated_time % 3600.0) / 60.0)
+    seconds = total_estimated_time % 60.0
+    return "Total tomography scans duration: {:02.0f}h {:02.0f}m {:02.0f}s".format(hours, minutes, seconds)
+
+def get_current_time():
+    now = Date()
+    formatter = SimpleDateFormat("h:mm a")
+    return "Current time: " + formatter.format(now).upper()
+
+def get_end_time(total_estimated_time):
+    current_time_ms = System.currentTimeMillis()
+    end_time_ms = current_time_ms + (total_estimated_time * 1000)
+    end_date = Date(long(end_time_ms))
+    formatter = SimpleDateFormat("h:mm a")
+    return "Scans complete by: " + formatter.format(end_date).upper()
+
+formatted_mapping_time = format_time(scanEstimatedTime)
+print(formatted_mapping_time)
+formatted_total_time = format_total_time(totalEstimatedTime)
+print(formatted_total_time)
+print("")
+print(get_current_time())
+end_time = get_end_time(totalEstimatedTime)
+print(end_time)
+print("")
 
 scanRequest = marshaller_service.unmarshal(scanRequestJson, ScanRequest) #@Undefinedvariable
 
