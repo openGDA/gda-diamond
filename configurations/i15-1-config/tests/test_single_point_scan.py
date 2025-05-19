acda22ad46f6c17f1211eed9bd23981b468200b3
Mark Booth <Mark.Booth@diamond.ac.uk>
2025-05-19 16:23:45 +0100

    I15_1-1443 (9.37) Fix beamline integration tests
    Switch back to PE and use the 'background' sample
    
    Change-Id: I050ca0ce66cfaf996e429169aaf5648b65338558

--- configurations/i15-1-config/tests/test_single_point_scan.py
+++ configurations/i15-1-config/tests/test_single_point_scan.py
@@ -1,35 +1,36 @@
 from gda.configuration.properties import LocalProperties
 from gdascripts.parameters import beamline_parameters
 from rigs_local.j15 import j15
-from rigs_local.scanrequests.generic_scanrequests import collect_arc
-from rigs.ispyb.discovery import showMeTheSamples
-from rigs.sample import SCANNABLE_SAMPLE_NAME
+from rigs_local.scanrequests.generic_scanrequests import collect_pe2
 
 def test_single_point_scan_is_not_refused():
-	j15.dummy_mode = True # Do not try to move any motors (does not set dummy mode for detectors)
-	j15.setVisit(LocalProperties.get('gda.defVisit')) # Use the current commissioning visit
-	first_valid_sample=showMeTheSamples().keys()[0] # Any sample should do, so use the first valid sample
 	jythonNameMap = beamline_parameters.JythonNameSpaceMapping()
-	arc = jythonNameMap['arc']
-	arc.dummy_mode = True # Do not try to actually run the live detector
-	arc.filename = "/dls_sw/i15-1/scripts/Xpdf/ProcessingFiles/arc_sim_1_frame.hdf5"
-	# Tests on the beamline (2024-11-05) failed because setting arc.calibrationFilePath to an invalid path caused
-	# a file not found error.
+	robot=jythonNameMap['robot']
+	real_janet=jythonNameMap['real_janet']
 
-	collect_arc(sampleid=first_valid_sample, exposure_time=10, tths=None, frames=1, monitorsPerScan=[],
+	j15.dummy_mode = True # Do not try to move any motors (does not set dummy mode for detectors)
+	robot.dummy_mode = True
+	real_janet.dummy_mode = True
+
+	j15.setVisit(LocalProperties.get('gda.defVisit')) # Use the current commissioning visit
+
+	collect_pe2(sampleid="bkg", exposure_time=9, frames=1, monitorsPerScan=['sampleID_recorder'],
 				monitorsPerPoint=[],comment="",f2="auto",preSleep=None,blocking=True,samX=None)
 
-	# Tests on the beamline (2024-11-05) failed because `collect_arc` hadn't been run in 'live' mode since the beamline
-	# was powered up. This is because in dummy mode, it doesn't set up devices, to ensure processing isn't attempted.
-
 def test_multiple_point_scan_completes():
-	j15.dummy_mode = True # Do not try to move any motors (does not set dummy mode for detectors)
-	j15.setVisit(LocalProperties.get('gda.defVisit')) # Use the current commissioning visit
-	first_valid_sample=showMeTheSamples().keys()[0] # Any sample should do, so use the first valid sample
 	jythonNameMap = beamline_parameters.JythonNameSpaceMapping()
-	arc = jythonNameMap['arc']
-	arc.dummy_mode = True # Do not try to actually run the live detector
-	arc.filename = "/dls_sw/i15-1/scripts/Xpdf/ProcessingFiles/arc_sim_1_frame.hdf5"
+	robot=jythonNameMap['robot']
+	real_janet=jythonNameMap['real_janet']
+	pe2 = jythonNameMap['pe2']
+	i0 = jythonNameMap['i0']
 
-	collect_arc(sampleid=first_valid_sample, exposure_time=10, tths=None, frames=2, monitorsPerScan=[],
+	j15.dummy_mode = True # Do not try to move any motors (does not set dummy mode for detectors)
+	robot.dummy_mode = True
+	real_janet.dummy_mode = True
+	pe2.stopBetweenPoints = False
+	i0.stopBetweenPoints = False
+
+	j15.setVisit(LocalProperties.get('gda.defVisit')) # Use the current commissioning visit
+
+	collect_pe2(sampleid="bkg", exposure_time=9, frames=2, monitorsPerScan=['sampleID_recorder'],
 				monitorsPerPoint=[],comment="",f2="auto",preSleep=None,blocking=True,samX=None)
