9d0f6478d5f11b4c0cd807a01c13dd4230224283
Fajin Yuan <fajin.yuan@diamond.ac.uk>
2025-05-19 15:47:13 +0100

    i06-2 does not work with flyscan
    - remove flyscan import
    
    Change-Id: I7c6033ec983faeb57e2bf1db67aba4525ba31b06

--- configurations/i06-shared/scripts/i06shared/localStation.py
+++ configurations/i06-shared/scripts/i06shared/localStation.py
@@ -90,8 +90,9 @@ print("-"*100)
 print("create command for 'acquire_flat_field' and 'remove_flat_field'\n")
 from i06shared.commands.flatFieldAcqusition import acquire_flat_field, remove_flat_field  # @UnusedImport
 print("-"*100)
-from scan.flyscan_command import flyscannable, FlyScanPositionsProvider, flyscan, flyscancn  # @UnusedImport
-from  scan import flyscan_command; print(flyscan_command.__doc__)  # @UndefinedVariable
+if beamline != "i06-2":
+    from scan.flyscan_command import flyscannable, FlyScanPositionsProvider, flyscan, flyscancn  # @UnusedImport
+    from scan import flyscan_command; print(flyscan_command.__doc__)  # @UndefinedVariable
 
 print("-"*100)
 print("load 'xasscan' command for X-ray Absorption Spectroscopy experiments ")
