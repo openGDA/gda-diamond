dc676052f32191306113b9127a1024632e4ac3f9
grc37356 <david.garman@diamond.ac.uk>
2025-06-17 11:08:14 +0100

    I16-930:  Scans should not fail if UB matrix is not correctly defined.
    Change-Id: I8ca0e18f27103f052418ae9d13de66e14ee60053

--- configurations/i16-config/scripts/localStationScripts/UBCalcMetadata.py
+++ configurations/i16-config/scripts/localStationScripts/UBCalcMetadata.py
@@ -1,6 +1,7 @@
 from gda.device.scannable import ScannableBase
 import math
 import scisoftpy as np
+from diffcalc.util import DiffcalcException
 try:
 	import json
 except ImportError:
@@ -42,13 +43,17 @@ class UBCalcMetadata (ScannableBase):
 		return None
 
 	def getUbMatrix(self):
-		if self.ubcalc is not None and self.ubcalc.UB is not None:
-			ubMatrix = self.ubcalc.UB.tolist()
-			# Diffcalc's UB matrix is scaled up by 2*PI
-			ubMatrix = [ [_u * 0.5/math.pi for _u in _r] for _r in ubMatrix ]
-			#transform by [[1, 0, 0], [0, 0, -1], [0, 1, 0]] to get UB in lab frame
-			ubMatrix = np.dot(np.array(ubMatrix), np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])).tolist()
-			return [ubMatrix]
+		try :
+			if self.ubcalc is not None and self.ubcalc.UB is not None:
+				ubMatrix = self.ubcalc.UB.tolist()
+				# Diffcalc's UB matrix is scaled up by 2*PI
+				ubMatrix = [ [_u * 0.5/math.pi for _u in _r] for _r in ubMatrix ]
+				#transform by [[1, 0, 0], [0, 0, -1], [0, 1, 0]] to get UB in lab frame
+				ubMatrix = np.dot(np.array(ubMatrix), np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])).tolist()
+				return [ubMatrix]
+		except DiffcalcException :
+			#TODO this should only print once per scan but can't use atScanStart or something because it's not actually in the scan.
+			print "WARNING: Could not calculate UB Matrix, if this is required please check UB has been defined correctly"
 		return None
 
 	def rawAsynchronousMoveTo(self, position):
