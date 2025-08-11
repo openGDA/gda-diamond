cce6e65b0289fa418bdd05dcd75cf046574e4b9d
grc37356 <david.garman@diamond.ac.uk>
2025-08-11 16:52:23 +0100

    I07-757:  Rename and update pvs for replaced slits.
    Also removed a number of motors deemed no longer required by beamline
    scientists.
    
    Change-Id: Ifc224240ac6d97a6d38033bbcd166ecea11c3907

--- configurations/i07-config/scripts/BeamlineI07/useMotors.py
+++ configurations/i07-config/scripts/BeamlineI07/useMotors.py
@@ -1,19 +1,15 @@
 
-from Diamond.PseudoDevices.EpicsMotors import EpicsMotorClass, EpicsCallbackMotorClass, EpicsMotorOffsetClass;
+from Diamond.PseudoDevices.EpicsMotors import EpicsMotorOffsetClass;
 
 from gdaserver import MotorDELTA_DIFF1
 from gdaserver import MotorGAMMA_DIFF1
 from gdaserver import MotorOMEGA_DIFF1
-from gdaserver import MotorALPHA_DIFF1
 from gdaserver import MotorTHETA_DIFF1
 from gdaserver import MotorCHI_DIFF1
 
-#diff1delta = EpicsMotorClass('diff1delta',pvMotor, '%.6f');
 diff1vdeltaoffset = EpicsMotorOffsetClass('diff1vdeltaoffset', MotorDELTA_DIFF1.getPvName(), '%.6f');
-#diff1hgammaoffset = EpicsMotorOffsetClass('diff1hgammaoffset',pvMotor, '%.6f');
 
 diff1vgammaoffset = EpicsMotorOffsetClass('diff1vgammaoffset', MotorGAMMA_DIFF1.getPvName(), '%.6f');
-#diff1hdeltaoffset = EpicsMotorOffsetClass('diff1hdeltaoffset',pvMotor, '%.6f');
 
 diff1vomegaoffset = EpicsMotorOffsetClass('diff1vomegaoffset', MotorOMEGA_DIFF1.getPvName(), '%.6f');
 
