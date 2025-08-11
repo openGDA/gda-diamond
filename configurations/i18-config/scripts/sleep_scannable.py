a5fb540fc3f985b98cca482d5ef879409a2cc8e5
Iain Hall <iain.hall@diamond.ac.uk>
2025-08-11 11:59:02 +0100

    I18 : Updates from beamline
    - Remove Xmap from Malcolm configuration and add Xspress3Mini.
    - Add xspress3mini and medipix detector setup functions to
    detector_setup.py
    - spectrometer-setup.py - make sure radius scannable position is only
    set to the default value if a value has not been set.
    - Use automatically generated element list for XanesElementLists.
    
    Change-Id: I4cfa837d188603cc5386d36c461ca559e19ea788

--- configurations/i18-config/scripts/sleep_scannable.py
+++ configurations/i18-config/scripts/sleep_scannable.py
@@ -37,3 +37,56 @@ class SleepDetector(DetectorBase):
 sleep_detector = SleepDetector("sleep_detector")
 sleep_detector.setOutputFormat(["%.4f"])
 sleep_detector.configure()
+sleep_detector.setLevel(150)
+
+# Another instance, with level greater than medipix's, which only sleeps for 0.1 s
+sleep_detector2 = SleepDetector("sleep_detector2")
+sleep_detector2.setOutputFormat(["%.4f"])
+sleep_detector2.configure()
+sleep_detector2.setLevel(250) # after the medipix
+sleep_detector2.collectionTime = 0.1 # fixed collection time
+
+
+# This next attempt is probably closer to the real solution
+# Sleep as before, but in a thread, clearing the busy status when finished
+
+from threading import Thread
+from gda.device import Detector
+class AsyncSleepDetector(DetectorBase):
+    def __init__(self, name):
+        self.setName(name)
+        self.inputNames = []
+        self.outputFormat = ["%.4f"]
+        self.sleep_time = 0.1
+        self._status = Detector.IDLE
+        
+    def collectData(self):      
+        Thread(target=self.sleep).start()
+    
+    def sleep(self):
+        self._status = Detector.BUSY
+        time.sleep(self.sleep_time)
+        self._status = Detector.IDLE
+        
+    def getStatus(self):        
+        return self._status
+
+    def readout(self):
+        return self.sleep_time
+
+    def getDescription(self):
+        return " "
+
+    def getDetectorID(self):
+        return " "
+
+    def getDetectorType(self):
+        return " "
+    
+    def createsOwnFiles(self):
+        return False
+
+async_sleep_detector = AsyncSleepDetector("async_sleep_detector")
+async_sleep_detector.setOutputFormat(["%.4f"])
+async_sleep_detector.configure()
+async_sleep_detector.setLevel(200) # same as medipix
